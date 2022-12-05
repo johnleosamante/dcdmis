8831c0ba913e3b56857ea2b9ec4895ca2b9e7a49
John Leo Samante <johnleomuitsamante@gmail.com>
2022-12-06 06:07:12 +0800

    Added new component functions.

--- _includes_/layout/components.php
+++ _includes_/layout/components.php
@@ -128,44 +128,61 @@ function ScrollToTop()
 <?php
 }
 
-function ContentTitle($title, $withbutton = false, $link = '', $text = 'Back', $icon = 'fa-arrow-circle-left')
+function ContentTitle($title, $withbutton = false, $link='', $text='Back', $icon='fa-arrow-circle-left')
 {
-  echo '<div class="d-sm-flex align-items-center justify-content-between">';
-  echo '<h3 class="h4 mb-0">' . $title . '</h3>';
-  if ($withbutton) {
-    echo '<a href="' . $link . '" class="btn btn-primary btn-icon-split btn-sm">';
-    echo '<span class="icon text-white-50">';
-    echo '<i class="fas ' . $icon . '"></i>';
-    echo '</span>';
-    echo '<span class="text">' . $text . '</span>';
-    echo '</a>';
-  }
-  echo '</div>';
+?>
+  <div class="d-sm-flex align-items-center justify-content-between">
+  <h3 class="h4 mb-0"><?php echo $title; ?></h3>
+  <?php if ($withbutton) { ?>
+    <a href="<?php echo $link; ?>" class="btn btn-primary btn-icon-split btn-sm">';
+      <span class="icon text-white-50">'
+        <i class="fas <?php echo $icon; ?> fa-fw"></i>
+      </span>
+      <span class="text"><?php echo $text; ?></span>
+    </a>
+  <?php } ?>
+  </div>
+<?php
 }
 
 function Card($title, $link, $icon, $color = 'primary', $counter = false, $number = 0)
 {
-  echo '<div class="col-xl-3 col-lg-4 col-md-6 mb-4">';
-  echo '<div class="card border-left-' . $color . ' shadow h-100">';
-  echo '<div class="card-body">';
-  echo '<div class="row no-gutters align-items-center">';
-  echo '<div class="col mr-2">';
-  echo '<div class="font-weight-bold text-' . $color . ' text-uppercase mb-1">' . $title . '</div>';
-  if ($counter) {
-    echo '<div class="h3 mb-0 font-weight-bold text-gray-800">' . $number . '</div>';
-  }
-  echo '</div>';
+?>
+  <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
+    <div class="card border-left-' . $color . ' shadow h-100">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">';
+            <div class="font-weight-bold text-' . $color . ' text-uppercase mb-1"><?php echo $title; ?></div>
 
-  echo '<div class="col-auto">';
-  echo '<i class="fas ' . $icon . ' fa-3x text-' . $color . '" aria="hidden"></i>';
-  echo '</div>';
-  echo '</div>';
-  echo '</div>';
+            <?php if ($counter) { ?>
+              <div class="h3 mb-0 font-weight-bold text-gray-800"><?php echo $number; ?></div>
+            <?php } ?>
+          </div>
 
-  echo '<div class="card-footer py-1 text-right">';
-  echo '<a class="small" href="' . $link . '">View Details</a>';
-  echo '</div>';
-  echo '</div>';
-  echo '</div>';
+          <div class="col-auto">
+            <i class="fas <?php echo $icon; ?> fa-3x text-<?php echo $color ; ?>" aria="hidden"></i>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-footer py-1 text-right">
+        <a class="small" href="<?php echo $link; ?>">View Details</a>
+      </div>
+    </div>
+  </div>
+<?php
+}
+
+function NavigationItem($condition, $link, $title, $icon='') {
+  $class = $condition ? ' active' : '';
+?>
+  <li class="nav-item<?php echo $class; ?>">
+    <a class="nav-link" href="<?php echo $link; ?>">
+      <i class="fas fa-fw <?php echo $icon; ?>"></i>
+      <span><?php echo $title; ?></span>
+    </a>
+  </li>
+<?php
 }
 ?>
