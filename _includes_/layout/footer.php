b60fcd1b0b2bd6b6a581ba02a3d446a4da51153b
John Leo Samante <johnleomuitsamante@gmail.com>
2022-12-06 08:50:06 +0800

    Added white background to footer.

--- _includes_/layout/footer.php
+++ _includes_/layout/footer.php
@@ -2,7 +2,7 @@
 # _includes_/layout/footer.php
 ?>
 
-<footer class="p-4 mt-auto">
+<footer class="p-4 bg-white mt-auto">
   <div class="text-center small text-muted">
     <?php echo 'Copyright &copy; ' . date('Y') . ' ' . GetSiteTitle(); ?>
   </div>
