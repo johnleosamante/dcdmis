d262fdcd997eb9fc2ce4203bcd310b277481f76d
John Leo Samante <johnleomuitsamante@gmail.com>
2022-12-07 15:51:32 +0800

    Modified code.

--- personnel/my_family.php
+++ personnel/my_family.php
@@ -1,48 +1,49 @@
 <div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" onclick="window.location.reload()">&times;</button>
-			 <h4 class="modal-title">UPDATE MY FAMILY BACKGROUND </h4>
-		</div>
+	<h5 class="modal-title">Update Family Member</h5>
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+</div>
+
+<form action="" Method="POST" enctype="multipart/form-data">
 	<div class="modal-body">
-		<form action="" Method="POST" enctype="multipart/form-data">
-		 <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
-										<thead>
-										<tr>
-											<th width="20%">Family Name</th>
-											<th width="30%">First Name</th>
-											<th width="30%">Middle Name</th>
-											<th width="10%">Birthdate</th>
-											<th width="10%">Relation</th>
-											
-											
-										</tr>
-										
-										</thead>
-										<tbody>
-										<?php
-										session_start();
-										include("../pcdmis/vendor/jquery/function.php");
-										foreach ($_GET as $key => $data)
-											{
-											$id=$_GET[$key]=base64_decode(urldecode($data));
-												
-											}
-										$_SESSION['No']=$id;
-										$result1=mysqli_query($con,"SELECT * FROM  	family_background  WHERE  	family_background.Emp_ID='".$_SESSION['EmpID']."' AND  	family_background.No ='".$id."'") or die ("View Profile Family Background Error");
-											while($row1=mysqli_fetch_array($result1))
-											{
-											echo '<tr><td style="text-align:center;"><input type="text" name="Lname" class="form-control" value="'.$row1['Family_Name'].'"></td>
-													  <td style="text-align:center;"><input type="text" name="Fname" class="form-control" value="'.$row1['First_Name'].'"></td>
-													  <td style="text-align:center;"><input type="text" name="Mname" class="form-control" value="'.$row1['Middle_Name'].'"></td>
-													  <td style="text-align:center;"><input type="date" name="Bdate" class="form-control" value="'.$row1['Birthdate'].'"></td>
-													  <td style="text-align:center;"><input type="text" name="Relate" class="form-control" value="'.$row1['Relation'].'"></td>
+
+
+	
+		<table width="100%" class="table table-striped table-bordered table-hover">
+			<thead>
+				<tr>
+					<th width="20%">Family Name</th>
+					<th width="30%">First Name</th>
+					<th width="30%">Middle Name</th>
+					<th width="10%">Birthdate</th>
+					<th width="10%">Relation</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<?php
+				include_once('../_includes_/function.php');
+				include_once('../_includes_/database/database.php');
+				foreach ($_GET as $key => $data) {
+					$id = $_GET[$key] = base64_decode(urldecode($data));
+				}
+				$_SESSION['No'] = $id;
+				$result1 = mysqli_query($con, "SELECT * FROM family_background WHERE Emp_ID='" . $_SESSION['EmpID'] . "' AND No ='" . $id . "'");
+
+				while ($row1 = mysqli_fetch_array($result1)) {
+					echo '<tr><td style="text-align:center;"><input type="text" name="Lname" class="form-control" value="' . $row1['Family_Name'] . '"></td>
+													  <td style="text-align:center;"><input type="text" name="Fname" class="form-control" value="' . $row1['First_Name'] . '"></td>
+													  <td style="text-align:center;"><input type="text" name="Mname" class="form-control" value="' . $row1['Middle_Name'] . '"></td>
+													  <td style="text-align:center;"><input type="date" name="Bdate" class="form-control" value="' . $row1['Birthdate'] . '"></td>
+													  <td style="text-align:center;"><input type="text" name="Relate" class="form-control" value="' . $row1['Relation'] . '"></td>
 													  
-												  </tr>';	
-											}
-				
-										?>
-									</tbody>
-								</table>			
-										<input type="submit" name="update_family" value="Update" class="btn btn-primary">
-										</form>
-							
-								  </div>
+												  </tr>';
+				}
+				?>
+			</tbody>
+		</table>
+	</div><!-- .modal-body -->
+
+	<div class="modal-footer">
+		<input type="submit" name="update_family" value="Update" class="btn btn-primary">
+	</div><!-- .modal-footer -->
+</form>
