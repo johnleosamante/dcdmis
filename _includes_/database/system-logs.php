826ec98da3e8d8bca33a54c30e507e40d08115ed
John Leo Samante <johnleomuitsamante@gmail.com>
2022-12-28 15:12:22 +0800

    Added new function.

--- _includes_/database/system-logs.php
+++ _includes_/database/system-logs.php
@@ -1,7 +1,11 @@
 <?php
 # _includes_/database/system-logs.php
 
-function SystemLogout($station, $userid, $datetime, $clientip) {
-  DBNonQuery("INSERT INTO tbl_system_logs (SchoolID, Emp_ID, Time_Log, `Status`, IPAddress) VALUES ('$station', '$userid', '$datetime', 'Logout', '$clientip');");
+function SystemLogout($stationID, $userID, $dateTime, $clientIP) {
+  DBNonQuery("INSERT INTO tbl_system_logs (SchoolID, Emp_ID, Time_Log, `Status`, IPAddress) VALUES ('$stationID', '$userID', '$dateTime', 'Logout', '$clientIP');");
+}
+
+function SystemLogin($stationID, $userID, $dateTime, $clientIP) {
+  DBNonQuery("INSERT INTO tbl_system_logs (SchoolID, Emp_ID, Time_Log, `Status`, IPAddress) VALUES ('$stationID', '$userID', '$dateTime', 'Login', '$clientIP');");
 }
 ?>
