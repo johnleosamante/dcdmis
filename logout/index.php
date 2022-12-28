ddab92b68b0c7b4202e584d0d2d5d690b72f5928
John Leo Samante <johnleomuitsamante@gmail.com>
2022-12-28 16:22:59 +0800

    Added prefix for session.

--- logout/index.php
+++ logout/index.php
@@ -5,8 +5,8 @@ include_once('../_includes_/function.php');
 include_once('../_includes_/database/database.php');
 include_once('../_includes_/database/system-logs.php');
 
-if (isset($_SESSION['uid'])) {
-  SystemLogout($_SESSION['school_id'], $_SESSION['uid'], GetDateTime(), GetClientIP());
+if (isset($_SESSION[GetSiteAlias() . '_uid'])) {
+  SystemLogout($_SESSION[GetSiteAlias() . '_school_id'], $_SESSION[GetSiteAlias() . '_uid'], GetDateTime(), GetClientIP());
 }
 
 session_destroy();
