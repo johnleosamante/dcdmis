2773d44790e004f91b5f0eb5e9b260aee5c58253
John Leo Samante <johnleomuitsamante@gmail.com>
2023-01-15 14:57:53 +0800

    Code changes.

--- personnel/transfer-request.php
+++ personnel/transfer-request.php
@@ -40,7 +40,7 @@ if (isset($_POST['updatemyposition'])) {
 		<div class="card">
 			<div class="card-header">
 				<div class="d-sm-flex align-items-center justify-content-between">
-					<h3 class="h4 mb-0">Request for Transfer</h3>
+					<h3 class="h4 mb-0">Transfer Request</h3>
 
 					<div class="d-inline-block">
 						<a href="#newrequest" class="btn btn-primary btn-icon-split btn-sm" data-toggle="modal"><span class="icon text-white-50"><i class="fas fa-plus fa-fw"></i></span><span class="text">New Request</span></a>
@@ -74,55 +74,105 @@ if (isset($_POST['updatemyposition'])) {
 
 				<div class="row">
 					<div class="col-lg-4">
-						<div class="panel-body">
-							<?php
-							$result = mysqli_query($con, "SELECT * FROM tbl_employee INNER JOIN tbl_station ON tbl_employee.Emp_ID=tbl_station.Emp_ID INNER JOIN tbl_job ON tbl_station.Emp_Position=tbl_job.Job_code WHERE tbl_employee.Emp_ID='" . $_SESSION['EmpID'] . "' LIMIT 1");
-							$row = mysqli_fetch_assoc($result);
-							$date = date('F j\, Y', strtotime($row['Emp_DOA']));
-							$bdate = date('F j\, Y', strtotime($row['Emp_Month'] . '/' . $row['Emp_Day'] . '/' . $row['Emp_Year']));
-							$age = date("Y") - $row['Emp_Year'];
-							echo '<label>Name:</label><input type="text" class="form-control" value="' . $row['Emp_LName'] . ', ' . $row['Emp_FName'] . '" disabled>
-	<label>Date of birth:</label><input type="text" class="form-control" value="' . $bdate . '"disabled>
-	<label>Age:</label><input type="text" class="form-control" value="' . $age . '"disabled>
-	<label>Contact Number:</label><input type="text" class="form-control" value="' . $row['Emp_Cell_No'] . '"disabled>
-	<label>Address:</label><input type="text" class="form-control" disabled value="' . $row['Emp_Address'] . '">
-	<label>Position:</label><input type="text" class="form-control"disabled value="' . $row['Job_description'] . '"><a href="#updateposition" data-toggle="modal">Change</a><br/>
-	<label>Date of Original Appointment:</label><input type="text" class="form-control"disabled value="' . $date . '"><a href="#updateappointment" data-toggle="modal">Change</a>';
-							?>
+						<?php
+						$result = mysqli_query($con, "SELECT * FROM tbl_employee INNER JOIN tbl_station ON tbl_employee.Emp_ID=tbl_station.Emp_ID INNER JOIN tbl_job ON tbl_station.Emp_Position=tbl_job.Job_code WHERE tbl_employee.Emp_ID='" . $_SESSION['EmpID'] . "' LIMIT 1");
+						$row = mysqli_fetch_assoc($result);
+						$date = date('F j\, Y', strtotime($row['Emp_DOA']));
+						$bdate = date('F j\, Y', strtotime($row['Emp_Month'] . '/' . $row['Emp_Day'] . '/' . $row['Emp_Year']));
+						$age = date("Y") - $row['Emp_Year'];
+						$job = $row['Job_description'];
+						?>
+						<h3 class="h4">Employee Information</h3>
+
+						<div class="form-group">
+							<label class="mb-0">Name:</label>
+							<input type="text" class="form-control" value="<?php echo $row['Emp_LName'] . ', ' . $row['Emp_FName']; ?>" disabled>
+						</div>
+
+						<div class="row">
+							<div class="col-7 col-xs-12">
+								<div class="form-group">
+									<label class="mb-0">Date of birth:</label>
+									<input type="text" class="form-control" value="<?php echo $bdate; ?>" disabled>
+								</div>
+							</div>
+
+							<div class="col-5 col-xs-12">
+								<div class="form-group">
+									<label class="mb-0">Age:</label>
+									<input type="text" class="form-control" value="<?php echo $age; ?>" disabled>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="mb-0">Contact Number:</label>
+							<input type="text" class="form-control" value="<?php echo $row['Emp_Cell_No']; ?>" disabled>
+						</div>
+
+						<div class="form-group">
+							<label class="mb-0">Address:</label>
+							<input type="text" class="form-control" disabled value="<?php echo $row['Emp_Address']; ?>">
+						</div>
+
+						<div class="form-group">
+							<div class="d-sm-flex align-items-center justify-content-between">
+								<label class="mb-0">Position:</label>
+								<a class="d-inline-block small" href="#updateposition" data-toggle="modal"><i class="fas fa-edit fa-fw"></i> Edit</a>
+							</div>
+
+							<input type="text" class="form-control" disabled value="<?php echo $job; ?>">
+						</div>
+
+						<div class="form-group mb-0">
+							<div class="d-sm-flex align-items-center justify-content-between">
+								<label class="mb-0">Date of Original Appointment:</label>
+								<a class="d-inline-block small" href="#updateappointment" data-toggle="modal"><i class="fas fa-edit fa-fw"></i> Edit</a>
+							</div>
+
+							<input type="text" class="form-control" disabled value="<?php echo $date; ?>">
 						</div>
 					</div>
 
 					<div class="col-lg-8">
-						<h4>DEPLOYMENT HISTORY</h4>
+						<h3 class="h4">Deployment History</h3>
 						<table width="100%" class="table table-striped table-bordered table-hover">
 							<thead>
-								<tr>
-									<th style="text-align:center;">#</th>
-									<th>Date of Assignment</th>
-									<th>School Assigned</th>
-									<th>Years in service</th>
-									<th>Position</th>
-									<th>Subject Area</th>
+								<tr class="text-center">
+									<th class="align-middle">#</th>
+									<th class="align-middle">Date of Assignment</th>
+									<th class="align-middle">School Assigned</th>
+									<th class="align-middle">Years in service</th>
+									<th class="align-middle">Position</th>
+									<th class="align-middle">Subject Area</th>
 								</tr>
 							</thead>
 							<tbody>
 								<?php
 								$no = 0;
 								$myhistory = mysqli_query($con, "SELECT * FROM tbl_deployment_history INNER JOIN tbl_school ON tbl_deployment_history.station_assign = tbl_school.SchoolID INNER JOIN tbl_job ON tbl_deployment_history.position_assign=tbl_job.Job_code WHERE tbl_deployment_history.Emp_ID='" . $_SESSION['EmpID'] . "'");
-								while ($rowhist = mysqli_fetch_array($myhistory)) {
-									$no++;
-									echo '<tr>
-					<td>' . $no . '</td>
-					<td>' . $rowhist['Date_assignment'] . '</td>
-					<td>' . $rowhist['SchoolName'] . '</td>
-					<td style="text-align:center;">' . $rowhist['No_of_years'] . '</td>
-					<td style="text-align:center;">' . $rowhist['Job_description'] . '</td>
-					<td>' . $rowhist['SubjectArea'] . '</td>
-				  </tr>';
+
+								if (mysqli_num_rows($myhistory) > 0) {
+									while ($rowhist = mysqli_fetch_array($myhistory)) {
+										$no++; ?>
+										<tr class="text-center">
+											<td class="align-middle"><?php echo $no; ?></td>
+											<td class="align-middle"><?php echo $rowhist['Date_assignment']; ?></td>
+											<td class="align-middle"><?php echo $rowhist['SchoolName']; ?></td>
+											<td class="align-middle"><?php echo $rowhist['No_of_years']; ?></td>
+											<td class="align-middle"><?php echo $rowhist['Job_description']; ?></td>
+											<td class="align-middle"><?php echo $rowhist['SubjectArea']; ?></td>
+										</tr>
+									<?php
+									}
+								} else { ?>
+									<tr>
+										<td class="text-center align-middle" colspan="6">No data available in table.</td>
+									</tr>
+								<?php
 								}
 								?>
 							</tbody>
-
 						</table>
 					</div>
 				</div>
@@ -131,91 +181,106 @@ if (isset($_POST['updatemyposition'])) {
 	</div><!-- .col -->
 </div><!-- .row -->
 
-<div class="modal fade" id="updateposition" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+<div class="modal fade" id="updateposition" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h3 class="modal-title">
-					Update my position
-				</h3>
+				<h5 class="modal-title">Position</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
 			</div><!-- .modal-header -->
 
-			<form action="" Method="POST" enctype="multipart/form-data">
+			<form action="" Method="POST" role="form">
 				<div class="modal-body">
-					<label>Select Position:</label>
-					<select name="newposition" class="form-control" placeholder="Select Position" required>
-						<option value="">--select--</option>
-						<?php
-						$mynewpost = mysqli_query($con, "SELECT * FROM tbl_job ORDER BY Job_description Asc");
-						while ($rowpost = mysqli_fetch_array($mynewpost)) {
-							echo '<option value="' . $rowpost['Job_code'] . '">' . $rowpost['Job_description'] . '</option>';
-						}
-						?>
-					</select>
-					<label>Date of Assignment:</label>
-					<input type="date" name="date_of_assignment" class="form-control" required>
+					<div class="form-group">
+						<label for="newposition" class="mb-0">Position:</label>
+						<select id="newposition" name="newposition" class="form-control" placeholder="Select Position" required>
+							<?php
+							$mynewpost = mysqli_query($con, "SELECT * FROM tbl_job ORDER BY Job_description Asc");
+							while ($rowpost = mysqli_fetch_array($mynewpost)) { ?>
+								<option value="<?php echo $rowpost['Job_code']; ?>" <?php echo SetOptionSelected($job, $rowpost['Job_description']); ?>><?php echo $rowpost['Job_description']; ?></option>
+							<?php
+							}
+							?>
+						</select>
+					</div>
+
+					<div class="form-group mb-0">
+						<label for="date_of_assignment" class="mb-0">Date of Assignment:</label>
+						<input type="date" name="date_of_assignment" class="form-control" required>
+					</div>
 				</div><!-- .modal-body -->
 
 				<div class="modal-footer">
-					<input type="submit" name="updatemyposition" value="UPDATE" class="btn btn-primary">
-					<button type="button" class="btn tbn-default" data-dismiss="modal" onclick="window.location.reload();">Close</button>
+					<input type="submit" name="updatemyposition" value="Update" class="btn btn-primary">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 				</div><!-- .modal-footer -->
 			</form>
 		</div><!-- .modal-content -->
 	</div><!-- .modal-dialog -->
 </div><!-- .modal -->
 
-<div class="modal fade" id="updateappointment" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+<div class="modal fade" id="updateappointment" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h3 class="modal-title">
-					Update my Date of Original Appointment
-				</h3>
+				<h5 class="modal-title">Date of Original Appointment</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
 			</div><!-- .modal-header -->
 
-			<form action="" Method="POST" enctype="multipart/form-data">
+			<form action="" Method="POST" role="form">
 				<div class="modal-body">
-					<label>Date of Original Appointment:</label>
 					<input type="date" name="DOA" class="form-control" required>
 				</div><!-- .modal-body -->
 
 				<div class="modal-footer">
-					<input type="submit" name="updatemyappointment" value="UPDATE" class="btn btn-primary">
-					<button type="button" class="btn tbn-default" data-dismiss="modal" onclick="window.location.reload();">Close</button>
+					<input type="submit" name="updatemyappointment" value="Update" class="btn btn-primary">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 				</div><!-- .modal-footer -->
 			</form>
 		</div><!-- .modal-content -->
 	</div><!-- .modal-dialog -->
 </div><!-- .modal -->
 
-<div class="modal fade" id="newrequest" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+<div class="modal fade" id="newrequest" role="dialog" data-backdrop="static" data-keyboard="false">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h3 class="modal-title">
-					New Request for Transfer Information
-				</h3>
+				<h5 class="modal-title">New Transfer Request</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
 			</div><!-- .modal-header -->
 
-			<form action="" Method="POST" enctype="multipart/form-data">
+			<form action="" Method="POST" role="form">
 				<div class="modal-body">
-					<label>Date request:</label>
-					<input type="date" class="form-control" value="<?php echo date("Y-m-d"); ?>" disabled>
-					<input type="hidden" name="DRT" class="form-control" value="<?php echo date("Y-m-d"); ?>">
-					<label>Transfer to:</label>
-					<input type="text" name="transferto" class="form-control" required>
-					<label># of years render:</label>
-					<input type="text" name="no_of_years" class="form-control" required>
-					<label>Reason to Transfer:</label>
-					<textarea name="reason_to_transfer" class="form-control" required rows="2"></textarea>
-					<label>Subject Areas:</label>
-					<input type="text" name="SubjectArea" class="form-control" required>
+					<div class="form-group">
+						<label class="mb-0">Date of request:</label>
+						<input type="date" class="form-control" value="<?php echo date("Y-m-d"); ?>" disabled>
+						<input type="hidden" name="DRT" class="form-control" value="<?php echo date("Y-m-d"); ?>">
+					</div>
+
+					<div class="form-group">
+						<label for="transferto" class="mb-0">Transfer to:</label>
+						<input type="text" id="transferto" name="transferto" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="no_of_years" class="mb-0">Number of Years Rendered:</label>
+						<input type="text" id="no_of_years" name="no_of_years" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="reason_to_transfer" class="mb-0">Reason for Transfer:</label>
+						<textarea id="reason_to_transfer" name="reason_to_transfer" class="form-control" required rows="2"></textarea>
+					</div>
+
+					<div class="form-group mb-0">
+						<label for="SubjectArea" class="mb-0">Subject Area:</label>
+						<input type="text" id="SubjectArea" name="SubjectArea" class="form-control" required>
+					</div>
 				</div><!-- .modal-body -->
 
 				<div class="modal-footer">
-					<input type="submit" name="newrequest" value="SENT" class="btn btn-primary">
-					<button type="button" class="btn tbn-default" data-dismiss="modal" onclick="window.location.reload();">Close</button>
+					<input type="submit" name="newrequest" value="OK" class="btn btn-primary">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 				</div><!-- .modal-footer -->
 			</form>
 		</div><!-- .modal-content -->
