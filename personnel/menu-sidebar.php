cb85c3e92531bb629b0cbd90182ea84856f6e969
John Leo Samante <johnleomuitsamante@gmail.com>
2023-03-18 20:06:37 +0800

    Added Menu Items

--- personnel/menu-sidebar.php
+++ personnel/menu-sidebar.php
@@ -16,6 +16,14 @@
 
 	<hr class="sidebar-divider my-0">
 
+	<li class="nav-item<?php echo SetActiveNavigationItem($page === 'Personal Data Sheet'); ?>"><a class="nav-link" href="<?php echo GetHashURL('personnel', 'Personal Data Sheet'); ?>"><i class="fas fa-user fa-fw"></i><span>Personal Data Sheet</span></a></li>
+
+	<li class="nav-item<?php echo SetActiveNavigationItem($page === 'Service Record'); ?>"><a class="nav-link" href="<?php echo GetHashURL('personnel', 'Service Record'); ?>"><i class="fas fa-file-alt fa-fw"></i><span>Service Record</span></a></li>
+
+	<li class="nav-item<?php echo SetActiveNavigationItem($page === '201 File'); ?>"><a class="nav-link" href="<?php echo GetHashURL('personnel', '201 File'); ?>"><i class="fas fa-book-open fa-fw"></i><span>201 File</span></a></li>
+
+	<hr class="sidebar-divider my-0">
+
 	<li class="nav-item<?php echo SetActiveNavigationItem($page === 'Certificates'); ?>">
 		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCertificates" aria-expanded="true" aria-controls="collapseCertificates">
 			<i class="fas fa-certificate fa-fw"></i><span>Certificates</span>
@@ -23,6 +31,7 @@
 
 		<div id="collapseCertificates" class="collapse<?php echo SetActiveNavigationItem($page === 'Certificates'); ?>" aria-labelledby="headingCertificates" data-parent="#accordionSidebar">
 			<div class="bg-white py-2 collapse-inner rounded">
+				<a class="collapse-item" href="<?php echo GetHashURL('personnel', 'Certificates', 'All'); ?>">All Certificates</a>
 				<a class="collapse-item" href="<?php echo GetHashURL('personnel', 'Certificates', 'Division'); ?>">Division Level</a>
 				<a class="collapse-item" href="<?php echo GetHashURL('personnel', 'Certificates', 'Regional'); ?>">Regional Level</a>
 				<a class="collapse-item" href="<?php echo GetHashURL('personnel', 'Certificates', 'Central'); ?>">Central Level</a>
@@ -30,9 +39,11 @@
 		</div><!-- #collapseCertificates -->
 	</li>
 
-	<li class="nav-item<?php echo SetActiveNavigationItem($page === 'Payslip'); ?>"><a class="nav-link" href="<?php echo GetHashURL('personnel', 'Payslip'); ?>"><i class="fas fa-money-check fa-fw"></i><span>Payslip</span></a></li>
-
 	<li class="nav-item<?php echo SetActiveNavigationItem($page === 'Trainings'); ?>"><a class="nav-link" href="<?php echo GetHashURL('personnel', 'Trainings'); ?>"><i class="fas fa-chalkboard-teacher fa-fw"></i><span>Trainings</span></a></li>
+
+	<hr class="sidebar-divider my-0">
+
+	<li class="nav-item<?php echo SetActiveNavigationItem($page === 'Payslip'); ?>"><a class="nav-link" href="<?php echo GetHashURL('personnel', 'Payslip'); ?>"><i class="fas fa-money-check fa-fw"></i><span>Payslip</span></a></li>
 
 	<hr class="sidebar-divider d-none d-md-block">
 
