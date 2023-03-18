9111b32439d268d714cb14cbe1d7bd175974ec34
John Leo Samante <johnleomuitsamante@gmail.com>
2023-03-18 20:12:49 +0800

    Removed SQL Scripts

--- personnel/pds/tbl_recognition.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tbl_recognition AS SELECT `No`, `Recognation` AS `Recognition`, `Emp_ID` FROM other_information WHERE Recognation <> 'N/A' AND Recognation <> '' AND Recognation <> 'None';
-
-ALTER TABLE tbl_recognition
-  MODIFY `No` INT(11) NOT NULL AUTO_INCREMENT,
-  ADD PRIMARY KEY (`No`);
-;
--- personnel/pds/tbl_special_skills.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tbl_special_skills AS SELECT `No`, `Special_Skills`, `Emp_ID` FROM other_information WHERE Special_Skills <> 'N/A' OR Special_Skills <> '';
-
-ALTER TABLE tbl_special_skills
-  MODIFY `No` INT(11) NOT NULL AUTO_INCREMENT,
-  ADD PRIMARY KEY (`No`);
-;
