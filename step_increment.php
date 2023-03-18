9abc4d9dc19f62bed80881fbf0cae661ae6cb2dd
John Leo Samante <johnleomuitsamante@gmail.com>
2023-03-18 15:33:49 +0800

    Removed unneccessary files.

--- step_increment.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('_includes_/config.php');
-include_once('_includes_/database/database.php');
-
-mysqli_query($con, "UPDATE tbl_step_increment SET Date_last_step='1', Step_No='1', No_of_year='0';");
-?>
