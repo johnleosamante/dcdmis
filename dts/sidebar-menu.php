7edc12dd44c3c07a2ebe23e1be063d059197ec1a
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-09 05:31:57 +0800

    Removed unused session key

--- dts/sidebar-menu.php
+++ dts/sidebar-menu.php
@@ -5,10 +5,10 @@
 <hr class="sidebar-divider d-none d-md-block my-0">
 
 <?php
-sidebar_menu_item(isset($url) && str_contains($url, 'Incoming'), custom_uri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', num_rows(incoming_documents($_SESSION[alias() . '_station'], $_SESSION[alias() . '_station_is_school'])));
-sidebar_menu_item(isset($url) && str_contains($url, 'Pending'), custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', num_rows(pending_documents($_SESSION[alias() . '_station'], $_SESSION[alias() . '_station_is_school'])));
-sidebar_menu_item(isset($url) && str_contains($url, 'Outgoing'), custom_uri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', num_rows(outgoing_documents($_SESSION[alias() . '_station'], $_SESSION[alias() . '_station_is_school'])));
-sidebar_menu_item(isset($url) && str_contains($url, 'Ongoing'), custom_uri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', num_rows(ongoing_documents($_SESSION[alias() . '_station'], $_SESSION[alias() . '_station_is_school'])));
+sidebar_menu_item(isset($url) && str_contains($url, 'Incoming'), custom_uri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', num_rows(incoming_documents($_SESSION[alias() . '_station'])));
+sidebar_menu_item(isset($url) && str_contains($url, 'Pending'), custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', num_rows(pending_documents($_SESSION[alias() . '_station'])));
+sidebar_menu_item(isset($url) && str_contains($url, 'Outgoing'), custom_uri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', num_rows(outgoing_documents($_SESSION[alias() . '_station'])));
+sidebar_menu_item(isset($url) && str_contains($url, 'Ongoing'), custom_uri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', num_rows(ongoing_documents($_SESSION[alias() . '_station'])));
 ?>
 
 <hr class="sidebar-divider my-0">
