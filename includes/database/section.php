a77b9b86cef3a7a2a544a657f556a8f9c6eb26bd
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-09 07:02:57 +0800

    Added section id

--- includes/database/section.php
+++ includes/database/section.php
@@ -3,7 +3,7 @@
 // tbl_div_section
 
 function section($id, $include_id = false) {
-  $sql = "SELECT Section_Office AS `name` FROM tbl_div_section";
+  $sql = "SELECT Section_Code AS id, Section_Office AS `name` FROM tbl_div_section";
   if (!$include_id) {
     $sql .= " WHERE Section_Code <> '{$id}' ORDER BY Section_Office ASC;";
   } else {
