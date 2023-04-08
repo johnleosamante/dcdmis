f23a404868624d1c664a6d1639a424f6d380f8d9
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-09 06:10:38 +0800

    Removed unneccessary session key

--- modules/documents/completed-documents.php
+++ modules/documents/completed-documents.php
@@ -25,7 +25,7 @@ $_SESSION[alias() . '_previous_document'] = $page_title = 'Completed Documents';
         <tbody>
           <?php
           $no = 0;
-          $query = completed_documents($_SESSION[alias() . '_station'], $_SESSION[alias() . '_station_is_school']);
+          $query = completed_documents($_SESSION[alias() . '_station']);
           while ($row = fetch_array($query)) : ?>
             <tr>
               <td class="text-uppercase align-middle"><?php echo ++$no; ?></td>
