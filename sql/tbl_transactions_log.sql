aae1c6eb6f66bd4984b841ef4b8109e3035eef40
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-10 04:48:57 +0800

    Changed status count sql

--- sql/tbl_transactions_log.sql
+++ sql/tbl_transactions_log.sql
@@ -1,5 +1,6 @@
+/* TO BE EXECUTED BEFORE LAUNCH */
 UPDATE `tbl_transactions_log` SET `Trans_status`='Received' WHERE `Trans_status` LIKE '%on process%';
 
 SELECT * FROM `tbl_transactions_log` WHERE `Trans_status` LIKE '%on process%';
 
-SELECT `Transaction_code` AS `id`, COUNT(*) AS `instance`, `Status` AS `status` FROM `tbl_transactions_log` WHERE `From_office`='HRMO' GROUP BY `Transaction_code`, `Status` ORDER BY `status` DESC, COUNT(*) DESC;
+SELECT `Transaction_code` AS `id`, COUNT(*) AS `instance`, `Status` AS `status` FROM `tbl_transactions_log` GROUP BY `Transaction_code`, `Status` ORDER BY `status` DESC, COUNT(*) DESC;
