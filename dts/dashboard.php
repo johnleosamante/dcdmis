23646ac6cecc97456aed1c02c7e2fe9b1114a686
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-09 11:04:09 +0800

    Customized school dashboard

--- dts/dashboard.php
+++ dts/dashboard.php
@@ -5,16 +5,24 @@ content_title('Dashboard', true, custom_uri('dts', 'New Document'), 'New Documen
 
 <div class="row mt-4">
   <?php
-  card('Incoming Documents', custom_uri('dts', 'Incoming Documents'), 'fa-file-download', 'success', num_rows(incoming_documents($_SESSION[alias() . '_station'])));
+  $is_school_portal = $_SESSION[alias() . '_portal'] === 'school_portal';
 
-  card('Pending Documents', custom_uri('dts', 'Pending Documents'), 'fa-history', 'info', num_rows(pending_documents($_SESSION[alias() . '_station'])));
+  card('Incoming Documents', custom_uri('dts', 'Incoming Documents'), 'fa-file-download', 'primary', num_rows(incoming_documents($_SESSION[alias() . '_station'])));
 
-  card('Outgoing Documents', custom_uri('dts', 'Outgoing Documents'),'fa-file-upload', 'warning', num_rows(outgoing_documents($_SESSION[alias() . '_station'])));
+  if (!$is_school_portal) {
+    card('Pending Documents', custom_uri('dts', 'Pending Documents'), 'fa-history', 'success', num_rows(pending_documents($_SESSION[alias() . '_station'])));
+  }
 
-  card('Ongoing Documents', custom_uri('dts', 'Ongoing Documents'),'fa-tasks', 'danger', num_rows(ongoing_documents($_SESSION[alias() . '_station'])));
+  card('Outgoing Documents', custom_uri('dts', 'Outgoing Documents'), 'fa-file-upload', 'info', num_rows(outgoing_documents($_SESSION[alias() . '_station'])));
+
+  card('Ongoing Documents', custom_uri('dts', 'Ongoing Documents'), 'fa-tasks', 'warning', num_rows(ongoing_documents($_SESSION[alias() . '_station'])));
 
   card('Completed Documents', custom_uri('dts', 'Completed Documents'), 'fa-check-circle', 'secondary');
 
-  card('Received Documents', custom_uri('dts', 'Received Documents'), 'fa-hand-holding-medical', 'primary');
+  if (!$is_school_portal) {
+    card('Received Documents', custom_uri('dts', 'Received Documents'), 'fa-hand-holding-medical', 'dark');
+  }
+
+  card('Canceled Documents', custom_uri('dts', 'Canceled Documents'), 'fa-trash-alt', 'danger');
   ?>
 </div>
