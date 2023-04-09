b59450f0625c5d726574b8dd4b59889c166392f1
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-10 04:49:30 +0800

    Added comment

--- sql/tbl_transactions.sql
+++ sql/tbl_transactions.sql
@@ -1,3 +1,4 @@
+/* TO BE EXECUTED BEFORE LAUNCH */
 UPDATE `tbl_transactions` SET `Status`='Read' WHERE `Trans_Stats` LIKE '%complete%';
 UPDATE `tbl_transactions` SET `Status`='Read' WHERE `Trans_Stats` LIKE '%cancel%';
 UPDATE `tbl_transactions` SET `Trans_Stats`='Received' WHERE `Trans_stats` LIKE '%on process%';
