e834268186764f50fcac4dc33b36119489454796
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-09 10:33:28 +0800

    Removed pending and received menus

--- dts/sidebar-menu.php
+++ dts/sidebar-menu.php
@@ -5,9 +5,16 @@
 <hr class="sidebar-divider d-none d-md-block my-0">
 
 <?php
+$is_school_portal = $_SESSION[alias() . '_portal'] === 'school_portal';
+
 sidebar_menu_item(isset($url) && str_contains($url, 'Incoming'), custom_uri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', num_rows(incoming_documents($_SESSION[alias() . '_station'])));
-sidebar_menu_item(isset($url) && str_contains($url, 'Pending'), custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', num_rows(pending_documents($_SESSION[alias() . '_station'])));
+
+if (!$is_school_portal) {
+  sidebar_menu_item(isset($url) && str_contains($url, 'Pending'), custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', num_rows(pending_documents($_SESSION[alias() . '_station'])));
+}
+
 sidebar_menu_item(isset($url) && str_contains($url, 'Outgoing'), custom_uri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', num_rows(outgoing_documents($_SESSION[alias() . '_station'])));
+
 sidebar_menu_item(isset($url) && str_contains($url, 'Ongoing'), custom_uri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', num_rows(ongoing_documents($_SESSION[alias() . '_station'])));
 ?>
 
@@ -15,6 +22,10 @@ sidebar_menu_item(isset($url) && str_contains($url, 'Ongoing'), custom_uri('dts'
 
 <?php
 sidebar_menu_item(isset($url) && str_contains($url, 'Completed'), custom_uri('dts', 'Completed Documents'), 'Completed', 'fa-check-circle');
-sidebar_menu_item(isset($url) && str_contains($url, 'Received'), custom_uri('dts', 'Received Documents'), 'Received', 'fa-hand-holding-medical');
+
+if (!$is_school_portal) {
+  sidebar_menu_item(isset($url) && str_contains($url, 'Received'), custom_uri('dts', 'Received Documents'), 'Received', 'fa-hand-holding-medical');
+}
+
 sidebar_menu_item(isset($url) && str_contains($url, 'Canceled'), custom_uri('dts', 'Canceled Documents'), 'Canceled', 'fa-trash-alt');
 ?>
