cd8b69ca71f69e2b7d594ddac462583c93a9d419
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-10 17:00:04 +0800

    No results view

--- modules/error/not-found.php
+++ modules/error/not-found.php
@@ -1,1 +1,10 @@
-Not found
+<?php
+// modules/error/not-found.php
+?>
+
+<div class="text-center pt-5">
+  <div class="error mx-auto"><i class="fas fa-search fa-fw"></i></div>
+  <p class="lead text-gray-800 mb-0">No results found</p>
+  <p class="text-gray-500 mb-4">Sorry, we couldn't find what you're looking for...</p>
+  <a href="<?php echo uri() . '/' . $_SESSION[alias() . '_active_app']; ?>" title="Go back to dashboard">Back to Dashboard</a>
+</div>
