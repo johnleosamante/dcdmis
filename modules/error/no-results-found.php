91750d1228de9388aa743a97c5777956006da823
John Leo Samante <johnleomuitsamante@gmail.com>
2023-04-12 05:44:12 +0800

    Added search bar to no results page

--- modules/error/no-results-found.php
+++ modules/error/no-results-found.php
@@ -2,9 +2,26 @@
 // modules/error/not-found.php
 ?>
 
-<div class="text-center pt-5">
-  <div class="error mx-auto"><i class="fas fa-search fa-fw"></i></div>
+<div class="text-center pt-5 px-5">
+  <div class="error mx-auto text-gray-800"><i class="fas fa-search fa-fw"></i></div>
   <p class="lead text-gray-800 mb-0">No results found</p>
   <p class="text-gray-500 mb-4">Sorry, we couldn't find what you're looking for...</p>
-  <a href="<?php echo uri() . '/' . $_SESSION[alias() . '_active_app']; ?>" title="Go back to dashboard">Back to Dashboard</a>
+
+
+  <p class="text-gray-700 mb-1">Try a new search term instead</p>
+
+  <form class="mx-auto mb-4" method="POST" action="">
+    <div class="input-group">
+      <input type="text" class="form-control small" placeholder="Search for..." aria-label="Search" name="primary_search_text" value="" autofocus required>
+      <div class="input-group-append">
+        <button class="btn btn-primary" type="submit" name="primary_search_button">
+          <i class="fas fa-search fa-sm"></i>
+        </button>
+      </div>
+    </div>
+  </form>
+
+  <?php if (isset($_SESSION[alias() . '_user_id'])) : ?>
+    <a href="<?php echo uri() . '/' . $_SESSION[alias() . '_active_app']; ?>" title="Go back to dashboard">Go back to Dashboard</a>
+  <?php endif; ?>
 </div>
