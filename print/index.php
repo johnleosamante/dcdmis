14be2f8947b43fe6ed2ea29d67e19af4e2582520
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-14 15:49:38 +0800

    Added printable document

---
+++ print/index.php
@@ -0,0 +1,33 @@
+<?php
+// print/index.php
+require_once('../includes/function.php');
+require_once(root() . '/includes/plugin/fpdf/fpdf.php');
+include_once(root() . '/includes/database/database.php');
+include_once(root() . '/includes/database/document.php');
+
+foreach ($_GET as $key => $data) {
+  $url = $_GET[$key] = decode($data);
+  $page = real_escape_string($url);
+}
+
+$code = $_GET['id'];
+
+if (!isset($url) || $url === '') {
+  redirect(custom_uri('dts', '404'));
+} else {
+  $file = '';
+  switch ($url) {
+    case 'Print Document Tracking Slip':
+      $file = 'document-tracking-slip';
+      break;
+    case 'Print Document Information':
+      $file = 'document-information';
+      break;
+    default:
+      redirect(custom_uri('dts', '404'));
+      break;
+  }
+
+  include_once(root() . "/print/{$file}.php");
+}
+?>
