0c04d72596fcdc3772f513360318f8c92a08ceac
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-18 14:39:16 +0800

    Removed reusable functions

--- dts/app.php
+++ dts/app.php
@@ -11,23 +11,6 @@ if (!isset($_SESSION[alias() . '_portal'])) {
   exit;
 }
 
-function station_name($id) {
-  $section = section($id, true);
-
-  if (num_rows($section) > 0) {
-    return fetch_assoc($section)['name'];
-  } else {
-    $school = school_by_alias($id);
-    return num_rows($school) > 0 ? fetch_assoc($school)['name'] : $id;
-  }
-}
-
-function user_name($id) {
-  $user = fetch_assoc(employee($id));
-
-  return to_name($user['lname'], $user['fname'], $user['mname'], $user['ext'], true);
-}
-
 $show_prompt = false;
 $message = null;
 $type = 'success';
