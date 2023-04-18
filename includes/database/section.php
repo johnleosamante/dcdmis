f395f9d2e3d9ee3bb04a38b0039ab6bd55a22c35
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-18 14:41:39 +0800

    Added column

--- includes/database/section.php
+++ includes/database/section.php
@@ -3,14 +3,14 @@
 // tbl_div_section
 
 function sections_except($id) {
-  return query("SELECT Section_Code AS id, Section_Office AS `name` FROM tbl_div_section WHERE Section_Code <> '{$id}' ORDER BY Section_Office ASC;");
+  return query("SELECT Section_Code AS id, Section_Incharge AS `head`, Section_Office AS `name` FROM tbl_div_section WHERE Section_Code <> '{$id}' ORDER BY Section_Office ASC;");
 }
 
 function sections() {
-  return query("SELECT Section_Code AS id, Section_Office AS `name` FROM tbl_div_section ORDER BY Section_Office ASC;");
+  return query("SELECT Section_Code AS id, Section_Incharge AS `head`, Section_Office AS `name` FROM tbl_div_section ORDER BY Section_Office ASC;");
 }
 
 function section($id) {
-  return query("SELECT Section_Code AS id, Section_Office AS `name` FROM tbl_div_section WHERE Section_Code='{$id}' LIMIT 1;");
+  return query("SELECT Section_Code AS id, Section_Incharge AS `head`, Section_Office AS `name` FROM tbl_div_section WHERE Section_Code='{$id}' LIMIT 1;");
 }
 ?>
