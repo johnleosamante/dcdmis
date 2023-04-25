f71105d1a94c0eac33eeae32a5f458f443d96287
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-25 11:57:57 +0800

    Added sidebar options

--- hrmis/sidebar-menu.php
+++ hrmis/sidebar-menu.php
@@ -1,3 +1,15 @@
 <?php
-// dts/sidebar-menu.php
+// hrmis/sidebar-menu.php
+?>
+
+<hr class="sidebar-divider d-none d-md-block my-0">
+
+<div class="sidebar-heading mt-3">Employees</div>
+
+<?php
+sidebar_menu_item(isset($url) && str_contains($url, 'Active'), custom_uri('hrmis', 'Active Employees'), 'Active', 'fa-user-check', number_format(num_rows(active_employees())));
+
+sidebar_menu_item(isset($url) && str_contains($url, 'Retirable'), custom_uri('hrmis', 'Retirable Employees'), 'Retirable','fa-user-clock', number_format(num_rows(retirable_employees())));
+
+sidebar_menu_item(isset($url) && str_contains($url, 'Archived'), custom_uri('hrmis', 'Archived Employees'), 'Archived', 'fa-user-lock');
 ?>
