8f1529500758e4818e9fa114ef34974f715293d5
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-26 07:58:42 +0800

    Added about page files

---
+++ about/app.php
@@ -0,0 +1,4 @@
+<?php
+// about/app.php
+$page = 'About';
+?>
--- about/index.php
+++ about/index.php
@@ -1,1 +1,5 @@
-About
+<?php
+// about/index.php
+require_once('../includes/function.php');
+include_once(root() . '/includes/layout/theme-page.php');
+?>
