8aced6dcd486d83057711bd6323061800fd4ab92
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-25 11:57:27 +0800

    Added options

--- hrmis/dashboard.php
+++ hrmis/dashboard.php
@@ -5,6 +5,10 @@ content_title('Dashboard');
 
 <div class="row mt-4">
   <?php
+  card('Active Employees', custom_uri('hrmis', 'Active Employees'), 'fa-user-check', 'primary', number_format(num_rows(active_employees())));
 
+  card('Retirable Employees', custom_uri('hrmis', 'Retirable Employees'), 'fa-user-clock', 'success', number_format(num_rows(retirable_employees())));
+
+  card('Archived Employees', custom_uri('hrmis', 'Archived Employees'), 'fa-user-lock', 'danger');
   ?>
 </div>
