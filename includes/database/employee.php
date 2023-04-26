30ec489f35c54736124825037a4ddbf5bd7d21ff
johnleosamante <johnleomuitsamante@gmail.com>
2023-04-26 11:47:25 +0800

    Added id column

--- includes/database/employee.php
+++ includes/database/employee.php
@@ -1,7 +1,7 @@
 <?php
 // includes/database/employee.php
 function employee($id) {
-  return query("SELECT Emp_LName AS lname, Emp_FName AS fname, Emp_MName AS mname, Emp_Extension AS ext, Picture AS picture FROM tbl_employee WHERE Emp_ID='{$id}' LIMIT 1;");
+  return query("SELECT Emp_ID AS id, Emp_LName AS lname, Emp_FName AS fname, Emp_MName AS mname, Emp_Extension AS ext, Picture AS picture FROM tbl_employee WHERE Emp_ID='{$id}' LIMIT 1;");
 }
 
 function active_employees($station=null) {
