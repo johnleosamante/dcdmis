cd04023edca5095c5c9a9266960da4e7b4cecfee
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-02 16:32:58 +0800

    Added employee information

---
+++ modules/employees/view/personal-information.php
@@ -0,0 +1,13 @@
+<?php
+// modules/employees/view/personal-information.php
+?>
+
+<div class="tab-pane fade<?php echo set_active_navigation(!isset($_SESSION[alias() . '_employee_tab']) || $_SESSION[alias() . '_employee_tab'], 'show active'); ?>" id="personal-information">
+  <div class="d-sm-flex">
+    <h3 class="h4 mb-0">Personal Information</h3>
+  </div><!-- .d-sm-flex -->
+
+  <div class="row mt-3">
+
+  </div><!-- .row -->
+</div><!-- .tab-pane -->
