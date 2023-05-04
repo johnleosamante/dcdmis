7b3f0b5fa153d32602780ef39e0702b2612d56e8
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-04 16:43:01 +0800

    Fixed required files

--- hrmis/index.php
+++ hrmis/index.php
@@ -1,10 +1,11 @@
 <?php
 // hrmis/index.php
 require_once('../includes/function.php');
-include_once(root() . '/includes/string.php');
-include_once(root() . '/includes/database/database.php');
-include_once(root() . '/includes/database/employee.php');
+require_once(root() . '/includes/string.php');
+require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/employee.php');
 require_once(root() . '/includes/database/school.php');
 require_once(root() . '/includes/database/position.php');
-include_once(root() . '/includes/layout/theme-dashboard.php');
+require_once(root() . '/includes/database/family-background.php');
+require_once(root() . '/includes/layout/theme-dashboard.php');
 ?>
