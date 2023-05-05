353112e5ecf9ec67870e10172c3c629032640a6a
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-05 13:48:44 +0800

    Added eligibility data view

--- modules/employees/view/civil-service-eligibility.php
+++ modules/employees/view/civil-service-eligibility.php
@@ -3,7 +3,46 @@
 ?>
 
 <div class="tab-pane fade" id="civil-service-eligibility">
-  <div class="row mt-3">
+  <div class="row my-3">
+    <div class="col table-responsive">
+      <table width="100%" class="table table-striped table-bordered table-hover mb-0 text-center">
+        <thead>
+          <tr>
+            <th class="align-middle" width="30%" rowspan="2">Career Services / RA 1080 (Board / Bar) Under Special Laws / CES / CSEE Barangay Eligibility / Driver's License</th>
+            <th class="align-middle" width="10%" rowspan="2">Rating</th>
+            <th class="align-middle" width="10%" rowspan="2">Date of Examination / Conferment</th>
+            <th class="align-middle" width="25%" rowspan="2">Place of Examination / Conferment</th>
+            <th class="align-middle" width="20%" colspan="2">License</th>
+            <th class="align-middle" width="5%" rowspan="2">Attachment</th>
+          </tr>
+          <tr>
+            <th class="align-middle" width="10%">Number</th>
+            <th class="align-middle" width="10%">Date of Validity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $eligibilities = eligibility($employee['id']);
 
+          if (num_rows($eligibilities) > 0) {
+            while ($eligibility = fetch_assoc($eligibilities)) : ?>
+              <tr>
+                <td class="align-middle"><?php echo $eligibility['eligibility']; ?></td>
+                <td class="align-middle"><?php echo to_handle_null($eligibility['rating'], 'N/A'); ?></td>
+                <td class="align-middle"><?php echo to_date($eligibility['date'], '', 'F d, Y'); ?></td>
+                <td class="align-middle"><?php echo $eligibility['place']; ?></td>
+                <td class="align-middle"><?php echo to_handle_null($eligibility['license'], 'N/A'); ?></td>
+                <td class="align-middle"><?php echo to_date($eligibility['validity'], 'N/A', 'F d, Y'); ?></td>
+                <td class="align-middle">None</td>
+              </tr>
+            <?php endwhile;
+          } else { ?>
+            <tr>
+              <td colspan="7" class="align-middle">No data available in table</td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
   </div><!-- .row -->
 </div><!-- .tab-pane -->
