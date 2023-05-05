5a9ca4160a2a32a7fcbe3c49f6e15453bef5a077
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-05 13:47:08 +0800

    Added required files

--- hrmis/index.php
+++ hrmis/index.php
@@ -7,5 +7,8 @@ require_once(root() . '/includes/database/employee.php');
 require_once(root() . '/includes/database/school.php');
 require_once(root() . '/includes/database/position.php');
 require_once(root() . '/includes/database/family-background.php');
+require_once(root() . '/includes/database/children.php');
+require_once(root() . '/includes/database/education.php');
+require_once(root() . '/includes/database/eligibility.php');
 require_once(root() . '/includes/layout/theme-dashboard.php');
 ?>
