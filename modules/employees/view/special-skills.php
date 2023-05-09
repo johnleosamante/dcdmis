94c484eb6a7badd5cca4ca69ddf123ddb3b99420
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-09 10:25:06 +0800

    Populated special skills table

--- modules/employees/view/special-skills.php
+++ modules/employees/view/special-skills.php
@@ -4,6 +4,30 @@
 
 <div class="tab-pane fade" id="special-skills">
   <div class="row mt-3">
+    <div class="col table-responsive">
+      <table width="100%" class="table table-striped table-bordered table-hover mb-0 text-center">
+        <thead>
+          <tr>
+            <th class="align-middle">Special Skills &amp; Hobbies</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $skills = special_skill($employee['id']);
 
+          if (num_rows($skills) > 0) {
+            while ($skill = fetch_assoc($skills)) : ?>
+              <tr>
+                <td class="align-middle"><?php echo $skill['skill']; ?></td>
+              </tr>
+            <?php endwhile;
+          } else { ?>
+            <tr>
+              <td class="align-middle">No data available in table</td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
   </div><!-- .row -->
 </div><!-- .tab-pane -->
