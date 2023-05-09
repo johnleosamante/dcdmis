75121056b09135fffc68fd5323a115c8e143347c
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-09 10:21:57 +0800

    Added special skill data function

---
+++ includes/database/special-skill.php
@@ -0,0 +1,7 @@
+<?php
+// includes/database/special-skill.php
+
+function special_skill($id) {
+  return query("SELECT `No` AS `no`, Special_Skills AS skill, Emp_ID AS id FROM tbl_special_skills WHERE Emp_ID='{$id}' ORDER BY Special_Skills;");
+}
+?>
