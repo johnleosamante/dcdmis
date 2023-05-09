70fbce4c8dd7c830295a3c145ba6ac28536524ba
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-09 16:23:40 +0800

    Added search button function for hrmis

--- hrmis/app.php
+++ hrmis/app.php
@@ -14,4 +14,10 @@ $message = null;
 $type = 'success';
 $align = 'left';
 $page = $app_title = "Human Resource Management Information System";
+
+if (isset($_POST['primary_search_button'])) {
+  redirect(custom_uri('hrmis', 'Employee Search', real_escape_string($_POST['primary_search_text'])));
+}
+
+$user_id = $_SESSION[alias() . '_user_id'];
 ?>
