2ac2a6d12b315d9d950fac6a4fa3366889c8dc60
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-09 10:37:51 +0800

    Renamed reference

---
+++ modules/employees/view/reference.php
@@ -0,0 +1,9 @@
+<?php
+// modules/employees/view/reference.php
+?>
+
+<div class="tab-pane fade" id="reference">
+  <div class="row my-3">
+
+  </div><!-- .row -->
+</div><!-- .tab-pane -->
--- modules/employees/view/references.php
+++
@@ -1,9 +0,0 @@
-<?php
-// modules/employees/view/references.php
-?>
-
-<div class="tab-pane fade" id="references">
-  <div class="row mt-3">
-
-  </div><!-- .row -->
-</div><!-- .tab-pane -->
