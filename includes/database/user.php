ecf9468fca07db7d7bffa63767ffe443b68e977e
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-12 15:15:34 +0800

    Modified user function

--- includes/database/user.php
+++ includes/database/user.php
@@ -1,6 +1,6 @@
 <?php
 // includes/database/user.php
 function user($id) {
-  return query("SELECT usercode AS id, username AS email, Station AS station, Link AS portal FROM tbl_user WHERE usercode='{$id}' LIMIT 1;");
+  return query("SELECT tbl_user.usercode AS id, tbl_user.username AS email, tbl_user.Station AS station, tbl_station.Emp_Station AS station_code, tbl_user.Link AS portal FROM tbl_user INNER JOIN tbl_station ON tbl_user.usercode=tbl_station.Emp_ID WHERE usercode='{$id}' LIMIT 1;");
 }
 ?>
