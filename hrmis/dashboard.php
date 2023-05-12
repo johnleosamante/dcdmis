6e1640dd3283ce8b625ca044d0ef32565c08e79e
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-12 15:14:51 +0800

    Rearranged dashboard chart cards

--- hrmis/dashboard.php
+++ hrmis/dashboard.php
@@ -16,10 +16,11 @@ content_title('Dashboard');
 </div><!-- .row -->
 
 <script src="<?php echo uri(); ?>/assets/vendor/chart.js/Chart.min.js"></script>
+<script src="<?php echo uri(); ?>/assets/vendor/chart.js/chartjs-plugin-datalabels.min.js"></script>
 <script src="<?php echo uri(); ?>/assets/js/chart-custom.js"></script>
 
 <div class="row">
-  <div class="col-xl-4 col-lg-4 col-md-12 mb-4">
+  <div class="col-xl-3 col-lg-12 mb-4">
     <div class="card shadow">
       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by Gender</h6>
@@ -33,9 +34,27 @@ content_title('Dashboard');
         </div>
       </div>
     </div>
-  </div><!-- .col-xl-4 -->
+  </div><!-- .col-xl-3 -->
 
-  <div class="col-xl-4 col-lg-4 col-md-12 mb-4">
+  <div class="col-xl-9 col-lg-12 mb-4">
+    <div class="card shadow">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employee Gender by Category</h6>
+      </div>
+      <div class="card-body">
+        <div class="chart-bar h-auto">
+          <canvas id="gender-comparative-bar-chart"></canvas>
+          <script>
+            generate_comparative_bar_chart(<?php echo json_encode(fetch_all_assoc(employee_gender_category())); ?>, <?php echo json_encode(array('#02a3fe', '#ec49a6')); ?>, 'gender-comparative-bar-chart');
+          </script>
+        </div>
+      </div>
+    </div>
+  </div><!-- .col-xl-9 -->
+</div><!-- .row -->
+
+<div class="row">
+  <div class="col-xl-3 col-lg-12 mb-4">
     <div class="card shadow">
       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by Category</h6>
@@ -52,69 +71,9 @@ content_title('Dashboard');
         </div>
       </div>
     </div>
-  </div><!-- .col-xl-4 -->
+  </div><!-- .col-xl-3 -->
 
-  <div class="col-xl-4 col-lg-4 col-md-12 mb-4">
-    <div class="card shadow">
-      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by District</h6>
-      </div>
-      <div class="card-body">
-        <div class="chart-pie py-2">
-          <canvas id="district-polar-area-chart"></canvas>
-          <script>
-            <?php
-            $district_employees = district_employee();
-            ?>
-            generate_polar_area_chart(<?php echo json_encode(fetch_all_assoc($district_employees)); ?>, generate_color_pallete(<?php echo num_rows($district_employees); ?>), 'district-polar-area-chart');
-          </script>
-        </div>
-      </div>
-    </div>
-  </div><!-- .col-xl-4 -->
-</div><!-- .row -->
-
-<div class="row">
-  <div class="col-xl-8 mb-4">
-    <div class="card shadow">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employee Gender by Category</h6>
-      </div>
-      <div class="card-body">
-        <div class="chart-bar h-auto">
-          <canvas id="gender-comparative-bar-chart"></canvas>
-          <script>
-            generate_comparative_bar_chart(<?php echo json_encode(fetch_all_assoc(employee_gender_category())); ?>, <?php echo json_encode(array('#02a3fe', '#ec49a6')); ?>, 'gender-comparative-bar-chart');
-          </script>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-xl-8 col-md-12 mb-4">
-    <div class="card shadow">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by Assignment</h6>
-      </div>
-      <div class="card-body">
-        <div class="chart-bar h-auto">
-          <canvas id="station-bar-chart"></canvas>
-          <script>
-            <?php
-            $employee_stations = employee_station();
-            ?>
-            generate_bar_chart(<?php echo json_encode(fetch_all_assoc($employee_stations)); ?>, generate_color_pallete(<?php echo num_rows($employee_stations); ?>), 'station-bar-chart');
-          </script>
-        </div>
-      </div>
-    </div>
-  </div><!-- .col-xl-12 -->
-</div><!-- .row -->
-
-<div class="row">
-  <div class="col-xl-8 col-md-12">
+  <div class="col-xl-9 col-lg-12 mb-4">
     <div class="card shadow">
       <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by Position</h6>
@@ -131,5 +90,45 @@ content_title('Dashboard');
         </div>
       </div>
     </div>
-  </div><!-- .col-xl-12 -->
+  </div><!-- .col-xl-9 -->
+</div><!-- .row -->
+
+<div class="row">
+  <div class="col-xl-3 col-lg-12 mb-4">
+    <div class="card shadow">
+      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+        <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by District</h6>
+      </div>
+      <div class="card-body">
+        <div class="chart-pie py-2">
+          <canvas id="district-polar-area-chart"></canvas>
+          <script>
+            <?php
+            $district_employees = district_employee();
+            ?>
+            generate_polar_area_chart(<?php echo json_encode(fetch_all_assoc($district_employees)); ?>, generate_color_pallete(<?php echo num_rows($district_employees); ?>), 'district-polar-area-chart');
+          </script>
+        </div>
+      </div>
+    </div>
+  </div><!-- .col-xl-3 -->
+
+  <div class="col-xl-9 col-lg-12">
+    <div class="card shadow">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employees by Assignment</h6>
+      </div>
+      <div class="card-body">
+        <div class="chart-bar h-auto">
+          <canvas id="station-bar-chart"></canvas>
+          <script>
+            <?php
+            $employee_stations = employee_station();
+            ?>
+            generate_bar_chart(<?php echo json_encode(fetch_all_assoc($employee_stations)); ?>, generate_color_pallete(<?php echo num_rows($employee_stations); ?>), 'station-bar-chart');
+          </script>
+        </div>
+      </div>
+    </div>
+  </div><!-- .col-xl-9 -->
 </div><!-- .row -->
