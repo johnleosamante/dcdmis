14da288285c3f2f19e24853ffe543f0497649f1b
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-16 08:48:48 +0800

    Added button actions

--- hrmis/app.php
+++ hrmis/app.php
@@ -6,9 +6,7 @@ if (!isset($_SESSION[alias() . '_user_id'])) {
   redirect(uri() . '/login');
 }
 
-// echo 'HRMIS Warning: Unauthorized Access';
-// exit;
-
+$success = true; 
 $show_prompt = false;
 $message = null;
 $type = 'success';
@@ -20,4 +18,54 @@ if (isset($_POST['primary_search_button'])) {
 }
 
 $user_id = $_SESSION[alias() . '_user_id'];
+
+/* PERSONAL INFORMATION */
+if (isset($_POST['UpdatePersonalInformation'])) {
+  $employee_photo = $_SESSION['current_employee_photo'];
+  $employee_id = $_SESSION['current_employee_id'];
+
+  if ($_FILES['imageUpload']['size'] > 0 && $_FILES['imageUpload']['error'] == 0) {
+    $myfile = $_FILES['imageUpload']['name'];
+    $temp = $_FILES['imageUpload']['tmp_name'];
+    $type = $_FILES['imageUpload']['type'];
+    $ext = pathinfo($myfile, PATHINFO_EXTENSION);
+    $employee_photo = 'uploads/images/' . $employee_id . '/' . $employee_id . '.' . $ext;
+
+    move_uploaded_file($temp, '../' . $employee_photo);
+  }
+
+  $dob = strtotime($_POST['dob']);
+  $byear = date("Y", $dob);
+  $bmonth = date("m", $dob);
+  $bday = date("d", $dob);
+
+  update_employee(sanitize($_POST['lname']), sanitize($_POST['fname']), sanitize($_POST['mname']), sanitize($_POST['ext']), $bmonth, $bday, $byear, sanitize($_POST['pob']), sanitize($_POST['sex']), sanitize($_POST['civil_status']), sanitize($_POST['civil_status_others']), sanitize($_POST['citizenship']), sanitize($_POST['dual_citizenship']), sanitize($_POST['dual_citizenship_country']), sanitize($_POST['rlot']), sanitize($_POST['rstreet']), sanitize($_POST['rsubdivision']), sanitize($_POST['rbarangay']), sanitize($_POST['rcity']), sanitize($_POST['rprovince']), sanitize($_POST['rzip']), sanitize($_POST['plot']), sanitize($_POST['pstreet']), sanitize($_POST['psubdivision']), sanitize($_POST['pbarangay']), sanitize($_POST['pcity']), sanitize($_POST['pprovince']), sanitize($_POST['pzip']), sanitize($_POST['height']), sanitize($_POST['weight']), sanitize($_POST['blood_type']), sanitize($_POST['gsis']), sanitize($_POST['pagibig']), sanitize($_POST['philhealth']), sanitize($_POST['sss']), sanitize($_POST['telephone']), sanitize($_POST['mobile']), sanitize($_POST['email']), sanitize($_POST['tin']), sanitize($_POST['agency_id']), $employee_photo, $employee_id);
+
+  if (affected_rows() === 1) {
+    $success = true;
+    $message = 'Personal Information has been updated successfully!';
+    $show_prompt = true;
+  }
+
+  $_SESSION['pdstab'] = 'personal-information';
+}
+
+/* FAMILY BACKGROUND */
+if (isset($_POST['UpdateFamilyBackground'])) {
+  $employee_id = $_SESSION['current_employee_id'];
+
+  if (num_rows(family($employee_id)) === 0) {
+    create_family(sanitize($_POST['slast']), sanitize($_POST['sfirst']), sanitize($_POST['sext']), sanitize($_POST['smiddle']), sanitize($_POST['swork']), sanitize($_POST['sbusiness']), sanitize($_POST['sbusiness_address']), sanitize($_POST['stelephone']), sanitize($_POST['flast']), sanitize($_POST['ffirst']), sanitize($_POST['fext']), sanitize($_POST['fmiddle']), sanitize($_POST['mlast']), sanitize($_POST['mfirst']), sanitize($_POST['mmiddle']), $employee_id);
+  } else {
+    update_family(sanitize($_POST['slast']), sanitize($_POST['sfirst']), sanitize($_POST['sext']), sanitize($_POST['smiddle']), sanitize($_POST['swork']), sanitize($_POST['sbusiness']), sanitize($_POST['sbusiness_address']), sanitize($_POST['stelephone']), sanitize($_POST['flast']), sanitize($_POST['ffirst']), sanitize($_POST['fext']), sanitize($_POST['fmiddle']), sanitize($_POST['mlast']), sanitize($_POST['mfirst']), sanitize($_POST['mmiddle']), $employee_id);
+  }
+
+  if (affected_rows() === 1) {
+    $success = true;
+    $message = 'Family Background has been updated successfully!';
+    $show_prompt = true;
+  }
+
+  $_SESSION['pdstab'] = 'family-background';
+}
 ?>
