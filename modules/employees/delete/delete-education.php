ebfb7001f0e0c1f5bb2702e4ed1d19df59c23068
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-17 10:34:11 +0800

    Delete education confirm modal

---
+++ modules/employees/delete/delete-education.php
@@ -0,0 +1,13 @@
+<?php
+// modules/employees/delete/delete-education.php
+include_once('../../../includes/function.php');
+include_once(root() . '/includes/layout/components.php');
+
+foreach ($_GET as $key => $data) {
+  $id = $_GET[$key] = $data;
+}
+
+$_SESSION[alias() . '_current_education_id'] = $id;
+
+modal_confirm_delete('Are you sure you want to continue and delete this entry?', 'Delete Education?', 'DeleteEducation');
+?>
