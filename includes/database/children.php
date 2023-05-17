28014bf16003e6ff2223dea20bdb5e2779b498f9
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-17 10:15:16 +0800

    Modified children data function

--- includes/database/children.php
+++ includes/database/children.php
@@ -5,8 +5,8 @@ function children($id) {
   return query("SELECT `No` AS `no`, Family_Name AS `last`, First_Name AS `first`, Name_Extension AS ext, Middle_Name AS middle, Birthdate AS dob, Emp_ID AS id FROM family_background WHERE Emp_ID='{$id}' ORDER BY Birthdate ASC;");
 }
 
-function child($no) {
-  return query("SELECT `No` AS `no`, Family_Name AS `last`, First_Name AS `first`, Name_Extension AS ext, Middle_Name AS middle, Birthdate AS dob, Emp_ID AS id FROM family_background WHERE `No`='{$no}' LIMIT 1;");
+function child($id, $no) {
+  return query("SELECT `No` AS `no`, Family_Name AS `last`, First_Name AS `first`, Name_Extension AS ext, Middle_Name AS middle, Birthdate AS dob, Emp_ID AS id FROM family_background WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 
 function create_child($lname, $fname, $ext, $mname, $dob, $id) {
@@ -14,7 +14,7 @@ function create_child($lname, $fname, $ext, $mname, $dob, $id) {
 }
 
 function update_child($lname, $fname, $ext, $mname, $dob, $id, $no) {
-  non_query("UPDATE family_background SET Family_Name='{$lname}', First_Name='{$fname}', Name_Extension='{$ext}', Middle_Name='{$mname}', Birthdate='{$dob}' WHERE Emp_ID='{$id}' AND `No`='{$no}';");
+  non_query("UPDATE family_background SET Family_Name='{$lname}', First_Name='{$fname}', Name_Extension='{$ext}', Middle_Name='{$mname}', Birthdate='{$dob}' WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 
 function delete_child($id, $no) {
