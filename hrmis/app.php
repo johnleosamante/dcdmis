4a67711627d49b6ff8b6afb6c6bbf7c156380788
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-17 10:18:10 +0800

    Added form actions

--- hrmis/app.php
+++ hrmis/app.php
@@ -23,10 +23,6 @@ if (isset($_POST['UpdatePersonalInformation'])) {
   $employee_id = $_SESSION[alias() . '_current_employee_id'];
   $employee_photo = $_SESSION[alias() . '_current_employee_photo'];
 
-  if (num_rows(employee($employee_id)) === 0) {
-    return;
-  }
-
   if ($_FILES['imageUpload']['size'] > 0 && $_FILES['imageUpload']['error'] == 0) {
     $myfile = $_FILES['imageUpload']['name'];
     $temp = $_FILES['imageUpload']['tmp_name'];
@@ -37,12 +33,51 @@ if (isset($_POST['UpdatePersonalInformation'])) {
     move_uploaded_file($temp, '../' . $employee_photo);
   }
 
-  $dob = strtotime($_POST['dob']);
+  $dob = !empty($_POST['dob']) ? strtotime($_POST['dob']) : strtotime(date('Y-m-d'));
   $byear = date("Y", $dob);
   $bmonth = date("m", $dob);
   $bday = date("d", $dob);
 
-  update_employee(sanitize($_POST['lname']), sanitize($_POST['fname']), sanitize($_POST['mname']), sanitize($_POST['ext']), $bmonth, $bday, $byear, sanitize($_POST['pob']), sanitize($_POST['sex']), sanitize($_POST['civil_status']), sanitize($_POST['civil_status_others']), sanitize($_POST['citizenship']), sanitize($_POST['dual_citizenship']), sanitize($_POST['dual_citizenship_country']), sanitize($_POST['rlot']), sanitize($_POST['rstreet']), sanitize($_POST['rsubdivision']), sanitize($_POST['rbarangay']), sanitize($_POST['rcity']), sanitize($_POST['rprovince']), sanitize($_POST['rzip']), sanitize($_POST['plot']), sanitize($_POST['pstreet']), sanitize($_POST['psubdivision']), sanitize($_POST['pbarangay']), sanitize($_POST['pcity']), sanitize($_POST['pprovince']), sanitize($_POST['pzip']), sanitize($_POST['height']), sanitize($_POST['weight']), sanitize($_POST['blood_type']), sanitize($_POST['gsis']), sanitize($_POST['pagibig']), sanitize($_POST['philhealth']), sanitize($_POST['sss']), sanitize($_POST['telephone']), sanitize($_POST['mobile']), sanitize($_POST['email']), sanitize($_POST['tin']), sanitize($_POST['agency_id']), $employee_photo, $employee_id);
+  update_employee(
+    !empty($_POST['lname']) ? sanitize($_POST['lname']) : '',
+    !empty($_POST['fname']) ? sanitize($_POST['fname']) : '',
+    !empty($_POST['mname']) ? sanitize($_POST['mname']) : '',
+    !empty($_POST['ext']) ? sanitize($_POST['ext']) : '', 
+    $bmonth, $bday, $byear,
+    !empty($_POST['pob']) ? sanitize($_POST['pob']) : '',
+    !empty($_POST['sex']) ? sanitize($_POST['sex']) : '',
+    !empty($_POST['civil_status']) ? sanitize($_POST['civil_status']) : '',
+    !empty($_POST['civil_status_others']) ? sanitize($_POST['civil_status_others']) : '',
+    !empty($_POST['citizenship']) ? sanitize($_POST['citizenship']) : '',
+    !empty($_POST['dual_citizenship']) ? sanitize($_POST['dual_citizenship']) : '',
+    !empty($_POST['citizenship_country']) ? sanitize($_POST['citizenship_country']) : '',
+    !empty($_POST['rlot']) ? sanitize($_POST['rlot']) : '',
+    !empty($_POST['rstreet']) ? sanitize($_POST['rstreet']) : '',
+    !empty($_POST['rsubdivision']) ? sanitize($_POST['rsubdivision']) : '',
+    !empty($_POST['rbarangay']) ? sanitize($_POST['rbarangay']) : '',
+    !empty($_POST['rcity']) ? sanitize($_POST['rcity']) : '',
+    !empty($_POST['rprovince']) ? sanitize($_POST['rprovince']) : '',
+    !empty($_POST['rzip']) ? sanitize($_POST['rzip']) : '',
+    !empty($_POST['plot']) ? sanitize($_POST['plot']) : '',
+    !empty($_POST['pstreet']) ? sanitize($_POST['pstreet']) : '',
+    !empty($_POST['psubdivision']) ? sanitize($_POST['psubdivision']) : '',
+    !empty($_POST['pbarangay']) ? sanitize($_POST['pbarangay']) : '',
+    !empty($_POST['pcity']) ? sanitize($_POST['pcity']) : '',
+    !empty($_POST['pprovince']) ? sanitize($_POST['pprovince']) : '',
+    !empty($_POST['pzip']) ? sanitize($_POST['pzip']) : '',
+    !empty($_POST['height']) ? sanitize($_POST['height']) : '',
+    !empty($_POST['weight']) ? sanitize($_POST['weight']) : '',
+    !empty($_POST['blood_type']) ? sanitize($_POST['blood_type']) : '',
+    !empty($_POST['gsis']) ? sanitize($_POST['gsis']) : '',
+    !empty($_POST['pagibig']) ? sanitize($_POST['pagibig']) : '',
+    !empty($_POST['philhealth']) ? sanitize($_POST['philhealth']) : '',
+    !empty($_POST['sss']) ? sanitize($_POST['sss']) : '',
+    !empty($_POST['telephone']) ? sanitize($_POST['telephone']) : '',
+    !empty($_POST['mobile']) ? sanitize($_POST['mobile']) : '',
+    !empty($_POST['email']) ? sanitize($_POST['email']) : '',
+    !empty($_POST['tin']) ? sanitize($_POST['tin']) : '',
+    !empty($_POST['agency_id']) ? sanitize($_POST['agency_id']) : '', 
+    $employee_photo, $employee_id);
 
   if (affected_rows() === 1) {
     $success = true;
@@ -56,15 +91,26 @@ if (isset($_POST['UpdatePersonalInformation'])) {
 /* FAMILY BACKGROUND */
 if (isset($_POST['UpdateFamilyBackground'])) {
   $employee_id = $_SESSION[alias() . '_current_employee_id'];
-
-  if (num_rows(employee($employee_id)) === 0) {
-    return;
-  }
+  $slast = !empty($_POST['slast']) ? sanitize($_POST['slast']) : '';
+  $sfirst = !empty($_POST['sfirst']) ? sanitize($_POST['sfirst']) : '';
+  $sext = !empty($_POST['sext']) ? sanitize($_POST['sext']) : '';
+  $smiddle = !empty($_POST['smiddle']) ? sanitize($_POST['smiddle']) : '';
+  $swork = !empty($_POST['swork']) ? sanitize($_POST['swork']) : '';
+  $sbusiness = !empty($_POST['sbusiness']) ? sanitize($_POST['sbusiness']) : '';
+  $sbusiness_address = !empty($_POST['sbusiness_address']) ? sanitize($_POST['sbusiness_address']) : '';
+  $stelephone = !empty($_POST['stelephone']) ? sanitize($_POST['stelephone']) : '';
+  $flast = !empty($_POST['flast']) ? sanitize($_POST['flast']) : '';
+  $ffirst = !empty($_POST['ffirst']) ? sanitize($_POST['ffirst']) : '';
+  $fext = !empty($_POST['fext']) ? sanitize($_POST['fext']) : '';
+  $fmiddle = !empty($_POST['fmiddle']) ? sanitize($_POST['fmiddle']) : '';
+  $mlast = !empty($_POST['mlast']) ? sanitize($_POST['mlast']) : '';
+  $mfirst = !empty($_POST['mfirst']) ? sanitize($_POST['mfirst']) : '';
+  $mmiddle = !empty($_POST['mmiddle']) ? sanitize($_POST['mmiddle']) : '';
 
   if (num_rows(family($employee_id)) === 0) {
-    create_family(sanitize($_POST['slast']), sanitize($_POST['sfirst']), sanitize($_POST['sext']), sanitize($_POST['smiddle']), sanitize($_POST['swork']), sanitize($_POST['sbusiness']), sanitize($_POST['sbusiness_address']), sanitize($_POST['stelephone']), sanitize($_POST['flast']), sanitize($_POST['ffirst']), sanitize($_POST['fext']), sanitize($_POST['fmiddle']), sanitize($_POST['mlast']), sanitize($_POST['mfirst']), sanitize($_POST['mmiddle']), $employee_id);
+    create_family($slast, $sfirst, $sext, $smiddle, $swork, $sbusiness, $sbusiness_address, $stelephone, $flast, $ffirst, $fext, $fmiddle, $mlast, $mfirst, $mmiddle, $employee_id);
   } else {
-    update_family(sanitize($_POST['slast']), sanitize($_POST['sfirst']), sanitize($_POST['sext']), sanitize($_POST['smiddle']), sanitize($_POST['swork']), sanitize($_POST['sbusiness']), sanitize($_POST['sbusiness_address']), sanitize($_POST['stelephone']), sanitize($_POST['flast']), sanitize($_POST['ffirst']), sanitize($_POST['fext']), sanitize($_POST['fmiddle']), sanitize($_POST['mlast']), sanitize($_POST['mfirst']), sanitize($_POST['mmiddle']), $employee_id);
+    update_family($slast, $sfirst, $sext, $smiddle, $swork, $sbusiness, $sbusiness_address, $stelephone, $flast, $ffirst, $fext, $fmiddle, $mlast, $mfirst, $mmiddle, $employee_id);
   }
 
   if (affected_rows() === 1) {
@@ -80,13 +126,18 @@ if (isset($_POST['UpdateFamilyBackground'])) {
 if (isset($_POST['SaveChild'])) {
   $employee_id = $_SESSION[alias() . '_current_employee_id'];
   $child_id = $_SESSION[alias() . '_current_child_id'];
+  $clast = !empty($_POST['clast']) ? sanitize($_POST['clast']) : '';
+  $cfirst = !empty($_POST['cfirst']) ? sanitize($_POST['cfirst']) : '';
+  $cext = !empty($_POST['cext']) ? sanitize($_POST['cext']) : '';
+  $cmiddle = !empty($_POST['cmiddle']) ? sanitize($_POST['cmiddle']) : '';
+  $cdob = !empty($_POST['cdob']) ? sanitize($_POST['cdob']) : '';
 
-  if (num_rows(child($child_id)) === 0) {
-    create_child(sanitize($_POST['clast']), sanitize($_POST['cfirst']), sanitize($_POST['cext']), sanitize($_POST['cmiddle']), sanitize($_POST['cdob']), $employee_id);
+  if (num_rows(child($employee_id, $child_id)) === 0) {
+    create_child($clast, $cfirst, $cext, $cmiddle, $cdob, $employee_id);
 
     $message = 'Child has been added successfully!';
   } else {
-    update_child(sanitize($_POST['clast']), sanitize($_POST['cfirst']), sanitize($_POST['cext']), sanitize($_POST['cmiddle']), sanitize($_POST['cdob']), $employee_id, $child_id);
+    update_child($clast, $cfirst, $cext, $cmiddle, $cdob, $employee_id, $child_id);
 
     $message = 'Child has been updated successfully!';
   }
@@ -108,11 +159,59 @@ if (isset($_POST['DeleteChild'])) {
 
   if (affected_rows() === 1) {
     $success = true;
-    $message = 'Child has been removed successfully!';
+    $message = 'Child has been deleted successfully!';
     $show_prompt = true;
   }
 
   $_SESSION[alias() . '_current_child_id'] = '';
   $_SESSION[alias() . '_pds_tab'] = 'children';
+}
+
+/* EDUCATIONAL BACKGROUND */
+if (isset($_POST['SaveEducation'])) {
+  $employee_id = $_SESSION[alias() . '_current_employee_id'];
+  $education_id = $_SESSION[alias() . '_current_education_id'];
+  $level = !empty($_POST['level']) ? sanitize($_POST['level']) : '';
+  $school = !empty($_POST['school']) ? sanitize($_POST['school']) : '';
+  $course = !empty($_POST['course']) ? sanitize($_POST['course']) : '';
+  $from = !empty($_POST['from']) ? sanitize($_POST['from']) : '';
+  $to = !empty($_POST['to']) ? sanitize($_POST['to']) : '';
+  $highest = !empty($_POST['highest']) ? sanitize($_POST['highest']) : '';
+  $year = !empty($_POST['year']) ? sanitize($_POST['year']) : '';
+  $scholarship = !empty($_POST['scholarship']) ? sanitize($_POST['scholarship']) : '';
+
+  if (num_rows(educational_background($employee_id, $education_id)) === 0) {
+    create_education($level, $school, $course, $from, $to, $highest, $year, $scholarship, $employee_id);
+
+    $message = 'Educational Background has been added successfully!';
+  } else {
+    update_education($level, $school, $course, $from, $to, $highest, $year, $scholarship, $employee_id, $education_id);
+
+    $message = 'Educational Background has been updated successfully!';
+  }
+
+  if (mysqli_affected_rows($con) === 1) {
+    $success = true;
+    $show_prompt = true;
+  }
+
+  $_SESSION[alias() . '_current_education_id'] = '';
+  $_SESSION[alias() . '_pds_tab'] = 'educational-background';
+}
+
+if (isset($_POST['DeleteChild'])) {
+  $employee_id = $_SESSION[alias() . '_current_employee_id'];
+  $child_id = $_SESSION[alias() . '_current_child_id'];
+
+  delete_education($employee_id, $education_id);
+
+  if (affected_rows() === 1) {
+    $success = true;
+    $message = 'Educational background has been deleted successfully!';
+    $show_prompt = true;
+  }
+
+  $_SESSION[alias() . '_current_education_id'] = '';
+  $_SESSION[alias() . '_pds_tab'] = 'educational-background';
 }
 ?>
