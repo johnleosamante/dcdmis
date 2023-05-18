8d76a68fd52bd2dfc7a8bce3ac1786c06d57cf02
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-18 13:22:05 +0800

    Fixed function reference

--- hrmis/sidebar-menu.php
+++ hrmis/sidebar-menu.php
@@ -7,11 +7,11 @@
 <div class="sidebar-heading mt-3">Employees</div>
 
 <?php
-sidebar_menu_item(isset($url) && str_contains($url, 'Active'), custom_uri('hrmis', 'Active Employees'), 'Active', 'fa-user-check', number_format(num_rows(active_employees())));
+sidebar_menu_item(custom_uri('hrmis', 'Active Employees'), 'Active', 'fa-user-check', isset($url) && str_contains($url, 'Active'), number_format(num_rows(active_employees())));
 
-sidebar_menu_item(isset($url) && str_contains($url, 'Retirable'), custom_uri('hrmis', 'Retirable Employees'), 'Retirable','fa-user-clock', number_format(num_rows(retirable_employees())));
+sidebar_menu_item(custom_uri('hrmis', 'Retirable Employees'), 'Retirable','fa-user-clock',isset($url) && str_contains($url, 'Retirable'), number_format(num_rows(retirable_employees())));
 
-sidebar_menu_item(isset($url) && str_contains($url, 'Celebrant'), custom_uri('hrmis', 'Celebrant Employees'), 'Celebrant', 'fa-birthday-cake');
+sidebar_menu_item(custom_uri('hrmis', 'Celebrant Employees'), 'Celebrant', 'fa-birthday-cake', isset($url) && str_contains($url, 'Celebrant'));
 
-sidebar_menu_item(isset($url) && str_contains($url, 'Archived'), custom_uri('hrmis', 'Archived Employees'), 'Archived', 'fa-user-lock');
+sidebar_menu_item(custom_uri('hrmis', 'Archived Employees'), 'Archived', 'fa-user-lock', isset($url) && str_contains($url, 'Archived'));
 ?>
