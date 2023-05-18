b48232e7093672f75d930ec9e5cda61a4c005771
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-18 13:52:44 +0800

    Fixed function reference

--- modules/employees/celebrant-employees.php
+++ modules/employees/celebrant-employees.php
@@ -46,7 +46,7 @@ $now = date('Y-m-d');
           <?php $bmonth = date('m', strtotime($datetimeString)); ?>
           <div class="row">
             <div class="col table-responsive">
-              <table class="table table-hover mb-0 text-center" width="100%" cellspacing="0">
+              <table class="table table-hover mb-0 text-center" <?php echo $months === 1 ? 'id="dataTable"' : ''; ?>width="100%" cellspacing="0">
                 <thead>
                   <tr>
                     <th class="align-middle" width="5%">Photo</th>
@@ -61,7 +61,7 @@ $now = date('Y-m-d');
                 </thead>
                 <tbody>
                   <?php
-                  $query = celebrant_employees($bmonth, $_SESSION[alias() . '_station_code']);
+                  $query = celebrant_employees($bmonth);
                   if (num_rows($query) > 0) {
                     while ($row = fetch_array($query)) :
                       $employee_name =  to_name($row['lname'], $row['fname'], $row['mname'], $row['ext']);
@@ -75,7 +75,7 @@ $now = date('Y-m-d');
                         </td>
                         <td class="align-middle text-left"><?php echo $employee_name; ?></td>
                         <td class="align-middle"><?php sex($row['sex']); ?></td>
-                        <td class="align-middle"><?php echo to_date($row['month'] . '/' . $row['day'] . '/' . $row['year'], '', 'F d, Y'); ?></td>
+                        <td class="align-middle"><?php echo to_date($row['month'] . '/' . $row['day'] . '/' . $row['year'], 'F d, Y'); ?></td>
                         <td class="align-middle">
                           <?php echo get_age($row['year'], $row['month'], $row['day']); ?>
                         </td>
@@ -89,7 +89,7 @@ $now = date('Y-m-d');
                               link_dropdown_item(custom_uri('hrmis', 'Employee Information', $row['id']), 'View', 'fa-eye', 'View Employee');
                               link_dropdown_item(custom_uri('hrmis', 'Transfer Employee', $row['id']), 'Transfer', 'fa-share', 'Transfer Employee'); ?>
                               <div class="dropdown-divider"></div>
-                              <?php link_dropdown_item(custom_uri('hrmis', 'Remove Employee', $row['id']), 'Remove', 'fa-times-circle', 'Remove Employee', false, 'text-danger');
+                              <?php link_dropdown_item(custom_uri('hrmis', 'Remove Employee', $row['id']), 'Remove', 'fa-times-circle', 'Remove Employee', 'text-danger', false);
                               ?>
                             </div>
                           </div>
