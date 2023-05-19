2fe86ec512fd48faaa4e9f1a9f1eef74c7e0b109
John Leo Samante <johnleomuitsamante@gmail.com>
2023-05-19 14:36:01 +0800

    Added include file

--- hrmis/index.php
+++ hrmis/index.php
@@ -3,6 +3,7 @@
 include_once('../includes/function.php');
 include_once(root() . '/includes/string.php');
 include_once(root() . '/includes/database/database.php');
+include_once(root() . '/includes/database/user.php');
 include_once(root() . '/includes/database/employee.php');
 include_once(root() . '/includes/database/school.php');
 include_once(root() . '/includes/database/position.php');
@@ -17,7 +18,6 @@ include_once(root() . '/includes/database/special-skill.php');
 include_once(root() . '/includes/database/recognition.php');
 include_once(root() . '/includes/database/membership.php');
 include_once(root() . '/includes/database/other-information.php');
-include_once(root() . '/includes/database/references.php');
 include_once(root() . '/includes/database/references.php');
 include_once(root() . '/includes/layout/theme-dashboard.php');
 ?>
