45d662103a143ba827b6b001788391cc17c47fc6
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-25 14:38:06 +0800

    Added isapplicabledate column

--- includes/database/eligibility.php
+++ includes/database/eligibility.php
@@ -1,7 +1,11 @@
 <?php
 // includes/database/eligibility.php
 
-function eligibility($id) {
-  return query("SELECT `No` AS `no`, Carrer_Service AS eligibility, Rating AS rating, Date_of_Examination AS `date`, Place_of_Examination AS `place`, Number_of_Hour AS `license`, Date_of_Validity AS `validity`, Emp_ID AS `id` FROM civil_service WHERE Emp_ID='{$id}' ORDER BY Date_of_Examination ASC;");
+function eligibilities($id) {
+  return query("SELECT `No` AS `no`, Carrer_Service AS eligibility, Rating AS rating, Date_of_Examination AS `date`, Place_of_Examination AS `place`, Number_of_Hour AS `license`, isapplicabledate AS isapplicable, Date_of_Validity AS `validity`, Emp_ID AS `id` FROM civil_service WHERE Emp_ID='{$id}' ORDER BY Date_of_Examination ASC;");
+}
+
+function eligibility($id, $no) {
+  return query("SELECT `No` AS `no`, Carrer_Service AS eligibility, Rating AS rating, Date_of_Examination AS `date`, Place_of_Examination AS `place`, Number_of_Hour AS `license`, isapplicabledate AS isapplicable, Date_of_Validity AS `validity`, Emp_ID AS `id` FROM civil_service WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 ?>
