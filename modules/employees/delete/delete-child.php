2bdf1a6fb210134b063bf0f2ccd7c5f338b94259
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-25 14:46:08 +0800

    Modified delete modals

--- modules/employees/delete/delete-child.php
+++ modules/employees/delete/delete-child.php
@@ -4,7 +4,7 @@ include_once('../../../includes/function.php');
 include_once(root() . '/includes/layout/components.php');
 
 foreach ($_GET as $key => $data) {
-  $id = $_GET[$key] = $data;
+  $id = $_GET[$key] = decode($data);
 }
 
 $_SESSION[alias() . '_current_child_id'] = $id;
