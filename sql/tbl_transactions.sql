4010ae2ed1dba01870a0b8eda6c58ffd0e8bd9c7
John Leo Samante <johnleomuitsamante@gmail.com>
2023-05-27 21:14:27 +0800

    Added sql scripts

--- sql/tbl_transactions.sql
+++ sql/tbl_transactions.sql
@@ -7,6 +7,9 @@ SELECT * FROM `tbl_transactions` WHERE `Trans_Stats` LIKE '%complete%' OR `Trans
 
 /* CHANGE PHYSICAL TO GSS (GENERAL SERVICES SECTION) */
 SELECT * FROM `tbl_transactions` WHERE TransCode LIKE '%PHYSICAL%';
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'PHYSICAL', 'GSS');
+SELECT * FROM `tbl_transactions` WHERE TransCode LIKE '%GSS%';
+UPDATE tbl_transactions SET Trans_from = 'GSS' WHERE Trans_from = 'PHYSICAL';
 
 /* CHANGE DIVISION SCHOOL ID */
 UPDATE `tbl_transactions` SET `SchoolID`='143' WHERE `SchoolID`='123131';
--- sql/tbl_transactions_log.sql
+++ sql/tbl_transactions_log.sql
@@ -7,5 +7,8 @@ SELECT `Transaction_code` AS `id`, COUNT(*) AS `instance`, `Status` AS `status` 
 
 /* CHANGE PHYSICAL TO GSS (GENERAL SERVICES SECTION) */
 SELECT * FROM `tbl_transactions_log` WHERE Transaction_code LIKE '%PHYSICAL%' OR From_office='PHYSICAL' OR Forwarded_to='PHYSICAL';
-
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'PHYSICAL', 'GSS');
+UPDATE tbl_transactions_log SET From_office='GSS' WHERE From_office='PHYSICAL';
+UPDATE tbl_transactions_log SET Forwarded_to='GSS' WHERE Forwarded_to='PHYSICAL';
+SELECT * FROM `tbl_transactions_log` WHERE Transaction_code LIKE '%GSS%' OR From_office='GSS' OR Forwarded_to='GSS';
 ALTER TABLE `tbl_transactions_log` ADD `details` VARCHAR(500) NOT NULL AFTER `Status`;
