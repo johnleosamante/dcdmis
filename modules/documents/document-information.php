be5d7aa89e333f572252827d76081631ca07e38c
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-30 11:58:09 +0800

    Replaced unneccessary session keys

--- modules/documents/document-information.php
+++ modules/documents/document-information.php
@@ -1,27 +1,28 @@
 <?php
 // modules/documents/document-information.php
-$documents = document_from(sanitize($_GET['id']), $_SESSION[alias() . '_station']);
+$document_id = isset($_GET['id']) ? sanitize($_GET['id']) : null;
+$documents = document_from($document_id, $station);
 
 if (num_rows($documents) > 0) {
   $document = fetch_assoc($documents);
-  $_SESSION[alias() . '_document_id'] = $document['id'];
+  $document_id = $document['id'];
 
-  if (is_incoming_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Incoming Documents';
-  } elseif (is_pending_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Pending Documents';
-  } elseif (is_outgoing_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Outgoing Documents';
-  } elseif (is_ongoing_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Ongoing Documents';
-  } elseif (is_completed_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Completed Documents';
-  } elseif (is_received_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Received Documents';
-  } elseif (is_canceled_document($_SESSION[alias() . '_document_id'], $_SESSION[alias() . '_station'])) {
-    $_SESSION[alias() . '_previous_document'] = $previous_document = 'Canceled Documents';
+  if (is_incoming_document($document_id, $station)) {
+    $document_type = 'Incoming Documents';
+  } elseif (is_pending_document($document_id, $station)) {
+    $document_type = 'Pending Documents';
+  } elseif (is_outgoing_document($document_id, $station)) {
+    $document_type = 'Outgoing Documents';
+  } elseif (is_ongoing_document($document_id, $station)) {
+    $document_type = 'Ongoing Documents';
+  } elseif (is_completed_document($document_id, $station)) {
+    $document_type = 'Completed Documents';
+  } elseif (is_received_document($document_id, $station)) {
+    $document_type = 'Received Documents';
+  } elseif (is_canceled_document($document_id, $station)) {
+    $document_type = 'Canceled Documents';
   } else {
-    $previous_document = null;
+    $document_type = null;
   }
 } else {
   include_once(root() . '/modules/error/no-results-found.php');
@@ -31,7 +32,7 @@ if (num_rows($documents) > 0) {
 
 <div class="card border-left-primary shadow mb-4">
   <div class="card-header py-3">
-    <?php content_title("Document Information : " . strtoupper($document['id'])); ?>
+    <?php content_title("Document Information : " . strtoupper($document_id)); ?>
   </div>
 
   <div class="card-body">
@@ -63,29 +64,34 @@ if (num_rows($documents) > 0) {
     <div class="d-sm-flex align-items-center flex-row-reverse my-2">
       <div class="d-inline-block">
         <?php
-        if (($_SESSION[alias() . '_station']) === $document['from']) {
-          link_button_split(custom_uri('print', 'Document Tracking Slip', $document['id']), 'Print', 'fa-print', 'Print Document Tracking Slip', 'primary', true);
+        if ($station === $document['from']) {
+          link_button_split(custom_uri('print', 'Document Tracking Slip', $document_id), 'Print', 'fa-print', 'Print Document Tracking Slip', 'primary', true);
         }
 
-        switch ($previous_document) {
+        switch ($document_type) {
           case 'Incoming Documents':
-            if (!is_document($document['id'], 'Cancel')) {
-              modal_button_split(uri() . '/modules/documents/receive-document-dialog.php','Receive', 'fa-hand-holding-medical','Receive Document', 'success');
+            if (!is_document($document_id, 'Cancel')) {
+              modal_button_split(uri() . '/modules/documents/receive-document-dialog.php?id=' . encode($document_id),'Receive', 'fa-hand-holding-medical','Receive Document', 'success');
             }
             break;
           case 'Pending Documents':
-            modal_button_split(uri() . '/modules/documents/forward-document-dialog.php', 'Forward', 'fa-share', 'Forward Document', 'info');
-            modal_button_split(uri() . '/modules/documents/complete-document-dialog.php', 'Mark Completed', 'fa-check-circle', 'Mark Complete Document', 'success');
-            if (is_document_from($document['id'], $_SESSION[alias() . '_station']) && $document['from'] === $_SESSION[alias() . '_station'] && !is_document($document['id'], 'Cancel')) {
-              modal_button_split(uri() . '/modules/documents/cancel-document-dialog.php', 'Cancel', 'fa-trash-alt','Cancel Document', 'danger');
-            }
+            modal_button_split(uri() .'/modules/documents/forward-document-dialog.php?id=' . encode($document_id), 'Forward', 'fa-share', 'Forward Document', 'info');
+            modal_button_split(uri() .'/modules/documents/complete-document-dialog.php?id=' . encode($document_id), 'Mark Completed', 'fa-check-circle', 'Mark Complete Document', 'success');
             break;
           case 'Outgoing Documents':
-            if (!is_document($document['id'], 'Complete') && !is_document($document['id'], 'Cancel')) {
-              modal_button_split(uri() . '/modules/documents/save-document-dialog.php', 'Edit', 'fa-edit','Edit Document', 'info');
+            if (!is_document($document_id, 'Complete') && !is_document($document_id, 'Cancel')) {
+              modal_button_split(uri() .'/modules/documents/save-document-dialog.php?id=' . encode($document_id), 'Edit', 'fa-edit','Edit Document', 'info');
             }
-            if (is_document_from($document['id'], $_SESSION[alias() . '_station']) && $document['from'] === $_SESSION[alias() . '_station'] && !is_document($document['id'], 'Cancel')) {
-              modal_button_split(uri() . '/modules/documents/cancel-document-dialog.php', 'Cancel', 'fa-trash-alt', 'Cancel Document', 'danger');
+            break;
+          default:
+            break;
+        }
+
+        switch ($document_type) {
+          case 'Pending Documents':
+          case 'Outgoing Documents':
+            if (is_document_from($document_id, $station) && $document['from'] === $station && !is_document($document_id, 'Cancel')) {
+              modal_button_split(uri() . '/modules/documents/cancel-document-dialog.php?id=' . encode($document_id), 'Cancel', 'fa-trash-alt', 'Cancel Document', 'danger');
             }
             break;
           default:
@@ -109,7 +115,7 @@ if (num_rows($documents) > 0) {
 
         <tbody>
           <?php
-          $logs = document_logs($_GET['id']);
+          $logs = document_logs($document_id);
           while ($log = fetch_array($logs)) {
           ?>
             <tr class="text-uppercase">
