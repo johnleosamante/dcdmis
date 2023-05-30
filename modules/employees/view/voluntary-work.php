ad58b921fb63660eacc5126a8f7f4865874f1806
johnleosamante <johnleomuitsamante@gmail.com>
2023-05-30 16:51:54 +0800

    Added edit mode

--- modules/employees/view/voluntary-work.php
+++ modules/employees/view/voluntary-work.php
@@ -3,15 +3,24 @@
 ?>
 
 <div class="tab-pane fade<?php echo set_active_navigation(isset($_SESSION[alias() . '_pds_tab']) && $_SESSION[alias() . '_pds_tab'] === 'voluntary-work', 'show active'); ?>" id="voluntary-work">
+  <?php if ($editMode) : ?>
+    <div class="d-sm-flex justify-content-end my-3">
+      <?php modal_button_split(uri() . '/modules/employees/update/update-voluntary-work.php', 'Add', 'fa-plus', 'Add Voluntary Work', 'primary'); ?>
+    </div>
+  <?php endif; ?>
+
   <div class="row my-3">
     <div class="col table-responsive">
       <table width="100%" class="table table-striped table-bordered table-hover mb-0 text-center">
         <thead>
           <tr>
             <th class="align-middle" width="45" rowspan="2">Name &amp; Address of Organization</th>
-            <th class="align-middle" width="10%" rowspan="2">Inclusive Dates</th>
-            <th class="align-middle" width="10%" colspan="2">Number of Hours</th>
+            <th class="align-middle" width="10%" colspan="2">Inclusive Dates</th>
+            <th class="align-middle" width="10%" rowspan="2">Number of Hours</th>
             <th class="align-middle" width="30%" rowspan="2">Position / Nature of Work</th>
+            <?php if ($editMode) : ?>
+              <th class="align-middle" width="5%" rowspan="2">Action</th>
+            <?php endif; ?>
           </tr>
           <tr>
             <th class="align-middle" width="5%">From</th>
@@ -27,15 +36,27 @@
               <tr>
                 <td class="align-middle"><?php echo $voluntary['organization']; ?></td>
                 <td class="align-middle"><?php echo to_date($voluntary['from']); ?></td>
-                <td class="align-middle"><?php echo to_date($voluntary['to']); ?></td>
-                <td class="align-middle"><?php echo $voluntary['hours']; ?></td>
+                <td class="align-middle"><?php echo $voluntary['ispresent'] ? 'PRESENT' : to_date($voluntary['to']); ?></td>
+                <td class="align-middle"><?php echo to_handle_null($voluntary['hours'], 'N/A'); ?></td>
                 <td class="align-middle"><?php echo $voluntary['position']; ?></td>
+                <?php if ($editMode) : ?>
+                  <td class="align-middle text-capitalize">
+                    <div class="dropdown no-arrow">
+                      <?php dropdown_ellipsis(); ?>
+                      <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
+                        <?php modal_dropdown_item(uri() . '/modules/employees/update/update-voluntary-work.php?id=' . encode($voluntary['no']), 'Edit', 'fa-edit', 'Edit Work Experience'); ?>
+                        <div class="dropdown-divider"></div>
+                        <?php modal_dropdown_item(uri() . '/modules/employees/delete/delete-voluntary-work.php?id=' . encode($voluntary['no']), 'Delete', 'fa-trash', 'Delete Work Experience', 'text-danger'); ?>
+                      </div>
+                    </div>
+                  </td>
+                <?php endif; ?>
               </tr>
             <?php endwhile;
           } else {
             ?>
             <tr>
-              <td colspan="5" class="align-middle">No data available in table</td>
+              <td colspan="<?php echo $editMode ? '6' : '5'; ?>" class="align-middle">No data available in table</td>
             </tr>
           <?php } ?>
         </tbody>
