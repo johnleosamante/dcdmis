9bf9b28dea91ddc11f2a839b6ca9c0727b15614e
John Leo Samante <johnleomuitsamante@gmail.com>
2023-05-30 20:49:47 +0800

    Removed executed sql scripts

--- sql/tbl_transaction_flow.sql
+++
@@ -1,4 +0,0 @@
-/* CHANGE PHYSICAL TO GSS (GENERAL SERVICES SECTION) */
-SELECT * FROM `tbl_transaction_flow` WHERE TransactionCode LIKE '%PHYSICAL%' OR Destination_section='PHYSICAL';
-
-UPDATE tbl_transaction_flow SET SchoolID='143' WHERE SchoolID='123131';
