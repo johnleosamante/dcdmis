2e8afc20d57fe14aa2cef7857b02239ceac40750
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-01 15:56:27 +0800

    Removed session key

--- dts/dashboard.php
+++ dts/dashboard.php
@@ -1,25 +1,23 @@
 <?php
 // dts/dashboard.php
 content_title_with_modal('Dashboard', uri() . '/modules/documents/save-document-dialog.php', 'New Document', 'fa-plus');
-$portal = $_SESSION[alias() . '_portal'];
-$station = $_SESSION[alias() . '_station'];
 ?>
 
 <div class="row mt-4">
   <?php
   $is_school_portal = $portal === 'school_portal';
 
-  card('Incoming Documents', custom_uri('dts', 'Incoming Documents'), 'fa-file-download', 'primary', num_rows(incoming_documents($station)));
-
   if (!$is_school_portal) {
+    card('Incoming Documents', custom_uri('dts', 'Incoming Documents'), 'fa-file-download', 'primary', num_rows(incoming_documents($station)));
+
     card('Pending Documents', custom_uri('dts', 'Pending Documents'), 'fa-history', 'success', num_rows(pending_documents($station)));
   }
 
-  card('Outgoing Documents', custom_uri('dts', 'Outgoing Documents'), 'fa-file-upload', 'info', num_rows(outgoing_documents($station)));
+  card('Outgoing Documents', custom_uri('dts', 'Outgoing Documents'), 'fa-file-upload', $is_school_portal ? 'primary' : 'info', num_rows(outgoing_documents($station)));
 
-  card('Ongoing Documents', custom_uri('dts', 'Ongoing Documents'), 'fa-tasks', 'warning', num_rows(ongoing_documents($station)));
+  card('Ongoing Documents', custom_uri('dts', 'Ongoing Documents'), 'fa-tasks', $is_school_portal ? 'success' : 'warning', num_rows(ongoing_documents($station)));
 
-  card('Completed Documents', custom_uri('dts', 'Completed Documents'), 'fa-check-circle', 'secondary');
+  card('Completed Documents', custom_uri('dts', 'Completed Documents'), 'fa-check-circle', $is_school_portal ? 'info' :'secondary');
 
   if (!$is_school_portal) {
     card('Received Documents', custom_uri('dts', 'Received Documents'), 'fa-hand-holding-medical', 'dark');
--- dts/sidebar-menu.php
+++ dts/sidebar-menu.php
@@ -2,19 +2,21 @@
 // dts/sidebar-menu.php
 sidebar_divider();
 
-$is_school_portal = $_SESSION[alias() . '_portal'] === 'school_portal';
-
-sidebar_menu_item(custom_uri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', isset($url) && str_contains($url, 'Incoming'), number_format(num_rows(incoming_documents($_SESSION[alias() . '_station']))));
+$is_school_portal = $portal === 'school_portal';
 
 if (!$is_school_portal) {
-  sidebar_menu_item(custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', isset($url) && str_contains($url, 'Pending'), number_format(num_rows(pending_documents($_SESSION[alias() . '_station']))));
+  sidebar_menu_item(custom_uri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', isset($url) && str_contains($url, 'Incoming'), number_format(num_rows(incoming_documents($station))));
+
+  sidebar_menu_item(custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', isset($url) && str_contains($url, 'Pending'), number_format(num_rows(pending_documents($station))));
 }
 
-sidebar_menu_item(custom_uri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', isset($url) && str_contains($url, 'Outgoing'), number_format(num_rows(outgoing_documents($_SESSION[alias() . '_station']))));
+sidebar_menu_item(custom_uri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', isset($url) && str_contains($url, 'Outgoing'), number_format(num_rows(outgoing_documents($station))));
 
-sidebar_divider();
+if (!$is_school_portal) {
+  sidebar_divider();
+}
 
-sidebar_menu_item(custom_uri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', isset($url) && str_contains($url, 'Ongoing'), number_format(num_rows(ongoing_documents($_SESSION[alias() . '_station']))));
+sidebar_menu_item(custom_uri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', isset($url) && str_contains($url, 'Ongoing'), number_format(num_rows(ongoing_documents($station))));
 
 sidebar_divider();
 
