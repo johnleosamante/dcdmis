34a1c4ed67bbe6fd700d3789637a1fdd1850e629
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-02 16:41:53 +0800

    Fixed function reference

--- dts/sidebar-menu.php
+++ dts/sidebar-menu.php
@@ -1,28 +1,24 @@
 <?php
 // dts/sidebar-menu.php
-sidebar_divider();
+sidebarDivider();
 
-$is_school_portal = $portal === 'school_portal';
+sidebarMenuItem(customUri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', isset($url) && str_contains($url, 'Incoming'), number_format(numRows(incomingDocuments($station))));
 
-sidebar_menu_item(custom_uri('dts', 'Incoming Documents'), 'Incoming', 'fa-file-download', isset($url) && str_contains($url, 'Incoming'), number_format(num_rows(incoming_documents($station))));
+sidebarMenuItem(customUri('dts', 'Pending Documents'), 'Pending', 'fa-history', isset($url) && str_contains($url, 'Pending'), number_format(numRows(pendingDocuments($station))));
 
+sidebarMenuItem(customUri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', isset($url) && str_contains($url, 'Outgoing'), number_format(numRows(outgoingDocuments($station))));
 
-sidebar_menu_item(custom_uri('dts', 'Pending Documents'), 'Pending', 'fa-history', isset($url) && str_contains($url, 'Pending'), number_format(num_rows(pending_documents($station))));
+sidebarDivider();
 
-sidebar_menu_item(custom_uri('dts', 'Outgoing Documents'), 'Outgoing', 'fa-file-upload', isset($url) && str_contains($url, 'Outgoing'), number_format(num_rows(outgoing_documents($station))));
+sidebarMenuItem(customUri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', isset($url) && str_contains($url, 'Ongoing'), number_format(numRows(ongoingDocuments($station))));
 
+sidebarDivider();
 
-sidebar_divider();
-
-sidebar_menu_item(custom_uri('dts', 'Ongoing Documents'), 'Ongoing', 'fa-tasks', isset($url) && str_contains($url, 'Ongoing'), number_format(num_rows(ongoing_documents($station))));
-
-sidebar_divider();
-
-sidebar_menu_item(custom_uri('dts', 'Completed Documents'), 'Completed', 'fa-check-circle', isset($url) && str_contains($url, 'Completed'));
+sidebarMenuItem(customUri('dts', 'Completed Documents'), 'Completed', 'fa-check-circle', isset($url) && str_contains($url, 'Completed'));
 
 if (!$is_school_portal) {
-  sidebar_menu_item(custom_uri('dts', 'Received Documents'), 'Received', 'fa-hand-holding-medical', isset($url) && str_contains($url, 'Received'));
+  sidebarMenuItem(customUri('dts', 'Received Documents'), 'Received', 'fa-hand-holding-medical', isset($url) && str_contains($url, 'Received'));
 }
 
-sidebar_menu_item(custom_uri('dts', 'Canceled Documents'), 'Canceled', 'fa-trash-alt', isset($url) && str_contains($url, 'Canceled'));
+sidebarMenuItem(customUri('dts', 'Canceled Documents'), 'Canceled', 'fa-trash-alt', isset($url) && str_contains($url, 'Canceled'));
 ?>
