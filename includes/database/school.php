c1342e69f18560df412ef6dd058f0d40413f9adf
John Leo Samante <johnleomuitsamante@gmail.com>
2023-06-02 20:05:37 +0800

    Changed function name

--- includes/database/school.php
+++ includes/database/school.php
@@ -5,15 +5,15 @@ function schools() {
   return query("SELECT SchoolID AS id, SchoolName AS `name`, Abraviate AS alias FROM tbl_school;");
 }
 
-function school_by_alias($alias) {
+function schoolByAlias($alias) {
   return query("SELECT SchoolID AS id, SchoolName AS `name` FROM tbl_school WHERE Abraviate='{$alias}' LIMIT 1;");
 }
 
-function school_by_id($id) {
+function schoolById($id) {
   return query("SELECT Abraviate AS alias, SchoolName AS `name` FROM tbl_school WHERE SchoolID='{$id}' LIMIT 1;");
 }
 
-function school_details_by_id($id) {
+function schoolDetailsById($id) {
   return query("SELECT Abraviate AS alias, SchoolName AS `name`, `Address` AS `address`, Incharg_ID AS `head`, District_code AS `district`, SchoolLogo AS `logo`, telephone, email, website, fb_page FROM tbl_school WHERE SchoolID='{$id}' LIMIT 1;");
 }
 ?>
