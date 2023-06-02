888fdb7de76eb74f2577a1aa96487c319b4eef94
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-02 16:43:06 +0800

    Fixed function reference

--- error/page.php
+++ error/page.php
@@ -4,7 +4,7 @@
 <div class="col-xl-5 col-lg-5 col-md-8 col-sm-12">
   <div class="card o-hidden border-0 shadow-lg my-5">
     <div class="card-header text-center">
-      <?php display_logo(120, 120, '0', uri(), title()); ?>
+      <?php displayLogo(120, 120, '0', uri(), title()); ?>
     </div><!-- card-header -->
 
     <div class="card-body text-center">
