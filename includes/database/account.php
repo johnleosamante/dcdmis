dcca3eafc38c73857ed80d045e6b5d48bd9a6dca
John Leo Samante <johnleomuitsamante@gmail.com>
2023-06-04 08:33:32 +0800

    Modified account comment

--- includes/database/account.php
+++ includes/database/account.php
@@ -2,7 +2,6 @@
 // includes/database/account.php
 // tbl_employee
 // tbl_teacher_account
-
 function account($email, $password=null) {
   $sql = "SELECT `tbl_employee`.`Emp_ID` AS `id`, `tbl_teacher_account`.`Teacher_TIN` AS `email`, `tbl_employee`.`Emp_Status` AS `status` FROM `tbl_teacher_account` JOIN `tbl_employee` ON `tbl_teacher_account`.`Teacher_TIN` = `tbl_employee`.`Emp_Email` WHERE `tbl_teacher_account`.`Teacher_TIN`='{$email}'";
 
