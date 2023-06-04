e9a13843835cc42c98a78321c05ee246e6d6eaaa
John Leo Samante <johnleomuitsamante@gmail.com>
2023-06-04 08:34:45 +0800

    Changed naming convention

--- includes/database/children.php
+++ includes/database/children.php
@@ -1,6 +1,6 @@
 <?php
 // includes/database/children.php
-
+// family_background
 function children($id) {
   return query("SELECT `No` AS `no`, Family_Name AS `last`, First_Name AS `first`, Name_Extension AS ext, Middle_Name AS middle, Birthdate AS dob, Emp_ID AS id FROM family_background WHERE Emp_ID='{$id}' ORDER BY Birthdate ASC;");
 }
@@ -9,15 +9,15 @@ function child($id, $no) {
   return query("SELECT `No` AS `no`, Family_Name AS `last`, First_Name AS `first`, Name_Extension AS ext, Middle_Name AS middle, Birthdate AS dob, Emp_ID AS id FROM family_background WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 
-function create_child($lname, $fname, $ext, $mname, $dob, $id) {
-  non_query("INSERT INTO family_background (Family_Name, First_Name, Name_Extension, Middle_Name, Birthdate, Emp_ID) VALUES ('{$lname}', '{$fname}', '{$ext}', '{$mname}', '{$dob}', '{$id}');");
+function createChild($lname, $fname, $ext, $mname, $dob, $id) {
+  nonQuery("INSERT INTO family_background (Family_Name, First_Name, Name_Extension, Middle_Name, Birthdate, Emp_ID) VALUES ('{$lname}', '{$fname}', '{$ext}', '{$mname}', '{$dob}', '{$id}');");
 }
 
-function update_child($lname, $fname, $ext, $mname, $dob, $id, $no) {
-  non_query("UPDATE family_background SET Family_Name='{$lname}', First_Name='{$fname}', Name_Extension='{$ext}', Middle_Name='{$mname}', Birthdate='{$dob}' WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
+function updateChild($lname, $fname, $ext, $mname, $dob, $id, $no) {
+  nonQuery("UPDATE family_background SET Family_Name='{$lname}', First_Name='{$fname}', Name_Extension='{$ext}', Middle_Name='{$mname}', Birthdate='{$dob}' WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 
-function delete_child($id, $no) {
-  non_query("DELETE FROM family_background WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
+function deleteChild($id, $no) {
+  nonQuery("DELETE FROM family_background WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 ?>
