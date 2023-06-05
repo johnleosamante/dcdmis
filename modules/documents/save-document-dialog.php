a2810170aa0c376b544ebe210616a73364a385af
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-05 11:03:35 +0800

    Fixed hasDocument condition

--- modules/documents/save-document-dialog.php
+++ modules/documents/save-document-dialog.php
@@ -13,8 +13,9 @@ $documentId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
 $documents = documentLog($documentId);
 $description = $destination = $purpose = $details = $attribute = '';
 $modalTitle = 'New Document';
-$hasDocument = false;
+$hasDocument = true;
 $forRelease = false;
+$notFound = true;
 
 if (numRows($documents) > 0) {
   $document = fetchAssoc($documents);
@@ -40,8 +41,7 @@ if (numRows($documents) > 0) {
     }
   }
 
-  $forRelease = str_contains(strtolower($document['purpose']), 'release') && $destination === 'RECORD' && $isRecordsPortal;
-  $modalTitle = 'Edit Document';
+  $forRelease = str_contains(strtolower($document['purpose']), 'release') && $documentLogs['from'] === 'RECORD' && $isRecordsPortal;
   $notFound = false;
 }
 ?>
@@ -52,70 +52,74 @@ if (numRows($documents) > 0) {
 
     <form action="" method="POST">
       <div class="modal-body">
-        <?php if ($hasDocument) : ?>
+        <?php if ($hasDocument) { ?>
+          <?php if (!$notFound) : ?>
+            <div class="form-group">
+              <label for="code" class="mb-0">Code</label>
+              <input id="code" type="text" value="<?php echo $documentId; ?>" class="form-control text-uppercase" disabled>
+            </div>
+          <?php endif; ?>
+
           <div class="form-group">
-            <label for="code" class="mb-0">Code</label>
-            <input id="code" type="text" value="<?php echo $documentId; ?>" class="form-control text-uppercase" disabled>
+            <label for="description" class="mb-0">Description</label>
+            <textarea id="description" name="description" class="form-control" rows="3" placeholder="Type description..." <?php echo $attribute; ?> required><?php echo $description; ?></textarea>
           </div>
-        <?php endif; ?>
 
-        <div class="form-group">
-          <label for="description" class="mb-0">Description</label>
-          <textarea id="description" name="description" class="form-control" rows="3" placeholder="Type description..." <?php echo $attribute; ?> required><?php echo $description; ?></textarea>
-        </div>
+          <?php if (!$isSchoolPortal) : ?>
+            <div class="form-group">
+              <label class="mb-0" for="destination">Destination</label>
+              <?php if (!$forRelease) { ?>
+                <select name="destination" id="destination" class="form-control" required>
+                  <option value="">Select destination...</option>
+                  <?php $sections = sectionsExcept($station);
+                  while ($section = fetchArray($sections)) : ?>
+                    <option value="<?php echo $section['id']; ?>" <?php echo setOptionSelected($section['id'], $destination); ?>><?php echo $section['name']; ?></option>
+                  <?php endwhile; ?>
+                </select>
+              <?php } else {
+                $schools = schoolByAlias($destination);
+                $school = '';
 
-        <?php if (!$isSchoolPortal) : ?>
+                if (numRows($schools) > 0) {
+                  $school = fetchAssoc($schools)['name'];
+                }
+              ?>
+                <input id="destination" class="form-control" type="text" value="<?php echo $school; ?>" disabled>
+                <input name="destination" class="form-control" type="hidden" value="<?php echo $destination; ?>" required>
+              <?php } ?>
+            </div>
+          <?php endif; ?>
+
           <div class="form-group">
-            <label class="mb-0" for="destination">Destination</label>
-            <?php if (!$forRelease) { ?>
-              <select name="destination" id="destination" class="form-control" required>
-                <option value="">Select destination...</option>
-                <?php $sections = sectionsExcept($station);
-                while ($section = fetchArray($sections)) : ?>
-                  <option value="<?php echo $section['id']; ?>" <?php echo setOptionSelected($section['id'], $destination); ?>><?php echo $section['name']; ?></option>
+            <label class="mb-0" for="purpose">Purpose</label>
+            <?php if (!$forRelease) : ?>
+              <select name="purpose" id="purpose" class="form-control" required>
+                <option value="">Select purpose...</option>
+                <?php
+                $documentPurposes = documentPurpose();
+                while ($documentPurpose = fetchArray($documentPurposes)) : ?>
+                  <option value="<?php echo $documentPurpose['purpose']; ?>" <?php echo setOptionSelected($documentPurpose['purpose'], $purpose); ?>><?php echo $documentPurpose['purpose']; ?></option>
                 <?php endwhile; ?>
               </select>
-            <?php } else {
-              $schools = schoolByAlias($destination);
-              $school = '';
-
-              if (numRows($schools) > 0) {
-                $school = fetchAssoc($schools)['name'];
-              }
-            ?>
-              <input id="destination" class="form-control" type="text" value="<?php echo $school; ?>" disabled>
-              <input name="destination" class="form-control" type="hidden" value="<?php echo $destination; ?>" required>
-            <?php } ?>
+            <?php else : ?>
+              <input id="purpose" name="purpose" class="form-control" type="text" value="<?php echo $purpose; ?>" required readonly>
+            <?php endif; ?>
           </div>
-        <?php endif; ?>
 
-        <div class="form-group">
-          <label class="mb-0" for="purpose">Purpose</label>
-          <?php if (!$forRelease) : ?>
-            <select name="purpose" id="purpose" class="form-control" required>
-              <option value="">Select purpose...</option>
-              <?php
-              $documentPurposes = documentPurpose();
-              while ($documentPurpose = fetchArray($documentPurposes)) : ?>
-                <option value="<?php echo $documentPurpose['purpose']; ?>" <?php echo setOptionSelected($documentPurpose['purpose'], $purpose); ?>><?php echo $documentPurpose['purpose']; ?></option>
-              <?php endwhile; ?>
-            </select>
-          <?php else : ?>
-            <input id="purpose" name="purpose" class="form-control" type="text" value="<?php echo $purpose; ?>" required readonly>
-          <?php endif; ?>
-        </div>
-
-        <div class="form-group mb-0">
-          <label class="mb-0" for="details">Additional details (optional)</label>
-          <textarea id="details" name="details" class="form-control" rows="2" placeholder="Type additional details..."><?php echo $details; ?></textarea>
-        </div>
+          <div class="form-group mb-0">
+            <label class="mb-0" for="details">Additional details (optional)</label>
+            <textarea id="details" name="details" class="form-control" rows="2" placeholder="Type additional details..."><?php echo $details; ?></textarea>
+          </div>
+        <?php } else {
+          missingAlert($modalTitle, 'fa-times-circle');
+        } ?>
       </div>
 
       <div class="modal-footer">
         <?php if ($hasDocument) : ?>
-          <input type="hidden" name="verifier" value="<?php echo $_GET['id']; ?>">
+          <input type="hidden" name="verifier" value="<?php echo isset($_GET['id']) ? $_GET['id'] : null; ?>">
+          <button class="btn btn-primary" name="save-document" type="submit">Continue</button>
         <?php endif; ?>
-        <button class="btn btn-primary" name="save-document" type="submit">Continue</button>
         <?php cancelModalButton(); ?>
       </div>
     </form>
