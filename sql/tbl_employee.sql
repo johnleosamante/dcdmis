57644a79f873e12de30f5ec35836e35620a4e870
John Leo Samante <johnleomuitsamante@gmail.com>
2023-06-16 21:42:19 +0800

    Added sql script

--- sql/tbl_employee.sql
+++ sql/tbl_employee.sql
@@ -3,3 +3,5 @@ ALTER TABLE `tbl_employee` ADD `Emp_GSIS_BP` VARCHAR(20) NOT NULL AFTER `Emp_GSI
 ALTER TABLE `tbl_employee` ADD `Emp_Alternate_Email` VARCHAR(100) NOT NULL AFTER `Emp_Email`;
 
 ALTER TABLE `tbl_employee` ADD `Emp_Alternate_Cell_No` VARCHAR(20) NOT NULL AFTER `Emp_Cell_No`;
+
+UPDATE `tbl_employee` SET `Emp_Status`='Duplicate' WHERE `Emp_Status`='Removed';
