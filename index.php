07fc11f0e4c6ebc4742513c87aff0c16ace14dbb
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-22 10:30:57 +0800

    Renamed application

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('./includes/function.php');
 DepEd Dipolog City Schools Division
 
 <ul>
-  <li><a href="/dmis">Data Management Information System</a></li>
+  <li><a href="/dmis">Division Management Information System</a></li>
   <li><a href="/dts">Document Tracking System</a></li>
   <li><a href="/hrmis">Human Resource Management Information System</a></li>
   <li><a href="/pis">Personnel Information System</a></li>
