efe8cd6d4dcc895712ed51016b4166a6f7b07b6a
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-22 11:18:33 +0800

    Added users function

--- includes/database/user.php
+++ includes/database/user.php
@@ -8,4 +8,8 @@ function user($id) {
 function userRole($id, $station) {
   return query("SELECT usercode AS id FROM tbl_user WHERE usercode='{$id}' AND Station='{$station}' LIMIT 1;");
 }
+
+function users() {
+  return query("SELECT tbl_user.usercode AS id, tbl_user.username AS email, tbl_user.Station AS code, tbl_user.Link AS portal FROM tbl_user INNER JOIN tbl_station ON tbl_user.usercode=tbl_station.Emp_ID GROUP BY tbl_user.username ORDER BY tbl_user.username ASC;");
+}
 ?>
