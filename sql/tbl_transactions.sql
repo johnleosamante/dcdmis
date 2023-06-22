ddc6a4432a62c51e924e4d149b049739d5c565fc
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-22 16:03:08 +0800

    Added sql script

--- sql/tbl_transactions.sql
+++ sql/tbl_transactions.sql
@@ -3,15 +3,57 @@ UPDATE `tbl_transactions` SET `Status`='Read' WHERE `Trans_Stats` LIKE '%complet
 UPDATE `tbl_transactions` SET `Status`='Read' WHERE `Trans_Stats` LIKE '%cancel%';
 UPDATE `tbl_transactions` SET `Trans_Stats`='Received' WHERE `Trans_stats` LIKE '%on process%';
 
-SELECT * FROM `tbl_transactions` WHERE `Trans_Stats` LIKE '%complete%' OR `Trans_Stats` LIKE '%cancel%';
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'ACCOUNTING', 'ACC');
+UPDATE tbl_transactions SET Trans_from = 'ACC' WHERE Trans_from = 'ACCOUNTING';
 
-/* CHANGE PHYSICAL TO GSS (GENERAL SERVICES SECTION) */
-SELECT * FROM `tbl_transactions` WHERE TransCode LIKE '%PHYSICAL%';
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'ADMIN', 'ADM');
+UPDATE tbl_transactions SET Trans_from = 'ADM' WHERE Trans_from = 'ADMIN';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'ASDS', 'ASD');
+UPDATE tbl_transactions SET Trans_from = 'ASD' WHERE Trans_from = 'ASDS';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'BUDGET', 'BUD');
+UPDATE tbl_transactions SET Trans_from = 'BUD' WHERE Trans_from = 'BUDGET';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'CASHIER', 'CSH');
+UPDATE tbl_transactions SET Trans_from = 'CSH' WHERE Trans_from = 'CASHIER';
+
 UPDATE tbl_transactions SET TransCode = replace(TransCode, 'PHYSICAL', 'GSS');
-SELECT * FROM `tbl_transactions` WHERE TransCode LIKE '%GSS%';
 UPDATE tbl_transactions SET Trans_from = 'GSS' WHERE Trans_from = 'PHYSICAL';
 
-/* CHANGE DIVISION SCHOOL ID */
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'HEALTH', 'HNS');
+UPDATE tbl_transactions SET Trans_from = 'HNS' WHERE Trans_from = 'HEALTH';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'HRMO', 'PER');
+UPDATE tbl_transactions SET Trans_from = 'PER' WHERE Trans_from = 'HRMO';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'ITO', 'ICT');
+UPDATE tbl_transactions SET Trans_from = 'ICT' WHERE Trans_from = 'ITO';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'LEGAL', 'LGL');
+UPDATE tbl_transactions SET Trans_from = 'LGL' WHERE Trans_from = 'LEGAL';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'LRMDS', 'LRM');
+UPDATE tbl_transactions SET Trans_from = 'LRM' WHERE Trans_from = 'LRMDS';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'LRMS', 'LRM');
+UPDATE tbl_transactions SET Trans_from = 'LRM' WHERE Trans_from = 'LRMS';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'OSDS', 'SDS');
+UPDATE tbl_transactions SET Trans_from = 'SDS' WHERE Trans_from = 'OSDS';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'PSDS', 'PSD');
+UPDATE tbl_transactions SET Trans_from = 'PSD' WHERE Trans_from = 'PSDS';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'RECORD', 'REC');
+UPDATE tbl_transactions SET Trans_from = 'REC' WHERE Trans_from = 'RECORD';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'SGOD', 'SGO');
+UPDATE tbl_transactions SET Trans_from = 'SGO' WHERE Trans_from = 'SGOD';
+
+UPDATE tbl_transactions SET TransCode = replace(TransCode, 'SUPPLY', 'PSS');
+UPDATE tbl_transactions SET Trans_from = 'PSS' WHERE Trans_from = 'SUPPLY';
+
 UPDATE `tbl_transactions` SET `SchoolID`='143' WHERE `SchoolID`='123131';
 
 ALTER TABLE `tbl_transactions` ADD `details` VARCHAR(500) NOT NULL AFTER `Attachfile`;
--- sql/tbl_transactions_log.sql
+++ sql/tbl_transactions_log.sql
@@ -1,14 +1,72 @@
 /* TO BE EXECUTED BEFORE LAUNCH */
+ALTER TABLE `tbl_transactions_log` ADD `details` VARCHAR(500) NOT NULL AFTER `Status`;
+
 UPDATE `tbl_transactions_log` SET `Trans_status`='Received' WHERE `Trans_status` LIKE '%on process%';
 
-SELECT * FROM `tbl_transactions_log` WHERE `Trans_status` LIKE '%on process%';
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'ACCOUNTING', 'ACC');
+UPDATE tbl_transactions_log SET From_office='ACC' WHERE From_office='ACCOUNTING';
+UPDATE tbl_transactions_log SET Forwarded_to='ACC' WHERE Forwarded_to='ACCOUNTING';
 
-SELECT `Transaction_code` AS `id`, COUNT(*) AS `instance`, `Status` AS `status` FROM `tbl_transactions_log` GROUP BY `Transaction_code`, `Status` ORDER BY `status` DESC, COUNT(*) DESC;
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'ADMIN', 'ADM');
+UPDATE tbl_transactions_log SET From_office='ADM' WHERE From_office='ADMIN';
+UPDATE tbl_transactions_log SET Forwarded_to='ADM' WHERE Forwarded_to='ADMIN';
 
-/* CHANGE PHYSICAL TO GSS (GENERAL SERVICES SECTION) */
-SELECT * FROM `tbl_transactions_log` WHERE Transaction_code LIKE '%PHYSICAL%' OR From_office='PHYSICAL' OR Forwarded_to='PHYSICAL';
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'ASDS', 'ASD');
+UPDATE tbl_transactions_log SET From_office='ASD' WHERE From_office='ASDS';
+UPDATE tbl_transactions_log SET Forwarded_to='ASD' WHERE Forwarded_to='ASDS';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'BUDGET', 'BUD');
+UPDATE tbl_transactions_log SET From_office='BUD' WHERE From_office='BUDGET';
+UPDATE tbl_transactions_log SET Forwarded_to='BUD' WHERE Forwarded_to='BUDGET';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'CASHIER', 'CSH');
+UPDATE tbl_transactions_log SET From_office='CSH' WHERE From_office='CASHIER';
+UPDATE tbl_transactions_log SET Forwarded_to='CSH' WHERE Forwarded_to='CASHIER';
+
 UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'PHYSICAL', 'GSS');
 UPDATE tbl_transactions_log SET From_office='GSS' WHERE From_office='PHYSICAL';
 UPDATE tbl_transactions_log SET Forwarded_to='GSS' WHERE Forwarded_to='PHYSICAL';
-SELECT * FROM `tbl_transactions_log` WHERE Transaction_code LIKE '%GSS%' OR From_office='GSS' OR Forwarded_to='GSS';
-ALTER TABLE `tbl_transactions_log` ADD `details` VARCHAR(500) NOT NULL AFTER `Status`;
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'HEALTH', 'HNS');
+UPDATE tbl_transactions_log SET From_office='HNS' WHERE From_office='HEALTH';
+UPDATE tbl_transactions_log SET Forwarded_to='HNS' WHERE Forwarded_to='HEALTH';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'HRMO', 'PER');
+UPDATE tbl_transactions_log SET From_office='PER' WHERE From_office='HRMO';
+UPDATE tbl_transactions_log SET Forwarded_to='PER' WHERE Forwarded_to='HRMO';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'ITO', 'ICT');
+UPDATE tbl_transactions_log SET From_office='ICT' WHERE From_office='ITO';
+UPDATE tbl_transactions_log SET Forwarded_to='ICT' WHERE Forwarded_to='ITO';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'LEGAL', 'LGL');
+UPDATE tbl_transactions_log SET From_office='LGL' WHERE From_office='LEGAL';
+UPDATE tbl_transactions_log SET Forwarded_to='LGL' WHERE Forwarded_to='LEGAL';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'LRMDS', 'LRM');
+UPDATE tbl_transactions_log SET From_office='LRM' WHERE From_office='LRMDS';
+UPDATE tbl_transactions_log SET Forwarded_to='LRM' WHERE Forwarded_to='LRMDS';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'LRMS', 'LRM');
+UPDATE tbl_transactions_log SET From_office='LRM' WHERE From_office='LRMS';
+UPDATE tbl_transactions_log SET Forwarded_to='LRM' WHERE Forwarded_to='LRMS';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'OSDS', 'SDS');
+UPDATE tbl_transactions_log SET From_office='SDS' WHERE From_office='OSDS';
+UPDATE tbl_transactions_log SET Forwarded_to='SDS' WHERE Forwarded_to='OSDS';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'PSDS', 'PSD');
+UPDATE tbl_transactions_log SET From_office='PSD' WHERE From_office='PSDS';
+UPDATE tbl_transactions_log SET Forwarded_to='PSD' WHERE Forwarded_to='PSDS';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'RECORD', 'REC');
+UPDATE tbl_transactions_log SET From_office='REC' WHERE From_office='RECORD';
+UPDATE tbl_transactions_log SET Forwarded_to='REC' WHERE Forwarded_to='RECORD';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'SGOD', 'SGO');
+UPDATE tbl_transactions_log SET From_office='SGO' WHERE From_office='SGOD';
+UPDATE tbl_transactions_log SET Forwarded_to='SGO' WHERE Forwarded_to='SGOD';
+
+UPDATE tbl_transactions_log SET Transaction_code = replace(Transaction_code, 'SUPPLY', 'PSS');
+UPDATE tbl_transactions_log SET From_office='PSS' WHERE From_office='SUPPLY';
+UPDATE tbl_transactions_log SET Forwarded_to='PSS' WHERE Forwarded_to='SUPPLY';
