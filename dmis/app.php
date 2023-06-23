65af4a74fabd7473ae6d8aa1fca0933bd58a221d
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-23 16:08:57 +0800

    Added actions

--- dmis/app.php
+++ dmis/app.php
@@ -17,5 +17,18 @@ if (isset($_POST['primary-search-button'])) {
   redirect(customUri('dmis', 'Search', sanitize($_POST['primary-search-text'])));
 }
 
+if (isset($_POST['reset-user'])) {
+  $employeeId = sanitize(decipher($_POST['verifier']));
+  $temporaryPassword = sanitize(decipher($_POST['data-verifier']));
 
+  $showAlert = true;
+  $message = 'User password has been reset successfully. ' . $temporaryPassword;
+  $success = true;
+}
+
+if (isset($_POST['remove-user'])) {
+  $showAlert = true;
+  $message = 'User has been removed successfully.';
+  $success = true;
+}
 ?>
