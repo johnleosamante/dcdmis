36d0cb08c410f95f4a1b1e4ee8abf54ed608cfd4
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-27 13:15:10 +0800

    Added edit user dialog

---
+++ modules/users/edit-user-dialog.php
@@ -0,0 +1,125 @@
+<?php
+// modules/users/edit-user-dialog.php
+require_once('../../includes/function.php');
+require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/employee.php');
+require_once(root() . '/includes/database/position.php');
+require_once(root() . '/includes/database/section.php');
+require_once(root() . '/includes/database/user.php');
+require_once(root() . '/includes/database/school.php');
+require_once(root() . '/includes/layout/components.php');
+require_once(root() . '/includes/string.php');
+
+$employeeId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
+$employees = employee($employeeId);
+$dtsUser = $hrmisUser = $dmisUser = $hrtdmsUser = $dtsDivisionUser = false;
+$stationId = $depedEmail = $dtsUserStation = '';
+$modalTitle = 'User not found';
+$hasUser = false;
+$notFound = true;
+
+if (numRows($employees) > 0) {
+  $employee = fetchAssoc($employees);
+  $employeeId = $employee['id'];
+  $employeeName = toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true);
+  $positions = fetchAssoc(position($employeeId));
+  $stationId = $positions['station_id'];
+  $station = $positions['station'];
+  $positionId = $positions['position_id'];
+  $position = $positions['position'];
+  $depedEmail = $employee['email'];
+  $picture = uri() . '/' . $employee['picture'];
+  $modalTitle = 'Edit User';
+  $hasUser = true;
+  $dts = dtsUser($employeeId);
+
+  if (numRows($dts) > 0) {
+    $dtsUser = true;
+    $userData = fetchArray($dts);
+    $dtsDivisionUser = $stationId === '143';
+    $dtsUserStation = $userData['station'];
+  }
+
+  $hrmisUser = isStationUser($employeeId, 'hrmis');
+  $dmisUser = isStationUser($employeeId, 'dmis');
+  $hrtdmsUser = isStationUser($employeeId, 'hrtdms');
+}
+?>
+
+<div class="modal-dialog <?php echo !$hasUser ? 'modal-sm' : ''; ?>">
+  <div class="modal-content">
+    <?php modalHeader($modalTitle); ?>
+
+    <form action="" method="POST">
+      <div class="modal-body">
+        <?php if ($hasUser) { ?>
+          <div class="d-flex justify-content-center align-middle employee-photo photo-2x rounded-circle overflow-hidden mx-auto mb-3">
+            <img height="100%" src="<?php echo $picture; ?>" alt="<?php echo $employeeName; ?>">
+          </div>
+          <div class="text-center text-uppercase h4 mt-1 mb-0"><?php echo $employeeName; ?></div>
+          <div class="text-center text-lowercase m-0 small"><?php echo $depedEmail; ?></div>
+          <div class="text-center text-uppercase h5 mt-2 mb-1"><?php echo $position; ?></div>
+          <div class="text-center text-uppercase h6 my-1"><?php echo $station; ?></div>
+
+          <hr>
+
+          <div class="text-center text-capitalize h5 px-2 mb-3">User Assignment</div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" id="dts" type="checkbox" name="dts" <?php echo setActiveItem($dtsUser, true, 'checked'); ?>>
+              <label class="form-check-label" for="dts">Document Tracking System</label>
+            </div>
+          </div>
+
+          <?php if ($dtsDivisionUser) : ?>
+            <div class="form-group pl-3 mt-n3">
+              <select name="dts-verifier" class="form-control">
+                <option value="">Select section...</option>
+                <?php $sections = sectionsExcept($station);
+                while ($section = fetchArray($sections)) : ?>
+                  <option value="<?php echo $section['id']; ?>" <?php echo setOptionSelected($section['id'], $dtsUserStation); ?>><?php echo $section['name']; ?></option>
+                <?php endwhile; ?>
+              </select>
+            </div>
+          <?php else : ?>
+            <input type="hidden" name="dts-verifier" value="<?php echo $stationId; ?>">
+          <?php endif; ?>
+
+
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" id="hrmis" type="checkbox" name="hrmis" <?php echo setActiveItem($hrmisUser, true, 'checked'); ?>>
+              <label class="form-check-label" for="hrmis">Human Resource Management Information System</label>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" id="dmis" type="checkbox" name="dmis" <?php echo setActiveItem($dmisUser, true, 'checked'); ?>>
+              <label class="form-check-label" for="dmis">Division Management Information System</label>
+            </div>
+          </div>
+
+          <div class="form-group mb-0">
+            <div class="form-check">
+              <input class="form-check-input" id="hrtdms" type="checkbox" name="hrtdms" <?php echo setActiveItem($hrtdmsUser, true, 'checked'); ?>>
+              <label class="form-check-label" for="hrtdms">HR Training &amp; Development Management System</label>
+            </div>
+          </div>
+        <?php } else {
+          missingAlert($modalTitle);
+        } ?>
+      </div>
+
+      <div class="modal-footer">
+        <?php if ($hasUser) : ?>
+          <input type="hidden" name="verifier" value="<?php echo isset($_GET['id']) ? $_GET['id'] : null; ?>">
+          <input type="hidden" name="data-verifier" value="<?php echo cipher($depedEmail); ?>">
+          <button class="btn btn-primary" name="edit-user" type="submit">Continue</button>
+        <?php endif; ?>
+        <?php cancelModalButton(); ?>
+      </div>
+    </form>
+  </div>
+</div>
