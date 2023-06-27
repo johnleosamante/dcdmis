ab8c2fa51c9cd84520aae285973dacaa0b117147
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-27 14:58:49 +0800

    Removed unused variable

--- includes/initialization.php
+++ includes/initialization.php
@@ -9,7 +9,6 @@ ini_set('max_execution_time', 300);
 ini_set('memory_limit', '1024M');
 //ini_set('display_errors', 0);
 
-$defaultImage = '/assets/img/user.png';
 $activeApp = isset($_SESSION[alias() . '_activeApp']) ? $_SESSION[alias() . '_activeApp'] : null;
 $userId = isset($_SESSION[alias() . '_userId']) ? $_SESSION[alias() . '_userId'] : null;
 $email = isset($_SESSION[alias() . '_email']) ? $_SESSION[alias() . '_email'] : null;
