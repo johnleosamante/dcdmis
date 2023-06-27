7474f0ca9321923122e2ca20ca8a4648ef2cefba
johnleosamante <johnleomuitsamante@gmail.com>
2023-06-27 14:33:28 +0800

    Added edit user assignment

--- dmis/app.php
+++ dmis/app.php
@@ -17,13 +17,69 @@ if (isset($_POST['primary-search-button'])) {
   redirect(customUri('dmis', 'Search', sanitize($_POST['primary-search-text'])));
 }
 
+if (isset($_POST['edit-user'])) {
+  $employeeId = isset($_POST['verifier']) ? sanitize(decipher($_POST['verifier'])) : null;
+  $userEmail = isset($_POST['data-verifier']) ? sanitize(decipher($_POST['data-verifier'])) : null;
+  $userRole = 'Administrator';
+  $isDtsUser = isset($_POST['dts']);
+  $dtsStation = isset($_POST['dts-verifier']) ? sanitize($_POST['dts-verifier']) : null;
+  $dtsPortal = numRows(section($dtsStation)) > 0 ? strtoupper($dtsStation . '_portal') : 'sch_portal';
+  $isHrmisUser = isset($_POST['hrmis']);
+  $isDmisUser = isset($_POST['dmis']);
+  $isHrtdmsUser = isset($_POST['hrtdms']);
+
+  if (empty($employeeId)) {
+    return;
+  }
+
+  if ($isDtsUser) {
+    if (numRows(dtsUser($employeeId)) > 0) {
+      updateUserRole($employeeId, $dtsStation, $dtsPortal);
+    } else {
+      createUserRole($employeeId, $userEmail, $userRole, $dtsStation, $dtsPortal);
+    }
+  } else {
+    deleteUserRole($employeeId, $dtsStation);
+  }
+
+  if ($isHrmisUser) {
+    if (!isStationUser($employeeId, 'HRMIS')) {
+      createUserRole($employeeId, $userEmail, $userRole, 'HRMIS');
+    } 
+  } else {
+    deleteUserRole($employeeId, 'HRMIS');
+  }
+
+  if ($isHrmisUser) {
+    if (!isStationUser($employeeId, 'DMIS')) {
+      createUserRole($employeeId, $userEmail, $userRole, 'DMIS');
+    } 
+  } else {
+    deleteUserRole($employeeId, 'DMIS');
+  }
+
+  if ($isHrtdmsUser) {
+    if (!isStationUser($employeeId, 'HRTDMS')) {
+      createUserRole($employeeId, $userEmail, $userRole, 'HRTDMS');
+    } 
+  } else {
+    deleteUserRole($employeeId, 'HRTDMS');
+  }
+
+  $showAlert = true;
+  $message = 'User assignment has been set successfully.';
+  $success = true;
+}
+
 if (isset($_POST['reset-user'])) {
   $employeeId = sanitize(decipher($_POST['verifier']));
   $temporaryPassword = sanitize(decipher($_POST['data-verifier']));
 
-  $showAlert = true;
-  $message = 'User password has been reset successfully. ' . $temporaryPassword;
-  $success = true;
+  if (affectedRows() === 1) {
+    $showAlert = true;
+    $message = 'User password has been reset successfully.';
+    $success = true;
+  }
 }
 
 if (isset($_POST['remove-user'])) {
