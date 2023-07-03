0153b5bd40c022929b7fac29ef3e070e709a4d9b
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-03 16:59:21 +0800

    Added save training

--- hrtdms/app.php
+++ hrtdms/app.php
@@ -12,4 +12,28 @@ if (!isset($userId)) {
 if (numRows(userRole($userId, $activeApp)) === 0) {
   redirect(uri() . '/pis');
 }
+
+if (isset($_POST['save-training'])) {
+  $trainingId = isset($_POST['verifier']) ? sanitize(decipher($_POST['verifier'])) : null;
+  $title = sanitize($_POST['title']);
+  $from = sanitize($_POST['from']);
+  $to = sanitize($_POST['to']);
+  $type = sanitize($_POST['type']);
+  $sponsor = sanitize($_POST['sponsor']);
+  $venue = sanitize($_POST['venue']);
+
+  if (numRows(training($trainingId)) === 0) {
+    $status = 'saved';
+    createTraining($title, $from, $to, $type, $sponsor, $venue);
+  } else {
+    $status = 'updated';
+    updateTraining($title, $from, $to, $type, $sponsor, $venue, $trainingId);
+  }
+
+  if (affectedRows()) {
+    $showResult = true;
+    $success = true;
+    $message = "Training has been {$status} successfully.";
+  }
+}
 ?>
