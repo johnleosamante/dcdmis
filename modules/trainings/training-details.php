2dea80dcdd409e13e245eacab482f5d55948cba1
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-04 14:19:53 +0800

    Added training details

---
+++ modules/trainings/training-details.php
@@ -0,0 +1,14 @@
+<?php
+// modules/trainings/training-details.php
+messageAlert($showAlert, $message, $success);
+?>
+
+<div class="card border-left-primary shadow mb-4">
+  <div class="card-header py-3">
+    <?php contentTitle('Training Details'); ?>
+  </div>
+
+  <div class="card-body">
+    
+  </div>
+</div>
