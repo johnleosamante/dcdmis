54443d8cab8303c69f41500b745b11f11fa3eae8
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-05 16:19:28 +0800

    Added login page to redirect

--- logout/index.php
+++ logout/index.php
@@ -4,5 +4,5 @@ require_once('../includes/function.php');
 
 session_destroy();
 setcookie(alias() . '_login', '', time() - getSeconds(8), '/', uri(), false, true);
-redirect(uri());
+redirect(uri() . '/login');
 ?>
