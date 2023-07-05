8be36c066ca2d27d89c8312626f02d71783a32e3
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-05 16:16:56 +0800

    Removed temporary links

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 // index.php
 require_once('./includes/function.php');
+redirect(uri() . '/login');
 ?>
-
-DepEd Dipolog City Schools Division
-
-<ul>
-  <li><a href="/dmis">Division Management Information System</a></li>
-  <li><a href="/dts">Document Tracking System</a></li>
-  <li><a href="/hrmis">Human Resource Management Information System</a></li>
-  <li><a href="/pis">Personnel Information System</a></li>
-</ul>
