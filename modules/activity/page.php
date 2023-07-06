35a017e88d3bec6fc642ca69a6ab2aa73674ba38
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-06 16:57:45 +0800

    Added activity log form view

--- modules/activity/page.php
+++ modules/activity/page.php
@@ -8,6 +8,30 @@
   </div>
 
   <div class="card-body">
-    Work in progress
+    <div class="table-responsive">
+      <table class="table table-hover table-striped table-bordered mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th class="align-middle" width="5%">#</th>
+            <th class="align-middle" width="15%">Date/Time</th>
+            <th class="align-middle" width="65%">Activity</th>
+            <th class="align-middle" width="15%">IP Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $query = userLog($userId);
+          $no = 0;
+          while ($row = fetchAssoc($query)) : ?>
+            <tr class="text-uppercase">
+              <td class="align-middle"><?php echo ++$no; ?></td>
+              <td class="align-middle"><?php echo toDatetime($row['datetime']); ?></td>
+              <td class="text-left align-middle"><?php echo $row['activity']; ?></td>
+              <td class="align-middle"><?php echo $row['ip']; ?></td>
+            </tr>
+          <?php endwhile; ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
