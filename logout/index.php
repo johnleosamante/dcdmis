303b9389c4a63a380d189d9f2eccc6337b3c8f3b
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-07 13:56:53 +0800

    Added create system log function

--- logout/index.php
+++ logout/index.php
@@ -1,7 +1,10 @@
 <?php
 // logout/index.php
 require_once('../includes/function.php');
+require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/system-log.php');
 
+createSystemLog($stationId, $userId, 'Logged out', $userId, clientIp());
 session_destroy();
 setcookie(alias() . '_login', '', time() - getSeconds(8), '/', uri(), false, true);
 redirect(uri() . '/login');
