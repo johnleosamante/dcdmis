e5e23705877e0057f08592ee9f282954149680bc
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-07 13:55:49 +0800

    Added create system log function

--- includes/database/system-log.php
+++ includes/database/system-log.php
@@ -9,4 +9,8 @@ function userLog($id) {
 function systemLogs() {
   return query("SELECT `tbl_employee`.`Emp_LName` AS lname, `tbl_employee`.`Emp_FName` AS fname, `tbl_employee`.`Emp_MName` AS mname, `tbl_employee`.`Emp_Extension` AS ext, `tbl_system_logs`.`Time_Log` AS `datetime`, `tbl_system_logs`.`Status` AS `activity`, `tbl_system_logs`.`IPAddress` AS ip FROM tbl_employee INNER JOIN tbl_system_logs ON tbl_employee.Emp_ID=tbl_system_logs.Emp_ID ORDER BY tbl_system_logs.Time_Log DESC;");
 }
+
+function createSystemLog($stationId, $id, $status, $targetId, $ip) {
+  nonQuery("INSERT INTO tbl_system_logs (`SchoolID`, `Emp_ID`, `Time_Log`, `Status`, `target_id`, `IPAddress`) VALUES ('{$stationId}', '{$id}', NOW(), '{$status}', '{$targetId}', '{$ip}');");
+}
 ?>
