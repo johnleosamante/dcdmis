2d9bd3a52fc3ebc6ce37362291a7cb517bb2df29
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-07 16:08:24 +0800

    Added sql script

--- sql/educational_background.sql
+++ sql/educational_background.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `educational_background` ADD `ispresent` BOOLEAN NOT NULL AFTER `To`;
+
+UPDATE `educational_background` SET `Level`='Graduate Studies' WHERE `Level`="Masteral" OR `Level`='Doctoral';
+
+UPDATE `educational_background` SET `Level`='Secondary' WHERE `Level`="High School";
