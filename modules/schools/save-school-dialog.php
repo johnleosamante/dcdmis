8a102aa949b63efd6ec4f1c2d7978145ec8190e8
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-10 17:04:04 +0800

    Added school dialog

---
+++ modules/schools/save-school-dialog.php
@@ -0,0 +1,65 @@
+<?php
+// modules/schools/save-training-dialog.php
+require_once('../../includes/function.php');
+require_once(root() . '/includes/string.php');
+require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/school.php');
+require_once(root() . '/includes/layout/components.php');
+
+$schoolId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
+$schools = schoolDetailsById($schoolId);
+$school = $schoolName = $address = $category = $district = $alias = null;
+$modalTitle = 'New School';
+$notFound = true;
+
+if (numRows($schools) > 0) {
+  $school = fetchAssoc($schools);
+  $schoolName = $school['name'];
+  $address = $school['address'];
+  $category = $school['category'];
+  $district = $school['district'];
+  $alias = $school['alias'];
+  $modalTitle = 'Edit School';
+}
+?>
+
+<div class="modal-dialog">
+  <div class="modal-content">
+    <?php modalHeader($modalTitle); ?>
+
+    <form action="" method="POST">
+      <div class="modal-body">
+        <div class="form-group">
+          <label for="school-id" class="mb-0">School ID <?php showAsterisk(); ?></label>
+          <input type="text" id="school-id" name="school-id" class="form-control" value="<?php echo $schoolId; ?>" required>
+        </div>
+        <div class="form-group">
+          <label for="school-name" class="mb-0">Name <?php showAsterisk(); ?></label>
+          <input type="text" id="school-name" name="school-name" class="form-control" value="<?php echo $schoolName; ?>" required>
+        </div>
+        <div class="form-group">
+          <label for="alias" class="mb-0">Alias <?php showAsterisk(); ?></label>
+          <input type="text" id="alias" name="alias" class="form-control" value="<?php echo $alias; ?>" required>
+        </div>
+        <div class="form-group">
+          <label for="address" class="mb-0">Address <?php showAsterisk(); ?></label>
+          <input type="text" id="address" name="address" class="form-control" value="<?php echo $address; ?>" required>
+        </div>
+        <div class="form-group">
+          <label for="district" class="mb-0">District <?php showAsterisk(); ?></label>
+          <input type="text" id="district" name="district" class="form-control" value="<?php echo $district; ?>" required>
+        </div>
+        <div class="form-group">
+          <label for="category" class="mb-0">Category <?php showAsterisk(); ?></label>
+          <input type="text" id="category" name="category" class="form-control" value="<?php echo $category; ?>" required>
+        </div>
+        <?php requiredLegend(0); ?>
+      </div>
+      <div class="modal-footer">
+        <input type="hidden" name="verifier" value="<?php echo isset($_GET['id']) ? $_GET['id'] : null; ?>">
+        <button class="btn btn-primary" name="save-school" type="submit">Continue</button>
+        <?php cancelModalButton(); ?>
+      </div>
+    </form>
+  </div>
+</div>
