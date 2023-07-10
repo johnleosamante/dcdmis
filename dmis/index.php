a561635350caafffc02d4506565c16d8d5c53967
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-10 13:07:14 +0800

    Added required files

--- dmis/index.php
+++ dmis/index.php
@@ -5,6 +5,7 @@ require_once(root() . '/includes/string.php');
 require_once(root() . '/includes/database/database.php');
 require_once(root() . '/includes/database/account.php');
 require_once(root() . '/includes/database/user.php');
+require_once(root() . '/includes/database/position.php');
 require_once(root() . '/includes/database/employee.php');
 require_once(root() . '/includes/database/school.php');
 require_once(root() . '/includes/database/district.php');
