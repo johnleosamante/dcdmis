ed6534607b03607fff295ac2b1ff3362056dc57a
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-11 11:15:22 +0800

    Added school data functions

--- includes/database/school.php
+++ includes/database/school.php
@@ -22,7 +22,15 @@ function schoolDetailsById($id) {
 }
 
 function schoolDetails($id=null) {
-  $schoolId = empty($id) ? ' ' : " AND tbl_school.SchoolID='{$id}' ";
-  return query("SELECT tbl_school.SchoolLogo AS `logo`, tbl_school.Abraviate AS alias, tbl_school.SchoolID AS `id`, tbl_school.SchoolName AS school, tbl_school.Address AS address, tbl_district.District_Name AS district, tbl_school.Incharg_ID AS `head`, tbl_school.telephone, tbl_school.email, tbl_school.website, tbl_school.fb_page, SUM(CASE WHEN tbl_employee.Emp_Sex = 'Male' THEN 1 ELSE 0 END) as male, SUM(CASE WHEN tbl_employee.Emp_Sex = 'Female' THEN 1 ELSE 0 END) as female, COUNT(*) as `total` FROM tbl_employee INNER JOIN tbl_station ON tbl_employee.Emp_ID=tbl_station.Emp_ID INNER JOIN tbl_school ON tbl_station.Emp_Station=tbl_school.SchoolID INNER JOIN tbl_district ON tbl_school.District_code=tbl_district.District_code WHERE tbl_employee.Emp_Status='Active'" . $schoolId . "GROUP BY tbl_school.SchoolName ORDER BY tbl_school.SchoolName;");
+  $filter = empty($id) ? ' ' : " AND tbl_school.SchoolID='{$id}' ";
+  return query("SELECT tbl_school.SchoolLogo AS `logo`, tbl_school.Abraviate AS alias, tbl_school.SchoolID AS `id`, tbl_school.SchoolName AS school, tbl_school.Address AS address, tbl_district.District_Name AS district, tbl_school.School_Category AS category, tbl_school.Incharg_ID AS `head`, tbl_school.telephone, tbl_school.email, tbl_school.website, tbl_school.fb_page, SUM(CASE WHEN tbl_employee.Emp_Sex = 'Male' THEN 1 ELSE 0 END) as male, SUM(CASE WHEN tbl_employee.Emp_Sex = 'Female' THEN 1 ELSE 0 END) as female, COUNT(*) as `total` FROM tbl_employee INNER JOIN tbl_station ON tbl_employee.Emp_ID=tbl_station.Emp_ID INNER JOIN tbl_school ON tbl_station.Emp_Station=tbl_school.SchoolID INNER JOIN tbl_district ON tbl_school.District_code=tbl_district.District_code WHERE tbl_employee.Emp_Status='Active'" . $filter . "GROUP BY tbl_school.SchoolName ORDER BY tbl_school.SchoolName;");
+}
+
+function createSchool($id, $name, $address, $district, $category) {
+  nonQuery("INSERT INTO tbl_school (`SchoolID`, `SchoolName`, `Address`, `District_code`, `School_Category`) VALUES ('{$id}', '{$name}', '{$address}', '{$district}', '{$category}');");
+}
+
+function updateSchool($id, $name, $address, $district,$category, $referenceId) {
+  nonQuery("UPDATE tbl_school SET `SchoolID`='{$id}', `SchoolName`='{$name}', `Address`='{$address}', `District_code`='{$district}', `School_Category`='{$category}' WHERE `SchoolID`='{$referenceId}' LIMIT 1;");
 }
 ?>
