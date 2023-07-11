dac0a6d535fc6ab0cffbe8167275a71a314c4264
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-11 16:07:00 +0800

    Fixed link reference

--- dmis/sidebar-menu.php
+++ dmis/sidebar-menu.php
@@ -8,11 +8,11 @@ $sectionCount = number_format(numRows(sections()));
 $userCount = number_format(numRows(users()));
 $employeeCount = number_format(numRows(activeEmployees()));
 
-sidebarMenuItem(customUri('dmis', 'Districts'), 'Districts', 'fa-map-marked-alt', isset($url) && str_contains($url, 'Districts'), $districtCount);
+sidebarMenuItem(customUri('dmis', 'Districts'), 'Districts', 'fa-map-marked-alt', isset($url) && str_contains($url, 'District'), $districtCount);
 
 sidebarMenuItem(customUri('dmis', 'Schools'), 'Schools', 'fa-school', isset($url) && str_contains($url, 'School'), $schoolCount);
 
-sidebarMenuItem(customUri('dmis', 'Sections'), 'Sections', 'fa-map-signs', isset($url) && str_contains($url, 'Sections'), $sectionCount);
+sidebarMenuItem(customUri('dmis', 'Sections'), 'Sections', 'fa-map-signs', isset($url) && str_contains($url, 'Section'), $sectionCount);
 
 sidebarMenuItem(customUri('dmis', 'Users'), 'Users', 'fa-user-friends', isset($url) && str_contains($url, 'Users'), $userCount);
 
