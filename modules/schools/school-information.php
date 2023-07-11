d35d08e0b2de96cdd87e60ba9adca277823caf77
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-11 11:17:09 +0800

    Removed edit school button

--- modules/schools/school-information.php
+++ modules/schools/school-information.php
@@ -13,6 +13,7 @@ if (numRows($schools) > 0) {
   $alias = $school['alias'];
   $address = $school['address'];
   $district = $school['district'];
+  $category = $school['category'];
   $head = $school['head'];
   $telephone = $school['telephone'];
   $email = $school['email'];
@@ -30,21 +31,15 @@ messageAlert($showAlert, $message, $success);
 
 <div class="card border-left-primary shadow mb-4">
   <div class="card-header py-3">
-    <?php
-    if ($activeApp === 'dmis') {
-      contentTitleWithModal('School Information', uri() . '/modules/schools/save-school-dialog.php?id=' . cipher($schoolId), 'Edit', 'fa-edit');
-    } else {
-      contentTitle('School Information');
-    }
-    ?>
+    <?php contentTitle('School Information'); ?>
   </div>
 
   <div class="card-body">
     <div class="row">
-      <div class="col-sm-12 col-md-2 col-lg-2 col-xl-2">
+      <div class="col-sm-12 col-md-12 col-lg-4 col-xl-2">
         <img src="<?php echo $logo; ?>" width="100%">
       </div>
-      <div class="col-sm-12 col-md-10 col-lg-10 col-xl-10">
+      <div class="col-sm-12 col-md-12 col-lg-8 col-xl-10">
         <div class="table-responsive pt-3">
           <table cellspacing="0">
             <tr>
@@ -62,6 +57,10 @@ messageAlert($showAlert, $message, $success);
             <tr>
               <th class="pr-5 align-top" scoper="row">District</th>
               <td class="text-uppercase"><?php echo $district; ?></td>
+            </tr>
+            <tr>
+              <th class="pr-5 align-top" scoper="row">Category</th>
+              <td class="text-uppercase"><?php echo $category; ?></td>
             </tr>
             <tr>
               <th class="pr-5 align-top" scoper="row">Head of Office</th>
@@ -146,7 +145,7 @@ messageAlert($showAlert, $message, $success);
                     <?php dropdownEllipsis(); ?>
                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
                       <?php
-                      linkDropdownItem(customUri('hrmis', 'Employee Information', $row['id']), 'View', 'fa-eye', 'View Employee');
+                      linkDropdownItem(customUri('hrmis', 'Employee Information', $row['id']), 'View', 'fa-eye', 'View Employee', true);
                       modalDropdownItem(uri() . '/modules/employees/reassign-employee-dialog.php?id=' . cipher($row['id']), 'Reassign', 'fa-share', 'Reassign Employee');
                       ?>
                       <div class="dropdown-divider"></div>
