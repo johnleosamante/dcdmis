f7c99afaf7116d1e4032a9365bddb9d895f9b9a9
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-12 15:24:33 +0800

    Added progress column

--- modules/employees/active-employees.php
+++ modules/employees/active-employees.php
@@ -22,8 +22,9 @@ $isHrmis = $activeApp === 'hrmis';
             <th class="align-middle" width="25%">Name</th>
             <th class="align-middle" width="15%">Date of Birth</th>
             <th class="align-middle" width="5%">Age</th>
-            <th class="align-middle" width="20%">Position</th>
-            <th class="align-middle" width="25%">Station</th>
+            <th class="align-middle" width="15%">Position</th>
+            <th class="align-middle" width="20%">Station</th>
+            <th class="align-middle" width="10%">Progress</th>
             <th class="align-middle" width="5%">Action</th>
           </tr>
         </thead>
@@ -49,6 +50,9 @@ $isHrmis = $activeApp === 'hrmis';
               <td class="align-middle"><?php echo getAge($row['year'], $row['month'], $row['day']); ?></td>
               <td class="align-middle"><?php echo fetchAssoc(positions($row['position']))['position']; ?></td>
               <td class="align-middle"><?php echo fetchAssoc(schoolById($row['station']))['name']; ?></td>
+              <td class="align-middle">
+                <?php progressBar(pdsProgress($row['id'])); ?>
+              </td>
               <td class="align-middle text-capitalize">
                 <div class="dropdown no-arrow">
                   <?php dropdownEllipsis(); ?>
