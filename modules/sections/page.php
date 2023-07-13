c92a9e7b8d47b7941eab012a7b885b4a7d0a201a
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-13 11:55:00 +0800

    Modified section page

--- modules/sections/page.php
+++ modules/sections/page.php
@@ -1,13 +1,71 @@
 <?php
 // modules/sections/page.php
+messageAlert($showAlert, $message, $success);
 ?>
 
 <div class="card border-left-primary shadow mb-4">
   <div class="card-header py-3">
-    <?php contentTitle('Sections'); ?>
+        <?php if ($activeApp === 'dmis') {
+      contentTitleWithModal('Sections', uri() . '/modules/districts/save-section-dialog.php', 'Add', 'fa-plus');
+    } else {
+      contentTitle('Sections');
+    } ?>
   </div>
 
   <div class="card-body">
-    Work in progress
+    <div class="table-responsive">
+      <table class="table table-hover table-bordered table-striped mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th class="align-middle" rowspan="2" width="30%">Section</th>
+            <th class="align-middle" rowspan="2" width="50%">Section Head</th>
+            <th class="align-middle" colspan="3" width="15%">Personnel</th>
+            <th class="align-middle" rowspan="2" width="5%">Action</th>
+          </tr>
+
+          <tr>
+            <th class="align-middle text-mars" width="5%"><i class="fa fa-user fw"></i></th>
+            <th class="align-middle text-venus" width="5%"><i class="fa fa-user fw"></i></th>
+            <th class="align-middle" width="5%"><i class="fa fa-user-friends fw"></i></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php
+          $query = sections();
+          while ($row = fetchAssoc(($query))) : ?>
+            <tr class="text-uppercase">
+              <td class="align-middle"><?php echo $row['name']; ?></td>
+              <td class="align-middle">
+                <div><?php echo userName($row['head']); ?></div>
+                <div class="small"><?php echo fetchAssoc(position($row['head']))['position']; ?></div>
+              </td>
+              <?php
+              $sectionCount = sectionEmployeeCount($row['id']);
+              $male = $female = $total = 0;
+
+              if (numRows($sectionCount) > 0) {
+                $count = fetchAssoc($sectionCount);
+                $male = $count['male'];
+                $female = $count['female'];
+                $total = $count['total'];
+              }
+              ?>
+              <td class="align-middle"><?php echo $male; ?></td>
+              <td class="align-middle"><?php echo $female; ?></td>
+              <td class="align-middle"><?php echo $total; ?></td>
+              <td class="align-middle text-capitalize">
+                <div class="dropdown no-arrow">
+                  <?php dropdownEllipsis(); ?>
+                  <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
+                    <?php linkDropdownItem(customUri($activeApp, 'Section Information', $row['id']), 'View', 'fa-eye', 'View Section'); ?>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          <?php endwhile; ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
