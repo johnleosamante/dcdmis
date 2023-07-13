b7bdd21328683c91e52afda3cb61b12d42dd2d38
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-13 11:55:28 +0800

    Added section employee count function

--- includes/database/section.php
+++ includes/database/section.php
@@ -12,4 +12,8 @@ function sections() {
 function section($id) {
   return query("SELECT Section_Code AS id, Section_Incharge AS `head`, Section_Office AS `name` FROM tbl_div_section WHERE Section_Code='{$id}' LIMIT 1;");
 }
+
+function sectionEmployeeCount($id) {
+  return query("SELECT SUM(CASE WHEN tbl_employee.Emp_Sex = 'Male' THEN 1 ELSE 0 END) AS male, SUM(CASE WHEN tbl_employee.Emp_Sex='Female' THEN 1 ELSE 0 END) AS female, COUNT(*) AS `total` FROM tbl_employee INNER JOIN tbl_user ON tbl_employee.Emp_ID=tbl_user.usercode INNER JOIN tbl_div_section ON tbl_user.Station=tbl_div_section.Section_Code WHERE tbl_employee.Emp_Status='Active' AND tbl_user.Station='{$id}' GROUP BY tbl_div_section.Section_Office;");
+}
 ?>
