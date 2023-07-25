d1bfa335b2d87c72f9b32ddedfabedb602497287
johnleosamante <johnleomuitsamante@gmail.com>
2023-07-25 16:59:39 +0800

    Added dts redirect for public access

--- logout/index.php
+++ logout/index.php
@@ -7,5 +7,9 @@ require_once(root() . '/includes/database/system-log.php');
 createSystemLog($stationId, $userId, 'Logged out', $userId, clientIp());
 session_destroy();
 setcookie(alias() . '_login', '', time() - getSeconds(8), '/', uri(), false, true);
-redirect(uri() . '/login');
+if (!$isPublic) { 
+  redirect(uri() . '/login');
+} else {
+  redirect(uri() . '/dts');
+}
 ?>
