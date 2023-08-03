d5a6954b2299ccf5ab7ee4e40cf46feeb95a53cf
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-03 16:13:27 +0800

    Added required file

--- dts/index.php
+++ dts/index.php
@@ -3,6 +3,7 @@
 require_once('../includes/function.php');
 require_once(root() . '/includes/string.php');
 require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/activity.php');
 require_once(root() . '/includes/database/account.php');
 require_once(root() . '/includes/database/employee.php');
 require_once(root() . '/includes/database/document.php');
--- pis/index.php
+++ pis/index.php
@@ -3,6 +3,7 @@
 require_once('../includes/function.php');
 require_once(root() . '/includes/string.php');
 require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/activity.php');
 require_once(root() . '/includes/database/account.php');
 require_once(root() . '/includes/database/user.php');
 require_once(root() . '/includes/database/employee.php');
