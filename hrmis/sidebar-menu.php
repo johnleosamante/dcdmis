b105a8232e2c21f913d98645c660efec0a434fb8
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-04 13:09:00 +0800

    Modified label

--- hrmis/sidebar-menu.php
+++ hrmis/sidebar-menu.php
@@ -16,7 +16,7 @@ sidebarMenuItem(customUri('hrmis', 'Active Employees'), 'Active', 'fa-user-check
 
 sidebarMenuItem(customUri('hrmis', 'Retirable Employees'), 'Retirable','fa-user-clock',isset($url) && str_contains($url, 'Retirable'), $countRetirable);
 
-sidebarMenuItem(customUri('hrmis', 'Celebrant Employees'), 'Celebrant', 'fa-birthday-cake', isset($url) && str_contains($url, 'Celebrant'));
+sidebarMenuItem(customUri('hrmis', 'Celebrant Employees'), 'Celebrants', 'fa-birthday-cake', isset($url) && str_contains($url, 'Celebrant'));
 
 sidebarMenuItem(customUri('hrmis', 'Archived Employees'), 'Archived', 'fa-user-lock', isset($url) && str_contains($url, 'Archived'));
 
