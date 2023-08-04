54adfeb45570410ac0b54e0dae66fd722d51ddf4
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-04 16:08:27 +0800

    Added calendar data function

--- includes/database/activity.php
+++ includes/database/activity.php
@@ -4,4 +4,8 @@
 function hasHoliday() {
   return numRows(query("SELECT `No` FROM `tbl_calendar_of_activity` WHERE (`EndDate` >= CURDATE() AND `StartDate` <= CURDATE()) AND `isHoliday`=1;")) > 0;
 }
+
+function calendar() {
+  return query("SELECT `Activity` AS `activity`, `StartDate` AS `from`, `EndDate` AS `to`, isHoliday FROM `tbl_calendar_of_activity` WHERE `EndDate` >= CURDATE() ORDER BY `StartDate`;");
+}
 ?>
