353f16ed46e76df0dd484ddff61e886ab1fef2f2
John Leo Samante <johnleomuitsamante@gmail.com>
2023-08-06 21:29:24 +0800

    Added export button

--- modules/employees/active-employees.php
+++ modules/employees/active-employees.php
@@ -14,6 +14,14 @@ $isHrmis = $activeApp === 'hrmis';
   </div>
 
   <div class="card-body">
+    <?php if ($isHrmis) { ?>
+      <div class="d-sm-flex align-items-center flex-row-reverse my-2">
+        <div class="d-inline-block">
+          <?php linkButtonSplit(customUri('export', 'active-employees'), 'Export', 'fa-file-excel', 'Export as Excel file', 'success'); ?>
+        </div>
+      </div>
+    <?php } ?>
+
     <div class="table-responsive">
       <table class="table table-hover mb-0 text-center" id="data-table" width="100%" cellspacing="0">
         <thead>
