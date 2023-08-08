a7750bd356f719d2f3d4d8eb171826daf5a288dd
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-08 17:01:37 +0800

    Added school export data

---
+++ export/schools.php
@@ -0,0 +1,41 @@
+<?php echo '<table border=1>
+  <tr>
+    <th rowspan="3">School ID</th>
+    <th rowspan="3">School Name</th>
+    <th rowspan="3">District</th>
+    <th rowspan="3">Category</th>
+    <th rowspan="3">Head of Office</th>
+    <th colspan="9">Personnel</th>
+  </tr>
+  <tr>
+    <th colspan="4">Male</th>
+    <th colspan="4">Female</th>
+    <th rowspan="2">Total</th>
+  </tr>
+  <tr>
+    <th>Teaching</th>
+    <th>Teaching-Related</th>
+    <th>Non-Teaching</th>
+    <th>Total</th>
+    <th>Teaching</th>
+    <th>Teaching-Related</th>
+    <th>Non-Teaching</th>
+    <th>Total</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>'?>
