52ec340b0cc078448e49ad4c7ffd43d0d4a25b8d
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-10 11:28:54 +0800

    Modified new employee dialog

--- modules/employees/new-employee-dialog.php
+++ modules/employees/new-employee-dialog.php
@@ -16,25 +16,25 @@ require_once(root() . '/includes/layout/components.php');
       <div class="modal-body">
         <div class="form-group">
           <label for="lname" class="mb-0">Last Name <?php showAsterisk(); ?></label>
-          <input id="lname" name="lname" class="form-control" type="text" required>
+          <input id="lname" name="lname" class="form-control" type="text" title="Required field" required>
         </div>
 
         <div class="form-group">
           <label for="fname" class="mb-0">First Name <?php showAsterisk(); ?></label>
-          <input id="fname" name="fname" class="form-control" type="text" required>
+          <input id="fname" name="fname" class="form-control" type="text" title="Required field" required>
         </div>
 
         <div class="row">
           <div class="col-9">
             <div class="form-group">
               <label for="mname" class="mb-0">Middle Name</label>
-              <input id="mname" name="mname" class="form-control" type="text">
+              <input id="mname" name="mname" class="form-control" title="Leave blank if not applicable" type="text">
             </div>
           </div>
           <div class="col-3">
             <div class="form-group">
               <label for="ext" class="mb-0">Extension</label>
-              <input id="ext" name="ext" class="form-control" type="text">
+              <input id="ext" name="ext" class="form-control" title="Example: Jr., Sr., III (Leave blank if not applicable)" type="text">
             </div>
           </div>
         </div>
@@ -43,7 +43,7 @@ require_once(root() . '/includes/layout/components.php');
           <div class="col-5">
             <div class="form-group">
               <label for="sex" class="mb-0">Sex <?php showAsterisk(); ?></label>
-              <select name="sex" class="form-control" id="sex" required>
+              <select name="sex" class="form-control" id="sex" title="Required field" required>
                 <option value="">Select sex...</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
@@ -54,14 +54,24 @@ require_once(root() . '/includes/layout/components.php');
           <div class="col-7">
             <div class="form-group">
               <label for="bdate" class="mb-0">Date of Birth <?php showAsterisk(); ?></label>
-              <input type="date" id="bdate" name="bdate" value="<?php echo date('Y-m-d'); ?>" class="form-control" required>
+              <input type="date" id="bdate" name="bdate" value="<?php echo date('Y-m-d'); ?>" class="form-control" title="Required field" required>
             </div>
           </div>
         </div>
 
         <div class="form-group">
+          <label for="email" class="mb-0">DepEd Email Address <?php showAsterisk(); ?></label>
+          <input id="email" name="email" class="form-control" type="email" title="Required field" placeholder="juan.delacruz@deped.gov.ph" required>
+        </div>
+
+        <div class="form-group">
+          <label for="mobile" class="mb-0">Mobile Number <?php showAsterisk(); ?></label>
+          <input id="mobile" name="mobile" class="form-control" type="text" title="Required field" placeholder="XXXX-XXX-XXXX" required>
+        </div>
+
+        <div class="form-group">
           <label for="position" class="mb-0">Position <?php showAsterisk(); ?></label>
-          <select id="position" name="position" class="form-control" required>
+          <select id="position" name="position" class="form-control" title="Required field" required>
             <option value="">Select position...</option>
             <?php $jobPositions = positions();
             while ($jobPosition = fetchArray($jobPositions)) : ?>
@@ -72,7 +82,7 @@ require_once(root() . '/includes/layout/components.php');
 
         <div class="form-group">
           <label for="station" class="mb-0">Station <?php showAsterisk(); ?></label>
-          <select id="station" name="station" class="form-control" required>
+          <select id="station" name="station" class="form-control" title="Required field" required>
             <option value="">Select station...</option>
             <?php echo $currentStation = isset($_GET['s']) ? sanitize(decode($_GET['s'])) : '';
             $assignments = schoolsExcept($stationId);
@@ -80,16 +90,6 @@ require_once(root() . '/includes/layout/components.php');
               <option value="<?php echo $assignment['id']; ?>" <?php echo setOptionSelected($assignment['id'], $currentStation); ?>><?php echo $assignment['name']; ?></option>
             <?php endwhile; ?>
           </select>
-        </div>
-
-        <div class="form-group">
-          <label for="email" class="mb-0">DepEd Email Address <?php showAsterisk(); ?></label>
-          <input id="email" name="email" class="form-control" type="email" required>
-        </div>
-
-        <div class="form-group">
-          <label for="mobile" class="mb-0">Mobile Number <?php showAsterisk(); ?></label>
-          <input id="mobile" name="mobile" class="form-control" type="text" required>
         </div>
 
         <?php requiredLegend(0); ?>
