e687d709f8b664ea753fe55cbe8a182fe6d4cd01
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-11 11:14:50 +0800

    Fixed default error

--- modules/error/error.php
+++ modules/error/error.php
@@ -1,14 +1,14 @@
 <?php
 // modules/error/error.php
-$isRestricted = isWeekend() || !isOfficialTime() || $isHoliday;
+$isAccessible = !isWeekend() && isOfficialTime() && !$isHoliday;
 ?>
 
 <div class="text-center py-0">
   <div class="error mx-auto w-100">Oops!</div>
-  <p class="lead text-gray-800 mt-1 mb-0"><?php echo $isRestricted ? 'Restricted Access' : 'Unexpected error'; ?></p>
-  <p class="text-gray-500 mb-4"><?php echo $isRestricted ? 'It seems that the system is currently not accessible...' : 'It seems you have encountered a glitch in the system...'; ?></p>
+  <p class="lead text-gray-800 mt-1 mb-0"><?php echo $isAccessible ? 'Unexpected error' : 'Restricted Access'; ?></p>
+  <p class="text-gray-500 mb-4"><?php echo $isAccessible ? 'It seems you have encountered a glitch in the system...' : 'It seems that the system is currently not accessible...'; ?></p>
 
-  <?php if (!$isRestricted && isset($userId)) { ?>
+  <?php if ($isAccessible && isset($userId)) { ?>
     <a href="<?php echo uri() . '/' . $activeApp; ?>" title="Go to dashboard">Go to dashboard</a>
   <?php } else { ?>
     <a href="<?php echo uri(); ?>" title="Go to home page">Go to home page</a>
