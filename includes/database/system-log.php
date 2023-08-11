edf730747680714219bc72212894110ed039756a
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-11 11:54:58 +0800

    Modified system logs

--- includes/database/system-log.php
+++ includes/database/system-log.php
@@ -3,11 +3,11 @@
 // tbl_system_logs
 
 function userLog($id) {
-  return query("SELECT `Time_Log` AS `datetime`, `Status` AS `activity`, `IPAddress` AS `ip` FROM tbl_system_logs WHERE Emp_ID='{$id}' ORDER BY `Time_Log` DESC;");
+  return query("SELECT `Time_Log` AS `datetime`, `Status` AS `activity`, `target_id` AS `target`, `IPAddress` AS `ip` FROM tbl_system_logs WHERE Emp_ID='{$id}' ORDER BY `Time_Log` DESC;");
 }
 
 function systemLogs() {
-  return query("SELECT `tbl_employee`.`Emp_LName` AS lname, `tbl_employee`.`Emp_FName` AS fname, `tbl_employee`.`Emp_MName` AS mname, `tbl_employee`.`Emp_Extension` AS ext, `tbl_system_logs`.`Time_Log` AS `datetime`, `tbl_system_logs`.`Status` AS `activity`, `tbl_system_logs`.`IPAddress` AS ip FROM tbl_employee INNER JOIN tbl_system_logs ON tbl_employee.Emp_ID=tbl_system_logs.Emp_ID ORDER BY tbl_system_logs.Time_Log DESC;");
+  return query("SELECT `tbl_employee`.`Emp_LName` AS lname, `tbl_employee`.`Emp_FName` AS fname, `tbl_employee`.`Emp_MName` AS mname, `tbl_employee`.`Emp_Extension` AS ext, `tbl_system_logs`.`Time_Log` AS `datetime`, `tbl_system_logs`.`Status` AS `activity`, `tbl_system_logs`.`target_id` AS `target`, `tbl_system_logs`.`IPAddress` AS ip FROM tbl_employee INNER JOIN tbl_system_logs ON tbl_employee.Emp_ID=tbl_system_logs.Emp_ID ORDER BY tbl_system_logs.Time_Log DESC;");
 }
 
 function createSystemLog($stationId, $id, $status, $targetId, $ip) {
--- modules/activity/page.php
+++ modules/activity/page.php
@@ -13,9 +13,9 @@
         <thead>
           <tr>
             <th class="align-middle" width="5%">#</th>
-            <th class="align-middle" width="15%">Date/Time</th>
-            <th class="align-middle" width="65%">Activity</th>
-            <th class="align-middle" width="15%">IP Address</th>
+            <th class="align-middle" width="20%">Date/Time</th>
+            <th class="align-middle" width="50%">Activity</th>
+            <th class="align-middle" width="25%">Target</th>
           </tr>
         </thead>
         <tbody>
@@ -27,7 +27,9 @@
               <td class="align-middle"><?php echo ++$no; ?></td>
               <td class="align-middle"><?php echo toDatetime($row['datetime']); ?></td>
               <td class="text-left align-middle"><?php echo $row['activity']; ?></td>
-              <td class="align-middle"><?php echo $row['ip']; ?></td>
+              <td class="text-center align-middle">
+                <?php echo $userId === $row['target'] ? 'YOUR ACCOUNT' : userName($row['target']); ?>
+              </td>
             </tr>
           <?php endwhile; ?>
         </tbody>
