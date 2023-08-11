3944f4ac5e6a8c4fac9a85b981754e21e7820989
johnleosamante <johnleomuitsamante@gmail.com>
2023-08-11 11:16:19 +0800

    Modified layout

--- login/page.php
+++ login/page.php
@@ -1,12 +1,12 @@
 <?php
 // login/page.php
-require_once(root() . '/modules/calendar/page.php');
+require_once('services.php');
 ?>
 
 <div id="login-panel" class="col-xl-4 col-lg-4 col-md-5 col-sm-12">
   <div class="card o-hidden border-0 shadow-lg my-5">
     <div class="card-header">
-      <h3 class="text-center text-gray-900 my-2"><?php echo $page; ?></h3>
+      <h3 class="text-center my-2"><?php echo $page; ?></h3>
     </div><!-- card-header -->
 
     <div class="card-body text-center">
@@ -18,12 +18,12 @@ require_once(root() . '/modules/calendar/page.php');
 
       <form action="" method="POST" class="text-left">
         <div class="form-group">
-          <label for="email" class="text-gray-900 font-weight-bold mb-1">Email Address</label>
+          <label for="email" class="font-weight-bold mb-1">Email Address</label>
           <input class="form-control" id="email" name="email" type="email" placeholder="juan.delacruz@deped.gov.ph" autofocus required>
         </div><!-- .form-group -->
 
         <div class="form-group">
-          <label for="password" class="text-gray-900 font-weight-bold mb-1">Password</label>
+          <label for="password" class="font-weight-bold mb-1">Password</label>
           <div class="input-group">
             <input class="form-control border-right-0" id="password" name="password" type="password" placeholder="Password" required>
             <div class="input-group-append">
