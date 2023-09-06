19e5d224cd8a5b17c8d55364a7f987c6f6fb4d3c
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-06 16:10:09 +0800

    Added required file

--- login/index.php
+++ login/index.php
@@ -7,6 +7,7 @@ require_once(root() . '/includes/database/activity.php');
 require_once(root() . '/includes/database/account.php');
 require_once(root() . '/includes/database/user.php');
 require_once(root() . '/includes/database/school.php');
+require_once(root() . '/includes/database/station.php');
 require_once(root() . '/includes/database/system-log.php');
 require_once(root() . '/includes/layout/theme-page.php');
 ?>
