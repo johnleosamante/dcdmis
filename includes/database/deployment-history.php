288daa8b6fbaabc80181ea160058d207f9624894
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:47:17 +0800

    Added braces to variables

--- includes/database/deployment-history.php
+++ includes/database/deployment-history.php
@@ -2,6 +2,6 @@
 // includes/database/deployment-history.php
 // tbl_deployment_history
 function createDeployment($date, $station, $position, $years, $step, $subject, $id) {
-  nonQuery("INSERT INTO tbl_deployment_history (`Date_assignment`, `station_assign`, `position_assign`, `No_of_years`, `StepNo`, `SubjectArea`, `Emp_ID`) VALUES ('$date', '$station', '$position', '$years', '$step', '$subject', '$id');");
+  nonQuery("INSERT INTO tbl_deployment_history (`Date_assignment`, `station_assign`, `position_assign`, `No_of_years`, `StepNo`, `SubjectArea`, `Emp_ID`) VALUES ('{$date}', '{$station}', '{$position}', '{$years}', '{$step}', '{$subject}', '{$id}');");
 }
 ?>
