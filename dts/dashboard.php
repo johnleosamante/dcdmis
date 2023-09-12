bc21cc59c890f589a41db72d635a9c38872c31b1
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:29:56 +0800

    Format dts dashboard

--- dts/dashboard.php
+++ dts/dashboard.php
@@ -1,20 +1,15 @@
 <?php
 // dts/dashboard.php
 messageAlert($showAlert, $message, $success);
-
 contentTitleWithModal('Dashboard', uri() . '/modules/documents/save-document-dialog.php', 'New Document', 'fa-plus');
 ?>
 
 <div class="row mt-4">
   <?php
   card('Incoming Documents', customUri('dts', 'Incoming Documents'), 'fa-file-download', 'primary', $countIncoming);
-
   card('Pending Documents', customUri('dts', 'Pending Documents'), 'fa-history', 'success', $countPending);
-
   card('Outgoing Documents', customUri('dts', 'Outgoing Documents'), 'fa-file-upload', 'info', $countOutgoing);
-
   card('Ongoing Documents', customUri('dts', 'Ongoing Documents'), 'fa-tasks', 'warning', $countOngoing);
-
   card('Completed Documents', customUri('dts', 'Completed Documents'), 'fa-check-circle', 'secondary');
 
   if (!$isSchoolPortal) {
