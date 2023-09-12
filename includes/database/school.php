a9698fa9fcb0010f5725f7c6ef5cd63e07415c61
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:47:57 +0800

    Added district and school data function

--- includes/database/school.php
+++ includes/database/school.php
@@ -1,6 +1,8 @@
 <?php
 // includes/database/school.php
 // tbl_school
+// tbl_station
+// tbl_district
 function schools() {
   return query("SELECT SchoolID AS id, SchoolName AS `name`, Abraviate AS alias, `Address` AS `address`, Incharg_ID AS `head`, District_code AS district, School_Category AS category, SchoolLogo AS logo FROM tbl_school ORDER BY SchoolName;");
 }
@@ -44,5 +46,41 @@ function createSchool($id, $name, $alias, $address, $district, $category, $logo)
 
 function updateSchool($id, $name, $alias, $address, $district, $category, $referenceId) {
   nonQuery("UPDATE tbl_school SET `SchoolID`='{$id}', `SchoolName`='{$name}', `Abraviate`='{$alias}', `Address`='{$address}', `District_code`='{$district}', `School_Category`='{$category}' WHERE `SchoolID`='{$referenceId}' LIMIT 1;");
+}
+
+function station($id) {
+  return query("SELECT `No`, Emp_Position AS position_id, Emp_Station AS station_id, Emp_ID AS id FROM tbl_station WHERE Emp_ID='{$id}' ORDER BY Emp_DOA DESC LIMIT 1;");
+}
+
+function createStation($date, $stationId, $positionId, $id) {
+  nonQuery("INSERT INTO tbl_station (`Emp_DOA`, Emp_Station, Emp_Position, Emp_ID) VALUES ('{$date}', '{$stationId}', '{$positionId}', '{$id}');");
+}
+
+function updateStation($date, $stationId, $positionId, $id) {
+  nonQuery("UPDATE tbl_station SET `Emp_DOA`='{$date}', Emp_Position='{$positionId}', Emp_Station='{$stationId}' WHERE Emp_ID='{$id}';");
+}
+
+function updateStationID($newStationId, $oldStationId) {
+  nonQuery("UPDATE tbl_station SET `Emp_Station`='{$newStationId}' WHERE `Emp_Station`='{$oldStationId}';");
+}
+
+function district($id) {
+  return query("SELECT District_code AS `id`, District_Name AS `name`, Emp_ID AS `psds` FROM tbl_district WHERE District_code='{$id}' LIMIT 1;");
+}
+
+function districts() {
+  return query("SELECT District_code AS `id`, District_Name AS `name`, Emp_ID AS `psds` FROM tbl_district ORDER BY District_Name ASC;");
+}
+
+function districtSchoolCount($id) {
+  return query("SELECT SUM(CASE WHEN tbl_school.School_Category='Elementary' THEN 1 ELSE 0 END) AS es, SUM(CASE WHEN tbl_school.School_Category='Secondary' THEN 1 ELSE 0 END) AS hs, SUM(CASE WHEN tbl_school.School_Category='Integrated' THEN 1 ELSE 0 END) AS `is`, COUNT(*) AS `total` FROM tbl_school INNER JOIN tbl_district ON tbl_school.District_code=tbl_district.District_code WHERE tbl_school.District_code='{$id}' LIMIT 1;");
+}
+
+function createDistrict($id, $district, $head) {
+  nonQuery("INSERT INTO tbl_district (`District_code`, `District_Name`, `Emp_ID`) VALUES ('{$id}', '{$district}', '{$head}');");
+}
+
+function updateDistrict($newCode, $district, $head, $oldCode) {
+  nonQuery("UPDATE tbl_district SET `District_code`='{$newCode}', `District_Name`='{$district}', `Emp_ID`='{$head}' WHERE `District_code`='{$oldCode}' LIMIT 1;");
 }
 ?>
