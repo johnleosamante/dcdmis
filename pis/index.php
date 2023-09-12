9ceae6e89a7e9f01a85ee430b18d13a47a52b3ed
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 14:28:08 +0800

    Added required file

--- pis/index.php
+++ pis/index.php
@@ -28,6 +28,7 @@ require_once(root() . '/includes/database/references.php');
 require_once(root() . '/includes/database/utility.php');
 require_once(root() . '/includes/database/card-type.php');
 require_once(root() . '/includes/database/service-record.php');
+require_once(root() . '/includes/database/201-file.php');
 require_once(root() . '/includes/database/system-log.php');
 require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/layout/theme-dashboard.php');
