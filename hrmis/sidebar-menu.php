25ffd4db0b6aaaf4346b695010c053c21ce2eb23
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:44:02 +0800

    Fixed reference

--- hrmis/sidebar-menu.php
+++ hrmis/sidebar-menu.php
@@ -1,7 +1,6 @@
 <?php
 // hrmis/sidebar-menu.php
 sidebarDivider();
-
 sidebarHeading('Employees');
 
 $countActive = number_format(numRows(activeEmployees()));
@@ -10,21 +9,13 @@ $districtCount = number_format(numRows(districts()));
 $schoolCount = number_format(numRows(schools()));
 $sectionCount = number_format(numRows(sections()));
 
-sidebarModalItem(uri() . '/modules/employees/new-employee-dialog.php', 'New Employee', 'fa-plus');
-
+sidebarModalItem(uri() . '/modules/employees/save-employee-dialog.php', 'Add Employee', 'fa-plus');
 sidebarMenuItem(customUri('hrmis', 'Active Employees'), 'Active', 'fa-user-check', isset($url) && str_contains($url, 'Active'), $countActive);
-
 sidebarMenuItem(customUri('hrmis', 'Retirable Employees'), 'Retirable','fa-user-clock',isset($url) && str_contains($url, 'Retirable'), $countRetirable);
-
 sidebarMenuItem(customUri('hrmis', 'Celebrant Employees'), 'Celebrants', 'fa-birthday-cake', isset($url) && str_contains($url, 'Celebrant'));
-
 sidebarMenuItem(customUri('hrmis', 'Archived Employees'), 'Archived', 'fa-archive', isset($url) && str_contains($url, 'Archived'));
-
 sidebarDivider();
-
 sidebarMenuItem(customUri('hrmis', 'Districts'), 'Districts', 'fa-map-marked-alt', isset($url) && str_contains($url, 'District'), $districtCount);
-
 sidebarMenuItem(customUri('hrmis', 'Schools'), 'Schools', 'fa-school', isset($url) && str_contains($url, 'School'), $schoolCount);
-
 sidebarMenuItem(customUri('hrmis', 'Sections'), 'Sections', 'fa-map-signs', isset($url) && str_contains($url, 'Section'), $sectionCount);
 ?>
