bb5a7a7d12ff97b43795a04825187c959e28d847
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 11:04:53 +0800

    Updated dashboard links

--- pis/dashboard.php
+++ pis/dashboard.php
@@ -9,20 +9,20 @@ contentTitle('Dashboard');
   <?php
   card('Personal Data Sheet', customUri('pis', 'Personal Data Sheet', $userId), 'fa-user', 'primary');
 
-  card('Service Record', customUri('pis', 'Service Record'), 'fa-file-alt', 'success');
+  card('Service Record', customUri('pis', 'Service Record', $userId), 'fa-file-alt', 'success');
 
-  card('201 File', customUri('pis', '201 File'), 'fa-book-open', 'info');
+  card('201 File', customUri('pis', '201 File', $userId), 'fa-folder-open', 'info');
 
-  card('IPCRF', customUri('pis', 'IPCRF'), 'fa-tasks', 'warning');
+  card('IPCRF', customUri('pis', 'IPCRF', $userId), 'fa-tasks', 'warning');
 
-  card('Certificates', customUri('pis', 'Certificates'), 'fa-certificate', 'danger');
+  card('Certificates', customUri('pis', 'Certificates', $userId), 'fa-certificate', 'danger');
 
-  card('Trainings', customUri('pis', 'Trainings'), 'fa-chalkboard-teacher', 'secondary');
+  card('Trainings', customUri('pis', 'Trainings', $userId), 'fa-chalkboard-teacher', 'secondary');
 
-  card('Payslip', customUri('pis', 'Payslip'), 'fa-money-check', 'primary');
+  card('Payslip', customUri('pis', 'Payslip', $userId), 'fa-money-check', 'primary');
 
-  card('DepEd Account Request', customUri('pis', 'DepEd Account Request'), 'fa-edit', 'success');
+  card('DepEd Account Request', customUri('pis', 'DepEd Account Request', $userId), 'fa-edit', 'success');
 
-  card('Transfer Request', customUri('pis', 'Transfer Request'), 'fa-map', 'info');
+  card('Transfer Request', customUri('pis', 'Transfer Request', $userId), 'fa-map', 'info');
   ?>
 </div><!-- .row -->
