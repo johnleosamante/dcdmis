14d88b53ef2a4d0fa3665dd0e3bc70c6f0d8f17f
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:45:04 +0800

    Format hrtdms sidebar-menu

--- hrtdms/sidebar-menu.php
+++ hrtdms/sidebar-menu.php
@@ -1,14 +1,11 @@
 <?php
 // hrtdms/sidebar-menu.php
 sidebarDivider();
-
 sidebarHeading('Trainings');
 
 $countScheduled = number_format(numRows(scheduledTrainings()));
 
 sidebarModalItem(uri() . '/modules/trainings/save-training-dialog.php', 'New Training', 'fa-plus');
-
 sidebarMenuItem(customUri('hrtdms', 'Scheduled Trainings'), 'Scheduled', 'fa-calendar-alt', isset($url) && str_contains($url, 'Scheduled'), $countScheduled);
-
 sidebarMenuItem(customUri('hrtdms', 'Conducted Trainings'), 'Conducted', 'fa-chalkboard-teacher', isset($url) && str_contains($url, 'Conducted'));
 ?>
