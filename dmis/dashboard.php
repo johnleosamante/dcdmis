d01e633b088a384f7f819fc723e068a5b7cab440
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:27:50 +0800

    Format dmis dashboard

--- dmis/dashboard.php
+++ dmis/dashboard.php
@@ -1,19 +1,15 @@
 <?php
+// dmis/dashboard.php
 messageAlert($showAlert, $message, $success);
-
 contentTitle('Dashboard');
 ?>
 
 <div class="row mt-4">
   <?php
   card('Districts', customUri('dmis', 'Districts'), 'fa-map-marked-alt', 'primary', $districtCount);
-
   card('Schools', customUri('dmis', 'Schools'), 'fa-school', 'success', $schoolCount);
-
   card('Sections', customUri('dmis', 'Sections'), 'fa-map-signs', 'info', $sectionCount);
-
   card('Users', customUri('dmis', 'Users'), 'fa-user-friends', 'warning', $userCount);
-
   card('Employees', customUri('dmis', 'Active Employees'), 'fa-users', 'danger', $employeeCount);
   ?>
 </div>
