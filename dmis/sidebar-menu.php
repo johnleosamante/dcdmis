51053f6a50dc68edfcb9f64089266c32481d3aee
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-12 16:29:15 +0800

    Format dmis sidebar-menu

--- dmis/sidebar-menu.php
+++ dmis/sidebar-menu.php
@@ -9,12 +9,8 @@ $userCount = number_format(numRows(users()));
 $employeeCount = number_format(numRows(activeEmployees()));
 
 sidebarMenuItem(customUri('dmis', 'Districts'), 'Districts', 'fa-map-marked-alt', isset($url) && str_contains($url, 'District'), $districtCount);
-
 sidebarMenuItem(customUri('dmis', 'Schools'), 'Schools', 'fa-school', isset($url) && str_contains($url, 'School'), $schoolCount);
-
 sidebarMenuItem(customUri('dmis', 'Sections'), 'Sections', 'fa-map-signs', isset($url) && str_contains($url, 'Section'), $sectionCount);
-
 sidebarMenuItem(customUri('dmis', 'Users'), 'Users', 'fa-user-friends', isset($url) && str_contains($url, 'Users'), $userCount);
-
 sidebarMenuItem(customUri('dmis', 'Active Employees'), 'Employees', 'fa-users', isset($url) && str_contains($url, 'Employees'), $employeeCount);
 ?>
