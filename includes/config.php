bca18f75dbc323f53dbc5c2159c76b493d1fb119
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 10:21:35 +0800

    Modified constants

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,11 @@
 <?php
 // include/config.php
-// All constant variables for database connection details are found here.
-
 define('HOSTNAME', 'localhost');
 define('USER', 'chickenstalker');
 define('PASSWORD', '82%a*f25RPG6');
 define('DATABASE', 'dcdmis');
-define('PORT', 3306);
 define('DOMAIN', 'depeddipolog.net');
 define('PUBLIC_IP', '120.28.189.147');
-
 define('SITE_TITLE', 'DepEd Dipolog City Schools Division');
 define('SITE_ALIAS', 'sdodipolog');
 define('SITE_DESCRIPTION', 'Department of Education Dipolog City Schools Division Data Management Information System');
