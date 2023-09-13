2545a1220c162035e1c6f89ed8e62d15b594fb2b
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 15:18:21 +0800

    Modified print index

--- print/index.php
+++ print/index.php
@@ -3,63 +3,40 @@
 require_once('../includes/function.php');
 require_once(root() . '/includes/string.php');
 
-if (!isset($_SESSION[alias() . '_userId']) || !isset($_SESSION[alias() . '_portal'])) {
+if (!isset($_SESSION[alias() . '_userId'])) {
   redirect(uri() . '/login');
 }
 
 $page = $url = sanitize(decode($_GET['v']));
-
-if (!isset($url) || $url === '') {
-  redirect(customUri($activeApp, '404'));
-}
-
-$code = strtoupper(sanitize(decode($_GET['id'])));
 $file = $title = '';
-$logoSize = 24;
-$margin = 25.4;
-$width = 210;
-$height = 297;
-$lineY = 60;
 
 switch ($url) {
   case 'Document Tracking Slip':
-    $title = $url . ' : ' . $code;
+    if (!isset($_SESSION[alias() . '_portal'])) {
+      redirect(uri() . '/login');
+    }
     $file = 'document-tracking-slip';
+    break;
+  case 'Certificate of Participation':
+    if (!isset($_GET['id']) || !isset($_GET['p'])) {
+      redirect(uri() . '/login');
+    }
+    $file = 'certificate-of-participation';
     break;
   default:
     redirect(customUri('dts', '404'));
     break;
 }
 
+$code = strtoupper(sanitize(decode($_GET['id'])));
+$title = $url . ' : ' . $code;
+
 require_once(root() . '/includes/database/database.php');
 require_once(root() . '/includes/database/section.php');
 require_once(root() . '/includes/database/school.php');
-require_once(root() . '/includes/database/district.php');
 require_once(root() . '/includes/database/utility.php');
 require_once(root() . '/includes/plugin/fpdf/fpdf.php');
 require_once(root() . '/includes/plugin/phpqrcode/qrlib.php');
-
-$section = strtoupper(stationName($station));
-$school = fetchArray(schoolDetailsById($stationId));
-$district = fetchAssoc(district($school['district']))['name'];
-$stationLogo = root() . '/' . $school['logo'];
-$address = $school['address'];
-$telephone = $school['telephone'];
-$email = $school['email'];
-$website = $school['website'];
-$fbPage = $school['fb_page'];
-
-require_once(root() . '/print/print-layout.php');
-
-$pdf = new PDF('P', 'mm', array($width, $height));
-$pdf->SetTitle($title);
-$pdf->AliasNbPages();
-$pdf->SetMargins($margin, 11 + $logoSize, $margin);
-$pdf->SetAutoPageBreak(true, 35);
-$pdf->AddPage();
-$pdf->AddFont('calibri', '', 'calibri.php');
-$pdf->AddFont('calibrib', 'B', 'calibrib.php');
-
 require_once(root() . "/print/{$file}.php");
 
 $pdf->Output("{$title}.pdf", "I");
