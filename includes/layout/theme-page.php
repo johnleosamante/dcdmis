85181fd9a011a70424e9917727426882d5c7e581
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 10:40:39 +0800

    Format theme-page

--- includes/layout/theme-page.php
+++ includes/layout/theme-page.php
@@ -6,29 +6,27 @@ require_once(root() . '/includes/layout/components.php');
 
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <?php require_once(root() . '/includes/layout/header.php'); ?>
+  </head>
 
-<head>
-  <?php require_once(root() . '/includes/layout/header.php'); ?>
-</head>
-
-<body id="page-top" class="background-cover">
-  <div id="layout">
-    <div id="layout-content" class="container-xl">
-      <div id="main-content" class="row justify-content-center">
-        <?php require_once('page.php'); ?>
+  <body id="page-top" class="background-cover">
+    <div id="layout">
+      <div id="layout-content" class="container-xl">
+        <div id="main-content" class="row justify-content-center">
+          <?php require_once('page.php'); ?>
+        </div>
       </div>
-    </div><!-- .row -->
-
-    <div id="layout-footer">
-      <?php require_once(root() . '/includes/layout/footer.php'); ?>
+      
+      <div id="layout-footer">
+        <?php require_once(root() . '/includes/layout/footer.php'); ?>
+      </div>
     </div>
-  </div>
 
-  <?php scrollToTop(); ?>
+    <?php scrollToTop(); ?>
 
-  <script src="<?php echo uri(); ?>/assets/vendor/jquery/jquery.min.js"></script>
-  <script src="<?php echo uri(); ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="<?php echo uri(); ?>/assets/js/script.js"></script>
-</body>
-
+    <script src="<?php echo uri(); ?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo uri(); ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo uri(); ?>/assets/js/script.js"></script>
+  </body>
 </html>
