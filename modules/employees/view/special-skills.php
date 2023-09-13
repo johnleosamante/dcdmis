319f107603d4af3047a60160fe6423f0712e8130
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 10:56:49 +0800

    Fixed file reference

--- modules/employees/view/special-skills.php
+++ modules/employees/view/special-skills.php
@@ -5,7 +5,7 @@
 <div class="tab-pane fade<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'special-skills', 'show active'); ?>" id="special-skills">
   <?php if ($editMode) : ?>
     <div class="d-sm-flex justify-content-end my-3">
-      <?php modalButtonSplit(uri() . '/modules/employees/update/update-special-skill.php?e=' . cipher($employeeId), 'Add', 'fa-plus', 'Add Special Skill / Hobby', 'primary'); ?>
+      <?php modalButtonSplit(uri() . '/modules/employees/save/save-special-skill.php?e=' . cipher($employeeId), 'Add', 'fa-plus', 'Add Special Skill / Hobby', 'primary'); ?>
     </div>
   <?php endif; ?>
 
@@ -33,7 +33,7 @@
                     <div class="dropdown no-arrow">
                       <?php dropdownEllipsis(); ?>
                       <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
-                        <?php modalDropdownItem(uri() . '/modules/employees/update/update-special-skill.php?e=' . cipher($employeeId) . '&id=' . cipher($skill['no']), 'Edit', 'fa-edit', 'Edit Special Skill / Hobby'); ?>
+                        <?php modalDropdownItem(uri() . '/modules/employees/save/save-special-skill.php?e=' . cipher($employeeId) . '&id=' . cipher($skill['no']), 'Edit', 'fa-edit', 'Edit Special Skill / Hobby'); ?>
                         <div class="dropdown-divider"></div>
                         <?php modalDropdownItem(uri() . '/modules/employees/delete/delete-special-skill.php?e=' . cipher($employeeId) . '&id=' . cipher($skill['no']), 'Delete', 'fa-trash', 'Delete Special Skill / Hobby'); ?>
                       </div>
@@ -50,5 +50,5 @@
         </tbody>
       </table>
     </div>
-  </div><!-- .row -->
-</div><!-- .tab-pane -->
+  </div>
+</div>
