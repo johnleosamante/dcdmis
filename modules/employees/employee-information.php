5e6d23ff3484326aa6e4909b5a4fb0ca91bf2c92
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 10:51:32 +0800

    Code clean up

--- modules/employees/employee-information.php
+++ modules/employees/employee-information.php
@@ -45,53 +45,52 @@ $employeePhoto = '';
     <ul class="nav nav-tabs mb-3">
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(!isset($activeTab) || $activeTab === 'personal-information'); ?>" href="#personal-information" data-toggle="tab">Personal Information</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'family-background'); ?>" href="#family-background" data-toggle="tab">Family Background</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'children'); ?>" href="#children" data-toggle="tab">Children</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'educational-background'); ?>" href="#educational-background" data-toggle="tab">Educational Background</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'civil-service-eligibility'); ?>" href="#civil-service-eligibility" data-toggle="tab">Civil Service Eligibility</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'work-experience'); ?>" href="#work-experience" data-toggle="tab">Work Experience</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'voluntary-work'); ?>" href="#voluntary-work" data-toggle="tab">Voluntary Work</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'learning-development'); ?>" href="#learning-development" data-toggle="tab">Learning &amp; Development</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'special-skills'); ?>" href="#special-skills" data-toggle="tab">Special Skills &amp; Hobbies</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'recognition'); ?>" href="#recognition" data-toggle="tab">Non-Academic Distinctions / Recognition</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'membership'); ?>" href="#membership" data-toggle="tab">Membership in Association / Organization</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'other-information'); ?>" href="#other-information" data-toggle="tab">Other Information</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'reference'); ?>" href="#reference" data-toggle="tab">References</a>
-      </li><!-- .nav-item -->
+      </li>
       <li class="nav-item">
         <a class="nav-link text-secondary<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'government-id'); ?>" href="#government-id" data-toggle="tab">Government Issued ID</a>
-      </li><!-- .nav-item -->
-    </ul><!-- .nav-tabs -->
+      </li>
+    </ul>
 
     <div class="tab-content mt-2">
       <?php
       require_once(root() . '/modules/employees/view/personal-information.php');
       require_once(root() . '/modules/employees/view/family-background.php');
-      require_once(root() . '/modules/employees/view/children.php');
       require_once(root() . '/modules/employees/view/educational-background.php');
       require_once(root() . '/modules/employees/view/civil-service-eligibility.php');
       require_once(root() . '/modules/employees/view/work-experience.php');
@@ -104,6 +103,6 @@ $employeePhoto = '';
       require_once(root() . '/modules/employees/view/reference.php');
       require_once(root() . '/modules/employees/view/government-id.php');
       ?>
-    </div><!-- .tab-content -->
-  </div><!-- .card-body -->
-</div><!-- .card -->
+    </div>
+  </div>
+</div>
