37bc6fcea98f548d80afff4517af3ed53b9ade23
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 10:26:13 +0800

    Added variable braces

--- includes/database/psipop.php
+++ includes/database/psipop.php
@@ -2,6 +2,6 @@
 // includes/database/psipop.php
 // psipop
 function createPsipop($itemNo, $step, $status, $datePromoted, $eligibility, $id) {
-  nonQuery("INSERT INTO psipop (`Item_Number`, `Step`, `Job_status`, `Date_promoted`, `Elegibility`, `Emp_ID`) VALUES ('$itemNo', '$step', '$status', '$datePromoted', '$eligibility', '$id');");
+  nonQuery("INSERT INTO psipop (`Item_Number`, `Step`, `Job_status`, `Date_promoted`, `Elegibility`, `Emp_ID`) VALUES ('{$itemNo}', '{$step}', '{$status}', '{$datePromoted}', '{$eligibility}', '{$id}');");
 }
 ?>
