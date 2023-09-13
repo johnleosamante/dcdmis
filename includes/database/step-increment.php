42073ee07b367e436f3fed9eafedc13ea7fa801a
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-13 10:26:25 +0800

    Added variable braces

--- includes/database/step-increment.php
+++ includes/database/step-increment.php
@@ -2,6 +2,6 @@
 // includes/database/step-increment.php
 // tbl_step_increment
 function createStepIncrement($dateLastStep, $stepNo, $years, $id) {
-  nonQuery("INSERT INTO tbl_step_increment (`Date_last_step`, `Step_No`, `No_of_year`, `Emp_ID`) VALUES ('$dateLastStep', '$stepNo', '$years', '$id');");
+  nonQuery("INSERT INTO tbl_step_increment (`Date_last_step`, `Step_No`, `No_of_year`, `Emp_ID`) VALUES ('{$dateLastStep}', '{$stepNo}', '{$years}', '{$id}');");
 }
 ?>
