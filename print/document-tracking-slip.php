ba3d33f78dc848da455e74ddf6806002c465f54f
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-14 17:02:56 +0800

    Added encoding handler

--- print/document-tracking-slip.php
+++ print/document-tracking-slip.php
@@ -33,9 +33,9 @@ require_once(root() . '/includes/database/position.php');
 
 $document = fetchAssoc(documentOrigin($code));
 $dateCreated = toDate($document['datetime'], 'F d, Y');
-$description = $document['description'];
-$employee = strtoupper(userName($document['user']));
-$employeePosition = fetchAssoc(position($document['user']))['position'];
+$description = toHandleEncoding($document['description']);
+$employee = toHandleEncoding(userName($document['user'], true));
+$employeePosition = toHandleEncoding(fetchAssoc(position($document['user']))['position']);
 $documentStatus = strtolower($document['status']);
 $status = str_contains($documentStatus, 'complete') ? ' (Completed)' : '';
 $status = str_contains($documentStatus, 'cancel') ? ' (Canceled)' : $status;
@@ -49,10 +49,10 @@ $pdf->SetFont('calibri',  '', 11);
 $pdf->Cell(0, 0, $dateCreated, 0, 0, 'R');
 $pdf->Ln(10);
 $pdf->SetFont('calibrib',  'B', 17);
-$pdf->Cell(0, 0, $code . $status);
+$pdf->Cell(0, 0, $code . toHandleEncoding($status));
 $pdf->Ln(10);
 $pdf->SetFont('calibri',  '', 11);
-$pdf->Write(5, html_entity_decode($description, ENT_QUOTES));
+$pdf->Write(5, $description);
 $pdf->Ln(20);
 $innerPage = $width - ($margin * 2);
 $pdf->Cell($innerPage / 2, 0, 'Prepared by:');
@@ -64,8 +64,8 @@ $pdf->SetFont('calibri', '', 11);
 $pdf->Cell($innerPage / 2, 0, $employeePosition, 0, 0, 'C');
 
 if ($document['user'] !== $document['head']) {
-  $stationHead = userName($document['head'], true);
-  $stationHeadPosition = fetchAssoc(position($document['head']))['position'];
+  $stationHead = toHandleEncoding(userName($document['head'], true));
+  $stationHeadPosition = toHandleEncoding(fetchAssoc(position($document['head']))['position']);
   $pdf->Ln(10);
   $pdf->SetX($width / 2);
   $pdf->Cell($innerPage / 2, 0, 'Noted by:');
