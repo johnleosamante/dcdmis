42658fae690f8003c0701fdf963a110557c680cf
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-19 13:04:59 +0800

    Added restore employee dialog

---
+++ modules/employees/restore-employee-dialog.php
@@ -0,0 +1,56 @@
+<?php
+// modules/employees/restore-employee-dialog.php
+require_once('../../includes/function.php');
+require_once(root() . '/includes/database/database.php');
+require_once(root() . '/includes/database/employee.php');
+require_once(root() . '/includes/database/position.php');
+require_once(root() . '/includes/database/school.php');
+require_once(root() . '/includes/layout/components.php');
+require_once(root() . '/includes/string.php');
+
+$employeeId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
+$employees = employee($employeeId);
+$modalTitle = 'Employee not found';
+$hasEmployee = false;
+
+if (numRows($employees) > 0) {
+  $employee = fetchAssoc($employees);
+  $employeeId = $employee['id'];
+  $employeeName = toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true);
+  $sex = $employee['sex'];
+  $positions = fetchAssoc(position($employeeId));
+  $stationId = $positions['station_id'];
+  $station = $positions['station'];
+  $positionId = $positions['position_id'];
+  $position = $positions['position'];
+  $depedEmail = $employee['email'];
+  $picture = uri() . '/' . $employee['picture'];
+  $modalTitle = 'Restore Employee';
+  $hasEmployee = true;
+}
+?>
+
+<div class="modal-dialog <?php echo !$hasEmployee ? 'modal-sm' : ''; ?>">
+  <div class="modal-content">
+    <?php modalHeader($modalTitle); ?>
+
+    <form action="" method="POST">
+      <div class="modal-body">
+        <?php if ($hasEmployee) { 
+          employeeProfile($picture, $employeeName, $sex, $depedEmail, $position, $station);
+        } else {
+          missingAlert($modalTitle);
+        } ?>
+      </div>
+
+      <div class="modal-footer">
+        <?php if ($hasEmployee) : ?>
+          <input type="hidden" name="verifier" value="<?php echo $_GET['id']; ?>">
+          <button class="btn btn-danger" name="restore-employee" type="submit">Continue</button>
+        <?php endif; 
+        
+        cancelModalButton(); ?>
+      </div>
+    </form>
+  </div>
+</div>
