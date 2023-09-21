870465c1f1e8a862cff08d3e7ec80d12c54998d7
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-21 11:31:10 +0800

    Merged LD and trainings

--- modules/employees/view/learning-development.php
+++ modules/employees/view/learning-development.php
@@ -3,12 +3,6 @@
 ?>
 
 <div class="tab-pane fade<?php echo setActiveNavigation(isset($activeTab) && $activeTab === 'learning-development', 'show active'); ?>" id="learning-development">
-  <?php if ($editMode) : ?>
-    <div class="d-sm-flex justify-content-end my-3">
-      <?php modalButtonSplit(uri() .'/modules/employees/save/save-learning-development.php?e=' . cipher($employeeId), 'Add', 'fa-plus', 'Add Learning &amp; Development', 'primary'); ?>
-    </div>
-  <?php endif; ?>
-
   <div class="row my-3">
     <div class="col table-responsive">
       <table width="100%" class="table table-striped table-bordered table-hover mb-0 text-center">
@@ -19,9 +13,6 @@
             <th class="align-middle" width="5%" rowspan="2">Number of Hours</th>
             <th class="align-middle" width="10%" rowspan="2">Type of Learning &amp; Development</th>
             <th class="align-middle" width="30%" rowspan="2">Conducted / Sponsored by</th>
-            <?php if ($editMode) : ?>
-              <th class="align-middle" width="5%" rowspan="2">Action</th>
-            <?php endif; ?>
           </tr>
           <tr>
             <th class="align-middle" width="5%">From</th>
@@ -41,23 +32,11 @@
                 <td class="align-middle"><?php echo $learning['hours']; ?></td>
                 <td class="align-middle"><?php echo $learning['type']; ?></td>
                 <td class="align-middle"><?php echo $learning['sponsor']; ?></td>
-                <?php if ($editMode) : ?>
-                  <td class="align-middle text-capitalize">
-                    <div class="dropdown no-arrow">
-                      <?php dropdownEllipsis(); ?>
-                      <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
-                        <?php modalDropdownItem(uri() .'/modules/employees/save/save-learning-development.php?e=' . cipher($employeeId) . '&id=' . cipher($learning['no']), 'Edit', 'fa-edit', 'Edit Learning &amp; Development'); ?>
-                        <div class="dropdown-divider"></div>
-                        <?php modalDropdownItem(uri() .'/modules/employees/delete/delete-learning-development.php?e=' . cipher($employeeId) . '&id=' . cipher($learning['no']), 'Delete', 'fa-trash', 'Delete Learning &amp; Development'); ?>
-                      </div>
-                    </div>
-                  </td>
-                <?php endif; ?>
               </tr>
             <?php endwhile;
           } else { ?>
             <tr>
-              <td colspan="<?php echo $editMode ? '7' : '6'; ?>" class="align-middle">No data available in table</td>
+              <td colspan="6" class="align-middle">No data available in table</td>
             </tr>
           <?php } ?>
         </tbody>
