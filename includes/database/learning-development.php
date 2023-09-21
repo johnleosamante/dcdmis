6b16151f14ff17116f9edd5ab15a5feda0a67e9a
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-21 11:46:08 +0800

    Removed unused LD data function

--- includes/database/learning-development.php
+++ includes/database/learning-development.php
@@ -11,18 +11,6 @@ function learningAndDevelopment($id, $no) {
   return query("SELECT `No` AS `no`, Title_of_Training AS `title`, `From` AS `from`, `To` AS `to`, `Number_of_Hours` AS `hours`, `Managerial` AS `type`, `Conducted` AS `sponsor`, Emp_ID AS `id` FROM learning_and_development WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
 }
 
-function createlearningAndDevelopment($title, $from, $to, $hours, $type, $sponsor, $id) {
-  nonQuery("INSERT INTO learning_and_development (`Title_of_Training`, `From`, `To`, `Number_of_Hours`, `Managerial`, `Conducted`, Emp_ID) VALUES ('{$title}', '{$from}', '{$to}', '{$hours}', '{$type}', '{$sponsor}', '{$id}');");
-}
-
-function updateLearningAndDevelopment($title, $from, $to, $hours, $type, $sponsor, $id, $no) {
-  nonQuery("UPDATE learning_and_development SET `Title_of_Training`='{$title}', `From`='{$from}', `To`='{$to}', `Number_of_Hours`='{$hours}', `Managerial`='{$type}', `Conducted`='{$sponsor}' WHERE `Emp_ID`='{$id}' AND `No`='{$no}' LIMIT 1;");
-}
-
-function deleteLearningAndDevelopment($id, $no) {
-  nonQuery("DELETE FROM learning_and_development WHERE Emp_ID='{$id}' AND `No`='{$no}' LIMIT 1;");
-}
-
 function trainings() {
   return query("SELECT `Training_Code` AS `no`, Title_of_Training AS `title`, `covered_from` AS `from`, `covered_to` AS `to`, `hours`, `Category` AS `type`, `conducted_by` AS `sponsor`, `TVenue` AS `venue` FROM tbl_seminar ORDER BY `From` DESC, `To` DESC;");
 }
