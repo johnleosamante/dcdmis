01c72728c47b6bbd9fcb049137c72583bc566c96
johnleosamante <johnleomuitsamante@gmail.com>
2023-09-28 08:49:40 +0800

    Removed update-identification from settings

--- modules/settings/app.php
+++ modules/settings/app.php
@@ -65,29 +65,6 @@ if (isset($_POST['update-contact-details'])) {
   }
 }
 
-if (isset($_POST['update-identification'])) {
-  $card = sanitize($_POST['card-type']);
-  $number = sanitize($_POST['card-number']);
-  $place = sanitize($_POST['card-place']);
-  $date = sanitize($_POST['card-date']);
-
-  if (numRows(employeeIdentification($userId)) === 0) {
-    createIdentification($card, $number, $place, $date, $userId);
-  } else {
-    updateIdentification($card, $number, $place, $date, $userId);
-  }
-
-  $showAlert = true;
-
-  if (affectedRows()) {
-    $message = 'Your identification details have been updated successfully.';
-    createSystemLog($stationId, $userId, 'Updated identification details', $userId, clientIp());
-  } else {
-    $message = 'No changes have been made to your identification details.';
-    $success = false;
-  }
-}
-
 if (isset($_POST['update-professional-titles'])) {
   $before = sanitize($_POST['before-title']);
   $after = sanitize($_POST['after-title']);
