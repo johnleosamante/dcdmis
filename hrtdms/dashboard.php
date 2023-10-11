e9e50e106a6d7e029b8419e443a07ce4026facea
johnleosamante <johnleomuitsamante@gmail.com>
2023-10-11 16:33:09 +0800

    Added menus

--- hrtdms/dashboard.php
+++ hrtdms/dashboard.php
@@ -8,5 +8,8 @@ contentTitleWithModal('Dashboard', uri() . '/modules/trainings/save-training-dia
   <?php
   card('Scheduled Trainings', customUri('hrtdms', 'Scheduled Trainings'), 'fa-calendar-alt', 'primary', $countScheduled);
   card('Conducted Trainings', customUri('hrtdms', 'Conducted Trainings'), 'fa-chalkboard-teacher', 'success');
+  card('Districts', customUri('hrtdms', 'Districts'), 'fa-map-marked-alt', 'info', $districtCount);
+  card('Schools', customUri('hrtdms', 'Schools'), 'fa-school', 'warning', $schoolCount);
+  card('Sections', customUri('hrtdms', 'Sections'), 'fa-map-signs', 'danger', $sectionCount);
   ?>
 </div>
