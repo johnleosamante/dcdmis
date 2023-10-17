0372b283690f00c7c55c3b4bd8fd2deb3105ce73
johnleosamante <johnleomuitsamante@gmail.com>
2023-10-17 15:04:39 +0800

    Removed unused variables

--- includes/database/deployment-history.php
+++ includes/database/deployment-history.php
@@ -1,7 +1,7 @@
 <?php
 // includes/database/deployment-history.php
 // tbl_deployment_history
-function createDeployment($date, $station, $position, $years, $step, $subject, $id) {
-  nonQuery("INSERT INTO tbl_deployment_history (`Date_assignment`, `station_assign`, `position_assign`, `No_of_years`, `StepNo`, `SubjectArea`, `Emp_ID`) VALUES ('{$date}', '{$station}', '{$position}', '{$years}', '{$step}', '{$subject}', '{$id}');");
+function createDeployment($date, $station, $position, $id) {
+  nonQuery("INSERT INTO tbl_deployment_history (`Date_assignment`, `station_assign`, `position_assign`, `Emp_ID`) VALUES ('{$date}', '{$station}', '{$position}', '{$id}');");
 }
 ?>
