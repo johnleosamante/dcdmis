545acd64f575e7b7132f3e548c5c667fee0ecca0
johnleosamante <johnleomuitsamante@gmail.com>
2023-10-17 10:50:47 +0800

    Created step increment page

---
+++ modules/step-increment/page.php
@@ -0,0 +1,100 @@
+<?php
+// modules/step-increment/page.php
+if (!$isHrmis) {
+  require_once(root() . '/modules/error/403.php');
+  return;
+}
+
+messageAlert($showAlert, $message, $success);
+?>
+
+<div class="card border-left-primary shadow mb-4">
+  <div class="card-header py-3">
+    <?php contentTitleWithLink('Employees Eligible for Step Increment', uri() . '/hrmis'); ?>
+  </div>
+
+  <div class="card-body">
+    <div class="d-sm-flex align-items-center flex-row-reverse mb-2">
+      <div class="d-inline-block">
+        <?php linkButtonSplit(customUri('export', 'step-increment'), 'Export', 'fa-file-excel', 'Export as Excel file', 'success'); ?>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <table class="table table-hover mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th class="align-middle" width="5%">Photo</th>
+            <th class="align-middle" width="20%">Name</th>
+            <th class="align-middle" width="20%">Position</th>
+            <th class="align-middle" width="25%">Station</th>
+            <th class="align-middle" width="5%">From</th>
+            <th class="align-middle" width="5%">To</th>
+            <th class="align-middle" width="15%">Effectivity</th>
+            <th class="align-middle" width="5%">Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php
+          $query = stepIncrement();
+          while ($row = fetchArray($query)) :
+            $employeeName = toName($row['lname'], $row['fname'], $row['mname'], $row['ext']);
+            $photo = uri() . '/' . $row['picture'];
+            $sg = $row['sg'];
+            $step = is_int($row['step']) ? $row['step'] : 1;
+            $nextStep = $step + 1;
+            $datePromoted = strtotime($row['date_promoted']) ? strtotime($row['date_promoted']) : date('Y-m-d');
+          ?>
+            <tr class="text-uppercase">
+              <td class="align-middle">
+                <div class="image-container">
+                  <span class="d-flex justify-content-center align-middle employee-photo rounded-circle overflow-hidden">
+                    <img height="100%" src="<?php echo $photo; ?>" alt="<?php echo $employeeName; ?>">
+                  </span>
+                  <div class="sex-sign"><?php sex($row['sex']); ?></div>
+                </div>
+              </td>
+              <td class="align-middle text-left">
+                <?php linkItem(customUri('hrmis', 'Employee Information', $row['id']), $employeeName); ?>
+              </td>
+              <td class="align-middle"><?php echo fetchAssoc(positions($row['position']))['position']; ?></td>
+              <td class="align-middle">
+                <?php linkItem(customUri($activeApp, 'School Information', $row['station']), fetchAssoc(schoolById($row['station']))['name']); ?>
+              </td>
+              <td class="align-middle"><?php echo "{$sg}-{$step}"; ?></td>
+              <td class="align-middle"><?php echo "{$sg}-{$nextStep}"; ?></td>
+              <td class="align-middle"><?php echo date('F j, Y', strtotime('+3 year', $datePromoted)); ?></td>
+              <td class="align-middle text-capitalize">
+                <div class="dropdown no-arrow">
+                  <?php dropdownEllipsis(); ?>
+                  <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
+                    <?php modalDropdownItem(uri() . '/modules/step-increment/approve-step-increment-dialog.php?id=' . cipher($row['id']), 'Approve', 'fa-thumbs-up', 'Approve Employee Step Increment'); ?>
+                    <div class="dropdown-divider"></div>
+                    <?php 
+                    modalDropdownItem(uri() . '/modules/employees/reassign-employee-dialog.php?id=' . cipher($row['id']), 'Reassign', 'fa-share', 'Reassign Employee');
+                    modalDropdownItem(uri() . '/modules/employees/remove-employee-dialog.php?id=' . cipher($row['id']), 'Remove', 'fa-trash', 'Remove Employee');
+                    ?>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          <?php endwhile; ?>
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <th class="align-middle" width="5%">Photo</th>
+            <th class="align-middle" width="20%">Name</th>
+            <th class="align-middle" width="20%">Position</th>
+            <th class="align-middle" width="25%">Station</th>
+            <th class="align-middle" width="5%">From</th>
+            <th class="align-middle" width="5%">To</th>
+            <th class="align-middle" width="15%">Effectivity</th>
+            <th class="align-middle" width="5%">Action</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </div>
+</div>
