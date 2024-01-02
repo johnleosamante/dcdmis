2094d72c12f5409c9399dfc25c4b05b36d213478
johnleosamante <johnleomuitsamante@gmail.com>
2024-01-02 10:51:54 +0800

    Modified indentation

--- modules/calendar/page.php
+++ modules/calendar/page.php
@@ -3,33 +3,33 @@
 ?>
 
 <div id="calendar-panel" class="card">
-  <div class="card-header text-uppercase text-center font-weight-bold">
-    Calendar of Activities
-  </div>
+    <div class="card-header text-uppercase text-center font-weight-bold">
+        Calendar of Activities
+    </div>
 
-  <div id="calendar" class="card-body">
-    <?php
-    $calendarActivities = calendar();
+    <div id="calendar" class="card-body">
+        <?php
+        $calendarActivities = calendar();
 
-    if (numRows($calendarActivities) === 0) { ?>
-      <div class="text-center">No scheduled activities</div>
-    <?php } else { ?>
-      <ul class="fa-ul mb-0 ml-4">
-        <?php while ($calendar = fetchAssoc($calendarActivities)) { ?>
-          <li class="py-1">
-            <?php $isHoliday = $calendar['isHoliday']; ?>
-            <span class="fa-li">
-              <i class="fas fa-calendar-alt <?php echo $isHoliday ? 'text-danger' : 'text-primary'; ?>"></i>
-            </span>
+        if (numRows($calendarActivities) === 0) { ?>
+            <div class="text-center">No scheduled activities</div>
+        <?php } else { ?>
+            <ul class="fa-ul mb-0 ml-4">
+                <?php while ($calendar = fetchAssoc($calendarActivities)) { ?>
+                    <li class="py-1">
+                        <?php $isHoliday = $calendar['isHoliday']; ?>
+                        <span class="fa-li">
+                            <i class="fas fa-calendar-alt <?php echo $isHoliday ? 'text-danger' : 'text-primary'; ?>"></i>
+                        </span>
 
-            <span class="<?php echo $isHoliday ? 'text-danger' : 'text-primary'; ?>">
-              <?php echo $calendar['activity']; ?>
-            </span>
+                        <span class="<?php echo $isHoliday ? 'text-danger' : 'text-primary'; ?>">
+                            <?php echo $calendar['activity']; ?>
+                        </span>
 
-            <div class="small"><?php echo toDateRange(strtotime($calendar['from']), strtotime($calendar['to'])); ?></div>
-          </li>
+                        <div class="small"><?php echo toDateRange(strtotime($calendar['from']), strtotime($calendar['to'])); ?></div>
+                    </li>
+                <?php } ?>
+            </ul>
         <?php } ?>
-      </ul>
-    <?php } ?>
-  </div>
+    </div>
 </div>
