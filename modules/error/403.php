c7a88a4bbb6747dfd2c253a6583541be097d6657
johnleosamante <johnleomuitsamante@gmail.com>
2024-01-02 11:08:00 +0800

    Modified indentation

--- modules/error/403.php
+++ modules/error/403.php
@@ -3,13 +3,13 @@
 ?>
 
 <div class="text-center py-0">
-  <div class="error mx-auto" data-text="403">403</div>
-  <p class="lead text-gray-800 mt-1 mb-0">Access denied</p>
-  <p class="text-gray-500 mb-4">Sorry, the page you're trying to access is restricted.</p>
+    <div class="error mx-auto" data-text="403">403</div>
+    <p class="lead text-gray-800 mt-1 mb-0">Access denied</p>
+    <p class="text-gray-500 mb-4">Sorry, the page you're trying to access is restricted.</p>
 
-  <?php if (isset($userId)) : ?>
-    <a href="<?php echo uri() . '/' . $activeApp; ?>" title="Go to dashboard">Go to dashboard</a>
-  <?php else : ?>
-    <a href="<?php echo uri(); ?>" title="Go to home page">Go to home page</a>
-  <?php endif; ?>
+    <?php if (isset($userId)) : ?>
+        <a href="<?php echo uri() . '/' . $activeApp; ?>" title="Go to dashboard">Go to dashboard</a>
+    <?php else : ?>
+        <a href="<?php echo uri(); ?>" title="Go to home page">Go to home page</a>
+    <?php endif; ?>
 </div>
--- modules/error/404.php
+++ modules/error/404.php
@@ -3,13 +3,13 @@
 ?>
 
 <div class="text-center py-0">
-  <div class="error mx-auto" data-text="404">404</div>
-  <p class="lead text-gray-800 mt-1 mb-0">Page not found</p>
-  <p class="text-gray-500 mb-4">Sorry, we couldn't find what you're looking for...</p>
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mt-1 mb-0">Page not found</p>
+    <p class="text-gray-500 mb-4">Sorry, we couldn't find what you're looking for...</p>
 
-  <?php if (isset($userId)) : ?>
-    <a href="<?php echo uri() . '/' . $activeApp; ?>" title="Go to dashboard">Go to dashboard</a>
-  <?php else : ?>
-    <a href="<?php echo uri(); ?>" title="Go to home page">Go to home page</a>
-  <?php endif; ?>
+    <?php if (isset($userId)) : ?>
+        <a href="<?php echo uri() . '/' . $activeApp; ?>" title="Go to dashboard">Go to dashboard</a>
+    <?php else : ?>
+        <a href="<?php echo uri(); ?>" title="Go to home page">Go to home page</a>
+    <?php endif; ?>
 </div>
