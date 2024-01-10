9e11cba18da0119375b6a040c048c2fd3b07a675
johnleosamante <johnleomuitsamante@gmail.com>
2024-01-10 16:30:51 +0800

    Modified transactions summary filter implementation

--- dts/app.php
+++ dts/app.php
@@ -148,10 +148,11 @@ if (isset($_POST['cancel-document'])) {
 	}
 }
 
-$from = date('Y') . '-01-01';
-$to = date('Y-m-d');
+$from = isset($_GET['from']) ? sanitize($_GET['from']) : date('Y') . '-01-01';
+$to = isset($_GET['to']) ? sanitize($_GET['to']) : date('Y-m-d');
 
 if (isset($_POST['transactions-summary-filter'])) {
 	$from = date('Y-m-d', strtotime($_POST['date-from']));
 	$to = date('Y-m-d', strtotime($_POST['date-to']));
+	redirect(customUri('dts', sanitize(decipher($_GET['v']))) . '&from=' . $from . '&to=' . $to);
 }
