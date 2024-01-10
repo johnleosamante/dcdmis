539b5e411e365732ac8aaa0570112dff5c0e1d48
johnleosamante <johnleomuitsamante@gmail.com>
2024-01-10 14:09:34 +0800

    Added transactions summary page

---
+++ modules/documents/transactions-summary.php
@@ -0,0 +1,94 @@
+<?php
+// modules/documents/summary-report.php
+if (!$isDts) {
+    require_once(root() . '/modules/error/403.php');
+    return;
+}
+
+messageAlert($showAlert, $message, $success);
+?>
+
+<div class="card border-left-primary shadow mb-4">
+    <div class="card-header py-3">
+        <?php contentTitle('Transactions Summary'); ?>
+    </div>
+
+    <div class="card-body">
+        <form name="Filter" method="POST" class="mb-3">
+            <div class="row">
+                <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-2 d-flex align-items-center">
+                                <label for="date-from" class="font-weight-bold m-0">From:</label>
+                            </div>
+                            <div class="col-10">
+                                <input class="form-control" id="date-from" type="date" name="date-from" value="<?php echo $from; ?>">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-2 d-flex align-items-center">
+                                <label for="date-to" class="font-weight-bold m-0">To:</label>
+                            </div>
+                            <div class="col-10">
+                                <input class="form-control" id="date-to" type="date" name="date-to" value="<?php echo $to; ?>">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12">
+                    <button type="submit" class="btn btn-primary btn-block" name="transactions-summary-filter">Filter Date <i class="fa fa-filter"></i></button>
+                </div>
+            </div>
+        </form>
+
+        <div class="table-responsive">
+            <table class="table table-hover table-striped table-bordered mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th class="align-middle" width="5%">Photo</th>
+                        <th class="align-middle" width="20%">User</th>
+                        <th class="align-middle" width="25%">Position</th>
+                        <th class="align-middle" width="10%">Created</th>
+                        <th class="align-middle" width="10%">Received</th>
+                        <th class="align-middle" width="10%">Forwarded</th>
+                        <th class="align-middle" width="10%">Completed</th>
+                        <th class="align-middle" width="10%">Canceled</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php $query = sectionUsers($station);
+                    while ($row = fetchArray($query)) :
+                        $employeeName = toName($row['lname'], $row['fname'], $row['mname'], $row['ext']);
+                        $photo = uri() . '/' . $row['picture'];
+                    ?>
+                        <tr class="text-uppercase">
+                            <td class="align-middle">
+                                <div class="image-container">
+                                    <span class="d-flex justify-content-center align-middle employee-photo rounded-circle overflow-hidden">
+                                        <img height="100%" src="<?php echo $photo; ?>" alt="<?php echo $employeeName; ?>">
+                                    </span>
+                                    <div class="sex-sign"><?php sex($row['sex']); ?></div>
+                                </div>
+                            </td>
+                            <td class="align-middle text-left"><?php modalItem(uri() . '/modules/users/user-info-dialog.php?id=' . cipher($row['id']), $employeeName); ?></td>
+                            <td class="align-middle"><?php echo fetchAssoc(positions($row['position']))['position']; ?></td>
+                            <td class="align-middle"><?php echo number_format(numRows(documentByStatus('Created Document', $row['id'], $from, $to))); ?></td>
+                            <td class="align-middle"><?php echo number_format(numRows(documentByStatus('Received Document', $row['id'], $from, $to))); ?></td>
+                            <td class="align-middle"><?php echo number_format(numRows(documentByStatus('Forwarded Document', $row['id'], $from, $to))); ?></td>
+                            <td class="align-middle"><?php echo number_format(numRows(documentByStatus('Completed Document', $row['id'], $from, $to))); ?></td>
+                            <td class="align-middle"><?php echo number_format(numRows(documentByStatus('Canceled Document', $row['id'], $from, $to))); ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
