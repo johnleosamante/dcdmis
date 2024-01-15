7f29af39b3f59d01aaf0b4277c37f6bf0e4c69d0
johnleosamante <johnleomuitsamante@gmail.com>
2024-01-15 14:06:52 +0800

    Modified station functions

--- includes/database/school.php
+++ includes/database/school.php
@@ -61,7 +61,7 @@ function updateSchool($id, $name, $alias, $address, $district, $category, $refer
 
 function station($id)
 {
-    return query("SELECT `No`, Emp_Position AS position_id, Emp_Station AS station_id, Emp_ID AS id FROM tbl_station WHERE Emp_ID='{$id}' ORDER BY Emp_DOA DESC LIMIT 1;");
+    return query("SELECT `No`, Emp_Position AS position_id, Emp_Station AS station_id, Emp_DOA AS doa, Emp_ID AS id FROM tbl_station WHERE Emp_ID='{$id}' ORDER BY Emp_DOA DESC LIMIT 1;");
 }
 
 function createStation($date, $stationId, $positionId, $id)
@@ -69,9 +69,9 @@ function createStation($date, $stationId, $positionId, $id)
     nonQuery("INSERT INTO tbl_station (`Emp_DOA`, Emp_Station, Emp_Position, Emp_ID) VALUES ('{$date}', '{$stationId}', '{$positionId}', '{$id}');");
 }
 
-function updateStation($stationId, $positionId, $id)
+function updateStation($date, $stationId, $positionId, $id)
 {
-    nonQuery("UPDATE tbl_station SET Emp_Position='{$positionId}', Emp_Station='{$stationId}' WHERE Emp_ID='{$id}';");
+    nonQuery("UPDATE tbl_station SET Emp_Position='{$positionId}', Emp_Station='{$stationId}', Emp_DOA='{$date}' WHERE Emp_ID='{$id}';");
 }
 
 function updateStationID($newStationId, $oldStationId)
