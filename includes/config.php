3806521dc8712ec1402972e49c7bd8e490089be6
johnleosamante <johnleomuitsamante@gmail.com>
2024-01-19 13:34:33 +0800

    Added site author email config

--- includes/config.php
+++ includes/config.php
@@ -9,4 +9,5 @@ define('PUBLIC_IP', '120.28.189.147');
 define('SITE_TITLE', 'Department of Education Schools Division of Dipolog City');
 define('SITE_ALIAS', 'sdodipolog');
 define('SITE_DESCRIPTION', 'Department of Education Schools Division of Dipolog City Data Management Information System');
-define('SITE_AUTHOR', 'DepEd Schools Division of Dipolog City ICT Unit');
+define('SITE_AUTHOR', 'DepEd Dipolog City ICT Unit');
+define('SITE_AUTHOR_EMAIL', 'depeddipolog.ict@deped.gov.ph');
