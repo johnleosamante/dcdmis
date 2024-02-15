c16f05f353add830fc02f62a6a7a2cc3cc2d09ea
John Leo Samante <johnleomuitsamante@gmail.com>
2024-02-15 11:45:30 +0800

    Removed access restriction

--- hrtdms/app.php
+++ hrtdms/app.php
@@ -1,11 +1,5 @@
 <?php
 // hrtdms/app.php
-if (isPublicDomain()) {
-    redirect(uri() . '/oops?e=403');
-}
-
-restrictPublicAccess(hasHoliday());
-
 $activeApp = $_SESSION[alias() . '_activeApp'] = 'hrtdms';
 $page = $appTitle = 'Human Resource Training &amp; Development Management System';
 
