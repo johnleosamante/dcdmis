800df1d9c5cc626333575b5db87d4ceb3934693f
John Leo Samante <johnleomuitsamante@gmail.com>
2024-03-06 11:07:44 +0800

    Removed div

--- hrtdms/repository/page.php
+++ hrtdms/repository/page.php
@@ -1,31 +1,30 @@
 <?php
 // hrtdms/repository/page.php
 ?>
-<div class="col-12">
-    <div class="mt-5 text-center">
-        <?php displayLogo(120, 120, '0', uri(), title()); ?>
-        <h1 class="my-2"><?php echo $appTitle; ?></h1>
-    </div>
 
-    <div class="card mt-3 mb-5 mx-auto">
-        <?php
-        if (!isset($url) || $url === 'conducted-trainings') {
-            require_once('conducted-trainings.php');
-        } else {
-            $file = '';
+<div class="mt-5 text-center">
+    <?php displayLogo(120, 120, '0', uri(), title()); ?>
+    <h1 class="my-2"><?php echo $appTitle; ?></h1>
+</div>
 
-            switch ($url) {
-                case 'Training Details':
-                    $file = 'training-details.php';
-                    break;
-                case '404':
-                default:
-                    $file = root() . "/modules/{$file}.php";
-                    break;
-            }
+<div class="card mt-3 mb-5 mx-auto">
+    <?php
+    if (!isset($url) || $url === 'conducted-trainings') {
+        require_once('conducted-trainings.php');
+    } else {
+        $file = '';
 
-            require_once("{$file}");
+        switch ($url) {
+            case 'Training Details':
+                $file = 'training-details.php';
+                break;
+            case '404':
+            default:
+                $file = 'conducted-trainings.php';
+                break;
         }
-        ?>
-    </div>
+
+        require_once("{$file}");
+    }
+    ?>
 </div>
