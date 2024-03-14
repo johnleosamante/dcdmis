ba1e6a9a1d0087e8d4c0c9a3a17dc276d8a28d38
John Leo Samante <johnleomuitsamante@gmail.com>
2024-03-14 10:27:26 +0800

    Added date filter

--- hrtdms/app.php
+++ hrtdms/app.php
@@ -169,3 +169,12 @@ if (isset($_POST['email-participants'])) {
 
     $success = true;
 }
+
+$fromDate = isset($_GET['from']) ? sanitize($_GET['from']) : date('Y') . '-01-01';
+$toDate = isset($_GET['to']) ? sanitize($_GET['to']) : date('Y-m-d');
+
+if (isset($_POST['transactions-summary-filter'])) {
+    $fromDate = date('Y-m-d', strtotime($_POST['date-from']));
+    $toDate = date('Y-m-d', strtotime($_POST['date-to']));
+    redirect(customUri('hrtdms', sanitize(decipher($_GET['v']))) . '&from=' . $fromDate . '&to=' . $toDate);
+}
