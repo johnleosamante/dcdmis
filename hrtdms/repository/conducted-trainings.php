34d7bf75b98006b8e457bbde19f600f9e1f7da2f
John Leo Samante <johnleomuitsamante@gmail.com>
2024-03-14 10:27:57 +0800

    Added date filter

--- hrtdms/repository/conducted-trainings.php
+++ hrtdms/repository/conducted-trainings.php
@@ -3,6 +3,40 @@
 ?>
 
 <div class="card-body">
+    <form action="" method="POST">
+        <div class="row">
+            <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-2 d-flex align-items-center">
+                            <label for="date-from" class="font-weight-bold m-0">From:</label>
+                        </div>
+                        <div class="col-10">
+                            <input class="form-control" id="date-from" type="date" name="date-from" value="<?php echo $fromDate; ?>">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-2 d-flex align-items-center">
+                            <label for="date-to" class="font-weight-bold m-0">To:</label>
+                        </div>
+                        <div class="col-10">
+                            <input class="form-control" id="date-to" type="date" name="date-to" value="<?php echo $toDate; ?>">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12">
+                <button type="submit" class="btn btn-primary btn-block" name="transactions-summary-filter">Filter Date <i class="fa fa-filter"></i></button>
+            </div>
+        </div>
+    </form>
+
     <div class="table-responsive">
         <table class="table table-striped table-bordered table-hover mb-0 text-center" id="data-table" width="100%" cellspacing="0">
             <thead>
@@ -14,7 +48,7 @@
 
             <tbody>
                 <?php
-                $trainings = conductedTrainings();
+                $trainings = conductedTrainings($fromDate, $toDate);
                 while ($training = fetchAssoc($trainings)) : ?>
                     <tr>
                         <td class="align-middle text-left">
