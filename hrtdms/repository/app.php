b80af8746ee8149e95128606ab36f05d0031aee4
John Leo Samante <johnleomuitsamante@gmail.com>
2024-03-14 10:27:41 +0800

    Added date filter

--- hrtdms/repository/app.php
+++ hrtdms/repository/app.php
@@ -8,3 +8,12 @@ $enableScripts = true;
 if (isset($userId)) {
     redirect(customUri('hrtdms', 'Conducted Trainings'));
 }
+
+$fromDate = isset($_GET['from']) ? sanitize($_GET['from']) : date('Y') . '-01-01';
+$toDate = isset($_GET['to']) ? sanitize($_GET['to']) : date('Y-m-d');
+
+if (isset($_POST['transactions-summary-filter'])) {
+    $fromDate = date('Y-m-d', strtotime($_POST['date-from']));
+    $toDate = date('Y-m-d', strtotime($_POST['date-to']));
+    redirect(customUri('hrtdms/repository', sanitize(decipher($_GET['v']))) . '&from=' . $fromDate . '&to=' . $toDate);
+}
