0f0304d5b3d322ca9b0ddca2733aff0a7252ee2e
John Leo Samante <johnleomuitsamante@gmail.com>
2024-03-19 08:52:14 +0800

    Added margin bottom

--- hrtdms/repository/conducted-trainings.php
+++ hrtdms/repository/conducted-trainings.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="card-body">
-    <form action="" method="POST">
+    <form action="" method="POST" class="mb-3">
         <div class="row">
             <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
                 <div class="form-group">
