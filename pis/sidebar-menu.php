2c3499aba87e6b7de1e72eab46d440cf638cb1cd
John Leo Samante <johnleomuitsamante@gmail.com>
2024-03-25 16:02:21 +0800

    Change font awesome icon

--- pis/sidebar-menu.php
+++ pis/sidebar-menu.php
@@ -5,4 +5,4 @@ sidebarMenuItem(customUri('pis', 'Employee Information', $userId), 'Employee Inf
 sidebarMenuItem(customUri('pis', 'Service Record', $userId), 'Service Record', 'fa-file-alt', isset($url) && str_contains($url, 'Service Record'));
 sidebarMenuItem(customUri('pis', '201 Files', $userId), '201 Files', 'fa-folder-open', isset($url) && str_contains($url, '201 Files'));
 sidebarMenuItem(customUri('pis', 'Trainings', $userId), 'Trainings', 'fa-chalkboard-teacher', isset($url) && str_contains($url, 'Trainings'));
-sidebarMenuItem(customUri('pis', 'Payslip', $userId), 'Payslip', 'fa-chalkboard-teacher', isset($url) && str_contains($url, 'Payslip'));
+sidebarMenuItem(customUri('pis', 'Payslip', $userId), 'Payslip', 'fa-money-check', isset($url) && str_contains($url, 'Payslip'));
