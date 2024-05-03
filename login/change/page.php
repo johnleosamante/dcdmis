0a1209be68572bdeba4ea95f907ab44cfb7b4105
John Leo Samante <johnleomuitsamante@gmail.com>
2024-05-03 08:07:30 +0800

    Apply interactive change password form

--- login/change/page.php
+++ login/change/page.php
@@ -12,14 +12,14 @@
             <?php displayLogo(120, 120, '3', uri(), title()); ?>
 
             <div class="text-left">
-                <p class="text-center mb-2">Use a password that has the following: </p>
+                <p class="text-center mb-2">New password should have the following:</p>
 
-                <ul>
-                    <li>Atleast ten (10) characters long</li>
-                    <li>Atleast one (1) uppercase letter</li>
-                    <li>Atleast one (1) lowercase letter</li>
-                    <li>Atleast one (1) number</li>
-                    <li>Atleast one (1) special character</li>
+                <ul id="password-requirements" class="p-0 list-unstyled">
+                    <li><i id="length" class="fa fa-times text-danger mr-1"></i>Atleast ten (10) characters long</li>
+                    <li><i id="uppercase" class="fa fa-times text-danger mr-1"></i>Atleast one (1) uppercase letter</li>
+                    <li><i id="lowercase" class="fa fa-times text-danger mr-1"></i>Atleast one (1) lowercase letter</li>
+                    <li><i id="number" class="fa fa-times text-danger mr-1"></i>Atleast one (1) number</li>
+                    <li><i id="special" class="fa fa-times text-danger mr-1"></i>Atleast one (1) special character</li>
                 </ul>
             </div>
 
@@ -28,15 +28,15 @@
             <form action="" method="POST" class="text-left mt-3">
                 <div class="form-group">
                     <label for="email-address" class="font-weight-bold mb-1">Email Address</label>
-                    <input id="email-address" type="text" class="form-control border-right-0" value="<?php echo $email; ?>" readonly>
+                    <input id="email-address" type="text" class="form-control border" value="<?php echo $email; ?>" readonly>
                 </div>
 
                 <div class="form-group">
                     <label for="password" class="font-weight-bold mb-1">New Password <?php showAsterisk(); ?></label>
-                    <div class="input-group">
-                        <input id="password" name="password" type="password" class="form-control border-right-0" value="<?php echo $password; ?>" required>
+                    <div id="password-group" class="input-group border rounded">
+                        <input id="password" name="password" type="password" class="form-control border-0" value="<?php echo $password; ?>" required>
                         <div class="input-group-append">
-                            <button type="button" id="eye-toggle" class="input-group-text border-left-0 bg-white">
+                            <button type="button" id="eye-toggle" class="input-group-text border-0 bg-transparent">
                                 <i id="eye" class="small fas fa-eye fa-sm"></i>
                             </button>
                         </div>
@@ -45,10 +45,10 @@
 
                 <div class="form-group">
                     <label for="password-confirm" class="font-weight-bold mb-1">Confirm New Password <?php showAsterisk(); ?></label>
-                    <div class="input-group">
-                        <input id="password-confirm" name="password-confirm" type="password" class="form-control border-right-0" value="<?php echo $passwordConfirm; ?>" required>
+                    <div id="password-confirm-group" class="input-group border rounded">
+                        <input id="password-confirm" name="password-confirm" type="password" class="form-control border-0" value="<?php echo $passwordConfirm; ?>" required>
                         <div class="input-group-append">
-                            <button type="button" id="eye-confirm-toggle" class="input-group-text border-left-0 bg-white">
+                            <button type="button" id="eye-confirm-toggle" class="input-group-text border-0 bg-transparent">
                                 <i id="eye-confirm" class="small fas fa-eye fa-sm"></i>
                             </button>
                         </div>
@@ -57,7 +57,7 @@
 
                 <?php requiredLegend(); ?>
 
-                <button type="submit" class="btn btn-primary btn-block" name="change-password">Change Password</button>
+                <button id="change-password" type="submit" class="btn btn-primary btn-block" name="change-password">Change Password</button>
             </form>
         </div>
     </div>
