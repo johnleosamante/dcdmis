04998483ab938a545ef473cddbb4f23f5c415d62
John Leo Samante <johnleomuitsamante@gmail.com>
2024-11-08 14:33:22 +0800

    Handle unexisting photo

--- modules/employees/archived-employees.php
+++ modules/employees/archived-employees.php
@@ -52,7 +52,7 @@ messageAlert($showAlert, $message, $success);
                     $query = archivedEmployees();
                     while ($row = fetchArray($query)) :
                         $employeeName =  toName($row['lname'], $row['fname'], $row['mname'], $row['ext']);
-                        $photo = uri() . '/' . $row['picture'];
+                        $photo = file_exists(root() . '/' . $row['picture']) ? uri() . '/' . $row['picture'] : uri() . '/assets/img/user.png';
                     ?>
                         <tr class="text-uppercase">
                             <td class="align-middle">
