34c889fcea6da05728acbddc5185923cad525997
John Leo Samante <johnleomuitsamante@gmail.com>
2024-11-08 11:12:28 +0800

    Added document type and uploads

--- modules/documents/save-document-dialog.php
+++ modules/documents/save-document-dialog.php
@@ -10,7 +10,7 @@ require_once(root() . '/includes/layout/components.php');
 
 $documentId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
 $documents = documentLog($documentId);
-$description = $destination = $purpose = $details = $attribute = '';
+$description = $destination = $purpose = $details = $attribute = $filename = '';
 $isDescriptionEditable = true;
 $modalTitle = 'New Document';
 $hasDocument = true;
@@ -20,12 +20,14 @@ $notFound = true;
 if (numRows($documents) > 0) {
     $document = fetchAssoc($documents);
     $documentId = $document['id'];
+    $type = $document['type'];
     $description = $document['description'];
     $destination = $document['destination'];
     $purpose = $document['purpose'];
     $details = $document['details'];
     $documentLogsResults = documentLogs($documentId);
     $documentLogs = fetchAssoc($documentLogsResults);
+    $filename = $documentLogs['attachment'];
     $hasDocument = !str_contains(strtolower($documentLogs['status']), 'complete') && !str_contains(strtolower($documentLogs['status']), 'cancel') && $documentLogs['from'] === $station;
     $modalTitle = $hasDocument ? 'Edit Document' : 'Document not found';
 
@@ -50,7 +52,7 @@ if (numRows($documents) > 0) {
     <div class="modal-content">
         <?php modalHeader($modalTitle); ?>
 
-        <form action="" method="POST">
+        <form action="" method="POST" enctype="multipart/form-data">
             <div class="modal-body">
                 <?php if ($hasDocument) { ?>
                     <?php if (!$notFound) : ?>
@@ -59,6 +61,18 @@ if (numRows($documents) > 0) {
                             <input id="code" type="text" value="<?php echo $documentId; ?>" class="form-control text-uppercase" disabled>
                         </div>
                     <?php endif; ?>
+
+                    <div class="form-group">
+                        <label for="document-type" class="mb-0">Document Type <?php showAsterisk($isDescriptionEditable); ?></label>
+                        <select name="document-type" id="document-type" class="form-control" title="Select document type..." required>
+                            <option value="">Select type...</option>
+                            <?php
+                            $documentTypes = documentTypes();
+                            while ($documentType = fetchAssoc($documentTypes)) : ?>
+                                <option value="<?php echo $documentType['id']; ?>" <?php echo setOptionSelected($documentType['id'], $type); ?>><?php echo $documentType['name']; ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                    </div>
 
                     <div class="form-group">
                         <label for="description" class="mb-0">Description <?php showAsterisk($isDescriptionEditable); ?></label>
@@ -121,6 +135,11 @@ if (numRows($documents) > 0) {
                         <textarea id="details" name="details" class="form-control" rows="2" placeholder="Type additional details..." title="Type additional details..."><?php echo $details; ?></textarea>
                     </div>
 
+                    <div class="form-group">
+                        <label class="mb-0" for="file-upload">Attachment</label>
+                        <input id="file-upload" name="file-upload" type="file" class="w-100">
+                    </div>
+
                     <?php requiredLegend(0); ?>
                 <?php } else {
                     missingAlert($modalTitle);
@@ -130,6 +149,7 @@ if (numRows($documents) > 0) {
             <div class="modal-footer">
                 <?php if ($hasDocument) : ?>
                     <input type="hidden" name="verifier" value="<?php echo isset($_GET['id']) ? $_GET['id'] : null; ?>">
+                    <input type="hidden" name="file-verifier" value="<?php echo cipher($filename); ?>">
                     <button class="btn btn-primary" name="save-document" type="submit">Continue</button>
                 <?php endif; ?>
                 <?php cancelModalButton(); ?>
