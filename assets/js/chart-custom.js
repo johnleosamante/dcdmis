c8760c5d6a6f6d9ed5df294fd0f39e05c10665e6
John Leo Samante <johnleomuitsamante@gmail.com>
2024-11-13 14:42:50 +0800

    Removed unnecessary code.

--- assets/js/chart-custom.js
+++ assets/js/chart-custom.js
@@ -1,11 +1,11 @@
 // assets/js/chart-custom.js
 const tooltips = {
 	titleMarginBottom: 10,
-	titleFontColor: '#6e707e',
+	titleFontColor: "#6e707e",
 	titleFontSize: 14,
 	backgroundColor: "rgb(255,255,255)",
 	bodyFontColor: "#858796",
-	borderColor: '#dddfeb',
+	borderColor: "#dddfeb",
 	borderWidth: 1,
 	xPadding: 15,
 	yPadding: 15,
@@ -22,186 +22,243 @@ const pieOptions = {
 	tooltips: tooltips,
 	legend: {
 		display: true,
-		position: 'bottom',
+		position: "bottom",
 		labels: {
 			usePointStyle: true,
-		}
+		},
 	},
 	plugins: {
 		datalabels: {
 			formatter: (value, ctx) => {
 				let sum = 0;
 				let dataArr = ctx.chart.data.datasets[0].data;
-				dataArr.map(data => {
-					sum += (data * 1);
+
+				dataArr.map((data) => {
+					sum += data * 1;
 				});
-				let percentage = (((value * 1) / sum) * 100).toFixed(2) + "%";
-				return percentage;
+
+				return (((value * 1) / sum) * 100).toFixed(2) + "%";
 			},
-			color: '#fff',
+			color: "#fff",
 			font: font,
-		}
-	}
-}
+		},
+	},
+};
 
 const plugins = {
 	datalabels: {
 		color: function (ctx) {
 			return ctx.dataset.backgroundColor;
 		},
-		anchor: 'end',
-		align: 'end',
+		anchor: "end",
+		align: "end",
 		font: font,
 		formatter: (value, context) => {
 			return value;
-		}
-	}
+		},
+	},
 };
 
 const barOptions = {
 	scales: {
-		xAxes: [{
-			gridLines: {
-				display: false,
-				drawBorder: false,
-			}
-		}],
-		yAxes: [{
-			ticks: {
-				beginAtZero: true,
-			}
-		}]
+		xAxes: [
+			{
+				gridLines: {
+					display: false,
+					drawBorder: false,
+				},
+			},
+		],
+		yAxes: [
+			{
+				ticks: {
+					beginAtZero: true,
+				},
+			},
+		],
 	},
 	legend: {
 		display: false,
 	},
 	tooltips: tooltips,
-	plugins: plugins
+	plugins: plugins,
 };
 
 function generateRandomColor() {
 	let num = Math.floor(Math.random() * 16777215);
 	let hex = num.toString(16).toUpperCase();
+
 	while (hex.length < 6) {
 		hex = "0" + hex;
 	}
+
 	return "#" + hex;
 }
 
 function generateColorPallete(items) {
 	let colors = [];
 	let counter = 0;
+
 	while (counter < items) {
 		colors.push(generateRandomColor());
 		counter++;
 	}
+
 	return colors;
 }
 
 function generatePieChart(data, colors, element) {
-	const pieChart = new Chart(document.getElementById(element).getContext('2d'), {
-		type: 'pie',
-		data: {
-			labels: data.map((item) => { return item.name; }),
-			datasets: [{
-				data: data.map((item) => { return item.count; }),
-				backgroundColor: colors,
-			}]
-		},
-		options: pieOptions,
-	});
-	pieChart.canvas.parentNode.style.minHeight = '400px';
+	const pieChart = new Chart(
+		document.getElementById(element).getContext("2d"),
+		{
+			type: "pie",
+			data: {
+				labels: data.map((item) => {
+					return item.name;
+				}),
+				datasets: [
+					{
+						data: data.map((item) => {
+							return item.count;
+						}),
+						backgroundColor: colors,
+					},
+				],
+			},
+			options: pieOptions,
+		}
+	);
+
+	pieChart.canvas.parentNode.style.minHeight = "400px";
+
 	return pieChart;
 }
 
 function generateDoughnutChart(data, colors, element) {
-	const doughnutChart = new Chart(document.getElementById(element).getContext('2d'), {
-		type: 'doughnut',
-		data: {
-			labels: data.map((item) => { return item.name; }),
-			datasets: [{
-				data: data.map((item) => { return item.count; }),
-				backgroundColor: colors,
-			}]
-		},
-		options: pieOptions,
-	});
-	doughnutChart.canvas.parentNode.style.minHeight = '400px';
+	const doughnutChart = new Chart(
+		document.getElementById(element).getContext("2d"),
+		{
+			type: "doughnut",
+			data: {
+				labels: data.map((item) => {
+					return item.name;
+				}),
+				datasets: [
+					{
+						data: data.map((item) => {
+							return item.count;
+						}),
+						backgroundColor: colors,
+					},
+				],
+			},
+			options: pieOptions,
+		}
+	);
+
+	doughnutChart.canvas.parentNode.style.minHeight = "400px";
+
 	return doughnutChart;
 }
 
 function generatePolarAreaChart(data, colors, element) {
-	const polarAreaChart = new Chart(document.getElementById(element).getContext('2d'), {
-		type: 'polarArea',
+	return new Chart(document.getElementById(element).getContext("2d"), {
+		type: "polarArea",
 		data: {
-			labels: data.map((item) => { return item.name; }),
-			datasets: [{
-				data: data.map((item) => { return item.count; }),
-				backgroundColor: colors,
-			}]
+			labels: data.map((item) => {
+				return item.name;
+			}),
+			datasets: [
+				{
+					data: data.map((item) => {
+						return item.count;
+					}),
+					backgroundColor: colors,
+				},
+			],
 		},
 		options: pieOptions,
 	});
-	polarAreaChart.canvas.parentNode.style.minHeight = '400px';
-	return polarAreaChart;
+
+	polarAreaChart.canvas.parentNode.style.minHeight = "400px";
 }
 
 function generateBarChart(data, colors, element) {
-	const barChart = new Chart(document.getElementById(element).getContext('2d'), {
-		type: 'bar',
+	return new Chart(document.getElementById(element).getContext("2d"), {
+		type: "bar",
 		data: {
-			labels: data.map((item) => { return item.name; }),
-			datasets: [{
-				data: data.map((item) => { return item.count; }),
-				backgroundColor: colors,
-			}]
+			labels: data.map((item) => {
+				return item.name;
+			}),
+			datasets: [
+				{
+					data: data.map((item) => {
+						return item.count;
+					}),
+					backgroundColor: colors,
+				},
+			],
 		},
 		options: barOptions,
 	});
-	return barChart;
 }
 
 function generateComparativeBarChart(data, colors, element) {
-	const barChart = new Chart(document.getElementById(element).getContext('2d'), {
-		type: 'bar',
+	return new Chart(document.getElementById(element).getContext("2d"), {
+		type: "bar",
 		data: {
-			labels: data.map((item) => { return item.name; }),
-			datasets: [{
-				backgroundColor: colors[0],
-				data: data.map((item) => { return item.male; })
-			},
-			{
-				backgroundColor: colors[1],
-				data: data.map((item) => { return item.female; })
-			}]
+			labels: data.map((item) => {
+				return item.name;
+			}),
+			datasets: [
+				{
+					backgroundColor: colors[0],
+					data: data.map((item) => {
+						return item.male;
+					}),
+				},
+				{
+					backgroundColor: colors[1],
+					data: data.map((item) => {
+						return item.female;
+					}),
+				},
+			],
 		},
 		options: barOptions,
 	});
-	return barChart;
 }
 
 function generateComparativeLineChart(data, colors, element) {
-	const lineChart = new Chart(document.getElementById(element).getContext('2d'), {
+	return new Chart(document.getElementById(element).getContext("2d"), {
 		data: {
-			labels: data.map((item) => { return item.name; }),
-			datasets: [{
-				type: 'line',
-				data: data.map((item) => { return item.dataOne; }),
-				backgroundColor: 'transparent',
-				borderColor: colors[0],
-				pointBorderColor: colors[0],
-				pointBackgroundColor: colors[0],
-				fill: false,
-			},
-			{
-				type: 'line',
-				data: data.map((item) => { return item.dataTwo; }),
-				backgroundColor: 'tansparent',
-				borderColor: colors[1],
-				pointBorderColor: colors[1],
-				pointBackgroundColor: colors[1],
-				fill: false,
-			}]
+			labels: data.map((item) => {
+				return item.name;
+			}),
+			datasets: [
+				{
+					type: "line",
+					data: data.map((item) => {
+						return item.dataOne;
+					}),
+					backgroundColor: "transparent",
+					borderColor: colors[0],
+					pointBorderColor: colors[0],
+					pointBackgroundColor: colors[0],
+					fill: false,
+				},
+				{
+					type: "line",
+					data: data.map((item) => {
+						return item.dataTwo;
+					}),
+					backgroundColor: "transparent",
+					borderColor: colors[1],
+					pointBorderColor: colors[1],
+					pointBackgroundColor: colors[1],
+					fill: false,
+				},
+			],
 		},
 		options: {
 			maintainAspectRatio: false,
@@ -217,27 +274,33 @@ function generateComparativeLineChart(data, colors, element) {
 				display: false,
 			},
 			scales: {
-				yAxes: [{
-					gridLines: {
+				yAxes: [
+					{
+						gridLines: {
+							display: true,
+							lineWidth: "4px",
+							color: "rgba(0, 0, 0, .2)",
+							zeroLineColor: "transparent",
+						},
+						ticks: $.extend(
+							{
+								beginAtZero: true,
+								suggestedMax: 200,
+							},
+							ticksStyle
+						),
+					},
+				],
+				xAxes: [
+					{
 						display: true,
-						lineWidth: '4px',
-						color: 'rgba(0, 0, 0, .2)',
-						zeroLineColor: 'transparent'
+						gridLines: {
+							display: false,
+						},
+						ticks: ticksStyle,
 					},
-					ticks: $.extend({
-						beginAtZero: true,
-						suggestedMax: 200
-					}, ticksStyle)
-				}],
-				xAxes: [{
-					display: true,
-					gridLines: {
-						display: false
-					},
-					ticks: ticksStyle
-				}]
-			}
-		}
+				],
+			},
+		},
 	});
-	return lineChart;
 }
