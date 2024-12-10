591cd80542b5e366dcc41aa7aef126aa5eca4abb
John Leo Samante <johnleomuitsamante@gmail.com>
2024-12-10 14:56:59 +0800

    Added variable username

--- modules/settings/professional-title.php
+++ modules/settings/professional-title.php
@@ -3,7 +3,8 @@
 $title = fetchAssoc(employee($userId));
 $before = $title['btitle'];
 $after = $title['atitle'];
-$professionalName = toString($before, '', ' ') . toName($title['lname'], $title['fname'], $title['mname'], $title['ext'], true, true) . toString($after, ', ');
+$userName = toName($title['lname'], $title['fname'], $title['mname'], $title['ext'], true, true);
+$professionalName = toString($before, '', ' ') . $userName . toString($after, ', ');
 ?>
 
 <div class="tab-pane fade" id="professional-title">
@@ -39,6 +40,7 @@ $professionalName = toString($before, '', ' ') . toName($title['lname'], $title[
 
         <div class="row">
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
+                <input type="hidden" id="user-name" value="<?= $userName ?>">
                 <input name="update-professional-titles" type="submit" value="Update Professional Titles" class="btn btn-primary btn-block btn-lg">
             </div>
         </div>
