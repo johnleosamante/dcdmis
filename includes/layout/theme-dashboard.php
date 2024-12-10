4acc0ffa3e77b651d879c9231eb9bb1323df84e1
John Leo Samante <johnleomuitsamante@gmail.com>
2024-12-10 14:53:20 +0800

    Update javascript version

--- includes/layout/theme-dashboard.php
+++ includes/layout/theme-dashboard.php
@@ -66,7 +66,7 @@ require_once(root() . '/includes/layout/components.php');
     <script src="<?= uri() ?>/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
     <script src="<?= uri() ?>/assets/vendor/datatables-responsive/js/dataTables.responsive.min.js"></script>
     <script src="<?= uri() ?>/assets/vendor/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <script src="<?= uri() ?>/assets/js/script.js?v=1.2.1"></script>
+    <script src="<?= uri() ?>/assets/js/script.js?v=1.2.2"></script>
 </body>
 
 </html>
