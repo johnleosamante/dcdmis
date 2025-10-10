60a8333834e8c46316091f12208e0b41b6461193
John Leo Samante <johnleomuitsamante@gmail.com>
2025-10-10 09:05:50 +0800

    Replaced define to const for constants

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 // include/config.php
-define('HOSTNAME', 'localhost');
-define('USER', 'chickenstalker');
-define('PASSWORD', '82%a*f25RPG6');
-define('DATABASE', 'dcdmis');
-define('DOMAIN', 'depeddipolog.com');
-define('PUBLIC_IP', '120.28.189.147');
-define('SITE_TITLE', 'Department of Education Schools Division of Dipolog City');
-define('SITE_ALIAS', 'sdodipolog');
-define('SITE_DESCRIPTION', 'Department of Education Schools Division of Dipolog City Data Management Information System');
-define('SITE_AUTHOR', 'DepEd Dipolog City ICT Unit');
-define('SITE_AUTHOR_EMAIL', 'depeddipolog.ict@deped.gov.ph');
-define('DIVISION_ID', '143');
-define('DIVISION', 'SCHOOLS DIVISION OF DIPOLOG CITY');
-define('REGION', 'REGION IX - ZAMBOANGA PENINSULA');
+const HOSTNAME = 'localhost';
+const USER = 'root';
+const PASSWORD = '';
+const DATABASE = 'dcdmis';
+const DOMAIN = 'depeddipolog.com';
+const PUBLIC_IP = '120.28.189.147';
+const SITE_TITLE = 'Department of Education Schools Division of Dipolog City';
+const SITE_ALIAS = 'sdodipolog';
+const SITE_DESCRIPTION = 'Department of Education Schools Division of Dipolog City Data Management Information System';
+const SITE_AUTHOR = 'DepEd Dipolog City ICT Unit';
+const SITE_AUTHOR_EMAIL = 'depeddipolog.ict@deped.gov.ph';
+const DIVISION_ID = '143';
+const DIVISION = 'SCHOOLS DIVISION OF DIPOLOG CITY';
+const REGION = 'REGION IX - ZAMBOANGA PENINSULA';
