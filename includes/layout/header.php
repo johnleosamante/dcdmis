5b373cd5908706eb251597f68e6b8ad5fbf2f7da
John Leo Samante <johnleomuitsamante@gmail.com>
2025-11-29 05:22:51 +0800

    update css version

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -10,6 +10,7 @@
 <title><?= title($page) ?></title>
 <link rel="shortcut icon" href="<?= uri() ?>/uploads/division/division.png">
 <link rel="stylesheet" href="<?= uri() ?>/assets/vendor/fontawesome-free/css/all.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
 <link rel="stylesheet" href="<?= uri() ?>/assets/css/sb-admin-2.min.css">
-<link rel="stylesheet" href="<?= uri() ?>/assets/css/custom.css?v=1.2">
+<link rel="stylesheet" href="<?= uri() ?>/assets/css/custom.css?v=1.3">
