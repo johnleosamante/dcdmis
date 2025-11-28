c485306bd0229010f74303c299d4bc5a3c331d2d
John Leo Samante <johnleomuitsamante@gmail.com>
2025-11-29 05:41:42 +0800

    add cards

--- hrmpsb/dashboard.php
+++ hrmpsb/dashboard.php
@@ -1,2 +1,16 @@
 <?php
 // hrmpsb/dashboard.php
+messageAlert($showAlert, $message, $success);
+contentTitleWithModal('Dashboard', uri() . '/modules/vacancies/save-vacancy-dialog.php', 'Add Vacancy', 'fa-plus');
+?>
+
+<div class="row mt-4">
+    <?php
+    card('Active Employees', customUri('hrmpsb', 'Active Employees'), 'fa-user-check', 'primary', $countActive);
+    card('Retirable Employees', customUri('hrmpsb', 'Retirable Employees'), 'fa-user-clock', 'success', $countRetirable);
+    card('Vacancies', customUri('hrmpsb', 'Vacancies'), 'fa-user-times', 'warning', $countVacancy);
+    card('Districts', customUri('hrmpsb', 'Districts'), 'fa-map-marked-alt', 'info', $districtCount);
+    card('Schools', customUri('hrmpsb', 'Schools'), 'fa-school', 'danger', $schoolCount);
+    card('Sections', customUri('hrmis', 'Sections'), 'fa-map-signs', 'secondary', $sectionCount);
+    ?>
+</div>
