49f458086912d2ba4cbbdf2a848e7a3563264d78
John Leo Samante <johnleomuitsamante@gmail.com>
2026-01-22 08:28:16 +0800

    Clear sensitive information from config.php
    Removed sensitive database configuration details.

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 // include/config.php
 const HOSTNAME = 'localhost';
-const USER = 'chickenstalker';
-const PASSWORD = '82%a*f25RPG6';
-const DATABASE = 'dcdmis';
-const DOMAIN = 'depeddipolog.com';
-const PUBLIC_IP = '120.28.189.147';
+const USER = '';
+const PASSWORD = '';
+const DATABASE = '';
+const DOMAIN = '';
+const PUBLIC_IP = '';
 const SITE_TITLE = 'Department of Education Schools Division of Dipolog City';
-const SITE_ALIAS = 'sdodipolog';
+const SITE_ALIAS = '';
 const SITE_DESCRIPTION = 'Department of Education Schools Division of Dipolog City Data Management Information System';
 const SITE_AUTHOR = 'DepEd Dipolog City ICT Unit';
-const SITE_AUTHOR_EMAIL = 'depeddipolog.ict@deped.gov.ph';
-const DIVISION_ID = '143';
+const SITE_AUTHOR_EMAIL = '';
+const DIVISION_ID = '';
 const DIVISION = 'SCHOOLS DIVISION OF DIPOLOG CITY';
 const REGION = 'REGION IX - ZAMBOANGA PENINSULA';
