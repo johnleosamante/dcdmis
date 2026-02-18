97ee3f24722860e28b0ea2f51736b08be9467f10
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-18 16:58:51 +0800

    Refactor psipop.php for sql injection prevention

--- includes/database/psipop.php
+++ includes/database/psipop.php
@@ -1,74 +1,135 @@
 <?php
-// includes/database/psipop.php
-// psipop
-// tbl_step_increment
-// tbl_loyalty_award
+// position_items
+// step_increments
+// loyalty_awards
 function psipop($id)
 {
-    return query("SELECT `No` AS `no`, `Item_Number` AS `item`, `Job_status` AS `status`, `Original_Appointment` AS `original_appointment`, `Date_promoted` AS `date_promoted`, `Elegibility` AS `eligibility` FROM psipop WHERE `Emp_ID`='{$id}';");
+    $sql = "SELECT 
+                `id` AS `no`, 
+                `item_number` AS `item`, 
+                `job_status` AS `status`, 
+                `original_appointment_date`, 
+                `date_promoted`, 
+                `eligibility` 
+            FROM `position_items` 
+            WHERE `person_id` = ? LIMIT 1";
+
+    return find($sql, [$id]);
 }
 
 function createPsipop($itemNo, $status, $originalAppointment, $datePromoted, $eligibility, $id)
 {
-    nonQuery("INSERT INTO psipop (`Item_Number`, `Job_status`, `Original_Appointment`, `Date_promoted`, `Elegibility`, `Emp_ID`) VALUES ('{$itemNo}', '{$status}', '{$originalAppointment}', '{$datePromoted}', '{$eligibility}', '{$id}');");
+    $data = [
+        'item_number' => $itemNo,
+        'job_status' => $status,
+        'original_appointment_date' => $originalAppointment,
+        'date_promoted' => $datePromoted,
+        'eligibility' => $eligibility,
+        'person_id' => $id
+    ];
+
+    return insert('position_items', $data);
 }
 
 function updatePsipop($itemNo, $status, $originalAppointment, $datePromoted, $eligibility, $id)
 {
-    nonQuery("UPDATE psipop SET `Item_Number`='{$itemNo}', `Job_status`='{$status}', `Original_Appointment`='{$originalAppointment}', `Date_promoted`='{$datePromoted}', `Elegibility`='{$eligibility}' WHERE `Emp_ID`='{$id}' LIMIT 1;");
+    $data = [
+        'item_number' => $itemNo,
+        'job_status' => $status,
+        'original_appointment_date' => $originalAppointment,
+        'date_promoted' => $datePromoted,
+        'eligibility' => $eligibility
+    ];
+
+    return update('position_items', $data, '`person_id` = ?', [$id]);
 }
 
 function deletePsipop($id)
 {
-    nonQuery("DELETE FROM psipop WHERE `Item_Number`='{$id}';");
+    return delete('position_items', '`person_id` = ?', [$id]);
 }
 
 function createStepIncrement($dateLastStep, $stepNo, $sg, $id)
 {
-    nonQuery("INSERT INTO tbl_step_increment (`Date_last_step`, `Step_No`, `No_of_year`, `Emp_ID`) VALUES ('{$dateLastStep}', '{$stepNo}', '{$sg}', '{$id}');");
+    $data = [
+        'last_step_date' => $dateLastStep,
+        'step' => $stepNo,
+        'salary_grade' => $sg,
+        'person_id' => $id
+    ];
+
+    return insert('step_increments', $data);
 }
 
 function updateStepIncrement($dateLastStep, $stepNo, $sg, $id)
 {
-    return query("UPDATE tbl_step_increment SET `Date_last_step`='{$dateLastStep}', `Step_No`='{$stepNo}', `No_of_year`='{$sg}' WHERE `Emp_ID`='{$id}';");
+    $data = [
+        'last_step_date' => $dateLastStep,
+        'step' => $stepNo,
+        'salary_grade' => $sg
+    ];
+
+    return update('step_increments', $data, '`person_id` = ?', [$id]);
 }
 
 function deleteStepIncrement($id)
 {
-    nonQuery("DELETE FROM tbl_step_increment WHERE Emp_ID='{$id}';");
+    return delete('step_increments', '`person_id` = ?', [$id]);
 }
 
 function createLoyaltyAward($dateLastAwarded, $id)
 {
-    nonQuery("INSERT INTO tbl_loyalty_award (`employee_id`, `last_awarded_on`) VALUES ('{$id}', '{$dateLastAwarded}');");
+    $data = [
+        'person_id' => $id,
+        'date_last_awarded' => $dateLastAwarded
+    ];
+
+    return insert('loyalty_awards', $data);
 }
 
 function updateLoyaltyAward($dateLastAwarded, $id)
 {
-    nonQuery("UPDATE tbl_loyalty_award SET `last_awarded_on`='{$dateLastAwarded}' WHERE `employee_id`='{$id}';");
+    $data = [
+        'date_last_awarded' => $dateLastAwarded
+    ];
+    return update('loyalty_awards', $data, '`person_id` = ?', [$id]);
 }
 
 function deleteLoyaltyAward($id)
 {
-    nonQuery("DELETE FROM tbl_loyalty_award WHERE employee_id='{$id}';");
+    return delete('loyalty_awards', '`person_id` = ?', [$id]);
 }
 
-function getEmployeeStepIncrement($id)
+function getEmployeeStepIncrement($person_id)
 {
-    return query("SELECT `Emp_ID` AS `id`, `Step_No` AS `step`, `Date_last_step` AS `date_last_step` FROM tbl_step_increment WHERE `Emp_ID`='{$id}';");
+    return find("SELECT * FROM `step_increments` WHERE `person_id` = ? LIMIT 1", [$person_id]);
 }
 
-function getEmployeeLoyaltyAward($id)
+function getEmployeeLoyaltyAward($person_id)
 {
-    return query("SELECT `employee_id` AS `id`, `last_awarded_on` FROM tbl_loyalty_award WHERE employee_id='{$id}';");
+    return find("SELECT person_id, date_last_awarded FROM `loyalty_awards` WHERE `person_id` = ? LIMIT 1", [$person_id]);
 }
 
-function loyaltyAward($id)
+// TODO
+function loyaltyAward($person_id)
 {
-    return query("SELECT * FROM (SELECT psipop.Emp_ID AS id, TIMESTAMPDIFF(YEAR, psipop.Original_Appointment, NOW()) AS years_active, TIMESTAMPDIFF(YEAR, tbl_loyalty_award.last_awarded_on, NOW()) AS last_awarded FROM psipop INNER JOIN tbl_loyalty_award ON psipop.Emp_ID=tbl_loyalty_award.employee_id) AS service_years WHERE years_active >= 10 AND last_awarded >= 5 AND id='{$id}';");
+    $sql = "SELECT * FROM (
+                SELECT pi.`person_id`, 
+                    TIMESTAMPDIFF(YEAR, pi.`original_appointment_date`, NOW()) AS years_active, 
+                    TIMESTAMPDIFF(YEAR, la.`date_last_awarded`, NOW()) AS last_awarded 
+                FROM `position_items` AS pi 
+                INNER JOIN `loyalty_awards` AS la ON pi.`person_id` = la.`person_id`
+            ) AS service_years 
+            WHERE `years_active` >= 10 AND `last_awarded` >= 5 AND `person_id` = ?";
+    return find($sql, [$person_id]);
 }
 
-function stepIncrement($id)
+// TODO
+function stepIncrement($person_id)
 {
-    return query("SELECT * FROM (SELECT Emp_ID AS id, Date_last_step AS last_step_date, Step_No AS step, TIMESTAMPDIFF(YEAR, Date_last_step, NOW()) AS years_active FROM tbl_step_increment ORDER BY Date_last_step) AS service_years WHERE years_active >= 3 AND step < 8 AND id='{$id}';");
+    $sql = "SELECT * FROM (
+                SELECT `person_id`, `last_step_date`, `step`, TIMESTAMPDIFF(YEAR, last_step_date, NOW()) AS `years_active` FROM `step_increments`
+            ) AS `service_years` 
+            WHERE `years_active` >= 3 AND `step` < 8 AND `person_id` = ?";
+    return find($sql, [$person_id]);
 }
