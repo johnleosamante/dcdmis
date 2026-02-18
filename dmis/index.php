d5a7f96cc3325d20ef1639a1e51dd6ee9eacd50b
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-18 15:54:55 +0800

    Remove activity.php reference

--- dmis/index.php
+++ dmis/index.php
@@ -4,7 +4,6 @@ require_once('../includes/function.php');
 require_once(root() . '/includes/string.php');
 require_once(root() . '/includes/email.php');
 require_once(root() . '/includes/database/database.php');
-require_once(root() . '/includes/database/activity.php');
 require_once(root() . '/includes/database/201-file.php');
 require_once(root() . '/includes/database/account.php');
 require_once(root() . '/includes/database/position.php');
