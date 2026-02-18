ef2759dd53f60ba004860248c8340838f1f4693f
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-18 15:58:22 +0800

    Remove activity.php reference

--- hrtdms/index.php
+++ hrtdms/index.php
@@ -4,7 +4,6 @@ require_once('../includes/function.php');
 require_once(root() . '/includes/string.php');
 require_once(root() . '/includes/email.php');
 require_once(root() . '/includes/database/account.php');
-require_once(root() . '/includes/database/activity.php');
 require_once(root() . '/includes/database/database.php');
 require_once(root() . '/includes/database/employee.php');
 require_once(root() . '/includes/database/position.php');
