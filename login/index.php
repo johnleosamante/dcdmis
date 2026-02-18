67fd3558cc47553cd991ed8e2f7171af0409010e
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-18 15:59:35 +0800

    Remove activity.php reference

--- login/index.php
+++ login/index.php
@@ -3,7 +3,6 @@
 require_once('../includes/function.php');
 require_once(root() . '/includes/string.php');
 require_once(root() . '/includes/database/database.php');
-require_once(root() . '/includes/database/activity.php');
 require_once(root() . '/includes/database/account.php');
 require_once(root() . '/includes/database/school.php');
 require_once(root() . '/includes/database/system-log.php');
