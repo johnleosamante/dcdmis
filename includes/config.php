3a4d72bb3e3948509f84208c780bdef5399a979f
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-18 16:22:07 +0800

    Add default values to config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 // include/config.php
 const HOSTNAME = 'localhost';
-const USER = '';
+const USER = 'root';
 const PASSWORD = '';
-const DATABASE = '';
-const DOMAIN = '';
-const PUBLIC_IP = '';
+const DATABASE = 'dcdmis';
+const DOMAIN = 'depeddipolog.test';
+const PUBLIC_IP = '127.0.0.1';
 const SITE_TITLE = 'Department of Education Schools Division of Dipolog City';
-const SITE_ALIAS = '';
+const SITE_ALIAS = 'depeddipolog';
 const SITE_DESCRIPTION = 'Department of Education Schools Division of Dipolog City Data Management Information System';
 const SITE_AUTHOR = 'DepEd Dipolog City ICT Unit';
-const SITE_AUTHOR_EMAIL = '';
-const DIVISION_ID = '';
+const SITE_AUTHOR_EMAIL = 'depeddipolog.ict@deped.gov.ph';
+const DIVISION_ID = '143';
 const DIVISION = 'SCHOOLS DIVISION OF DIPOLOG CITY';
 const REGION = 'REGION IX - ZAMBOANGA PENINSULA';
