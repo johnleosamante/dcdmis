9f32475c20d3de62bcd4c8ece2fc1567b9cf9c97
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 16:49:31 +0800

    Remove numRows and fetchAssoc and fix array name value reference

--- modules/employees/reassign-employee-dialog.php
+++ modules/employees/reassign-employee-dialog.php
@@ -9,22 +9,21 @@ require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/string.php');
 
 $employeeId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
-$employees = employee($employeeId);
+$employee = employee($employeeId);
 $modalTitle = 'Employee not found';
 $hasEmployee = false;
 
-if (numRows($employees) > 0) {
-    $employee = fetchAssoc($employees);
+if ($employee) {
     $employeeId = $employee['id'];
-    $employeeName = toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true);
+    $employeeName = toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension'], true);
     $sex = $employee['sex'];
-    $positions = fetchAssoc(position($employeeId));
-    $doa = $positions['date'];
+    $positions = position($employeeId);
+    $doa = $positions['assignment_date'];
     $stationId = $positions['station_id'];
     $station = $positions['station'];
     $positionId = $positions['position_id'];
-    $position = $positions['position'];
-    $picture = file_exists(root() . '/' . $employee['picture']) ? uri() . '/' . $employee['picture'] : uri() . '/assets/img/user.png';
+    $position = $positions['official_title'];
+    $picture = file_exists(root() . '/' . $employee['profile_picture']) ? uri() . '/' . $employee['profile_picture'] : uri() . '/assets/img/user.png';
     $modalTitle = 'Reassign Employee';
     $hasEmployee = true;
 }
@@ -38,7 +37,8 @@ if (numRows($employees) > 0) {
             <div class="modal-body">
                 <?php if ($hasEmployee) { ?>
                     <div class="image-container">
-                        <span class="d-flex justify-content-center align-middle employee-photo photo-4x rounded-circle overflow-hidden">
+                        <span
+                            class="d-flex justify-content-center align-middle employee-photo photo-4x rounded-circle overflow-hidden">
                             <img height="100%" src="<?= $picture ?>" alt="<?= $employeeName ?>">
                         </span>
                         <div class="sex-sign"><?php sex($sex) ?></div>
@@ -52,25 +52,29 @@ if (numRows($employees) > 0) {
 
                     <div class="form-group">
                         <label for="assignment" class="mb-0">Place of Assignment <?php showAsterisk() ?></label>
-                        <select id="assignment" name="assignment" class="form-control" title="Select employee place of assignment..." required>
+                        <select id="assignment" name="assignment" class="form-control"
+                            title="Select employee place of assignment..." required>
                             <option value="">Select place of assignment...</option>
                             <?php
                             $districts = districts();
-                            while ($district = fetchAssoc($districts)) : ?>
+                            foreach ($districts as $district): ?>
                                 <optgroup label="<?= $district['name'] ?>">
                                     <?php
                                     $schools = schoolsByDistrict($district['id']);
-                                    while ($school = fetchAssoc($schools)) : ?>
-                                        <option value="<?= $school['id'] ?>" <?= setOptionSelected($school['id'], $stationId) ?>><?= $school['name'] ?></option>
-                                    <?php endwhile ?>
+                                    foreach ($schools as $school): ?>
+                                        <option value="<?= $school['id'] ?>" <?= setOptionSelected($school['id'], $stationId) ?>>
+                                            <?= $school['name'] ?>
+                                        </option>
+                                    <?php endforeach ?>
                                 </optgroup>
-                            <?php endwhile ?>
+                            <?php endforeach ?>
                         </select>
                     </div>
 
                     <div class="form-group">
                         <label for="assignment-date" class="mb-0">Date of Assignment <?php showAsterisk() ?></label>
-                        <input class="form-control" type="date" id="assignment-date" name="assignment-date" value="<?= toDate($doa, 'Y-m-d', date('Y-m-d')) ?>" title="Set date of assignment..." required>
+                        <input class="form-control" type="date" id="assignment-date" name="assignment-date"
+                            value="<?= toDate($doa, 'Y-m-d', date('Y-m-d')) ?>" title="Set date of assignment..." required>
                     </div>
 
                     <?php requiredLegend(0) ?>
@@ -80,7 +84,7 @@ if (numRows($employees) > 0) {
             </div>
 
             <div class="modal-footer">
-                <?php if ($hasEmployee) : ?>
+                <?php if ($hasEmployee): ?>
                     <input type="hidden" name="verifier" value="<?= $_GET['id'] ?>">
                     <button class="btn btn-primary" name="reassign-employee" type="submit">Continue</button>
                 <?php endif;
