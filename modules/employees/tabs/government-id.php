2389ec9c5a4191f79d305494b995b0a74eabb2f1
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 10:31:44 +0800

    Remove numRows and fetchAssoc and array name values

--- modules/employees/tabs/government-id.php
+++ modules/employees/tabs/government-id.php
@@ -7,64 +7,74 @@ if ($isPis) {
     $employeeId = $userId;
 }
 
-$employeeIdentifications = employeeIdentification($employeeId);
+$identification = employeeIdentification($employeeId);
 
-if (numRows($employeeIdentifications) > 0) {
-    $identification = fetchAssoc($employeeIdentifications);
-    $cardType = $identification['card'];
-    $cardTypes = cardType($identification['card']);
-    $card = numRows($cardTypes) > 0 ? fetchAssoc($cardTypes)['name'] : 'N/A';
-
-    $number = $identification['number'];
-    $place = $identification['place'];
-    $date = toDate($identification['date'], 'Y-m-d');
+if ($identification) {
+    $cardType = $identification['card_type_id'];
+    $cardTypes = cardType($cardType);
+    $card = $cardTypes ? $cardTypes['name'] : 'N/A';
+    $number = $identification['id_number'];
+    $place = $identification['place_issued'];
+    $date = toDate($identification['date_issued'], 'Y-m-d');
 }
 ?>
 
 <div class="tab-pane fade" id="government-id">
-    <?php if ($isPis) : ?>
+    <?php if ($isPis): ?>
         <form class="py-2" action="" method="POST">
         <?php endif ?>
         <div class="row">
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
                 <div class="form-group">
-                    <label for="card-type" class="mb-0">Government Issued ID <?php if ($isPis) showAsterisk() ?></label>
-                    <?php if ($isPis) : ?>
+                    <label for="card-type" class="mb-0">Government Issued ID <?php if ($isPis)
+                        showAsterisk() ?></label>
+                    <?php if ($isPis): ?>
                         <select class="form-control" id="card-type" name="card-type" required>
                             <option value="">Select...</option>
                             <?php $cardTypes = cardTypes();
-                            while ($type = fetchAssoc($cardTypes)) : ?>
-                                <option value="<?= $type['id'] ?>" <?= setOptionSelected($type['id'], $cardType) ?>><?= $type['name'] ?></option>
-                            <?php endwhile ?>
+                            foreach ($cardTypes as $type): ?>
+                                <option value="<?= $type['id'] ?>" <?= setOptionSelected($type['id'], $cardType) ?>>
+                                    <?= $type['name'] ?>
+                                </option>
+                            <?php endforeach ?>
                         </select>
-                    <?php else : ?>
-                        <input type="text" class="form-control" id="card-type" name="card-type" value="<?= $card ?>" readonly>
+                    <?php else: ?>
+                        <input type="text" class="form-control" id="card-type" name="card-type" value="<?= $card ?>"
+                            readonly>
                     <?php endif ?>
                 </div>
 
                 <div class="form-group">
-                    <label for="card-number" class="mb-0">ID/License/Passport No. <?php if ($isPis) showAsterisk() ?></label>
-                    <input type="text" class="form-control" id="card-number" name="card-number" value="<?= $number ?>" <?= setActiveNavigation(!$isPis, 'readonly') ?>>
+                    <label for="card-number" class="mb-0">ID/License/Passport No.
+                        <?php if ($isPis)
+                            showAsterisk() ?></label>
+                        <input type="text" class="form-control" id="card-number" name="card-number" value="<?= $number ?>"
+                        <?= setActiveNavigation(!$isPis, 'readonly') ?>>
                 </div>
 
                 <div class="form-group">
-                    <label for="card-date" class="mb-0">Date of Issuance <?php if ($isPis) showAsterisk() ?></label>
-                    <input type="<?= $isPis ? 'date' : 'text' ?>" class="form-control" id="card-date" name="card-date" value="<?= $date ?>" <?= setActiveNavigation(!$isPis, 'readonly') ?>>
+                    <label for="card-date" class="mb-0">Date of Issuance <?php if ($isPis)
+                        showAsterisk() ?></label>
+                        <input type="<?= $isPis ? 'date' : 'text' ?>" class="form-control" id="card-date" name="card-date"
+                        value="<?= $date ?>" <?= setActiveNavigation(!$isPis, 'readonly') ?>>
                 </div>
 
                 <div class="form-group">
-                    <label for="card-place" class="mb-0">Place of Issuance <?php if ($isPis) showAsterisk() ?></label>
-                    <input type="text" class="form-control" id="card-place" name="card-place" value="<?= $place ?>" <?= setActiveNavigation(!$isPis, 'readonly') ?>>
+                    <label for="card-place" class="mb-0">Place of Issuance <?php if ($isPis)
+                        showAsterisk() ?></label>
+                        <input type="text" class="form-control" id="card-place" name="card-place" value="<?= $place ?>"
+                        <?= setActiveNavigation(!$isPis, 'readonly') ?>>
                 </div>
 
-                <?php if ($isPis) : ?>
+                <?php if ($isPis): ?>
                     <?php requiredLegend() ?>
 
-                    <input name="update-identification" type="submit" value="Update Government Issued ID" class="btn btn-primary btn-block btn-lg">
+                    <input name="update-identification" type="submit" value="Update Government Issued ID"
+                        class="btn btn-primary btn-block btn-lg">
                 <?php endif ?>
             </div>
         </div>
-        <?php if ($isPis) : ?>
+        <?php if ($isPis): ?>
         </form>
     <?php endif ?>
 </div>
