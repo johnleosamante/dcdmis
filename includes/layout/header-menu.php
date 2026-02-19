fadfcce0f01c4353420be1a4d8e9ff0bac7b726a
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 13:13:19 +0800

    Remove fetchAssoc and numRows and fix array name values

--- includes/layout/header-menu.php
+++ includes/layout/header-menu.php
@@ -1,9 +1,9 @@
 <?php
 // includes/layout/header-menu.php
-$user = fetchAssoc(employee($userId));
-$displayName = toName($user['lname'], $user['fname'], $user['mname'], $user['ext'], true, true);
-$position = fetchAssoc(position($userId))['position'];
-$displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $user['picture'] : uri() . '/assets/img/user.png';
+$user = employee($userId);
+$displayName = toName($user['last_name'], $user['first_name'], $user['middle_name'], $user['name_extension'], true, true);
+$position = position($userId);
+$displayPhoto = file_exists(root() . '/' . $user['profile_picture']) ? uri() . '/' . $user['profile_picture'] : uri() . '/assets/img/user.png';
 ?>
 
 <nav class="navbar navbar-expand navbar-light bg-white topbar static-top shadow">
@@ -11,10 +11,12 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
         <i class="fa fa-bars"></i>
     </button>
 
-    <?php if ($isDts || $isHrmis || $isHrtdms) : ?>
-        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-0 my-2 my-md-0 mw-100 navbar-search" method="POST" action="">
+    <?php if ($isDts || $isHrmis || $isHrtdms): ?>
+        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-0 my-2 my-md-0 mw-100 navbar-search" method="POST"
+            action="">
             <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" placeholder="Search..." aria-label="Search" name="primary-search-text" autofocus required>
+                <input type="text" class="form-control bg-light border-0 small" placeholder="Search..." aria-label="Search"
+                    name="primary-search-text" autofocus required>
 
                 <div class="input-group-append">
                     <button class="btn btn-primary" type="submit" name="primary-search-button">
@@ -26,16 +28,19 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
     <?php endif ?>
 
     <ul class="navbar-nav ml-auto">
-        <?php if ($isDts || $isHrmis || $isHrtdms) : ?>
+        <?php if ($isDts || $isHrmis || $isHrtdms): ?>
             <li class="nav-item dropdown no-arrow d-sm-none">
-                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-search fa-fw"></i>
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                    aria-labelledby="searchDropdown">
                     <form class="form-inline mr-auto w-100 navbar-search" method="POST" action="">
                         <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search..." aria-label="Search" name="primary-search-text" required>
+                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search..."
+                                aria-label="Search" name="primary-search-text" required>
 
                             <div class="input-group-append">
                                 <button class="btn btn-primary" type="submit" name="primary-search-button">
@@ -53,36 +58,37 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
             $increment = 1;
             $alertCount = 0;
             $hasStepIncrement = $hasLoyaltyAward = false;
-            $employeeSteps = stepIncrement($userId);
+            $employeeStep = stepIncrement($userId);
             $employeeLoyalties = loyaltyAward($userId);
 
-            if (numRows($employeeSteps) > 0) {
-                $employeeStep = fetchAssoc($employeeSteps);
-                $increment = (int)$employeeStep['step'];
+            if ($employeeStep) {
+                $increment = (int) $employeeStep['step'];
                 $hasStepIncrement = true;
                 $alertCount++;
                 $increment++;
             }
 
-            if (numRows($employeeLoyalties) > 0) {
+            if ($employeeLoyalties) {
                 $hasLoyaltyAward = true;
                 $alertCount++;
             }
             ?>
 
-            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-bell fa-fw"></i>
-                <?php if ($alertCount) : ?>
+                <?php if ($alertCount): ?>
                     <span class="badge badge-danger badge-counter"><?= $alertCount ?></span>
                 <?php endif ?>
             </a>
 
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                aria-labelledby="alertsDropdown">
                 <h6 class="dropdown-header">
                     Alerts Center
                 </h6>
-                <?php if ($alertCount) : ?>
-                    <?php if ($hasStepIncrement) : ?>
+                <?php if ($alertCount): ?>
+                    <?php if ($hasStepIncrement): ?>
                         <a class="dropdown-item d-flex align-items-center" href="#">
                             <div class="mr-3">
                                 <div class="icon-circle bg-primary">
@@ -96,7 +102,7 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
                         </a>
                     <?php endif ?>
 
-                    <?php if ($hasLoyaltyAward) : ?>
+                    <?php if ($hasLoyaltyAward): ?>
                         <a class="dropdown-item d-flex align-items-center" href="#">
                             <div class="mr-3">
                                 <div class="icon-circle bg-success">
@@ -109,7 +115,7 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
                             </div>
                         </a>
                     <?php endif ?>
-                <?php else : ?>
+                <?php else: ?>
                     <div class="dropdown-item d-flex align-items-center">
                         <div class="font-weight-light text-center my-2">No new alerts at the moment.</div>
                     </div>
@@ -118,11 +124,13 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
         </li>
 
         <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="completionDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="completionDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-tasks fa-fw"></i>
             </a>
 
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="completionDropdown">
+            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                aria-labelledby="completionDropdown">
                 <h6 class="dropdown-header bg-primary border-primary">
                     My Employee Information Status
                 </h6>
@@ -143,11 +151,11 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
         <div class="topbar-divider d-none d-sm-block"></div>
 
         <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="<?= strtoupper($displayName) ?>">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" title="<?= strtoupper($displayName) ?>">
                 <span class="mr-2 d-none d-md-inline">
                     <div class="text-gray-600 small"><?= strtoupper($displayName) ?></div>
-
-                    <div class="text-xs text-gray-500"><?= strtoupper($position) ?></div>
+                    <div class="text-xs text-gray-500"><?= strtoupper($position['official_title']) ?></div>
                 </span>
 
                 <span class="d-flex justify-content-center align-middle img-profile rounded-circle overflow-hidden">
@@ -159,7 +167,7 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
                 <?php
                 linkDropdownItem(uri() . '/pis', 'Profile', 'fa-user-tie', 'Personnel Information System');
 
-                if (numRows(dtsUser($userId)) > 0) {
+                if (dtsUser($userId)) {
                     linkDropdownItem(uri() . '/dts', 'Tracking', 'fa-exchange-alt', 'Document Tracking System');
                 }
 
@@ -198,20 +206,18 @@ $displayPhoto = file_exists(root() . '/' . $user['picture']) ? uri() . '/' . $us
 
 <div class="background-cover banner text-uppercase text-gray-700">
     <?php
-    $schools = schoolDetailsById($stationId);
-
-    if (numRows($schools)) :
-        $school = fetchAssoc($schools) ?>
+    $school = schoolById($stationId);
+    if ($school): ?>
         <h1 class="h3 m-0"><?= $school['name'] ?></h1>
 
-        <?php if (!empty($school['address'])) : ?>
+        <?php if (!empty($school['address'])): ?>
             <div class="small m-0"><?= $school['address'] ?></div>
-    <?php endif;
+        <?php endif;
     endif ?>
 
     <h2 class="h1 m-0 mt-4"><?= strtoupper($appTitle) ?></h2>
 
-    <?php if ($hasPortal && !$isSchoolPortal && $isDts) : ?>
+    <?php if ($hasPortal && !$isSchoolPortal && $isDts): ?>
         <h3 class="h4 m-0"><?= stationName($station) ?></h3>
     <?php endif ?>
 </div>
