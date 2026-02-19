0ee69ca11b05ee766f48b5165374ada4672f3c96
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 11:56:20 +0800

    Remove fetchArray and switch to foreach loop

--- modules/activity/page.php
+++ modules/activity/page.php
@@ -1,10 +1,9 @@
 <?php
 // modules/activity/page.php
 $employeeId = isset($_GET['id']) ? sanitize(decode($_GET['id'])) : $userId;
-$employees = employee($employeeId);
+$employee = employee($employeeId);
 
-if (numRows($employees) > 0) {
-    $employee = fetchAssoc($employees);
+if ($employee) {
     $employeeId = $employee['id'];
 } else {
     require_once(root() . '/modules/error/no-results-found.php');
@@ -25,12 +24,13 @@ messageAlert($showAlert, $message, $success);
 
 <div class="card border-left-primary shadow mb-4">
     <div class="card-header py-3">
-        <?php contentTitle('Activity Log : ' . strtoupper(toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext']))) ?>
+        <?php contentTitle('Activity Log : ' . strtoupper(toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension']))) ?>
     </div>
 
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-hover table-striped table-bordered mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+            <table class="table table-hover table-striped table-bordered mb-0 text-center" id="data-table" width="100%"
+                cellspacing="0">
                 <thead>
                     <tr>
                         <th class="align-middle" width="5%">#</th>
@@ -44,13 +44,13 @@ messageAlert($showAlert, $message, $success);
                     <?php
                     $query = userLog($employeeId);
                     $no = 0;
-                    while ($row = fetchAssoc($query)) : ?>
+                    foreach ($query as $row): ?>
                         <tr class="text-uppercase">
                             <td class="align-middle"><?= ++$no ?></td>
                             <td class="align-middle"><?= toDatetime($row['datetime']) ?></td>
                             <td class="text-left align-middle"><?= $row['activity'] ?></td>
                             <td class="text-center align-middle">
-                                <?php if (numRows(employee($row['target'])) > 0) {
+                                <?php if (employee($row['target'])) {
                                     $userLabel = $userId === $row['target'] ? 'YOU' : userName($row['target']);
                                     modalItem(uri() . '/modules/users/user-info-dialog.php?id=' . cipher($row['target']), $userLabel);
                                 } else {
@@ -59,7 +59,7 @@ messageAlert($showAlert, $message, $success);
                                 <br><small><?= '(' . $row['ip'] . ')' ?></small>
                             </td>
                         </tr>
-                    <?php endwhile ?>
+                    <?php endforeach ?>
                 </tbody>
 
                 <tfoot>
