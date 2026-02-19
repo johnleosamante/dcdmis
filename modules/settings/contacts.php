9c40d3b629752d455fbd8642a84afab5a0b69e5d
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 09:03:54 +0800

    Remove numRows and fix array name values

--- modules/settings/contacts.php
+++ modules/settings/contacts.php
@@ -1,15 +1,14 @@
 <?php
 // modules/settings/contacts.php
 $primaryEmail = $alternateEmail = $telephone = $mobileNo = $alternateMobile = '';
-$employeeDetails = employeeContactDetails($userId);
+$contact = employeeContactDetails($userId);
 
-if (numRows($employeeDetails) > 0) {
-    $contact = fetchAssoc($employeeDetails);
-    $primaryEmail = $contact['email'];
-    $alternateEmail = $contact['alternate_email'];
+if ($contact) {
+    $primaryEmail = $contact['email_address'];
+    $alternateEmail = $contact['alternate_email_address'];
     $telephone = $contact['telephone'];
-    $mobileNo = $contact['mobile'];
-    $alternateMobile = $contact['alternate_mobile'];
+    $mobileNo = $contact['mobile_number'];
+    $alternateMobile = $contact['alternate_mobile_number'];
 }
 ?>
 <div class="tab-pane fade" id="contact-details">
@@ -18,7 +17,8 @@ if (numRows($employeeDetails) > 0) {
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
                 <div class="form-group">
                     <label for="primary-email" class="mb-0">DepEd Email Address</label>
-                    <input type="email" id="primary-email" class="form-control" autocomplete="false" value="<?= $primaryEmail ?>" readonly>
+                    <input type="email" id="primary-email" class="form-control" autocomplete="false"
+                        value="<?= $primaryEmail ?>" readonly>
                 </div>
             </div>
         </div>
@@ -42,7 +42,8 @@ if (numRows($employeeDetails) > 0) {
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
                 <div class="form-group">
                     <label for="alternate-email" class="mb-0">Alternate Email Address <?php showAsterisk() ?></label>
-                    <input type="email" id="alternate-email" name="alternate-email" class="form-control" autocomplete="false" value="<?= $alternateEmail ?>" required>
+                    <input type="email" id="alternate-email" name="alternate-email" class="form-control"
+                        autocomplete="false" value="<?= $alternateEmail ?>" required>
                 </div>
             </div>
         </div>
@@ -50,7 +51,8 @@ if (numRows($employeeDetails) > 0) {
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
                 <div class="form-group">
                     <label for="alternate-mobile" class="mb-0">Alternate Mobile Number <?php showAsterisk() ?></label>
-                    <input type="text" id="alternate-mobile" name="alternate-mobile" class="form-control" value="<?= $alternateMobile ?>" required>
+                    <input type="text" id="alternate-mobile" name="alternate-mobile" class="form-control"
+                        value="<?= $alternateMobile ?>" required>
                 </div>
             </div>
         </div>
@@ -59,7 +61,8 @@ if (numRows($employeeDetails) > 0) {
 
         <div class="row">
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
-                <input name="update-contact-details" type="submit" value="Update Contact Details" class="btn btn-primary btn-block btn-lg">
+                <input name="update-contact-details" type="submit" value="Update Contact Details"
+                    class="btn btn-primary btn-block btn-lg">
             </div>
         </div>
     </form>
