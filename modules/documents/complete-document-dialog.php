fdb223e6bd42842bd89a53e8f63e451b2f4d2808
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 13:11:16 +0800

    Remove numRows and fetchAssoc

--- modules/documents/complete-document-dialog.php
+++ modules/documents/complete-document-dialog.php
@@ -7,18 +7,17 @@ require_once(root() . '/includes/database/document.php');
 require_once(root() . '/includes/layout/components.php');
 
 $documentId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
-$documents = document($documentId);
+$document = document($documentId);
 $description = '';
 $modalTitle = 'Document not found';
 $hasDocument = false;
 
-if (numRows($documents) > 0) {
-    $document = fetchAssoc($documents);
+if ($document) {
     $documentId = $document['id'];
     $description = $document['description'];
-    $type = $document['type'];
-    $documentLogs = fetchAssoc(documentLogs($documentId));
-    $hasDocument = !str_contains(strtolower($documentLogs['status']), 'complete') && !str_contains(strtolower($documentLogs['status']), 'cancel') && $documentLogs['from'] === $station && $documentLogs['to'] === '-';
+    $type = $document['document_type_id'];
+    $documentLogs = documentLogs($documentId)[0];
+    $hasDocument = !str_contains(strtolower($documentLogs['status']), 'complete') && !str_contains(strtolower($documentLogs['status']), 'cancel') && $documentLogs['received_from'] === $station && $documentLogs['forwarded_to'] === '-';
     $modalTitle = $hasDocument ? 'Mark Completed Document' : $modalTitle;
 }
 ?>
@@ -32,22 +31,26 @@ if (numRows($documents) > 0) {
                 <?php if ($hasDocument) { ?>
                     <div class="form-group">
                         <label for="code" class="mb-0">Code</label>
-                        <input id="code" type="text" value="<?= $documentId ?>" class="form-control text-uppercase" disabled>
+                        <input id="code" type="text" value="<?= $documentId ?>" class="form-control text-uppercase"
+                            disabled>
                     </div>
 
                     <div class="form-group">
                         <label for="type" class="mb-0">Type</label>
-                        <input id="type" class="form-control text-uppercase" value="<?= fetchArray(documentType($type))['name'] ?>" disabled>
+                        <input id="type" class="form-control text-uppercase" value="<?= documentType($type)['name'] ?>"
+                            disabled>
                     </div>
 
                     <div class="form-group">
                         <label for="description" class="mb-0">Description</label>
-                        <textarea id="description" class="form-control text-uppercase" rows="3" disabled><?= $description ?></textarea>
+                        <textarea id="description" class="form-control text-uppercase" rows="3"
+                            disabled><?= $description ?></textarea>
                     </div>
 
                     <div class="form-group mb-0">
                         <label for="remarks" class="mb-0">Remarks</label>
-                        <textarea id="remarks" name="remarks" class="form-control" rows="3" autofocus placeholder="Type remarks..." title="Type document remarks..."></textarea>
+                        <textarea id="remarks" name="remarks" class="form-control" rows="3" autofocus
+                            placeholder="Type remarks..." title="Type document remarks..."></textarea>
                     </div>
                 <?php } else {
                     missingAlert($modalTitle, 'fa-times-circle');
@@ -55,7 +58,7 @@ if (numRows($documents) > 0) {
             </div>
 
             <div class="modal-footer">
-                <?php if ($hasDocument) : ?>
+                <?php if ($hasDocument): ?>
                     <input type="hidden" name="verifier" value="<?= $_GET['id'] ?>">
                     <button class="btn btn-primary" name="complete-document" type="submit">Continue</button>
                 <?php endif ?>
