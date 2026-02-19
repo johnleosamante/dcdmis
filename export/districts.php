784a11a87e8e3cc21349a0f513a053ae976244ce
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 15:03:19 +0800

    Remove fetchArray and numRows

--- export/districts.php
+++ export/districts.php
@@ -31,17 +31,16 @@ require_once(root() . '/includes/database/utility.php');
         $i = 1;
         $query = districts();
 
-        while ($row = fetchArray($query)) : ?>
+        foreach ($query as $row): ?>
             <tr>
                 <td><?= $i++ ?></td>
                 <td><?= strtoupper($row['name']) ?></td>
                 <td><?= userName($row['psds'], true) ?></td>
                 <?php
-                $schoolCount = districtSchoolCount($row['id']);
+                $count = districtSchoolCount($row['id']);
                 $es = $hs = $is = $total = 0;
 
-                if (numRows($schoolCount) > 0) {
-                    $count = fetchAssoc($schoolCount);
+                if ($count) {
                     $es = $count['es'];
                     $hs = $count['hs'];
                     $is = $count['is'];
@@ -53,7 +52,7 @@ require_once(root() . '/includes/database/utility.php');
                 <td><?= toHandleNull($is, '0') ?></td>
                 <td><?= $total ?></td>
             </tr>
-        <?php endwhile ?>
+        <?php endforeach ?>
         <tr>
             <td colspan="7"><?= 'Data as of ' . date("F j, Y, g:i a") ?></td>
         </tr>
