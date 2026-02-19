0f5830fced4b9376a4b37976352d3f308e71b724
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 15:00:27 +0800

    Replace numRows to count

--- dmis/sidebar-menu.php
+++ dmis/sidebar-menu.php
@@ -2,11 +2,11 @@
 // dmis/sidebar-menu.php
 sidebarDivider();
 
-$employeeCount = number_format(numRows(activeEmployees()));
-$districtCount = number_format(numRows(districts()));
-$schoolCount = number_format(numRows(schools()));
-$sectionCount = number_format(numRows(sections()));
-$userCount = number_format(numRows(users()));
+$employeeCount = number_format(count(activeEmployees()));
+$districtCount = number_format(count(districts()));
+$schoolCount = number_format(count(schools()));
+$sectionCount = number_format(count(sections()));
+$userCount = number_format(count(users()));
 
 sidebarMenuItem(customUri('dmis', 'Employees'), 'Employees', 'fa-users', isset($url) && str_contains($url, 'Employees'), $employeeCount);
 sidebarMenuItem(customUri('dmis', 'Districts'), 'Districts', 'fa-map-marked-alt', isset($url) && str_contains($url, 'District'), $districtCount);
