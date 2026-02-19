ed4433a19cc43d376569413b58054c3768264a23
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 10:25:36 +0800

    Remove numRows and fetchAssoc

--- modules/employees/tabs/work-experience.php
+++ modules/employees/tabs/work-experience.php
@@ -2,7 +2,8 @@
 // modules/employees/tabs/work-experience.php
 ?>
 
-<div class="tab-pane fade<?= setActiveNavigation(isset($activeTab) && $activeTab === 'work-experience', 'show active') ?>" id="work-experience">
+<div class="tab-pane fade<?= setActiveNavigation(isset($activeTab) && $activeTab === 'work-experience', 'show active') ?>"
+    id="work-experience">
     <div class="row my-3">
         <div class="col table-responsive">
             <table width="100%" class="table table-striped table-bordered table-hover mb-0 text-center">
@@ -16,7 +17,7 @@
                         <th class="align-middle" width="10%">Salary / Job / Pay Grade &amp; Step Increment</th>
                         <th class="align-middle" width="10%">Status of Appointment</th>
                         <th class="align-middle" width="10%">Government Service</th>
-                        <?php if ($editMode) : ?>
+                        <?php if ($editMode): ?>
                             <th class="align-middle" width="5%">Actions</th>
                         <?php endif ?>
                     </tr>
@@ -26,32 +27,37 @@
                     <?php
                     $experiences = experiences($employeeId);
 
-                    if (numRows($experiences) > 0) {
-                        while ($experience = fetchAssoc($experiences)) : ?>
+                    if ($experiences) {
+                        foreach ($experiences as $experience): ?>
                             <tr class="text-uppercase">
-                                <td class="align-middle"><?= toDate($experience['from']) ?></td>
-                                <td class="align-middle"><?= $experience['ispresent'] ? 'PRESENT' : toDate($experience['to']) ?></td>
-                                <td class="align-middle"><?= $experience['position'] ?></td>
-                                <td class="align-middle"><?= $experience['organization'] ?></td>
-                                <td class="align-middle"><?= !empty($experience['salary']) ? toCurrency($experience['salary']) : 'N/A' ?></td>
-                                <td class="align-middle"><?= toHandleNull($experience['sg'], 'N/A') ?></td>
-                                <td class="align-middle"><?= $experience['status'] ?></td>
-                                <td class="align-middle"><?= $experience['isgovernment'] ?></td>
-                                <?php if ($editMode) : ?>
+                                <td class="align-middle"><?= toDate($experience['from_date']) ?></td>
+                                <td class="align-middle">
+                                    <?= $experience['is_present'] ? 'PRESENT' : toDate($experience['to_date']) ?>
+                                </td>
+                                <td class="align-middle"><?= $experience['designation'] ?></td>
+                                <td class="align-middle"><?= $experience['agency_company'] ?></td>
+                                <td class="align-middle">
+                                    <?= !empty($experience['monthly_salary']) ? toCurrency($experience['monthly_salary']) : 'N/A' ?>
+                                </td>
+                                <td class="align-middle"><?php //TODO ?></td>
+                                <td class="align-middle"><?= $experience['appointment_status'] ?></td>
+                                <td class="align-middle"><?= (bool) $experience['is_government_service'] ? 'Y' : 'N' ?>
+                                </td>
+                                <?php if ($editMode): ?>
                                     <td class="align-middle text-capitalize">
                                         <div class="dropdown no-arrow">
                                             <?php dropdownEllipsis() ?>
                                             <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
-                                                <?php modalDropdownItem(uri() . '/modules/service-record/save-service-record-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($experience['no']), 'Edit', 'fa-edit', 'Edit Service Record');
-                                                modalDropdownItem(uri() . '/modules/service-record/save-service-record-dialog.php?c=' . cipher($employeeId) . '&e=' . cipher($employeeId) . '&id=' . cipher($experience['no']), 'Copy', 'fa-copy', 'Copy Service Record') ?>
+                                                <?php modalDropdownItem(uri() . '/modules/service-record/save-service-record-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($experience['id']), 'Edit', 'fa-edit', 'Edit Service Record');
+                                                modalDropdownItem(uri() . '/modules/service-record/save-service-record-dialog.php?c=' . cipher($employeeId) . '&e=' . cipher($employeeId) . '&id=' . cipher($experience['id']), 'Copy', 'fa-copy', 'Copy Service Record') ?>
                                                 <div class="dropdown-divider"></div>
-                                                <?php modalDropdownItem(uri() . '/modules/service-record/delete-service-record-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($experience['no']), 'Delete', 'fa-trash', 'Delete Service Record') ?>
+                                                <?php modalDropdownItem(uri() . '/modules/service-record/delete-service-record-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($experience['id']), 'Delete', 'fa-trash', 'Delete Service Record') ?>
                                             </div>
                                         </div>
                                     </td>
                                 <?php endif ?>
                             </tr>
-                        <?php endwhile;
+                        <?php endforeach;
                     } else { ?>
                         <tr>
                             <td colspan="<?= $editMode ? '8' : '7' ?>" class="align-middle">No data available in table</td>
@@ -69,7 +75,7 @@
                         <th class="align-middle" width="10%">Salary / Job / Pay Grade &amp; Step Increment</th>
                         <th class="align-middle" width="10%">Status of Appointment</th>
                         <th class="align-middle" width="10%">Government Service</th>
-                        <?php if ($editMode) : ?>
+                        <?php if ($editMode): ?>
                             <th class="align-middle" width="5%">Actions</th>
                         <?php endif ?>
                     </tr>
