9500d68334a4586ea4251ad5ee773f1d13165d21
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 11:56:53 +0800

    Remove numRows and fetchAssoc

--- modules/201-file/page.php
+++ modules/201-file/page.php
@@ -12,10 +12,9 @@ if ($isPis && $userId !== $employeeId) {
     return;
 }
 
-$employees = employee($employeeId);
+$employee = employee($employeeId);
 
-if (numRows($employees) > 0) {
-    $employee = fetchAssoc($employees);
+if ($employee) {
     $employeeId = $employee['id'];
 } else {
     require_once(root() . '/modules/error/no-results-found.php');
@@ -49,7 +48,7 @@ if (!is_dir($uploadDirectory)) {
 <div class="card border-left-primary shadow mb-4">
     <div class="card-header py-3">
         <?php if ($isHrmis) {
-            contentTitleWithModal('201 Files : ' . strtoupper(toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'])), uri() . '/modules/201-file/save-201-file-dialog.php?e=' . cipher($employeeId), 'Add', 'fa-plus');
+            contentTitleWithModal('201 Files : ' . strtoupper(toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension'])), uri() . '/modules/201-file/save-201-file-dialog.php?e=' . cipher($employeeId), 'Add', 'fa-plus');
         } else {
             contentTitleWithLink('201 Files', uri() . '/pis');
         } ?>
@@ -57,7 +56,8 @@ if (!is_dir($uploadDirectory)) {
 
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-hover table-bordered table-striped mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+            <table class="table table-hover table-bordered table-striped mb-0 text-center" id="data-table" width="100%"
+                cellspacing="0">
                 <thead>
                     <tr>
                         <th class="align-middle" width="25%">Uploaded on</th>
@@ -70,29 +70,29 @@ if (!is_dir($uploadDirectory)) {
                     <?php
                     $results = fileAttachments($employeeId);
 
-                    while ($row = fetchAssoc($results)) : ?>
+                    foreach ($results as $row): ?>
                         <tr class="text-uppercase">
-                            <td class="align-middle"><?= toDatetime($row['datetime']) ?></td>
+                            <td class="align-middle"><?= toDatetime($row['created_at']) ?></td>
                             <td class="align-middle text-left"><?= $row['description'] ?></td>
                             <td class="align-middle text-capitalize">
                                 <div class="dropdown no-arrow">
                                     <?php dropdownEllipsis() ?>
                                     <div class="dropdown-menu dropdown-menu-righ shadow animated--fade-in">
                                         <?php
-                                        previewLinkDropdownItem(uri() . '/' . $row['filename'], 'Preview', 'fa-eye', 'Preview ' . $row['description']);
-                                        downloadLinkDropdownItem(uri() . '/' . $row['filename'], 'Download', 'fa-download', 'Download ' . $row['description'], $row['description'] . '.' . $row['ext'], true);
+                                        previewLinkDropdownItem(uri() . '/' . $row['file_name'], 'Preview', 'fa-eye', 'Preview ' . $row['description']);
+                                        downloadLinkDropdownItem(uri() . '/' . $row['file_name'], 'Download', 'fa-download', 'Download ' . $row['description'], $row['description'] . '.' . $row['file_extension'], true);
 
                                         if ($isHrmis) {
-                                            modalDropdownItem(uri() . '/modules/201-file/save-201-file-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($row['no']), 'Edit', 'fa-edit', 'Edit 201 File');
-                                            modalDropdownItem(uri() . '/modules/201-file/save-201-file-dialog.php?c=' . cipher($employeeId) . '&e=' . cipher($employeeId) . '&id=' . cipher($row['no']), 'Copy', 'fa-copy', 'Copy 201 File') ?>
+                                            modalDropdownItem(uri() . '/modules/201-file/save-201-file-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($row['id']), 'Edit', 'fa-edit', 'Edit 201 File');
+                                            modalDropdownItem(uri() . '/modules/201-file/save-201-file-dialog.php?c=' . cipher($employeeId) . '&e=' . cipher($employeeId) . '&id=' . cipher($row['id']), 'Copy', 'fa-copy', 'Copy 201 File') ?>
                                             <div class="dropdown-divider"></div>
-                                        <?php modalDropdownItem(uri() . '/modules/201-file/delete-201-file-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($row['no']), 'Delete', 'fa-trash', 'Delete 201 File');
+                                            <?php modalDropdownItem(uri() . '/modules/201-file/delete-201-file-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($row['id']), 'Delete', 'fa-trash', 'Delete 201 File');
                                         } ?>
                                     </div>
                                 </div>
                             </td>
                         </tr>
-                    <?php endwhile ?>
+                    <?php endforeach ?>
                 </tbody>
 
                 <tfoot>
