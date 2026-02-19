5d06c7d3f20ce30f407a5c8771e36dca20c546be
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 09:22:01 +0800

    Remove fetchArray and fix array name values

--- print/document-tracking-slip.php
+++ print/document-tracking-slip.php
@@ -9,17 +9,23 @@ $multiplePage = true;
 $showBarCode = true;
 $showQR = true;
 $showStationInfo = false;
-$isSchoolPortal = $stationId !== divisionId();
-$section = strtoupper(stationName($station));
-$school = fetchArray(schoolDetailsById($stationId));
+
+require_once(root() . '/includes/database/document.php');
+require_once(root() . '/includes/database/employee.php');
+require_once(root() . '/includes/database/position.php');
+
+$code = strtoupper(sanitize(decode($_GET['id'])));
+$title = "{$url} : {$code}";
+$document = documentOrigin($code);
+$section = strtoupper(stationName($document['created_from']));
+
+$school = schoolById($document['created_from']);
 $stationLogo = !empty($school['logo']) ? root() . '/' . $school['logo'] : null;
 $address = $school['address'];
 $telephone = $school['telephone'];
 $email = $school['email'];
 $website = $school['website'];
 $fbPage = $school['fb_page'];
-$code = strtoupper(sanitize(decode($_GET['id'])));
-$title = $url . ' : ' . $code;
 
 require_once(root() . '/print/print-layout.php');
 
@@ -32,31 +38,26 @@ $pdf->AddPage();
 $pdf->AddFont('calibri', '', 'calibri.php');
 $pdf->AddFont('calibrib', 'B', 'calibrib.php');
 
-require_once(root() . '/includes/database/document.php');
-require_once(root() . '/includes/database/employee.php');
-require_once(root() . '/includes/database/position.php');
-
-$document = fetchAssoc(documentOrigin($code));
-$dateCreated = toDate($document['datetime'], 'F d, Y');
+$dateCreated = toDate($document['created_at'], 'F d, Y');
 $description = toHandleEncoding($document['description']);
-$employee = toHandleEncoding(userName($document['user'], true));
-$employeePosition = toHandleEncoding(fetchAssoc(position($document['user']))['position']);
+$employee = toHandleEncoding(userName($document['processed_by'], true));
+$employeePosition = toHandleEncoding(position($document['processed_by'])['official_title']);
 $documentStatus = strtolower($document['status']);
 $status = str_contains($documentStatus, 'complete') ? ' (Completed)' : '';
 $status = str_contains($documentStatus, 'cancel') ? ' (Canceled)' : $status;
-$pdf->SetFont('calibrib',  'B', 18);
+$pdf->SetFont('calibrib', 'B', 18);
 $pdf->Cell(0, 0, 'DOCUMENT TRACKING SLIP', 0, 0, 'C');
 $pdf->Ln(5);
-$pdf->SetFont('calibri',  '', 9);
+$pdf->SetFont('calibri', '', 9);
 $pdf->Cell(0, 0, '(Document Attachment)', 0, 0, 'C');
 $pdf->Ln(15);
-$pdf->SetFont('calibri',  '', 11);
+$pdf->SetFont('calibri', '', 11);
 $pdf->Cell(0, 0, $dateCreated, 0, 0, 'R');
 $pdf->Ln(10);
-$pdf->SetFont('calibrib',  'B', 17);
+$pdf->SetFont('calibrib', 'B', 17);
 $pdf->Cell(0, 0, $code . toHandleEncoding($status));
 $pdf->Ln(10);
-$pdf->SetFont('calibri',  '', 11);
+$pdf->SetFont('calibri', '', 11);
 $pdf->Write(5, $description);
 $pdf->Ln(20);
 $innerPage = $width - ($margin * 2);
@@ -68,9 +69,9 @@ $pdf->Ln(5);
 $pdf->SetFont('calibri', '', 11);
 $pdf->Cell($innerPage / 2, 0, $employeePosition, 0, 0, 'C');
 
-if ($document['user'] !== $document['head']) {
-    $stationHead = toHandleEncoding(userName($document['head'], true));
-    $stationHeadPosition = toHandleEncoding(fetchAssoc(position($document['head']))['position']);
+if ($document['processed_by'] !== $document['head_id']) {
+    $stationHead = toHandleEncoding(userName($document['head_id'], true));
+    $stationHeadPosition = toHandleEncoding(position($document['head_id'])['official_title']);
     $pdf->Ln(10);
     $pdf->SetX($width / 2);
     $pdf->Cell($innerPage / 2, 0, 'Noted by:');
