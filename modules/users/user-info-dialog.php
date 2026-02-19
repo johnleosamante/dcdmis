da7e307d0347ed83d387a74a0e5168a866f903cf
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-19 09:01:25 +0800

    Remove numRows and fix array name values

--- modules/users/user-info-dialog.php
+++ modules/users/user-info-dialog.php
@@ -9,23 +9,22 @@ require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/string.php');
 
 $employeeId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
-$employees = employee($employeeId);
+$employee = employee($employeeId);
 $modalTitle = 'Employee not found';
 $hasEmployee = false;
 
-if (numRows($employees) > 0) {
-    $employee = fetchAssoc($employees);
+if ($employee) {
     $employeeId = $employee['id'];
-    $employeeName = toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true);
+    $employeeName = toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension'], true);
     $sex = $employee['sex'];
     $status = $employee['status'];
-    $positions = fetchAssoc(position($employeeId));
-    $stationId = $positions['station_id'];
-    $station = $positions['station'];
-    $positionId = $positions['position_id'];
-    $position = $positions['position'];
-    $depedEmail = $employee['email'];
-    $picture = file_exists(root() . '/' . $employee['picture']) ? uri() . '/' . $employee['picture'] : uri() . '/assets/img/user.png';
+    $position = position($employeeId);
+    $stationId = $position['station_id'];
+    $station = $position['station'];
+    $positionId = $position['position_id'];
+    $position = $position['official_title'];
+    $depedEmail = $employee['email_address'];
+    $picture = file_exists(root() . '/' . $employee['profile_picture']) ? uri() . '/' . $employee['profile_picture'] : uri() . '/assets/img/user.png';
     $modalTitle = 'Employee Information';
     $hasEmployee = true;
 }
