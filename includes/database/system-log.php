1c9bdc28e328bbb850f7312a6948441b1a062c90
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-25 08:15:24 +0800

    Fix column reference status to action

--- includes/database/system-log.php
+++ includes/database/system-log.php
@@ -8,17 +8,17 @@ function userLog($person_id)
 
 function systemLogs($from_date, $to_date)
 {
-    $sql = "SELECT * FROM `system_logs` WHERE `status` NOT LIKE '%document%' 
+    $sql = "SELECT * FROM `system_logs` WHERE `action` NOT LIKE '%document%' 
             AND `created_at` BETWEEN ? AND DATE_ADD(?, INTERVAL 1 DAY) ORDER BY `created_at` DESC";
     return query($sql, [$from_date, $to_date]);
 }
 
-function createSystemLog($station_id, $person_id, $status, $target_id, $ip)
+function createSystemLog($station_id, $person_id, $action, $target_id, $ip)
 {
     $data = [
         'station_id' => $station_id,
         'person_id' => $person_id,
-        'status' => $status,
+        'action' => $action,
         'target_id' => $target_id,
         'ip' => $ip,
     ];
@@ -27,6 +27,6 @@ function createSystemLog($station_id, $person_id, $status, $target_id, $ip)
 
 function employeeEditHistory($target_id)
 {
-    $sql = "SELECT * FROM `system_logs` WHERE `target_id` = ? AND `Status` NOT LIKE '%logged%' ORDER BY `created_at` DESC";
+    $sql = "SELECT * FROM `system_logs` WHERE `target_id` = ? AND `action` NOT LIKE '%logged%' ORDER BY `created_at` DESC";
     return query($sql, [$target_id]);
 }
