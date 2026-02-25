4b051a227da51ad78145dd634d4c44eb009e0512
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-25 10:11:11 +0800

    Fix array name value reference

--- modules/employees/save/save-child-dialog.php
+++ modules/employees/save/save-child-dialog.php
@@ -15,16 +15,15 @@ $modalTitle = 'Add Child Name';
 
 if (isset($childId)) {
     $modalTitle = $employeeId === $copiedId ? 'Copy Child Name' : 'Edit Child Name';
-    $children = child($employeeId, $childId);
+    $child = child($employeeId, $childId);
 
-    if (numRows($children) > 0) {
-        $child = fetchArray($children);
-        $childId = $child['no'];
-        $fname = $child['first'];
-        $mname = $child['middle'];
-        $lname = $child['last'];
-        $ext = $child['ext'];
-        $bdate = $child['dob'];
+    if ($child) {
+        $childId = $child['id'];
+        $fname = $child['first_name'];
+        $mname = $child['middle_name'];
+        $lname = $child['last_name'];
+        $ext = $child['name_extension'];
+        $bdate = $child['birthdate'];
     }
 }
 ?>
@@ -37,33 +36,40 @@ if (isset($childId)) {
             <div class="modal-body">
                 <div class="form-group">
                     <label for="clast" class="mb-0">Last Name <?php showAsterisk() ?></label>
-                    <input id="clast" type="text" name="clast" class="form-control" placeholder="ex. DELA CRUZ" title="ex. DELA CRUZ" value="<?= $lname ?>" required>
+                    <input id="clast" type="text" name="clast" class="form-control" placeholder="ex. DELA CRUZ"
+                        title="ex. DELA CRUZ" value="<?= $lname ?>" required>
                 </div>
 
                 <div class="form-group">
                     <label for="cfirst" class="mb-0">First Name <?php showAsterisk() ?></label>
-                    <input id="cfirst" type="text" name="cfirst" class="form-control" placeholder="ex. JUAN" title="ex. JUAN" value="<?= $fname ?>" required>
+                    <input id="cfirst" type="text" name="cfirst" class="form-control" placeholder="ex. JUAN"
+                        title="ex. JUAN" value="<?= $fname ?>" required>
                 </div>
 
                 <div class="row">
                     <div class="col-8">
                         <div class="form-group">
                             <label for="cmiddle" class="mb-0">Middle Name</label>
-                            <input id="cmiddle" type="text" name="cmiddle" class="form-control" placeholder="ex. BAUTISTA" title=" ex. BAUTISTA, Leave blank if not applicable" value="<?= $mname ?>">
+                            <input id="cmiddle" type="text" name="cmiddle" class="form-control"
+                                placeholder="ex. BAUTISTA" title=" ex. BAUTISTA, Leave blank if not applicable"
+                                value="<?= $mname ?>">
                         </div>
                     </div>
 
                     <div class="col-4">
                         <div class="form-group">
                             <label for="cext" class="mb-0">Extension</label>
-                            <input id="cext" type="text" name="cext" class="form-control" placeholder="ex. JR., SR., III" title=" ex. JR., SR., III, Leave blank if not applicable" value="<?= $ext ?>">
+                            <input id="cext" type="text" name="cext" class="form-control"
+                                placeholder="ex. JR., SR., III"
+                                title=" ex. JR., SR., III, Leave blank if not applicable" value="<?= $ext ?>">
                         </div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="cdob" class="mb-0">Date of Birth <?php showAsterisk() ?></label>
-                    <input id="cdob" type="date" name="cdob" class="form-control" title="Set child date of birth..." value="<?= toDate($bdate, "Y-m-d") ?>" required>
+                    <input id="cdob" type="date" name="cdob" class="form-control" title="Set child date of birth..."
+                        value="<?= toDate($bdate, "Y-m-d") ?>" required>
                 </div>
 
                 <?php requiredLegend(0) ?>
