08881f4aa5260984fcb58dd72ec74360d44983d6
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-26 16:38:24 +0800

    Refactor code

--- logout/index.php
+++ logout/index.php
@@ -4,9 +4,9 @@ require_once('../includes/function.php');
 require_once(root() . '/includes/database/database.php');
 require_once(root() . '/includes/database/system-log.php');
 
-if (isset($_SESSION[alias() . '_userId'])) {
+if (isset($_SESSION["{$prefix}userId"])) {
     createSystemLog($stationId, $userId, 'Logged out', $userId, clientIp());
     session_destroy();
-    setcookie(alias() . '_login', '', time() - getSeconds(8), '/', uri(), false, true);
-    redirect(uri() . '/login');
+    setcookie("{$prefix}login", '', time() - getSeconds(8), '/', uri(), false, true);
+    redirect("{$baseUri}/login");
 }
