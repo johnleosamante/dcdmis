60bb4006e0e9d5228442bba97c813a307fa10d6a
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-26 11:27:12 +0800

    Modify text color for background contrast and readability

--- dts/track/track-document.php
+++ dts/track/track-document.php
@@ -7,7 +7,8 @@
     <p class="lead text-gray-800 mt-1 mb-0">Search document</p>
     <div class="row justify-content-center">
         <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12">
-            <p class="text-gray-500 px-2 mb-4">Track paper trail of documents submitted to the Schools Division Office created by schools, sections and offices within the schools division.</p>
+            <p class="text-gray-600 px-2 mb-4">Track paper trail of documents submitted to the Schools Division Office
+                created by schools, sections and offices within the schools division.</p>
         </div>
     </div>
 
@@ -15,7 +16,8 @@
         <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12">
                 <div class="input-group">
-                    <input type="text" class="form-control small" placeholder="Search document..." aria-label="Search" name="primary-search-text" value="<?= $searchText ?>" autofocus required>
+                    <input type="text" class="form-control small" placeholder="Search document..." aria-label="Search"
+                        name="primary-search-text" value="<?= $searchText ?>" autofocus required>
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="submit" name="primary-search-button">
                             <i class="fas fa-search fa-sm"></i>
