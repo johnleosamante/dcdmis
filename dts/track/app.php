5c0af32a6560efb074455cfdf81482838f454d62
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-26 11:25:32 +0800

    Disabled scripts

--- dts/track/app.php
+++ dts/track/app.php
@@ -2,7 +2,6 @@
 // dts/track/app.php
 $page = $appTitle = 'Document Tracking System';
 $searchText = '';
-$enableScripts = true;
 
 if (isset($_POST['primary-search-button'])) {
     redirect(customUri('dts/track', 'Document Information', sanitize($_POST['primary-search-text'])));
