1533e0359cd036398f35675e12318d7c0c51c930
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-26 09:30:17 +0800

    Fix array name values

--- modules/employees/tabs/membership.php
+++ modules/employees/tabs/membership.php
@@ -2,8 +2,9 @@
 // modules/employees/tabs/membership.php
 ?>
 
-<div class="tab-pane fade<?= setActiveNavigation(isset($activeTab) && $activeTab === 'membership', 'show active') ?>" id="membership">
-    <?php if ($editMode) : ?>
+<div class="tab-pane fade<?= setActiveNavigation(isset($activeTab) && $activeTab === 'membership', 'show active') ?>"
+    id="membership">
+    <?php if ($editMode): ?>
         <div class="d-sm-flex justify-content-end my-3">
             <?php modalButtonSplit(uri() . '/modules/employees/save/save-membership-dialog.php?e=' . cipher($employeeId), 'Add', 'fa-plus', 'Add Membership', 'primary') ?>
         </div>
@@ -15,7 +16,7 @@
                 <thead>
                     <tr>
                         <th class="align-middle" width="100%">Membership in Association / Organization</th>
-                        <?php if ($editMode) : ?>
+                        <?php if ($editMode): ?>
                             <th class="align-middle" width="5%">Action</th>
                         <?php endif ?>
                     </tr>
@@ -26,18 +27,18 @@
                     $memberships = memberships($employeeId);
 
                     if ($memberships) {
-                        foreach ($memberships as $membership) : ?>
+                        foreach ($memberships as $membership): ?>
                             <tr class="text-uppercase">
                                 <td class="align-middle"><?= $membership['organization'] ?></td>
-                                <?php if ($editMode) : ?>
+                                <?php if ($editMode): ?>
                                     <td class="align-middle text-capitalize">
                                         <div class="dropdown no-arrow">
                                             <?php dropdownEllipsis() ?>
                                             <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
-                                                <?php modalDropdownItem(uri() . '/modules/employees/save/save-membership-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($membership['no']), 'Edit', 'fa-edit', 'Edit Membership');
-                                                modalDropdownItem(uri() . '/modules/employees/save/save-membership-dialog.php?c=' . cipher($employeeId) . '&e=' . cipher($employeeId) . '&id=' . cipher($membership['no']), 'Copy', 'fa-copy', 'Copy Membership') ?>
+                                                <?php modalDropdownItem(uri() . '/modules/employees/save/save-membership-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($membership['id']), 'Edit', 'fa-edit', 'Edit Membership');
+                                                modalDropdownItem(uri() . '/modules/employees/save/save-membership-dialog.php?c=' . cipher($employeeId) . '&e=' . cipher($employeeId) . '&id=' . cipher($membership['id']), 'Copy', 'fa-copy', 'Copy Membership') ?>
                                                 <div class="dropdown-divider"></div>
-                                                <?php modalDropdownItem(uri() . '/modules/employees/delete/delete-membership-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($membership['no']), 'Delete', 'fa-trash', 'Delete Membership') ?>
+                                                <?php modalDropdownItem(uri() . '/modules/employees/delete/delete-membership-dialog.php?e=' . cipher($employeeId) . '&id=' . cipher($membership['id']), 'Delete', 'fa-trash', 'Delete Membership') ?>
                                             </div>
                                         </div>
                                     </td>
@@ -54,7 +55,7 @@
                 <tfoot>
                     <tr>
                         <th class="align-middle" width="100%">Membership in Association / Organization</th>
-                        <?php if ($editMode) : ?>
+                        <?php if ($editMode): ?>
                             <th class="align-middle" width="5%">Action</th>
                         <?php endif ?>
                     </tr>
