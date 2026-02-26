a2801f9ac0921a91a9395cb5c0abb5294450875a
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-26 09:00:44 +0800

    Fix array name values and remove unused fields

--- modules/service-record/save-service-record-dialog.php
+++ modules/service-record/save-service-record-dialog.php
@@ -12,29 +12,27 @@ $copiedId = isset($_GET['c']) ? sanitize(decipher($_GET['c'])) : null;
 $service = $position = $positionCode = $organization = $organizationAlias = $status = $isgovernment = $leave = $separationCause = $sg = null;
 $salary = 0;
 $from = $to = $separationDate = date('Y-m-d');
-$isPresent = $isSeparation =  false;
+$isPresent = $isSeparation = false;
 $modalTitle = 'Add Service Record';
 
 if (isset($serviceRecordId)) {
     $modalTitle = $employeeId === $copiedId ? 'Copy Service Record' : 'Edit Service Record';
-    $services = experience($employeeId, $serviceRecordId);
+    $service = experience($employeeId, $serviceRecordId);
 
-    if (numRows($services) > 0) {
-        $service = fetchAssoc($services);
-        $serviceRecordId = $service['no'];
-        $from = toDate($service['from'], 'Y-m-d');
-        $isPresent = $service['ispresent'] === '1';
-        $to = $isPresent ? date('Y-m-d') : toDate($service['to'], 'Y-m-d');
-        $position = $service['position'];
-        $positionCode = $service['position_code'];
-        $isgovernment = $service['isgovernment'];
-        $sg = $service['sg'];
-        $salary = $service['salary'];
-        $organization = $service['organization'];
-        $organizationAlias = $service['organization_alias'];
-        $status = $service['status'];
-        $leave = $service['leave_dates'];
-        $isSeparation = $service['isseparation'];
+    if ($service) {
+        $serviceRecordId = $service['id'];
+        $from = toDate($service['from_date'], 'Y-m-d');
+        $isPresent = $service['is_present'];
+        $to = $isPresent ? date('Y-m-d') : toDate($service['to_date'], 'Y-m-d');
+        $position = $service['designation'];
+        $positionCode = $service['position_id'];
+        $isgovernment = $service['is_government_service'];
+        $sg = $service['salary_grade_step_increment'];
+        $salary = $service['monthly_salary'];
+        $organization = $service['agency_company'];
+        $status = $service['appointment_status'];
+        $leave = $service['leave_wo_pay'];
+        $isSeparation = $service['for_separation'];
         $separationDate = !empty($service['separation_date']) ? toDate($service['separation_date'], 'Y-m-d') : date('Y-m-d');
         $separationCause = $service['separation_cause'];
     }
@@ -51,7 +49,8 @@ if (isset($serviceRecordId)) {
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="from" class="mb-0">Inclusive Dates From <?php showAsterisk() ?></label>
-                            <input id="from" type="date" name="from" class="form-control" title="Required field" value="<?= $from ?>" required>
+                            <input id="from" type="date" name="from" class="form-control" title="Required field"
+                                value="<?= $from ?>" required>
                         </div>
                     </div>
 
@@ -63,27 +62,24 @@ if (isset($serviceRecordId)) {
                                 </div>
                                 <div class="col-6">
                                     <div class="form-check" title="Check if present work">
-                                        <input class="form-check-input" id="is-present" type="checkbox" name="is-present" value="1" <?= setItemChecked($isPresent) ?>>
+                                        <input class="form-check-input" id="is-present" type="checkbox"
+                                            name="is-present" value="1" <?= setItemChecked($isPresent) ?>>
                                         <label class="form-check-label" for="is-present">Present</label>
                                     </div>
                                 </div>
                             </div>
-                            <input id="to" type="date" name="to" class="form-control" title="Required field" value="<?= $to ?>" required>
+                            <input id="to" type="date" name="to" class="form-control" title="Required field"
+                                value="<?= $to ?>" required>
                         </div>
                     </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-8">
+                    <div class="col-12">
                         <div class="form-group">
                             <label for="position" class="mb-0">Designation <?php showAsterisk() ?></label>
-                            <input id="position" type="text" name="position" class="form-control" title="Required field" value="<?= $position ?>" required>
-                        </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="form-group">
-                            <label for="position-code" class="mb-0">Alias <?php showAsterisk() ?></label>
-                            <input id="position-code" type="text" name="position-code" class="form-control" title="Required field" value="<?= $positionCode ?>" required>
+                            <input id="position" type="text" name="position" class="form-control" title="Required field"
+                                value="<?= $position ?>" required>
                         </div>
                     </div>
                 </div>
@@ -93,14 +89,22 @@ if (isset($serviceRecordId)) {
                         <div class="form-group">
                             <label for="status" class="mb-0">Employment Status <?php showAsterisk() ?></label>
                             <select name="status" id="status" class="form-control" title="Required field" required>
-                                <option value="Permanent" <?= setOptionSelected("Permanent", $status) ?>>Permanent</option>
-                                <option value="Temporary" <?= setOptionSelected("Temporary", $status) ?>>Temporary</option>
-                                <option value="Coterminus" <?= setOptionSelected("Coterminus", $status) ?>>Coterminus</option>
-                                <option value="Fixed Term" <?= setOptionSelected("Fixed Term", $status) ?>>Fixed Term</option>
-                                <option value="Contractual" <?= setOptionSelected("Contractual", $status) ?>>Contractual</option>
-                                <option value="Substitute" <?= setOptionSelected("Substitute", $status) ?>>Substitute</option>
-                                <option value="Provisional" <?= setOptionSelected("Provisional", $status) ?>>Provisional</option>
-                                <option value="Volunteer" <?= setOptionSelected("Volunteer", $status) ?>>Volunteer</option>
+                                <option value="Permanent" <?= setOptionSelected("Permanent", $status) ?>>Permanent
+                                </option>
+                                <option value="Temporary" <?= setOptionSelected("Temporary", $status) ?>>Temporary
+                                </option>
+                                <option value="Coterminus" <?= setOptionSelected("Coterminus", $status) ?>>Coterminus
+                                </option>
+                                <option value="Fixed Term" <?= setOptionSelected("Fixed Term", $status) ?>>Fixed Term
+                                </option>
+                                <option value="Contractual" <?= setOptionSelected("Contractual", $status) ?>>Contractual
+                                </option>
+                                <option value="Substitute" <?= setOptionSelected("Substitute", $status) ?>>Substitute
+                                </option>
+                                <option value="Provisional" <?= setOptionSelected("Provisional", $status) ?>>Provisional
+                                </option>
+                                <option value="Volunteer" <?= setOptionSelected("Volunteer", $status) ?>>Volunteer
+                                </option>
                             </select>
                         </div>
                     </div>
@@ -108,7 +112,8 @@ if (isset($serviceRecordId)) {
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="is-government" class="mb-0">Government Service <?php showAsterisk() ?></label>
-                            <select name="is-government" id="is-government" title="Required field" class="form-control" required>
+                            <select name="is-government" id="is-government" title="Required field" class="form-control"
+                                required>
                                 <option value="Y" <?= setOptionSelected("Y", $isgovernment) ?>>Yes</option>
                                 <option value="N" <?= setOptionSelected("N", $isgovernment) ?>>No</option>
                             </select>
@@ -120,29 +125,27 @@ if (isset($serviceRecordId)) {
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="sg-step" class="mb-0">Salary Grade &amp; Step Increment</label>
-                            <input id="sg-step" type="text" name="sg-step" class="form-control" title="Leave blank if not applicable" value="<?= $sg ?>">
+                            <input id="sg-step" type="text" name="sg-step" class="form-control"
+                                title="Leave blank if not applicable" value="<?= $sg ?>">
                         </div>
                     </div>
 
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="salary" class="mb-0">Monthly<br>Salary</label>
-                            <input id="salary" type="number" name="salary" class="form-control" min="0" step="1" title="Leave blank if not applicable" value="<?= $salary ?>">
+                            <input id="salary" type="number" name="salary" class="form-control" min="0" step="1"
+                                title="Leave blank if not applicable" value="<?= $salary ?>">
                         </div>
                     </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-8">
+                    <div class="col-12">
                         <div class="form-group">
-                            <label for="station" class="mb-0">Office Entity / Division / Station / Place / Branch of Assignment <?php showAsterisk() ?></label>
-                            <input id="station" type="text" name="station" class="form-control" title="Required field" value="<?= $organization ?>" required>
-                        </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="form-group">
-                            <label for="station-alias" class="mb-0">Assignment<br>Alias <?php showAsterisk() ?></label>
-                            <input id="station-alias" type="text" name="station-alias" class="form-control" title="Required field" value="<?= $organizationAlias ?>" required>
+                            <label for="station" class="mb-0">Office Entity / Division / Station / Place / Branch of
+                                Assignment <?php showAsterisk() ?></label>
+                            <input id="station" type="text" name="station" class="form-control" title="Required field"
+                                value="<?= $organization ?>" required>
                         </div>
                     </div>
                 </div>
@@ -151,11 +154,13 @@ if (isset($serviceRecordId)) {
 
                 <div class="form-group">
                     <label for="leave" class="mb-0">Leave Without Pay</label>
-                    <input id="leave" type="text" name="leave" class="form-control" title="Leave blank if not applicable" value="<?= $leave ?>">
+                    <input id="leave" type="text" name="leave" class="form-control"
+                        title="Leave blank if not applicable" value="<?= $leave ?>">
                 </div>
 
                 <div class="form-check mb-2" title="Check for separation">
-                    <input class="form-check-input" id="is-separation" type="checkbox" name="is-separation" value="1" <?= setItemChecked($isSeparation) ?>>
+                    <input class="form-check-input" id="is-separation" type="checkbox" name="is-separation" value="1"
+                        <?= setItemChecked($isSeparation) ?>>
                     <label class="form-check-label" for="is-separation">Separation</label>
                 </div>
 
@@ -163,14 +168,16 @@ if (isset($serviceRecordId)) {
                     <div class="col-md-5">
                         <div class="form-group">
                             <label for="separation-date" class="mb-0">Date</label>
-                            <input id="separation-date" type="date" name="separation-date" class="form-control" title="Leave blank if not applicable" value="<?= $separationDate ?>">
+                            <input id="separation-date" type="date" name="separation-date" class="form-control"
+                                title="Leave blank if not applicable" value="<?= $separationDate ?>">
                         </div>
                     </div>
 
                     <div class="col-md-7">
                         <div class="form-group">
                             <label for="separation-cause" class="mb-0">Cause</label>
-                            <input id="separation-cause" type="text" name="separation-cause" class="form-control" title="Leave blank if not applicable" value="<?= $separationCause ?>">
+                            <input id="separation-cause" type="text" name="separation-cause" class="form-control"
+                                title="Leave blank if not applicable" value="<?= $separationCause ?>">
                         </div>
                     </div>
                 </div>
@@ -179,9 +186,9 @@ if (isset($serviceRecordId)) {
             </div>
 
             <div class="modal-footer">
-                <input type="hidden" name="verifier" value="<?= isset($_GET['e']) ? $_GET['e'] : null ?>">
+                <input type="hidden" name="verifier" value="<?= $_GET['e'] ?? null ?>">
                 <?php
-                $verifier = isset($_GET['id']) ? $_GET['id'] : null;
+                $verifier = $_GET['id'] ?? null;
                 $verifier = $employeeId === $copiedId ? null : $verifier;
                 ?>
                 <input type="hidden" name="data-verifier" value="<?= $verifier ?>">
