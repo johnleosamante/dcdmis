491ad84cb8c9e3eda8cab76b0bf29373176ad6b5
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-27 08:39:59 +0800

    Remove spaces

--- pis/sidebar-menu.php
+++ pis/sidebar-menu.php
@@ -1,6 +1,7 @@
 <?php
 // hrmis/sidebar-menu.php
 sidebarDivider();
+
 sidebarMenuItem(customUri('pis', 'Employee Information', $userId), 'Employee Information', 'fa-user-tie', isset($url) && str_contains($url, 'Employee Information'));
 sidebarMenuItem(customUri('pis', 'Service Record', $userId), 'Service Record', 'fa-file-alt', isset($url) && str_contains($url, 'Service Record'));
 sidebarMenuItem(customUri('pis', '201 Files', $userId), '201 Files', 'fa-folder-open', isset($url) && str_contains($url, '201 Files'));
