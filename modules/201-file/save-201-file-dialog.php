58405e5f289b10351f842c92c0f030bc78b131af
John Leo Samante <johnleomuitsamante@gmail.com>
2026-02-27 09:19:15 +0800

    Fix array name values and remove unused functions

--- modules/201-file/save-201-file-dialog.php
+++ modules/201-file/save-201-file-dialog.php
@@ -14,13 +14,12 @@ $modalTitle = 'Add 201 File';
 
 if (isset($attachmentId)) {
     $modalTitle = $employeeId === $copiedId ? 'Copy 201 File' : 'Edit 201 File';
-    $attachments = fileAttachment($employeeId, $attachmentId);
+    $attachment = fileAttachment($employeeId, $attachmentId);
 
-    if (numRows($attachments) > 0) {
-        $attachment = fetchAssoc($attachments);
-        $attachmentId = $attachment['no'];
+    if ($attachment) {
+        $attachmentId = $attachment['id'];
         $description = $attachment['description'];
-        $filename = $attachment['filename'];
+        $filename = $attachment['file_name'];
     }
 }
 ?>
@@ -32,21 +31,23 @@ if (isset($attachmentId)) {
         <form method="POST" action="" enctype="multipart/form-data">
             <div class="modal-body">
                 <div class="form-group">
-                    <input id="file-upload" name="file-upload" type="file" title="Upload 201 file (pdf, jpeg or png format)..." class="w-100">
+                    <input id="file-upload" name="file-upload" type="file" title="Upload 201 file (pdf)..."
+                        class="w-100" accept="application/pdf">
                 </div>
 
                 <div class="form-group">
                     <label for="description" class="mb-0">Description <?php showAsterisk() ?></label>
-                    <textarea id="description" name="description" class="form-control" placeholder="Type description..." title="Type 201 file description..." rows="3" required><?= $description ?></textarea>
+                    <textarea id="description" name="description" class="form-control" placeholder="Type description..."
+                        title="Type 201 file description..." rows="3" required><?= $description ?></textarea>
                 </div>
 
                 <?php requiredLegend(0) ?>
             </div>
 
             <div class="modal-footer">
-                <input type="hidden" name="verifier" value="<?= isset($_GET['e']) ? $_GET['e'] : null ?>">
+                <input type="hidden" name="verifier" value="<?= $_GET['e'] ?? null ?>">
                 <?php
-                $verifier = isset($_GET['id']) ? $_GET['id'] : null;
+                $verifier = $_GET['id'] ?? null;
                 $verifier = $employeeId === $copiedId ? null : $verifier;
                 $filename = !isset($_GET['c']) ? $filename : null;
                 ?>
