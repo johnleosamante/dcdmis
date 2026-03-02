a4fc70a9f431200c0d16d644eecb63681b9b32c5
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-02 11:37:21 +0800

    Organize constant and global variables

--- includes/initialization.php
+++ includes/initialization.php
@@ -8,21 +8,25 @@ ini_set('max_input_time', 300);
 ini_set('max_execution_time', 300);
 ini_set('memory_limit', '1024M');
 ini_set('display_errors', 1);
-$fileUploadSizeLimit = 1024 * 1024 * 20; // 20MB
-$imageUploadSizeLimit = 1024 * 1024 * 2.5; // 2.5MB
 
-$userId = $_SESSION[alias() . '_userId'] ?? null;
-$stationId = $_SESSION[alias() . '_stationId'] ?? null;
-$station = $_SESSION[alias() . '_station'] ?? null;
-$activeApp = $_SESSION[alias() . '_activeApp'] ?? 'pis';
-$activeTab = $_SESSION[alias() . '_activeTab'] ?? null;
-$code = $_SESSION[alias() . '_code'] ?? null;
-$portal = $_SESSION[alias() . '_portal'] ?? null;
+const FILE_UPLOAD_SIZE_LIMIT = 1024 * 1024 * 20; // 20MB
+const IMAGE_UPLOAD_SIZE_LIMIT = 1024 * 1024 * 2.5; // 2.5MB
+const HOME = 'pis'; // default home page
+
+$prefix = alias() . '_';
+$baseUri = uri();
+$enableScripts = false;
+$showAlert = false;
+$message = null;
+$success = false;
+$activeApp = $_SESSION["{$prefix}activeApp"] ?? HOME;
+$activeTab = $_SESSION["{$prefix}activeTab"] ?? null;
+$userId = $_SESSION["{$prefix}userId"] ?? null;
+$stationId = $_SESSION["{$prefix}stationId"] ?? null;
+$station = $_SESSION["{$prefix}station"] ?? null;
+$isDescriptionEditable = $_SESSION["{$prefix}editableDescription"] ?? false;
+$code = $_SESSION["{$prefix}code"] ?? null;
+$portal = $_SESSION["{$prefix}portal"] ?? null;
 $hasPortal = !empty($portal);
 $isSchoolPortal = $portal === 'sch_portal';
 $isRecordsPortal = $portal === 'rec_portal';
-$isDescriptionEditable = $_SESSION[alias() . '_editableDescription'] ?? false;
-$showAlert = false;
-$message = null;
-$success = true;
-$enableScripts = false;
