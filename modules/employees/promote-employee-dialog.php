3f9025de6dad7d6c3c3e54acd6319db660fbf0f9
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-03 09:46:57 +0800

    Remove unused functions and fix array name values

--- modules/employees/promote-employee-dialog.php
+++ modules/employees/promote-employee-dialog.php
@@ -9,22 +9,21 @@ require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/string.php');
 
 $employeeId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
-$employees = employee($employeeId);
+$employee = employee($employeeId);
 $modalTitle = 'Employee not found';
 $hasEmployee = false;
 
-if (numRows($employees) > 0) {
-    $employee = fetchAssoc($employees);
+if ($employee) {
     $employeeId = $employee['id'];
-    $employeeName = toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true);
+    $employeeName = toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension'], true);
     $sex = $employee['sex'];
-    $positions = fetchAssoc(position($employeeId));
-    $doa = $positions['date'];
-    $stationId = $positions['station_id'];
-    $station = $positions['station'];
-    $positionId = $positions['position_id'];
-    $position = $positions['position'];
-    $picture = file_exists(root() . '/' . $employee['picture']) ? uri() . '/' . $employee['picture'] : uri() . '/assets/img/user.png';
+    $position = position($employeeId);
+    $doa = $position['assignment_date'];
+    $stationId = $position['station_id'];
+    $station = $position['station'];
+    $positionId = $position['position_id'];
+    $position = $position['official_title'];
+    $picture = file_exists(root() . '/' . $employee['profile_picture']) ? "{$baseUri}/" . $employee['profile_picture'] : "{$baseUri}//assets/img/user.png";
     $modalTitle = 'Promote Employee';
     $hasEmployee = true;
 }
@@ -38,7 +37,8 @@ if (numRows($employees) > 0) {
             <div class="modal-body">
                 <?php if ($hasEmployee) { ?>
                     <div class="image-container">
-                        <span class="d-flex justify-content-center align-middle employee-photo photo-4x rounded-circle overflow-hidden">
+                        <span
+                            class="d-flex justify-content-center align-middle employee-photo photo-4x rounded-circle overflow-hidden">
                             <img height="100%" src="<?= $picture ?>" alt="<?= $employeeName ?>">
                         </span>
                         <div class="sex-sign"><?php sex($sex) ?></div>
@@ -52,24 +52,26 @@ if (numRows($employees) > 0) {
 
                     <div class="form-group">
                         <label for="position" class="mb-0">Position <?php showAsterisk() ?></label>
-                        <select id="position" name="position" class="form-control" title="Select employee position..." required>
+                        <select id="position" name="position" class="form-control" title="Select employee position..."
+                            required>
                             <option value="">Select position...</option>
                             <?php
                             $categories = positionCategories();
-                            while ($category = fetchAssoc($categories)) : ?>
+                            foreach ($categories as $category): ?>
                                 <optgroup label="<?= $category['category'] ?>">
                                     <?php $jobPositions = positionsByCategory($category['category']);
-                                    while ($jobPosition = fetchArray($jobPositions)) : ?>
-                                        <option value="<?= $jobPosition['id'] ?>" <?= setOptionSelected($jobPosition['id'], $positionId) ?>><?= $jobPosition['position'] ?></option>
-                                    <?php endwhile ?>
+                                    foreach ($jobPositions as $jobPosition): ?>
+                                        <option value="<?= $jobPosition['id'] ?>" <?= setOptionSelected($jobPosition['id'], $positionId) ?>><?= $jobPosition['official_title'] ?></option>
+                                    <?php endforeach ?>
                                 </optgroup>
-                            <?php endwhile ?>
+                            <?php endforeach ?>
                         </select>
                     </div>
 
                     <div class="form-group">
                         <label for="effectivity-date" class="mb-0">Date of Effectivity <?php showAsterisk() ?></label>
-                        <input class="form-control" type="date" id="effectivity-date" name="effectivity-date" value="<?= toDate($doa, 'Y-m-d', date('Y-m-d')) ?>" title="Set date of effectivity..." required>
+                        <input class="form-control" type="date" id="effectivity-date" name="effectivity-date"
+                            value="<?= toDate($doa, 'Y-m-d', date('Y-m-d')) ?>" title="Set date of effectivity..." required>
                     </div>
 
                     <?php requiredLegend(0) ?>
@@ -79,7 +81,7 @@ if (numRows($employees) > 0) {
             </div>
 
             <div class="modal-footer">
-                <?php if ($hasEmployee) : ?>
+                <?php if ($hasEmployee): ?>
                     <input type="hidden" name="verifier" value="<?= $_GET['id'] ?>">
                     <button class="btn btn-primary" name="promote-employee" type="submit">Continue</button>
                 <?php endif;
