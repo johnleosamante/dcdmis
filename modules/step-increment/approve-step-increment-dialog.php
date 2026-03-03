d4e87fc114ec13d3f0d25421671d6a3a298cd928
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-03 11:45:38 +0800

    Fix array name values

--- modules/step-increment/approve-step-increment-dialog.php
+++ modules/step-increment/approve-step-increment-dialog.php
@@ -9,23 +9,22 @@ require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/string.php');
 
 $employeeId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
-$employees = employee($employeeId);
+$employee = employee($employeeId);
 $modalTitle = 'Employee not found';
 $hasEmployee = false;
 
-if (numRows($employees) > 0) {
-    $employee = fetchAssoc($employees);
+if ($employee) {
     $employeeId = $employee['id'];
-    $employeeName = toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true);
+    $employeeName = toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension'], true);
     $sex = $employee['sex'];
     $status = $employee['status'];
-    $positions = fetchAssoc(position($employeeId));
-    $stationId = $positions['station_id'];
-    $station = $positions['station'];
-    $positionId = $positions['position_id'];
-    $position = $positions['position'];
-    $depedEmail = $employee['email'];
-    $picture = uri() . '/' . $employee['picture'];
+    $position = position($employeeId);
+    $stationId = $position['station_id'];
+    $station = $position['station'];
+    $positionId = $position['position_id'];
+    $position = $position['official_title'];
+    $depedEmail = $employee['email_address'];
+    $picture = file_exists(root() . '/' . $employee['profile_picture']) ? "{$baseUri}/" . $employee['profile_picture'] : "{$baseUri}/assets/img/user.png";
     $modalTitle = 'Approve Employee Step Increment';
     $hasEmployee = true;
 }
@@ -39,17 +38,17 @@ if (numRows($employees) > 0) {
             <div class="modal-body">
                 <?php if ($hasEmployee) {
                     employeeProfile($picture, $employeeName, $sex, $depedEmail, $position, $station, $status);
-                ?>
+                    ?>
                     <hr>
 
-                    This operation will approve the step increment of employee. Are you sure you want to continue?
+                    This operation will approve the step increment of this employee. Are you sure you want to continue?
                 <?php } else {
                     missingAlert($modalTitle);
                 } ?>
             </div>
 
             <div class="modal-footer">
-                <?php if ($hasEmployee) : ?>
+                <?php if ($hasEmployee): ?>
                     <input type="hidden" name="verifier" value="<?= $_GET['id'] ?>">
                     <button class="btn btn-success" name="approve-step-increment" type="submit">Yes, Continue</button>
                 <?php endif;
