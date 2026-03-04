77882ddc84ab31678ffe4d69e7bc32d45b0256b2
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-04 14:37:42 +0800

    Fix text

--- hrmis/dashboard.php
+++ hrmis/dashboard.php
@@ -40,7 +40,7 @@ contentTitleWithModal('Dashboard', uri() . '/modules/employees/save-employee-dia
     <div class="col-xl-9 col-lg-12 mb-4">
         <div class="card shadow">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employee Gender by Category</h6>
+                <h6 class="m-0 font-weight-bold text-primary text-uppercase">Employee Categories By Gender</h6>
             </div>
             <div class="card-body">
                 <div class="chart-bar h-auto">
