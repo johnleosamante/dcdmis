7f361d8e2116e870a871175f472e40683113462f
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-06 07:53:46 +0800

    Add xss and csrf protection

--- login/change/page.php
+++ login/change/page.php
@@ -5,7 +5,7 @@
 <div class="col-xl-5 col-lg-5 col-md-8 col-sm-12">
     <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="card-header">
-            <h3 class="text-center my-2"><?= $page ?></h3>
+            <h3 class="text-center my-2"><?= e($page) ?></h3>
         </div>
 
         <div class="card-body text-center">
@@ -26,15 +26,17 @@
             <?php messageAlert($showAlert, $message, $success) ?>
 
             <form action="" method="POST" class="text-left mt-3">
+                <?= csrf_field(); ?>
                 <div class="form-group">
                     <label for="email-address" class="font-weight-bold mb-1">Email Address</label>
-                    <input id="email-address" type="text" class="form-control border" value="<?= $email ?>" readonly>
+                    <input id="email-address" type="text" class="form-control border" value="<?= e($email) ?>" readonly>
                 </div>
 
                 <div class="form-group">
                     <label for="password" class="font-weight-bold mb-1">New Password <?php showAsterisk() ?></label>
                     <div id="password-group" class="input-group border rounded">
-                        <input id="password" name="password" type="password" class="form-control border-0" value="<?= $password ?>" required>
+                        <input id="password" name="password" type="password" class="form-control border-0"
+                            value="<?= e($password) ?>" required>
                         <div class="input-group-append">
                             <button type="button" id="eye-toggle" class="input-group-text border-0 bg-transparent">
                                 <i id="eye" class="small fas fa-eye fa-sm"></i>
@@ -44,11 +46,14 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="password-confirm" class="font-weight-bold mb-1">Confirm New Password <?php showAsterisk() ?></label>
+                    <label for="password-confirm" class="font-weight-bold mb-1">Confirm New Password
+                        <?php showAsterisk() ?></label>
                     <div id="password-confirm-group" class="input-group border rounded">
-                        <input id="password-confirm" name="password-confirm" type="password" class="form-control border-0" value="<?= $passwordConfirm ?>" required>
+                        <input id="password-confirm" name="password-confirm" type="password"
+                            class="form-control border-0" value="<?= e($passwordConfirm) ?>" required>
                         <div class="input-group-append">
-                            <button type="button" id="eye-confirm-toggle" class="input-group-text border-0 bg-transparent">
+                            <button type="button" id="eye-confirm-toggle"
+                                class="input-group-text border-0 bg-transparent">
                                 <i id="eye-confirm" class="small fas fa-eye fa-sm"></i>
                             </button>
                         </div>
@@ -57,7 +62,8 @@
 
                 <?php requiredLegend() ?>
 
-                <button id="change-password" type="submit" class="btn btn-primary btn-block" name="change-password">Change Password</button>
+                <button id="change-password" type="submit" class="btn btn-primary btn-block"
+                    name="change-password">Change Password</button>
             </form>
         </div>
     </div>
