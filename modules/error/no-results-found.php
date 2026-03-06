bcb9d7946b27007e4bf4fba4898585bf4e479f12
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-06 12:56:53 +0800

    Add xss and csrf protection

--- modules/error/no-results-found.php
+++ modules/error/no-results-found.php
@@ -8,10 +8,12 @@
     <p class="text-gray-700 mt-4 mb-1">Try a new search term instead</p>
 
     <form class="mx-auto mb-4" method="POST" action="">
+        <?= csrf_field(); ?>
         <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12">
                 <div class="input-group">
-                    <input type="text" class="form-control small" placeholder="Search..." aria-label="Search" name="primary-search-text" autofocus required>
+                    <input type="text" class="form-control small" placeholder="Search..." aria-label="Search"
+                        name="primary-search-text" autofocus required>
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="submit" name="primary-search-button">
                             <i class="fas fa-search fa-sm"></i>
@@ -22,7 +24,7 @@
         </div>
     </form>
 
-    <?php if (isset($userId)) : ?>
+    <?php if (isset($userId)): ?>
         <a href="<?= uri() . '/' . $activeApp ?>" title="Go to dashboard">Go to Dashboard</a>
     <?php endif ?>
 </div>
