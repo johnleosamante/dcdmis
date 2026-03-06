ecc8985595930071d0376f2fea7a137fcf286f19
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-06 10:08:47 +0800

    Add xss and csrf protection

--- modules/employees/save/save-reference-dialog.php
+++ modules/employees/save/save-reference-dialog.php
@@ -30,35 +30,36 @@ if (isset($referenceId)) {
         <?php modalHeader($modalTitle) ?>
 
         <form method="POST" action="">
+            <?= csrf_field(); ?>
             <div class="modal-body">
                 <div class="form-group">
                     <label for="name" class="mb-0">Name: <?php showAsterisk() ?></label>
                     <input type="text" id="name" name="name" class="form-control" title="Required field"
-                        value="<?= $name ?>" required>
+                        value="<?= e($name) ?>" required>
                 </div>
 
                 <div class="form-group">
                     <label for="address" class="mb-0">Address: <?php showAsterisk() ?></label>
                     <input type="text" id="address" name="address" class="form-control" title="Required field"
-                        value="<?= $address ?>" required>
+                        value="<?= e($address) ?>" required>
                 </div>
 
                 <div class="form-group">
                     <label for="telephone" class="mb-0">Contact Number: <?php showAsterisk() ?></label>
                     <input type="text" id="telephone" name="telephone" class="form-control" title="Required field"
-                        value="<?= $contact ?>" required>
+                        value="<?= e($contact) ?>" required>
                 </div>
 
                 <?php requiredLegend(0) ?>
             </div>
 
             <div class="modal-footer">
-                <input type="hidden" name="verifier" value="<?= isset($_GET['e']) ? $_GET['e'] : null ?>">
+                <input type="hidden" name="verifier" value="<?= $_GET['e'] ?? null ?>">
                 <?php
-                $verifier = isset($_GET['id']) ? $_GET['id'] : null;
+                $verifier = $_GET['id'] ?? null;
                 $verifier = $employeeId === $copiedId ? null : $verifier;
                 ?>
-                <input type="hidden" name="data-verifier" value="<?= $verifier ?>">
+                <input type="hidden" name="data-verifier" value="<?= e($verifier) ?>">
                 <button type="submit" class="btn btn-primary" name="save-reference">Continue</button>
                 <?php cancelModalButton() ?>
             </div>
