1b6b6760a2d5e06e0798cb7d72b08d7e0efc9373
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-06 13:26:03 +0800

    Fix array name values and add xss and csrf protection

--- modules/trainings/save-training-dialog.php
+++ modules/trainings/save-training-dialog.php
@@ -8,15 +8,14 @@ require_once(root() . '/includes/database/learning-development.php');
 require_once(root() . '/includes/layout/components.php');
 
 $trainingId = isset($_GET['id']) ? sanitize(decipher($_GET['id'])) : null;
-$trainings = training($trainingId);
-$title = $hours = $trainingType = $trainingLevel = $sponsorName =  $venue = $unconsecutiveDates = $functionalDivisionId = '';
+$training = training($trainingId);
+$title = $hours = $trainingType = $trainingLevel = $sponsorName = $venue = $unconsecutiveDates = $functionalDivisionId = '';
 $dateFrom = $dateTo = date('Y-m-d');
 $generateCertificate = false;
 $modalTitle = 'Add Training';
-$notFound  = true;
+$notFound = true;
 
-if (numRows($trainings) > 0) {
-    $training = fetchAssoc($trainings);
+if ($training) {
     $trainingId = $training['no'];
     $title = $training['title'];
     $dateFrom = $training['from'];
@@ -39,44 +38,53 @@ if (numRows($trainings) > 0) {
         <?php modalHeader($modalTitle) ?>
 
         <form action="" method="POST">
+            <?= csrf_field(); ?>
             <div class="modal-body">
-                <?php if (!$notFound) : ?>
+                <?php if (!$notFound): ?>
                     <div class="form-group">
                         <label for="code" class="mb-0">Code <?php showAsterisk() ?></label>
-                        <input type="text" id="code" class="form-control text-uppercase" value="<?= $trainingId ?>" disabled>
+                        <input type="text" id="code" class="form-control text-uppercase" value="<?= e($trainingId) ?>"
+                            disabled>
                     </div>
                 <?php endif ?>
 
                 <div class="form-group">
                     <label for="title" class="mb-0">Title <?php showAsterisk() ?></label>
-                    <textarea id="title" name="title" class="form-control" rows="3" placeholder="Type title..." title="Type training title..."><?= $title ?></textarea>
+                    <textarea id="title" name="title" class="form-control" rows="3" placeholder="Type title..."
+                        title="Type training title..."><?= e($title) ?></textarea>
                 </div>
 
                 <div class="row">
                     <div class="col-6">
                         <div class="form-group">
                             <label for="from" class="mb-0">Date from <?php showAsterisk() ?></label>
-                            <input type="date" name="from" id="from" class="form-control" title="Set training start date..." value="<?= $dateFrom ?>" required>
+                            <input type="date" name="from" id="from" class="form-control"
+                                title="Set training start date..." value="<?= e($dateFrom) ?>" required>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group">
                             <label for="from" class="mb-0">Date to <?php showAsterisk() ?></label>
-                            <input type="date" name="to" id="to" class="form-control" title="Set training end date..." value="<?= $dateTo ?>" required>
+                            <input type="date" name="to" id="to" class="form-control" title="Set training end date..."
+                                value="<?= e($dateTo) ?>" required>
                         </div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="unconsecutive-dates" class="mb-0">For non-consecutive days, please specify</label>
-                    <input type="text" name="unconsecutive-dates" id="unconsecutive-dates" placeholder="Type non-consecutive days..." title="Type training date for non-consecutive days, Leave blank if not applicable..." class="form-control" value="<?= $unconsecutiveDates ?>">
+                    <input type="text" name="unconsecutive-dates" id="unconsecutive-dates"
+                        placeholder="Type non-consecutive days..."
+                        title="Type training date for non-consecutive days, Leave blank if not applicable..."
+                        class="form-control" value="<?= e($unconsecutiveDates) ?>">
                 </div>
 
                 <div class="row">
                     <div class="col-4">
                         <div class="form-group">
                             <label for="hours" class="mb-0">Number of hours <?php showAsterisk() ?></label>
-                            <input type="number" name="hours" id="hours" class="form-control" title="Set training number of hours..." value="<?= $hours ?>" required>
+                            <input type="number" name="hours" id="hours" class="form-control"
+                                title="Set training number of hours..." value="<?= e($hours) ?>" required>
                         </div>
                     </div>
 
@@ -87,9 +95,9 @@ if (numRows($trainings) > 0) {
                                 <option value="">Select type...</option>
                                 <?php
                                 $types = trainingTypes();
-                                while ($type = fetchAssoc($types)) : ?>
-                                    <option value="<?= $type['id'] ?>" <?= setOptionSelected($type['id'], $trainingType) ?>><?= $type['type'] ?></option>
-                                <?php endwhile ?>
+                                foreach ($types as $type): ?>
+                                    <option value="<?= e($type['id']) ?>" <?= setOptionSelected($type['id'], $trainingType) ?>><?= e($type['type']) ?></option>
+                                <?php endforeach ?>
                             </select>
                         </div>
                     </div>
@@ -99,13 +107,14 @@ if (numRows($trainings) > 0) {
                     <div class="col-4">
                         <div class="form-group">
                             <label for="level" class="mb-0">Level <?php showAsterisk() ?></label>
-                            <select id="level" name="level" class="form-control" title="Select training level..." required>
+                            <select id="level" name="level" class="form-control" title="Select training level..."
+                                required>
                                 <option value="">Select level...</option>
                                 <?php
                                 $sponsors = trainingSponsors();
-                                while ($sponsor = fetchAssoc($sponsors)) : ?>
-                                    <option value="<?= $sponsor['id'] ?>" <?= setOptionSelected($sponsor['id'], $trainingLevel) ?>><?= $sponsor['sponsor'] ?></option>
-                                <?php endwhile ?>
+                                foreach ($sponsors as $sponsor): ?>
+                                    <option value="<?= e($sponsor['id']) ?>" <?= setOptionSelected($sponsor['id'], $trainingLevel) ?>><?= e($sponsor['sponsor']) ?></option>
+                                <?php endforeach ?>
                             </select>
                         </div>
                     </div>
@@ -113,31 +122,39 @@ if (numRows($trainings) > 0) {
                     <div class="col-8">
                         <div class="form-group">
                             <label for="sponsor" class="mb-0">Sponsor <?php showAsterisk() ?></label>
-                            <input type="text" id="sponsor" name="sponsor" class="form-control" placeholder="Type sponsor..." title="Type training sponsor..." value="<?= $sponsorName ?>" required>
+                            <input type="text" id="sponsor" name="sponsor" class="form-control"
+                                placeholder="Type sponsor..." title="Type training sponsor..."
+                                value="<?= e($sponsorName) ?>" required>
                         </div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="functional-division" class="mb-0">Functional Division <?php showAsterisk() ?></label>
-                    <select id="functional-division" name="functional-division" class="form-control" title="Select training functional division..." required>
+                    <select id="functional-division" name="functional-division" class="form-control"
+                        title="Select training functional division..." required>
                         <option value="">Select functional division...</option>
                         <?php
                         $functionalDivisions = functionalDivisions();
-                        while ($functionalDivision = fetchAssoc($functionalDivisions)) : ?>
-                            <option value="<?= $functionalDivision['id'] ?>" <?= setOptionSelected($functionalDivision['id'], $functionalDivisionId) ?>><?= $functionalDivision['name'] ?></option>
-                        <?php endwhile ?>
+                        foreach ($functionalDivisions as $functionalDivision): ?>
+                            <option value="<?= e($functionalDivision['id']) ?>"
+                                <?= setOptionSelected($functionalDivision['id'], $functionalDivisionId) ?>>
+                                <?= e($functionalDivision['name']) ?>
+                            </option>
+                        <?php endforeach ?>
                         <option values="n/a">Not applicable</option>
                     </select>
                 </div>
 
                 <div class="form-group">
                     <label for="venue" class="mb-0">Venue <?php showAsterisk() ?></label>
-                    <input id="venue" name="venue" type="text" class="form-control" placeholder="Type venue..." title="Type training venue..." value="<?= $venue ?>" required>
+                    <input id="venue" name="venue" type="text" class="form-control" placeholder="Type venue..."
+                        title="Type training venue..." value="<?= e($venue) ?>" required>
                 </div>
 
                 <div class="form-check mb-3">
-                    <input class="form-check-input" id="has-certificate" type="checkbox" name="has-certificate" value="1" <?= setItemChecked($generateCertificate) ?>>
+                    <input class="form-check-input" id="has-certificate" type="checkbox" name="has-certificate"
+                        value="1" <?= setItemChecked($generateCertificate) ?>>
                     <label class="form-check-label" for="has-certificate">Generate certificate</label>
                 </div>
 
@@ -145,7 +162,7 @@ if (numRows($trainings) > 0) {
             </div>
 
             <div class="modal-footer">
-                <input type="hidden" name="verifier" value="<?= isset($_GET['id']) ? $_GET['id'] : null ?>">
+                <input type="hidden" name="verifier" value="<?= $_GET['id'] ?? null ?>">
                 <button class="btn btn-primary" name="save-training" type="submit">Continue</button>
                 <?php cancelModalButton() ?>
             </div>
