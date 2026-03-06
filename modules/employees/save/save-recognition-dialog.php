fa710aa1f9c632b15e56ea5c3817e00bae3faa46
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-06 10:08:02 +0800

    Add xss and csrf protection

--- modules/employees/save/save-recognition-dialog.php
+++ modules/employees/save/save-recognition-dialog.php
@@ -28,12 +28,13 @@ if (isset($recognitionId)) {
         <?php modalHeader($modalTitle) ?>
 
         <form method="POST" action="">
+            <?= csrf_field(); ?>
             <div class="modal-body">
                 <div class="form-group">
                     <label for="recognition" class="mb-0">Non-Academic Distinction / Recognition:
                         <?php showAsterisk() ?></label>
                     <input id="recognition" type="text" name="recognition" class="form-control" title="Required field"
-                        value="<?= $recognition ?>" required>
+                        value="<?= e($recognition) ?>" required>
                 </div>
 
                 <?php requiredLegend(0) ?>
@@ -45,7 +46,7 @@ if (isset($recognitionId)) {
                 $verifier = $_GET['id'] ?? null;
                 $verifier = $employeeId === $copiedId ? null : $verifier;
                 ?>
-                <input type="hidden" name="data-verifier" value="<?= $verifier ?>">
+                <input type="hidden" name="data-verifier" value="<?= e($verifier) ?>">
                 <button type="submit" class="btn btn-primary" name="save-recognition">Continue</button>
                 <?php cancelModalButton() ?>
             </div>
