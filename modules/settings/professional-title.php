fe9f5467489c559525e89fb33e2bc0d26c1da535
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-06 13:10:35 +0800

    Add xss and csrf protection

--- modules/settings/professional-title.php
+++ modules/settings/professional-title.php
@@ -9,6 +9,7 @@ $professionalName = toString($before, '', ' ') . $userName . toString($after, ',
 
 <div class="tab-pane fade" id="professional-title">
     <form class="py-2" action="" method="POST">
+        <?= csrf_field(); ?>
         <div class="row">
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
                 <div class="row">
@@ -16,7 +17,7 @@ $professionalName = toString($before, '', ' ') . $userName . toString($after, ',
                         <div class="form-group">
                             <label for="before-title" class="mb-0">Prefix</label>
                             <input type="text" class="form-control" id="before-title" name="before-title"
-                                value="<?= $before ?>" title="Leave blank if not applicable">
+                                value="<?= e($before) ?>" title="Leave blank if not applicable">
                         </div>
                     </div>
 
@@ -24,7 +25,7 @@ $professionalName = toString($before, '', ' ') . $userName . toString($after, ',
                         <div class="form-group">
                             <label for="after-title" class="mb-0">Suffix</label>
                             <input type="text" class="form-control" id="after-title" name="after-title"
-                                value="<?= $after ?>" title="Leave blank if not applicable">
+                                value="<?= e($after) ?>" title="Leave blank if not applicable">
                         </div>
                     </div>
                 </div>
@@ -35,14 +36,14 @@ $professionalName = toString($before, '', ' ') . $userName . toString($after, ',
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
                 <div class="form-group">
                     <label for="preview" class="mb-0">Preview</label>
-                    <input type="text" id="preview" class="form-control" value="<?= $professionalName ?>" readonly>
+                    <input type="text" id="preview" class="form-control" value="<?= e($professionalName) ?>" readonly>
                 </div>
             </div>
         </div>
 
         <div class="row">
             <div class="col-sm-12 col-md-10 col-lg-6 col-xl-4 col">
-                <input type="hidden" id="user-name" value="<?= $userName ?>">
+                <input type="hidden" id="user-name" value="<?= e($userName) ?>">
                 <input name="update-professional-titles" type="submit" value="Update Professional Titles"
                     class="btn btn-primary btn-block btn-lg">
             </div>
