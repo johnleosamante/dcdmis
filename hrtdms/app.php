f289c75cb3320539b3bafb4171f557aa382c98e5
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-09 14:47:51 +0800

    Fix array name values and remove unused functions

--- hrtdms/app.php
+++ hrtdms/app.php
@@ -7,7 +7,7 @@ if (!isset($userId)) {
     redirect(uri() . '/login');
 }
 
-if (numRows(userRole($userId, $activeApp)) === 0) {
+if (!userRole($userId, $activeApp)) {
     redirect(uri() . '/pis');
 }
 
@@ -16,48 +16,50 @@ if (isset($_POST['primary-search-button'])) {
 }
 
 if (isset($_POST['save-training'])) {
-    $trainingId = isset($_POST['verifier']) ? sanitize(decipher($_POST['verifier'])) : null;
-    $title = sanitize($_POST['title']);
-    $from = sanitize($_POST['from']);
-    $to = sanitize($_POST['to']);
-    $hours = sanitize($_POST['hours']);
-    $type = sanitize($_POST['type']);
-    $level = sanitize($_POST['level']);
-    $sponsor = sanitize($_POST['sponsor']);
-    $division = sanitize($_POST['functional-division']);
-    $venue = sanitize($_POST['venue']);
-    $logMessage = '';
-    $unconsecutiveDates = sanitize($_POST['unconsecutive-dates']);
-    $hasCertificate = isset($_POST['has-certificate']) ? '1' : '0';
-    $signatory = isset($_POST['has-certificate']) ? fetchAssoc(section('SDS'))['head'] : null;
     $showAlert = true;
+    $rawId = isset($_POST['verifier']) ? decipher($_POST['verifier']) : null;
+    $trainingId = sanitize($rawId);
+    $data = [
+        'title' => sanitize($_POST['title']),
+        'from' => sanitize($_POST['from']),
+        'to' => sanitize($_POST['to']),
+        'hours' => (int) $_POST['hours'],
+        'type' => sanitize($_POST['type']),
+        'level' => sanitize($_POST['level']),
+        'sponsor' => sanitize($_POST['sponsor']),
+        'division' => sanitize($_POST['functional-division']),
+        'venue' => sanitize($_POST['venue']),
+        'dates' => sanitize($_POST['unconsecutive-dates']),
+        'hasCert' => isset($_POST['has-certificate']) ? '1' : '0',
+    ];
+    $signatory = ($data['hasCert'] === '1') ? section('SDS')['head_id'] : null;
 
-    if (numRows(training($trainingId)) === 0) {
+    if (!training($trainingId)) {
         $logMessage = 'Added training';
         $status = 'saved';
-        $year = toDate($from, 'y', date('y'));
-        $trainingId = 'HRTD-' . $year . '-' . sprintf("%04d", countTrainings($year) + 1);
-
-        createTraining($trainingId, $title, $from, $to, $hours, $type, $level, $sponsor, $venue, $unconsecutiveDates, $signatory, $hasCertificate, $division);
+        $year = toDate($data['from'], 'y', date('y'));
+        $trainingId = "HRTD-$year-" . sprintf("%04d", countTrainings($year) + 1);
+        $affectedTraining = createTraining($trainingId, $data['title'], $data['from'], $data['to'], $data['hours'], $data['type'], $data['level'], $data['sponsor'], $data['venue'], $data['dates'], $signatory, $data['hasCert'], $data['division']);
     } else {
         $logMessage = 'Updated training';
         $status = 'updated';
-
-        updateTraining($trainingId, $title, $from, $to, $hours, $type, $level, $sponsor, $venue, $unconsecutiveDates, $signatory, $hasCertificate, $division);
+        $affectedTraining = updateTraining($trainingId, $data['title'], $data['from'], $data['to'], $data['hours'], $data['type'], $data['level'], $data['sponsor'], $data['venue'], $data['dates'], $signatory, $data['hasCert'], $data['division']);
     }
 
-    if (affectedRows()) {
-        $message = 'Training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>] has been ' . $status . ' successfully.';
-
-        createSystemLog($stationId, $userId, $logMessage, $trainingId, clientIp());
-    } else {
+    if ($affectedTraining === false) {
         $message = $status === 'saved' ? 'No new training has been created.' : 'No training has been updated';
-        $success = false;
+        return;
     }
+
+    $message = 'Training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>] has been ' . $status . ' successfully.';
+    $success = true;
+
+    createSystemLog($stationId, $userId, $logMessage, $trainingId, clientIp());
 }
 
 if (isset($_POST['add-participants'])) {
     $showAlert = true;
+    $trainingId = isset($_POST['verifier']) ? sanitize(decipher($_POST['verifier'])) : null;
 
     if (!isset($_POST['participants'])) {
         $message = 'No training participant was added to training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
@@ -65,64 +67,59 @@ if (isset($_POST['add-participants'])) {
         return;
     }
 
-    $trainingId = isset($_POST['verifier']) ? sanitize(decipher($_POST['verifier'])) : null;
-
-    $trainings = training($trainingId);
-    $training = fetchAssoc($trainings);
+    $training = training($trainingId);
     $title = strtoupper(toHandleEncoding($training['title']));
-    $division = $training['functional_division'];
-    $trainingDate = strtotime($training['to']);
+    $division = $training['functional_division_id'];
+    $trainingDate = strtotime($training['end_date']);
     $month = date('m', $trainingDate);
     $year = date('Y', $trainingDate);
-
     $participants = $_POST['participants'];
     $no = 0;
-
-    $count = numRows(trainingParticipants($trainingId));
+    $count = count(trainingParticipants($trainingId));
 
     foreach ($participants as $participant) {
         $id = sanitize(decipher($participant));
 
         if (!isTrainingParticipant($trainingId, $id)) {
             ++$no;
-            $ctrlNo = $division . '-' . $month . '-' . sprintf("%03d", $count + $no) . '-' . $year;
-            createTrainingParticipant($trainingId, $id, $ctrlNo);
+            $ctrlNo = "$division-$month-" . sprintf("%03d", $count + $no) . "-$year";
+            $affectedParticipants = createTrainingParticipant($trainingId, $id, $ctrlNo);
 
-            if (affectedRows()) {
-                $employee = fetchAssoc(trainingParticipants($trainingId, $id));
-                $employeeEmail = $employee['email'];
-                $employeeName = strtoupper(toHandleEncoding(toName($employee['lname'], $employee['fname'], $employee['mname'], $employee['ext'], true)));
+            if ($affectedParticipants) {
+                $employee = trainingParticipants($trainingId, $id)[0];
+                $employeeEmail = $employee['email_address'];
+                $employeeName = strtoupper(toHandleEncoding(toName($employee['last_name'], $employee['first_name'], $employee['middle_name'], $employee['name_extension'], true)));
                 $certificate = customUri('print', 'Certificate of Participation', $trainingId, DOMAIN) . '&p=' . encode($id);
                 $appearance = customUri('print', 'Certificate of Appearance', $trainingId, DOMAIN) . '&p=' . encode($id);
-
-                $emailMessage =
-                    'Good day ' . $employeeName . '!' . PHP_EOL . PHP_EOL .
-                    'Congratulations you have successfully completed "' . $title . '".' . PHP_EOL .
-                    'Get your certificates by clicking the links below.' . PHP_EOL . PHP_EOL .
-                    'Certificate of Appearance: ' . $appearance . PHP_EOL . PHP_EOL .
-                    'Certificate of Participation: ' . $certificate . PHP_EOL . PHP_EOL .
-                    'If nothing happens when you click the link, copy the links above and paste to your web browser instead.' . PHP_EOL . PHP_EOL .
-                    'You can also go to the DepEd Dipolog City Division Training Repository (' . uri(DOMAIN) . '/hrtdms/repository' . ') to view your trainings. Thank you.' . PHP_EOL . PHP_EOL . PHP_EOL .
-                    '***** THIS IS A SYSTEM GENERATED EMAIL. PLEASE DO NOT REPLY. *****';
-
+                $repositoryUrl = uri(DOMAIN) . '/hrtdms/repository';
+                $emailMessage = "Good day $employeeName!\n\n
+                        Congratulations you have successfully completed $title\n
+                        Get your certificates by clicking the links below.\n\n
+                        Certificate of Appearance: $appearance\n\n
+                        Certificate of Participation: $certificate\n\n
+                        If nothing happens when you click the link, copy the links above and paste to your web browser instead.\n\n
+                        You can also go to the DepEd Dipolog City Division Training Repository ($repositoryUrl) to view your trainings. Thank you.\n\n\n
+                        ***** THIS IS A SYSTEM GENERATED EMAIL. PLEASE DO NOT REPLY. *****";
                 sendMail($employeeEmail, $title, $emailMessage);
             }
         }
     }
 
-    if (affectedRows()) {
-        if ($no > 1) {
-            $message = $no . ' training participants were added successfully to training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
-        } else {
-            $participantId = sanitize(decipher($participants[0]));
-            $message = 'Employee [<a href="#" data-toggle="modal" data-target="#modal" class="text-uppercase" onclick="loadData(\'' . uri() . '/modules/users/user-info-dialog.php?id=' . cipher($participantId) . '\')" title="View ' . userName($participantId) . ' employee information">' . userName($participantId, true) . '</a>] has been added successfully as participant to training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
-        }
-
-        createSystemLog($stationId, $userId, 'Added ' . $no . ' training participants', $trainingId, clientIp());
-    } else {
+    if ($no === 0) {
         $message = 'No training participant was added to training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
-        $success = false;
+        return;
     }
+
+    if ($no > 1) {
+        $message = $no . ' training participants were added successfully to training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
+    } else {
+        $participantId = sanitize(decipher($participants[0]));
+        $message = 'Employee [<a href="#" data-toggle="modal" data-target="#modal" class="text-uppercase" onclick="loadData(\'' . uri() . '/modules/users/user-info-dialog.php?id=' . cipher($participantId) . '\')" title="View ' . userName($participantId) . ' employee information">' . userName($participantId, true) . '</a>] has been added successfully as participant to training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
+    }
+
+    $success = true;
+
+    createSystemLog($stationId, $userId, 'Added ' . $no . ' training participants', $trainingId, clientIp());
 }
 
 if (isset($_POST['remove-participant'])) {
@@ -130,49 +127,48 @@ if (isset($_POST['remove-participant'])) {
     $trainingId = isset($_POST['data-verifier']) ? sanitize(decipher($_POST['data-verifier'])) : null;
     $showAlert = true;
 
-    deleteTrainingParticipant($trainingId, $participantId);
+    $affectedParticipants = deleteTrainingParticipant($trainingId, $participantId);
 
-    if (affectedRows()) {
-        $message = 'Employee [<a href="#" data-toggle="modal" data-target="#modal" class="text-uppercase" onclick="loadData(\'' . uri() . '/modules/users/user-info-dialog.php?id=' . cipher($participantId) . '\')" title="View ' . userName($participantId) . ' employee information">' . userName($participantId, true) . '</a>] has been successfully removed as participant from training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
-
-        createSystemLog($stationId, $userId, 'Removed training participant', $trainingId, clientIp());
-    } else {
+    if (!$affectedParticipants) {
         $message = 'Employee [<a href="#" data-toggle="modal" data-target="#modal" class="text-uppercase" onclick="loadData(\'' . uri() . '/modules/users/user-info-dialog.php?id=' . cipher($participantId) . '\')" title="View ' . userName($participantId) . ' employee information">' . userName($participantId, true) . '</a>] was not removed as participant from training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
-        $success = false;
+        return;
     }
+
+    $message = 'Employee [<a href="#" data-toggle="modal" data-target="#modal" class="text-uppercase" onclick="loadData(\'' . uri() . '/modules/users/user-info-dialog.php?id=' . cipher($participantId) . '\')" title="View ' . userName($participantId) . ' employee information">' . userName($participantId, true) . '</a>] has been successfully removed as participant from training code [<a href="' . customUri('hrtdms', 'Training Details', $trainingId) . '" title="View ' . $trainingId . ' training details">' . strtoupper($trainingId) . '</a>].';
+    $success = true;
+
+    createSystemLog($stationId, $userId, 'Removed training participant', $trainingId, clientIp());
 }
 
 if (isset($_POST['email-participants'])) {
     $trainingId = isset($_POST['verifier']) ? sanitize(decipher($_POST['verifier'])) : null;
     $participantId = isset($_POST['data-verifier']) ? sanitize(decipher($_POST['data-verifier'])) : null;
     $showAlert = true;
-    $trainings = training($trainingId);
+    $training = training($trainingId);
 
-    if (numRows($trainings) === 0) {
+    if (!$training) {
         $message = 'No training has been found.';
         return;
     }
 
-    $training = fetchAssoc($trainings);
     $title = strtoupper(toHandleEncoding($training['title']));
     $trainingParticipants = trainingParticipants($trainingId, $participantId);
-    $name = '';
     $participants = 0;
 
-    while ($participant = fetchAssoc($trainingParticipants)) {
-        $userEmail = $participant['email'];
+    foreach ($trainingParticipants as $participant) {
+        $userEmail = $participant['email_address'];
         $certificate = customUri('print', 'Certificate of Participation', $trainingId, DOMAIN) . '&p=' . encode($participant['id']);
         $appearance = customUri('print', 'Certificate of Appearance', $trainingId, DOMAIN) . '&p=' . encode($participant['id']);
-        $name = strtoupper(toHandleEncoding(toName($participant['lname'], $participant['fname'], $participant['mname'], $participant['ext'], true)));
-
-        $emailMessage = 'Good day ' . $name . '!' . PHP_EOL . PHP_EOL .
-            'Congratulations you have successfully completed "' . $title . '".' . PHP_EOL .
-            'Get your certificates by clicking the links below.' . PHP_EOL . PHP_EOL .
-            'Certificate of Appearance: ' . $appearance . PHP_EOL . PHP_EOL .
-            'Certificate of Participation: ' . $certificate . PHP_EOL . PHP_EOL .
-            'If nothing happens when you click the link, copy the links above and paste to your web browser instead.' . PHP_EOL . PHP_EOL .
-            'You can also go to the DepEd Dipolog City Division Training Repository (' . uri(DOMAIN) . '/hrtdms/repository' . ') to view your trainings. Thank you.' . PHP_EOL . PHP_EOL . PHP_EOL .
-            '***** THIS IS A SYSTEM GENERATED EMAIL. PLEASE DO NOT REPLY. *****';
+        $name = strtoupper(toHandleEncoding(toName($participant['last_name'], $participant['first_name'], $participant['middle_name'], $participant['name_extension'], true)));
+        $repositoryUrl = uri(DOMAIN) . '/hrtdms/repository';
+        $emailMessage = "Good day $name!\n\n
+                        Congratulations you have successfully completed $title\n
+                        Get your certificates by clicking the links below.\n\n
+                        Certificate of Appearance: $appearance\n\n
+                        Certificate of Participation: $certificate\n\n
+                        If nothing happens when you click the link, copy the links above and paste to your web browser instead.\n\n
+                        You can also go to the DepEd Dipolog City Division Training Repository ($repositoryUrl) to view your trainings. Thank you.\n\n\n
+                        ***** THIS IS A SYSTEM GENERATED EMAIL. PLEASE DO NOT REPLY. *****";
 
         if (sendMail($userEmail, $title, $emailMessage)) {
             $participants++;
@@ -182,7 +178,7 @@ if (isset($_POST['email-participants'])) {
     if ($participants === 0) {
         $message = "No email has been sent successfully.";
         return;
-    } else if ($participants === 1) {
+    } elseif ($participants === 1) {
         $message = 'Email has been sent successfully to selected training participant: [<a href="#" data-toggle="modal" data-target="#modal" class="text-uppercase" onclick="loadData(\'' . uri() . '/modules/users/user-info-dialog.php?id=' . cipher($participantId) . '\')" title="View ' . userName($participantId) . ' employee information">' . userName($participantId, true) . '</a>]';
     } else {
         $message = "Email has been sent successfully to all {$participants} training participants.";
