d1e2ded58100c395b104743b66aa7ebb86ab7988
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-09 15:20:26 +0800

    Remove unused functions and fix array names

--- modules/trainings/scheduled-trainings.php
+++ modules/trainings/scheduled-trainings.php
@@ -28,11 +28,13 @@ messageAlert($showAlert, $message, $success);
 
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-striped table-bordered table-hover mb-0 text-center" id="data-table" width="100%" cellspacing="0">
+            <table class="table table-striped table-bordered table-hover mb-0 text-center" id="data-table" width="100%"
+                cellspacing="0">
                 <thead>
                     <tr>
                         <th class="align-middle" width="15%">Code</th>
-                        <th class="align-middle" width="30%">Title of Learning &amp; Development Interventions / Training Programs</th>
+                        <th class="align-middle" width="30%">Title of Learning &amp; Development Interventions /
+                            Training Programs</th>
                         <th class="align-middle" width="5%">From</th>
                         <th class="align-middle" width="5%">To</th>
                         <th class="align-middle" width="15%">Type of Learning &amp; Development</th>
@@ -42,30 +44,29 @@ messageAlert($showAlert, $message, $success);
                 </thead>
 
                 <tbody>
-                    <?php
-                    $trainings = scheduledTrainings();
-                    if (numRows($trainings) > 0) {
-                        while ($training = fetchAssoc($trainings)) : ?>
+                    <?php $trainings = scheduledTrainings();
+                    if ($trainings) {
+                        foreach ($trainings as $training): ?>
                             <tr class="text-uppercase">
                                 <td class="align-middle">
-                                    <?php linkItem(customUri('hrtdms', 'Training Details', $training['no']), $training['no']) ?>
+                                    <?php linkItem(customUri('hrtdms', 'Training Details', $training['id']), $training['id']) ?>
                                 </td>
                                 <td class="align-middle text-left"><?= e($training['title']) ?></td>
-                                <td class="align-middle"><?= toDate($training['from']) ?></td>
-                                <td class="align-middle"><?= toDate($training['to']) ?></td>
-                                <td class="align-middle"><?= trainingType($training['type']) ?></td>
-                                <td class="align-middle"><?= e($training['sponsor']) ?></td>
+                                <td class="align-middle"><?= toDate($training['start_date']) ?></td>
+                                <td class="align-middle"><?= toDate($training['end_date']) ?></td>
+                                <td class="align-middle"><?= trainingType($training['training_type_id']) ?></td>
+                                <td class="align-middle"><?= e($training['sponsored_by']) ?></td>
                                 <td class="align-middle text-capitalize">
                                     <div class="dropdown no-arrow">
                                         <?php dropdownEllipsis() ?>
                                         <div class="dropdown-menu dropdown-menu-righ shadow animated--fade-in">
-                                            <?php linkDropdownItem(customUri('hrtdms', 'Training Details', $training['no']), 'View', 'fa-eye', 'View Training');
-                                            modalDropdownItem(uri() . '/modules/trainings/save-training-dialog.php?id=' . cipher($training['no']), 'Edit', 'fa-edit', 'Edit Training') ?>
+                                            <?php linkDropdownItem(customUri('hrtdms', 'Training Details', $training['id']), 'View', 'fa-eye', 'View Training');
+                                            modalDropdownItem(uri() . '/modules/trainings/save-training-dialog.php?id=' . cipher($training['id']), 'Edit', 'fa-edit', 'Edit Training') ?>
                                         </div>
                                     </div>
                                 </td>
                             </tr>
-                    <?php endwhile;
+                        <?php endforeach;
                     }
                     ?>
                 </tbody>
@@ -73,7 +74,8 @@ messageAlert($showAlert, $message, $success);
                 <tfoot>
                     <tr>
                         <th class="align-middle" width="15%">Code</th>
-                        <th class="align-middle" width="30%">Title of Learning &amp; Development Interventions / Training Programs</th>
+                        <th class="align-middle" width="30%">Title of Learning &amp; Development Interventions /
+                            Training Programs</th>
                         <th class="align-middle" width="5%">From</th>
                         <th class="align-middle" width="5%">To</th>
                         <th class="align-middle" width="15%">Type of Learning &amp; Development</th>
