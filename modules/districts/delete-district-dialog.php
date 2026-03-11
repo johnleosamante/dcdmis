b49015dce67e60c02faa376119b3a281ab6c4389
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-11 11:29:53 +0800

    Simplify ternary operation

--- modules/districts/delete-district-dialog.php
+++ modules/districts/delete-district-dialog.php
@@ -3,6 +3,6 @@
 require_once('../../includes/function.php');
 require_once(root() . '/includes/layout/components.php');
 
-$districtCode = isset($_GET['id']) ? $_GET['id'] : null;
+$districtCode = $_GET['id'] ?? null;
 
 modalConfirmDelete('This operation cannot be undone. Are you sure you want to continue and delete this entry?', 'Delete District?', 'delete-district', $districtCode);
