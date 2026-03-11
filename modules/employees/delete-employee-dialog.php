a6254deb47231130118f67bbd8f2b3a2694fb83e
John Leo Samante <johnleomuitsamante@gmail.com>
2026-03-11 11:30:44 +0800

    Simplify ternary operator

--- modules/employees/delete-employee-dialog.php
+++ modules/employees/delete-employee-dialog.php
@@ -3,6 +3,6 @@
 require_once('../../includes/function.php');
 require_once(root() . '/includes/layout/components.php');
 
-$employeeId = isset($_GET['id']) ? $_GET['id'] : null;
+$employeeId = $_GET['id'] ?? null;
 
 modalConfirmDelete('This operation cannot be undone. Are you sure you want to continue and delete this entry?', 'Delete Employee?', 'delete-employee', $employeeId);
