e7caf86b5faf5aafa935100c039af4bff61258d7
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-20 16:27:49 +0800

    Add page description and back links for authenticated and non-authenticated user

--- hrtdms/repository/page.php
+++ hrtdms/repository/page.php
@@ -8,6 +8,19 @@
         <h1 class="my-2"><?= e($appTitle) ?></h1>
     </div>
 
+    <div class="text-center py-0">
+        <div class="error mx-auto"><i class="fas fa-search fa-fw"></i></div>
+        <p class="lead text-gray-800 mt-1 mb-0">Search trainings</p>
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12">
+                <p class="text-gray-600 px-2 mb-0">View completed workshops and training sessions conducted by the
+                    schools
+                    division. Instantly access and download training attendees' professional development credentials.
+                </p>
+            </div>
+        </div>
+    </div>
+
     <div class="card mt-3 mb-4 mx-auto">
         <?php
         if (!isset($url) || $url === 'conducted-trainings') {
@@ -28,6 +41,11 @@
         ?>
     </div>
 
-    <a class="d-block text-center mx-2 mb-5" href="<?= uri() . '/login' ?>" title="Go to login page">Already have an
-        account? Login instead</a>
+    <?php if (isset($userId)): ?>
+        <a class="d-block text-center mx-2 mb-5" href="<?= uri() . '/' . $activeApp ?>" title="Go to dashboard">Go to
+            Dashboard</a>
+    <?php else: ?>
+        <a class="d-block text-center mx-2 mb-5" href="<?= uri() . '/login' ?>" title="Go to login page">Already have an
+            account? Login instead</a>
+    <?php endif ?>
 </div>
