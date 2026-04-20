f60f11ec12d5863e0558939304b0901a29a54553
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-20 15:13:14 +0800

    Add hrmis apply index file

---
+++ hrmis/apply/index.php
@@ -0,0 +1,9 @@
+<?php
+// hrmpsb/apply/index.php
+require_once '../../includes/function.php';
+require_once root() . '/includes/string.php';
+require_once root() . '/includes/database/database.php';
+require_once root() . '/includes/database/vacancy.php';
+require_once root() . '/includes/database/employee.php';
+require_once root() . '/includes/database/system-log.php';
+require_once root() . '/includes/layout/theme-page.php';
