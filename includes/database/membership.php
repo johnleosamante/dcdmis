f591db99ddcefcc2f0531efa570298374339db5c
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 08:40:34 +0800

    Fix variable names

--- includes/database/membership.php
+++ includes/database/membership.php
@@ -1,44 +1,41 @@
 <?php
 // memberships
-function memberships($person_id)
+function memberships($employee_id)
 {
-    $results = query("SELECT * FROM `memberships` WHERE `person_id` = ? ORDER BY `organization` ASC", [$person_id]);
+    $results = query("SELECT * FROM `memberships` WHERE `employee_id` = ? ORDER BY `organization` ASC", [$employee_id]);
     return is_array($results) ? $results : [];
 }
 
-function membership($person_id, $membership_id)
+function membership($employee_id, $membership_id)
 {
-    return find("SELECT * FROM `memberships` WHERE `person_id` = ? AND `id` = ? LIMIT 1", [$person_id, $membership_id]);
+    return find("SELECT * FROM `memberships` WHERE `employee_id` = ? AND `id` = ? LIMIT 1", [$employee_id, $membership_id]);
 }
 
-function createMembership($organization, $person_id)
+function createMembership($organization, $employee_id)
 {
     $data = [
         'organization' => $organization,
-        'person_id' => $person_id,
-        'created_at' => date('Y-m-d H:i:s'),
-        'updated_at' => date('Y-m-d H:i:s')
+        'employee_id' => $employee_id
     ];
 
     return insert('memberships', $data);
 }
 
-function updateMembership($organization, $person_id, $membership_id)
+function updateMembership($organization, $employee_id, $membership_id)
 {
     $data = [
-        'organization' => $organization,
-        'updated_at' => date('Y-m-d H:i:s')
+        'organization' => $organization
     ];
 
-    return update('memberships', $data, '`person_id` = ? AND `id` = ?', [$person_id, $membership_id]);
+    return update('memberships', $data, '`employee_id` = ? AND `id` = ?', [$employee_id, $membership_id]);
 }
 
-function deleteMembership($person_id, $membership_id)
+function deleteMembership($employee_id, $membership_id)
 {
-    return delete('memberships', '`person_id` = ? AND `id` = ?', [$person_id, $membership_id]);
+    return delete('memberships', '`employee_id` = ? AND `id` = ?', [$employee_id, $membership_id]);
 }
 
-function deleteMemberships($person_id)
+function deleteMemberships($employee_id)
 {
-    return delete('memberships', '`person_id` = ?', [$person_id]);
+    return delete('memberships', '`employee_id` = ?', [$employee_id]);
 }
