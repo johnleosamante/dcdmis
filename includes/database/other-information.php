b94e6c9a8d6160e66595403cb270044f1467fabd
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 08:40:46 +0800

    Fix variable names

--- includes/database/other-information.php
+++ includes/database/other-information.php
@@ -1,11 +1,11 @@
 <?php
 // other_informations
-function otherInformation($person_id)
+function otherInformation($employee_id)
 {
-    return find("SELECT * FROM `other_informations` WHERE `person_id` = ? LIMIT 1;", [$person_id]);
+    return find("SELECT * FROM `other_informations` WHERE `employee_id` = ? LIMIT 1;", [$employee_id]);
 }
 
-function createOtherInformation($has_third_degree, $has_fourth_degree, $relation_details, $was_guilty, $guilty_details, $was_charged, $date_filed, $case_status, $was_convicted, $conviction_details, $was_separated, $separation_details, $was_candidate, $candidacy_details, $have_resigned, $resignation_details, $is_immigrant, $immigrant_country_id, $is_indigenous, $indigenous_group, $with_disability, $disability, $is_solo_parent, $solo_parent_id, $person_id)
+function createOtherInformation($has_third_degree, $has_fourth_degree, $relation_details, $was_guilty, $guilty_details, $was_charged, $date_filed, $case_status, $was_convicted, $conviction_details, $was_separated, $separation_details, $was_candidate, $candidacy_details, $have_resigned, $resignation_details, $is_immigrant, $immigrant_country_id, $is_indigenous, $indigenous_group, $with_disability, $disability, $is_solo_parent, $solo_parent_id, $employee_id)
 {
     $data = [
         'has_third_degree' => $has_third_degree,
@@ -32,14 +32,12 @@ function createOtherInformation($has_third_degree, $has_fourth_degree, $relation
         'disability' => $disability,
         'is_solo_parent' => $is_solo_parent,
         'solo_parent_id' => $solo_parent_id,
-        'person_id' => $person_id,
-        'created_at' => date('Y-m-d H:i:s'),
-        'updated_at' => date('Y-m-d H:i:s')
+        'employee_id' => $employee_id
     ];
     return insert('other_informations', $data);
 }
 
-function updateOtherInformation($has_third_degree, $has_fourth_degree, $relation_details, $was_guilty, $guilty_details, $was_charged, $date_filed, $case_status, $was_convicted, $conviction_details, $was_separated, $separation_details, $was_candidate, $candidacy_details, $have_resigned, $resignation_details, $is_immigrant, $immigrant_country_id, $is_indigenous, $indigenous_group, $with_disability, $disability, $is_solo_parent, $solo_parent_id, $person_id)
+function updateOtherInformation($has_third_degree, $has_fourth_degree, $relation_details, $was_guilty, $guilty_details, $was_charged, $date_filed, $case_status, $was_convicted, $conviction_details, $was_separated, $separation_details, $was_candidate, $candidacy_details, $have_resigned, $resignation_details, $is_immigrant, $immigrant_country_id, $is_indigenous, $indigenous_group, $with_disability, $disability, $is_solo_parent, $solo_parent_id, $employee_id)
 {
     $data = [
         'has_third_degree' => $has_third_degree,
@@ -65,18 +63,17 @@ function updateOtherInformation($has_third_degree, $has_fourth_degree, $relation
         'with_disability' => $with_disability,
         'disability' => $disability,
         'is_solo_parent' => $is_solo_parent,
-        'solo_parent_id' => $solo_parent_id,
-        'updated_at' => date('Y-m-d H:i:s')
+        'solo_parent_id' => $solo_parent_id
     ];
-    return update('other_informations', $data, '`person_id` = ?', [$person_id]);
+    return update('other_informations', $data, '`employee_id` = ?', [$employee_id]);
 }
 
-function deleteOtherInformation($person_id)
+function deleteOtherInformation($employee_id)
 {
-    return delete('other_informations', '`person_id` = ?', [$person_id]);
+    return delete('other_informations', '`employee_id` = ?', [$employee_id]);
 }
 
-// persons, other_informations
+// employees, other_informations
 function indigenousEmployeeCount()
 {
     $sql = "SELECT 
@@ -86,8 +83,8 @@ function indigenousEmployeeCount()
                     ELSE 'No' 
                 END AS `name`, 
                 COUNT(p.`id`) AS `count` 
-            FROM `persons` p 
-            LEFT JOIN `other_informations` o ON p.`id` = o.`person_id` 
+            FROM `employees` p 
+            LEFT JOIN `other_informations` o ON p.`id` = o.`employee_id` 
             WHERE p.`status` = 'Active' GROUP BY `name` ORDER BY `count` DESC";
     return query($sql);
 }
@@ -101,8 +98,8 @@ function pwdEmployeeCount()
                     ELSE 'No' 
                 END AS `name`, 
                 COUNT(p.`id`) AS `count` 
-            FROM `persons` p 
-            LEFT JOIN `other_informations` o ON p.`id` = o.`person_id` 
+            FROM `employees` p 
+            LEFT JOIN `other_informations` o ON p.`id` = o.`employee_id` 
             WHERE p.`status` = 'Active' GROUP BY `name` ORDER BY `count` DESC";
     return query($sql);
 }
@@ -112,8 +109,8 @@ function soloParentEmployeeCount()
     $sql = "SELECT 
                 CASE WHEN o.`is_solo_parent` = 1 THEN 'Yes' ELSE 'No' END AS `name`, 
                 COUNT(p.`id`) AS `count` 
-            FROM `persons` p 
-            LEFT JOIN `other_informations` o ON p.`id` = o.`person_id` 
+            FROM `employees` p 
+            LEFT JOIN `other_informations` o ON p.`id` = o.`employee_id` 
             WHERE p.`status` = 'Active' GROUP BY `name` ORDER BY `name` DESC";
     return query($sql);
 }
