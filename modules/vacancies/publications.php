c2c0e5dedb80c5f9fbc24e23684f9d18b91435eb
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 11:47:24 +0800

    Modify publications layout

--- modules/vacancies/publications.php
+++ modules/vacancies/publications.php
@@ -1,6 +1,6 @@
 <?php
 // modules/vacancies/publications.php
-if (!$isHrmis && !$isHrmpsb) {
+if (!$isHrmis) {
     require_once root() . '/modules/error/403.php';
     return;
 }
@@ -20,8 +20,8 @@ messageAlert($showAlert, $message, $success);
 <div class="card border-left-primary shadow mb-4">
     <div class="card-header py-3">
         <?php
-        if ($isHrmpsb) {
-            contentTitleWithLink('Publications', customUri('hrmpsb', 'Publish Vacancies'), 'Create Publication', 'fa-plus');
+        if ($isHrmis) {
+            contentTitleWithLink('Publications', customUri('hrmis', 'Publish Vacancies'), 'Add', 'fa-plus');
         } else {
             contentTitle('Publications');
         }
@@ -33,9 +33,7 @@ messageAlert($showAlert, $message, $success);
             <table class="table table-hover mb-0 text-center" id="data-table" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th class="align-middle" width="5%">#</th>
-                        <th class="align-middle" width="10%">Code</th>
-                        <th class="align-middle" width="30%">Title</th>
+                        <th class="align-middle" width="35%">Title</th>
                         <th class="align-middle" width="10%">Vacancies</th>
                         <th class="align-middle" width="15%">Open Date</th>
                         <th class="align-middle" width="15%">Close Date</th>
@@ -46,26 +44,25 @@ messageAlert($showAlert, $message, $success);
 
                 <tbody>
                     <?php
-                    $count = 0;
                     $query = allPublications();
-                    while ($row = fetchArray($query)):
+                    foreach ($query as $row):
                         $vacancyCount = countPublicationItems($row['id']);
-                        $appsCount = countApplicationsByPublication($row['id']);
                         ?>
-                        <tr>
-                            <td class="align-middle"><?= ++$count ?></td>
-                            <td class="align-middle font-weight-bold"><?= e($row['code']) ?></td>
-                            <td class="align-middle text-left"><?= e($row['title']) ?></td>
+                        <tr class="text-uppercase">
+                            <td class="align-middle text-left"><a
+                                    href="<?= customUri('hrmis', 'Publication Details', $row['id']) ?>">
+                                    <?= e($row['title']) ?>
+                                </a></td>
                             <td class="align-middle">
-                                <span class="badge badge-secondary"><?= e($vacancyCount) ?> Items</span>
+                                <span class="badge badge-secondary badge-pill"><?= e($vacancyCount) ?> Items</span>
                             </td>
                             <td class="align-middle"><?= toLongDate($row['open_date']) ?></td>
                             <td class="align-middle"><?= toLongDate($row['close_date']) ?></td>
                             <td class="align-middle">
-                                <?php if ($row['status'] == 'open'): ?>
-                                    <span class="badge badge-success">Open</span>
+                                <?php if ($row['status'] === 'open'): ?>
+                                    <span class="badge badge-success badge-pill">Open</span>
                                 <?php else: ?>
-                                    <span class="badge badge-secondary"><?= ucfirst($row['status']) ?></span>
+                                    <span class="badge badge-secondary badge-pill"><?= $row['status'] ?></span>
                                 <?php endif; ?>
                             </td>
                             <td class="align-middle text-capitalize">
@@ -73,18 +70,29 @@ messageAlert($showAlert, $message, $success);
                                     <?php dropdownEllipsis() ?>
                                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in">
                                         <?php
-                                        linkDropdownItem(customUri('hrmpsb', 'Publication Details', $row['id']), 'View', 'fa-eye', 'View Publication Details');
-
-                                        linkDropdownItem(customUri('hrmpsb', 'Publish Vacancies', $row['id']), 'Edit', 'fa-edit', 'Edit Publication');
-
+                                        linkDropdownItem(customUri('hrmis', 'Publication Details', $row['id']), 'View', 'fa-eye', 'View Publication Details');
+                                        linkDropdownItem(customUri('hrmis', 'Publish Vacancies', $row['id']), 'Edit', 'fa-edit', 'Edit Publication'); ?>
+                                        <div class="dropdown-divider"></div>
+                                        <?php
                                         modalDropdownItem(uri() . '/modules/vacancies/delete-publication-dialog.php?id=' . cipher($row['id']), 'Delete', 'fa-trash-alt', 'Delete Publication');
                                         ?>
                                     </div>
                                 </div>
                             </td>
                         </tr>
-                    <?php endwhile ?>
+                    <?php endforeach ?>
                 </tbody>
+
+                <tfoot>
+                    <tr>
+                        <th class="align-middle" width="35%">Title</th>
+                        <th class="align-middle" width="10%">Vacancies</th>
+                        <th class="align-middle" width="15%">Open Date</th>
+                        <th class="align-middle" width="15%">Close Date</th>
+                        <th class="align-middle" width="10%">Status</th>
+                        <th class="align-middle" width="5%">Action</th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     </div>
