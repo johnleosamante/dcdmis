63181fbf1570a53a27bb323976574842b1c458ec
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 08:34:30 +0800

    Add file type to query for fileAttachments and remove created_at for fileAttachment

--- includes/database/201-file.php
+++ includes/database/201-file.php
@@ -1,22 +1,34 @@
 <?php
-// files
+// file_types
+function fileTypes()
+{
+    $result = query("SELECT `id`, `name` FROM `file_types` ORDER BY `id` ASC");
+    return is_array($result) ? $result : [];
+}
+
+// files, file_types
 function fileAttachments($employee_id)
 {
     $results = query(
-        "SELECT `id`, `employee_id`, `file_type_id`, `description`, `file_name`, `file_extension`, `created_at` FROM `files` WHERE `employee_id` = ? ORDER BY `created_at` DESC",
+        "SELECT f.id, f.employee_id, t.name AS file_type, f.description, 
+            f.file_name, f.file_extension, f.created_at 
+        FROM `files` AS f INNER JOIN `file_types` AS t ON f.file_type_id = t.id 
+        WHERE f.employee_id = ? ORDER BY f.created_at DESC",
         [$employee_id]
     );
     return is_array($results) ? $results : [];
 }
 
+// files
 function fileAttachment($employee_id, $file_attachment_id)
 {
     return find(
-        "SELECT `id`, `employee_id`, `file_type_id`, `description`, `file_name`, `file_extension`, `created_at` FROM `files` WHERE `employee_id` = ? AND `id` = ? LIMIT 1",
+        "SELECT `id`, `employee_id`, `file_type_id`, `description`, `file_name`, `file_extension` FROM `files` WHERE `employee_id` = ? AND `id` = ? LIMIT 1",
         [$employee_id, $file_attachment_id]
     );
 }
 
+// files
 function createFileAttachment($file_type_id, $description, $file_name, $file_extension, $employee_id)
 {
     $data = [
@@ -29,6 +41,7 @@ function createFileAttachment($file_type_id, $description, $file_name, $file_ext
     return insert('files', $data);
 }
 
+// files
 function updateFileAttachment($file_type_id, $description, $file_name, $file_extension, $employee_id, $file_attachment_id)
 {
     $data = [
@@ -40,18 +53,14 @@ function updateFileAttachment($file_type_id, $description, $file_name, $file_ext
     return update('files', $data, '`employee_id` = ? AND `id` = ?', [$employee_id, $file_attachment_id]);
 }
 
+// files
 function deleteFileAttachment($employee_id, $file_attachment_id)
 {
     return delete('files', '`employee_id` = ? AND `id` = ?', [$employee_id, $file_attachment_id]);
 }
 
+// files
 function deleteFileAttachments($employee_id)
 {
     return delete('files', '`employee_id` = ?', [$employee_id]);
-}
-
-function fileTypes()
-{
-    $result = query("SELECT `id`, `name` FROM `file_types` ORDER BY `id` ASC");
-    return is_array($result) ? $result : [];
 }
