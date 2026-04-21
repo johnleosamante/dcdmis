ffb30fd891570a39bdd4eca6f50b35380bc89f24
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 08:43:17 +0800

    Fix variable names

--- includes/database/references.php
+++ includes/database/references.php
@@ -1,47 +1,44 @@
 <?php
 // references
-function references($person_id)
+function references($employee_id)
 {
-    $results = query("SELECT * FROM `references` WHERE `person_id` = ? ORDER BY `name` ASC", [$person_id]);
+    $results = query("SELECT * FROM `reference_contacts` WHERE `employee_id` = ? ORDER BY `name` ASC", [$employee_id]);
     return is_array($results) ? $results : [];
 }
 
-function reference($person_id, $reference_id)
+function reference($employee_id, $reference_id)
 {
-    return find("SELECT * FROM `references` WHERE `person_id` = ? AND `id` = ? LIMIT 1", [$person_id, $reference_id]);
+    return find("SELECT * FROM `reference_contacts` WHERE `employee_id` = ? AND `id` = ? LIMIT 1", [$employee_id, $reference_id]);
 }
 
-function createReference($name, $address, $contact, $person_id)
+function createReference($name, $address, $contact, $employee_id)
 {
     $data = [
         'name' => $name,
         'address' => $address,
         'contact' => $contact,
-        'person_id' => $person_id,
-        'created_at' => date('Y-m-d H:i:s'),
-        'updated_at' => date('Y-m-d H:i:s')
+        'employee_id' => $employee_id
     ];
-    return insert('references', $data);
+    return insert('reference_contacts', $data);
 }
 
-function updateReference($name, $address, $contact, $person_id, $reference_id)
+function updateReference($name, $address, $contact, $employee_id, $reference_id)
 {
     $data = [
         'name' => $name,
         'address' => $address,
-        'contact' => $contact,
-        'updated_at' => date('Y-m-d H:i:s')
+        'contact' => $contact
     ];
 
-    return update('references', $data, '`person_id` = ? AND `id` = ?', [$person_id, $reference_id]);
+    return update('reference_contacts', $data, '`employee_id` = ? AND `id` = ?', [$employee_id, $reference_id]);
 }
 
-function deleteReference($person_id, $reference_id)
+function deleteReference($employee_id, $reference_id)
 {
-    return delete('references', '`person_id` = ? AND `id` = ?', [$person_id, $reference_id]);
+    return delete('reference_contacts', '`employee_id` = ? AND `id` = ?', [$employee_id, $reference_id]);
 }
 
-function deleteReferences($person_id)
+function deleteReferences($employee_id)
 {
-    return delete('references', '`person_id` = ?', [$person_id]);
+    return delete('reference_contacts', '`employee_id` = ?', [$employee_id]);
 }
