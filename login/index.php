225f2d70f443f3af04262802294db5def0edd5d8
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 08:58:56 +0800

    Remove parentheses

--- login/index.php
+++ login/index.php
@@ -1,9 +1,9 @@
 <?php
 // login/index.php
-require_once('../includes/function.php');
-require_once(root() . '/includes/string.php');
-require_once(root() . '/includes/database/database.php');
-require_once(root() . '/includes/database/account.php');
-require_once(root() . '/includes/database/school.php');
-require_once(root() . '/includes/database/system-log.php');
-require_once(root() . '/includes/layout/theme-page.php');
+require_once '../includes/function.php';
+require_once root() . '/includes/string.php';
+require_once root() . '/includes/database/database.php';
+require_once root() . '/includes/database/account.php';
+require_once root() . '/includes/database/school.php';
+require_once root() . '/includes/database/system-log.php';
+require_once root() . '/includes/layout/theme-page.php';
