9ab7f0fb0dbf11337467e81a49209b5be489a495
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-21 08:39:43 +0800

    Fix variable names

--- includes/database/experience.php
+++ includes/database/experience.php
@@ -1,20 +1,20 @@
 <?php
 // service_records
 
-function experiences($person_id)
+function experiences($employee_id)
 {
-    $sql = "SELECT * FROM `service_records` WHERE `person_id` = ? ORDER BY `from_date` DESC, `to_date` DESC";
-    $results = query($sql, [$person_id]);
+    $sql = "SELECT * FROM `service_records` WHERE `employee_id` = ? ORDER BY `from_date` DESC, `to_date` DESC";
+    $results = query($sql, [$employee_id]);
     return is_array($results) ? $results : [];
 }
 
-function experience($person_id, $service_record_id)
+function experience($employee_id, $service_record_id)
 {
-    $sql = "SELECT * FROM `service_records` WHERE `person_id` = ? AND `id` = ? LIMIT 1";
-    return find($sql, [$person_id, $service_record_id]);
+    $sql = "SELECT * FROM `service_records` WHERE `employee_id` = ? AND `id` = ? LIMIT 1";
+    return find($sql, [$employee_id, $service_record_id]);
 }
 
-function createExperience($from_date, $to_date, $is_present, $designation, $appointment_status, $is_government_service, $salary_grade_step_increment, $monthly_salary, $agency_company, $leave_wo_pay, $for_separation, $separation_date, $separation_cause, $person_id)
+function createExperience($from_date, $to_date, $is_present, $designation, $appointment_status, $is_government_service, $salary_grade_step_increment, $monthly_salary, $agency_company, $leave_wo_pay, $for_separation, $separation_date, $separation_cause, $employee_id)
 {
     $data = [
         'from_date' => $from_date,
@@ -30,14 +30,12 @@ function createExperience($from_date, $to_date, $is_present, $designation, $appo
         'for_separation' => $for_separation,
         'separation_date' => $separation_date,
         'separation_cause' => $separation_cause,
-        'person_id' => $person_id,
-        'created_at' => date('Y-m-d H:i:s'),
-        'updated_at' => date('Y-m-d H:i:s')
+        'employee_id' => $employee_id
     ];
     return insert('service_records', $data);
 }
 
-function updateExperience($from_date, $to_date, $is_present, $designation, $appointment_status, $is_government_service, $salary_grade_step_increment, $monthly_salary, $agency_company, $leave_wo_pay, $for_separation, $separation_date, $separation_cause, $person_id, $service_record_id)
+function updateExperience($from_date, $to_date, $is_present, $designation, $appointment_status, $is_government_service, $salary_grade_step_increment, $monthly_salary, $agency_company, $leave_wo_pay, $for_separation, $separation_date, $separation_cause, $employee_id, $service_record_id)
 {
     $data = [
         'from_date' => $from_date,
@@ -52,24 +50,23 @@ function updateExperience($from_date, $to_date, $is_present, $designation, $appo
         'leave_wo_pay' => $leave_wo_pay,
         'for_separation' => $for_separation,
         'separation_date' => $separation_date,
-        'separation_cause' => $separation_cause,
-        'updated_at' => date('Y-m-d H:i:s')
+        'separation_cause' => $separation_cause
     ];
-    return update('service_records', $data, '`person_id` = ? AND `id` = ?', [$person_id, $service_record_id]);
+    return update('service_records', $data, '`employee_id` = ? AND `id` = ?', [$employee_id, $service_record_id]);
 }
 
-function deleteExperience($person_id, $service_record_id)
+function deleteExperience($employee_id, $service_record_id)
 {
-    return delete('service_records', '`person_id` = ? AND `id` = ?', [$person_id, $service_record_id]);
+    return delete('service_records', '`employee_id` = ? AND `id` = ?', [$employee_id, $service_record_id]);
 }
 
-function deleteExperiences($person_id)
+function deleteExperiences($employee_id)
 {
-    return delete('service_records', '`person_id` = ?', [$person_id]);
+    return delete('service_records', '`employee_id` = ?', [$employee_id]);
 }
 
 function governmentService($id)
 {
-    $sql = "SELECT * FROM `service_records` WHERE `person_id` = ? AND `is_government_service` = '1' ORDER BY `from_date` DESC";
+    $sql = "SELECT * FROM `service_records` WHERE `employee_id` = ? AND `is_government_service` = '1' ORDER BY `from_date` DESC";
     return query($sql, [$id]);
 }
