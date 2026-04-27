766bc0ba3e015b28d9db73d690791e2ca3304366
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-27 13:48:12 +0800

    Add index file for monitoring tools

---
+++ modules/tools/index.php
@@ -0,0 +1,11 @@
+<?php
+// tools/index.php
+
+contentTitle('Monitoring Tools');
+?>
+
+<div class="row mt-4">
+    <?php
+    cardMini('Procurement Monitoring', 'https://docs.google.com/spreadsheets/d/1GJedbErwA0BFT4LfM-5QTBJMQeliHXMK/edit?gid=1975740446#gid=1975740446', 'fa-file-excel', 'primary', true);
+    ?>
+</div>
