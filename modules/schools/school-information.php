44a9cc62e631a1450328a1673b1ff6ae635f77d9
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-27 11:59:12 +0800

    Remove hrmpsb

--- modules/schools/school-information.php
+++ modules/schools/school-information.php
@@ -1,6 +1,6 @@
 <?php
 // modules/schools/school-information.php
-if (!$isHrmis && !$isHrtdms && !$isDmis && !$isHrmpsb) {
+if (!$isHrmis && !$isHrtdms && !$isDmis) {
     require_once(root() . '/modules/error/403.php');
     return;
 }
