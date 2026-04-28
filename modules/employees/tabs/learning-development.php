e83c6ecc62f960055fa89546cc611605bdf4dd0d
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-28 09:23:23 +0800

    Fix wording for empty table

--- modules/employees/tabs/learning-development.php
+++ modules/employees/tabs/learning-development.php
@@ -36,7 +36,7 @@
                         <?php endforeach;
                     } else { ?>
                         <tr>
-                            <td colspan="6" class="align-middle">No data available.</td>
+                            <td colspan="6" class="align-middle">No data available in table</td>
                         </tr>
                     <?php } ?>
                 </tbody>
