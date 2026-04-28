94bdd727cf185beb25ed50f2f610aa27abff8eb9
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-28 14:07:41 +0800

    Remove psipop reference

--- pis/index.php
+++ pis/index.php
@@ -23,7 +23,6 @@ require_once(root() . '/includes/database/references.php');
 require_once(root() . '/includes/database/utility.php');
 require_once(root() . '/includes/database/card-type.php');
 require_once(root() . '/includes/database/201-file.php');
-require_once(root() . '/includes/database/psipop.php');
 require_once(root() . '/includes/database/system-log.php');
 require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/layout/theme-dashboard.php');
