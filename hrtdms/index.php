2183d1b32a1826dd6a73407eaca3b789e43b5ce6
John Leo Samante <johnleomuitsamante@gmail.com>
2026-04-28 14:03:23 +0800

    Remove psipop reference

--- hrtdms/index.php
+++ hrtdms/index.php
@@ -15,7 +15,6 @@ require_once(root() . '/includes/database/eligibility.php');
 require_once(root() . '/includes/database/experience.php');
 require_once(root() . '/includes/database/learning-development.php');
 require_once(root() . '/includes/database/card-type.php');
-require_once(root() . '/includes/database/psipop.php');
 require_once(root() . '/includes/database/system-log.php');
 require_once(root() . '/includes/layout/components.php');
 require_once(root() . '/includes/layout/theme-dashboard.php');
