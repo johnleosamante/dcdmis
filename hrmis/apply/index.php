7ce406b8e922c0b83e3df30a93142eb6381907bf
John Leo Samante <johnleomuitsamante@gmail.com>
2026-05-06 16:52:16 +0800

    Merge applicant to vacancy

--- hrmis/apply/index.php
+++ hrmis/apply/index.php
@@ -3,7 +3,7 @@
 require_once '../../includes/function.php';
 require_once root() . '/includes/string.php';
 require_once root() . '/includes/database/database.php';
-require_once root() . '/includes/database/vacancy.php';
 require_once root() . '/includes/database/employee.php';
+require_once root() . '/includes/database/vacancy.php';
 require_once root() . '/includes/database/system-log.php';
 require_once root() . '/includes/layout/theme-page.php';
