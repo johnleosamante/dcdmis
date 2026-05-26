0b68164dc1c6f89dd97b77525ace11c49f16d5af
John Leo Samante <johnleomuitsamante@gmail.com>
2026-05-27 06:11:57 +0800

    Remove config.php file and add config-example.php

---
+++ includes/config-example.php
@@ -0,0 +1,17 @@
+<?php
+// include/config.php
+const HOSTNAME = 'localhost';
+const USER = '';
+const PASSWORD = '';
+const DATABASE = '';
+const DOMAIN = '';
+const PUBLIC_IP = '';
+const SITE_TITLE = '';
+const SITE_ALIAS = '';
+const SITE_DESCRIPTION = '';
+const SITE_AUTHOR = '';
+const SITE_AUTHOR_EMAIL = '';
+const DIVISION_ID = '';
+const DIVISION = '';
+const REGION = '';
+const ERROR_LOG_FILE = '';
