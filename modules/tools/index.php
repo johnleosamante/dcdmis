35af5045ae29eb5ee11114b8ac738727e780b3f1
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-01 14:15:10 +0800

    Add filled and unfilled items card for user position visibility

--- modules/tools/index.php
+++ modules/tools/index.php
@@ -11,5 +11,18 @@ contentTitle('Monitoring Tools');
     cardMini('Project CHIEF', 'https://depedph-my.sharepoint.com/:x:/g/personal/nur_hussien_deped_gov_ph/IQDsNFGvmEPFT79ZSoyQHxdpAYwuC5CAVxKo_4Ua4vajSvo?e=3BrEAi&fbclid=IwY2xjawQR5SxleHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEe6G2zEsEnEhevADQniaiyEoa6Hge2INXw1rWOpvUIBWmtOIv0T0_Yid5G8z4_aem_bNHrnmNi8Pq5N8yl4-yXSQ', 'fa-file-excel', 'success', true);
     cardMini('Project One Drive, One Data Management', 'https://depedph-my.sharepoint.com/:x:/g/personal/smme_dipolog_deped_gov_ph/Eccc1Z9Bi8hJiQgXX_WbQhsBxHgNljGwKUHOZMhixltFpw?e=j54Pne&fbclid=IwY2xjawQR521leHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEebYJqACyofMKxXSMvziJnaBiQkryQZrBQW6JKHiQlDnvzc5LzNGCboR0MVYc_aem_nUwz_RBdjJRubvz6UIYALQ', 'fa-file-excel', 'warning', true);
     cardMini('SGOD Sub-AROs Monitoring', 'https://docs.google.com/spreadsheets/d/1HhxfiUdDbzZFddKwi48TfrqEscGPaZtC/edit?gid=609413157#gid=609413157', 'fa-file-excel', 'danger', true);
+
+    $userPositionId = position($userId)['position_id'];
+    $allowedPositions = ['SDS', 'ASDS', 'CES', 'ATY3', 'ITO1', 'A3', 'ADOF5', 'PSDS', 'EPS'];
+    $showFilledUnfilledItems = in_array($userPositionId, $allowedPositions, true) || ($userPositionId === 'ADOF4' && $isPersonnel);
+    if ($showFilledUnfilledItems) {
+        cardMini(
+            'Filled and Unfilled Items',
+            'https://depedph-my.sharepoint.com/:x:/g/personal/love_ricafort_deped_gov_ph/IQALaDK4t-BhRI-wKJCb6qvAAR45KtJb8sRJwflKUZ1XJDU?e=gi7Njk&fbclid=IwY2xjawSKAEVleHRuA2FlbQIxMABicmlkETFIalZjVmNMOUZLanp0M3BHc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHoPs_8vDS_7XrlUXvaWJ1II2sYCXvTirrxXi6Jy_tLjL0vBmkPhgF2AOpdX8_aem_Sqcv-SPKcNW10XD3TZeXrg',
+            'fa-file-excel',
+            'primary',
+            true
+        );
+    }
     ?>
 </div>
