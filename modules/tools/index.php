0471684886ee9f43e996431a7b932e9e83e5220a
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-01 15:48:17 +0800

    Rearrange monitoring tools cards for improved organization and visibility

--- modules/tools/index.php
+++ modules/tools/index.php
@@ -8,9 +8,6 @@ contentTitle('Monitoring Tools');
     <?php
     cardMini('Procurement Monitoring', 'https://docs.google.com/spreadsheets/d/1GJedbErwA0BFT4LfM-5QTBJMQeliHXMK/edit?gid=1975740446#gid=1975740446', 'fa-file-excel', 'primary', true);
     cardMini('Agency Action Plan and Status of Implementation', 'https://docs.google.com/spreadsheets/d/15UW4kGOiJvMIViW-muOG7KTCX3Io5HcX9ZyRgj8rXAc/edit?usp=sharing', 'fa-file-excel', 'primary', true);
-    cardMini('Project CHIEF', 'https://depedph-my.sharepoint.com/:x:/g/personal/nur_hussien_deped_gov_ph/IQDsNFGvmEPFT79ZSoyQHxdpAYwuC5CAVxKo_4Ua4vajSvo?e=3BrEAi&fbclid=IwY2xjawQR5SxleHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEe6G2zEsEnEhevADQniaiyEoa6Hge2INXw1rWOpvUIBWmtOIv0T0_Yid5G8z4_aem_bNHrnmNi8Pq5N8yl4-yXSQ', 'fa-file-excel', 'orange', true);
-    cardMini('Project One Drive, One Data Management', 'https://depedph-my.sharepoint.com/:x:/g/personal/smme_dipolog_deped_gov_ph/Eccc1Z9Bi8hJiQgXX_WbQhsBxHgNljGwKUHOZMhixltFpw?e=j54Pne&fbclid=IwY2xjawQR521leHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEebYJqACyofMKxXSMvziJnaBiQkryQZrBQW6JKHiQlDnvzc5LzNGCboR0MVYc_aem_nUwz_RBdjJRubvz6UIYALQ', 'fa-file-excel', 'orange', true);
-    cardMini('SGOD Sub-AROs Monitoring', 'https://docs.google.com/spreadsheets/d/1HhxfiUdDbzZFddKwi48TfrqEscGPaZtC/edit?gid=609413157#gid=609413157', 'fa-file-excel', 'orange', true);
 
     $userPositionId = position($userId)['position_id'];
     $allowedPositions = ['SDS', 'ASDS', 'CES', 'ATY3', 'ITO1', 'A3', 'ADOF5', 'PSDS', 'EPS'];
@@ -24,5 +21,9 @@ contentTitle('Monitoring Tools');
             true
         );
     }
+
+    cardMini('Project CHIEF', 'https://depedph-my.sharepoint.com/:x:/g/personal/nur_hussien_deped_gov_ph/IQDsNFGvmEPFT79ZSoyQHxdpAYwuC5CAVxKo_4Ua4vajSvo?e=3BrEAi&fbclid=IwY2xjawQR5SxleHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEe6G2zEsEnEhevADQniaiyEoa6Hge2INXw1rWOpvUIBWmtOIv0T0_Yid5G8z4_aem_bNHrnmNi8Pq5N8yl4-yXSQ', 'fa-file-excel', 'orange', true);
+    cardMini('Project One Drive, One Data Management', 'https://depedph-my.sharepoint.com/:x:/g/personal/smme_dipolog_deped_gov_ph/Eccc1Z9Bi8hJiQgXX_WbQhsBxHgNljGwKUHOZMhixltFpw?e=j54Pne&fbclid=IwY2xjawQR521leHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEebYJqACyofMKxXSMvziJnaBiQkryQZrBQW6JKHiQlDnvzc5LzNGCboR0MVYc_aem_nUwz_RBdjJRubvz6UIYALQ', 'fa-file-excel', 'orange', true);
+    cardMini('SGOD Sub-AROs Monitoring', 'https://docs.google.com/spreadsheets/d/1HhxfiUdDbzZFddKwi48TfrqEscGPaZtC/edit?gid=609413157#gid=609413157', 'fa-file-excel', 'orange', true);
     ?>
 </div>
