78ee67eac0d4f6f998fb55d279a142b9cce5ba4e
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-08 10:36:37 +0800

    Add qualify application dialog for applicant qualification confirmation

---
+++ modules/vacancies/qualify-application-dialog.php
@@ -0,0 +1,14 @@
+<?php
+// modules/vacancies/qualify-application-dialog.php
+require_once '../../includes/function.php';
+require_once root() . '/includes/string.php';
+require_once root() . '/includes/layout/components.php';
+
+$applicationId = $_GET['id'] ?? null;
+
+modalConfirmDelete(
+    'This action will qualify the applicant for this position. Are you sure you want to continue?',
+    'Qualify Application?',
+    'qualify-application',
+    $applicationId
+);
