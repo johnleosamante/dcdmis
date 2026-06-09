9f3b67ea8060384ba2d091e22428c8b67a0b9fbd
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-09 09:14:06 +0800

    Refactor disqualification modal to include remarks input and improve user confirmation flow

--- modules/vacancies/disqualify-application-dialog.php
+++ modules/vacancies/disqualify-application-dialog.php
@@ -5,10 +5,31 @@ require_once root() . '/includes/string.php';
 require_once root() . '/includes/layout/components.php';
 
 $applicationId = $_GET['id'] ?? null;
+?>
 
-modalConfirmDelete(
-    'This action will disqualify the applicant from this position. Are you sure you want to continue?',
-    'Disqualify Application?',
-    'disqualify-application',
-    $applicationId
-);
+<div class="modal-dialog">
+    <div class="modal-content">
+        <?php modalHeader('Disqualify Application?') ?>
+
+        <form action="" method="POST" role="form">
+            <?= csrf_field(); ?>
+            <div class="modal-body">
+                <p>This action will disqualify the applicant from this position. Are you sure you want to continue?</p>
+                <div class="form-group mb-2">
+                    <label for="remarks" class="mb-0">Remarks
+                        <?php showAsterisk() ?>
+                    </label>
+                    <textarea id="remarks" name="remarks" class="form-control mt-1" rows="3" required autofocus
+                        placeholder="Type remarks..." title="Type disqualification remarks..."></textarea>
+                </div>
+                <?php requiredLegend(0) ?>
+            </div>
+
+            <div class="modal-footer">
+                <input type="hidden" name="verifier" value="<?= e($applicationId) ?>">
+                <button class="btn btn-danger" name="disqualify-application" type="submit">Yes, Continue</button>
+                <?php cancelModalButton() ?>
+            </div>
+        </form>
+    </div>
+</div>
