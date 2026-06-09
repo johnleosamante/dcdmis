c60bbbab0ddf5908c2d7b7cec6fb790aa56681dd
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-09 08:43:06 +0800

    Add missing database includes for email and position handling

--- hrmis/apply/index.php
+++ hrmis/apply/index.php
@@ -2,8 +2,10 @@
 // hrmpsb/apply/index.php
 require_once '../../includes/function.php';
 require_once root() . '/includes/string.php';
+require_once root() . '/includes/email.php';
 require_once root() . '/includes/database/database.php';
 require_once root() . '/includes/database/employee.php';
+require_once root() . '/includes/database/position.php';
 require_once root() . '/includes/database/vacancy.php';
 require_once root() . '/includes/database/system-log.php';
 require_once root() . '/includes/layout/theme-page.php';
