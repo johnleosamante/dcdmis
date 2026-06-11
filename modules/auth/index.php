375201510f8690709657ea6a4e64c057ba90cd66
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-11 17:00:43 +0800

    Enhance logout process by clearing remember token cookie and improving session termination

--- modules/auth/index.php
+++ modules/auth/index.php
@@ -6,7 +6,16 @@ require_once(root() . '/includes/database/system-log.php');
 
 if (isset($_SESSION["{$prefix}userId"])) {
     createSystemLog($stationId, $userId, 'Logged out', $userId, clientIp());
+    delete('remember_tokens', '`employee_id` = ?', [$userId]);
     session_destroy();
+    setcookie("{$prefix}remember_token", '', [
+        'expires' => time() - 3600,
+        'path' => '/',
+        'domain' => parse_url(uri(), PHP_URL_HOST),
+        'secure' => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on',
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
     setcookie("{$prefix}login", '', [
         'expires' => time() - getSeconds(8),
         'path' => '/',
