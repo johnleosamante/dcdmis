1b073ad80a4669cdae81eed87bb5694f12e023f4
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-16 06:32:46 +0800

    Update delete confirmation dialog to reflect 'Call for Application' terminology

--- modules/vacancies/delete-publication-dialog.php
+++ modules/vacancies/delete-publication-dialog.php
@@ -3,4 +3,4 @@
 require_once('../../includes/function.php');
 require_once(root() . '/includes/layout/components.php');
 
-modalConfirmDelete('This operation cannot be undone. Are you sure you want to continue and delete this entry?', 'Delete Publication?', 'delete-publication', $_GET['id'] ?? null);
+modalConfirmDelete('This operation cannot be undone. Are you sure you want to continue and delete this entry?', 'Delete Call for Application?', 'delete-publication', $_GET['id'] ?? null);
