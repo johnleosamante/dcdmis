072627be404a265f1b83db462709b3b1141060c4
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-16 06:33:09 +0800

    Update confirmation dialog text to reflect 'For Initial Screening' status

--- modules/vacancies/for-review-application-dialog.php
+++ modules/vacancies/for-review-application-dialog.php
@@ -7,8 +7,8 @@ require_once root() . '/includes/layout/components.php';
 $applicationId = $_GET['id'] ?? null;
 
 modalConfirmDelete(
-    'This action will mark the applicant application back to For Review status. Are you sure you want to continue?',
-    'Mark Application For Review?',
+    'This action will mark the applicant application back to For Initial Screening status. Are you sure you want to continue?',
+    'Mark Application For Initial Screening?',
     'for-review-application',
     $applicationId
 );
