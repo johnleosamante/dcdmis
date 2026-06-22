9fbed1e337b1be18e469f14d0705be09321dbe41
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-22 15:23:50 +0800

    Add missing school database inclusion in index.php

--- hrmis/apply/index.php
+++ hrmis/apply/index.php
@@ -5,6 +5,7 @@ require_once root() . '/includes/string.php';
 require_once root() . '/includes/email.php';
 require_once root() . '/includes/database/database.php';
 require_once root() . '/includes/database/employee.php';
+require_once root() . '/includes/database/school.php';
 require_once root() . '/includes/database/position.php';
 require_once root() . '/includes/database/vacancy.php';
 require_once root() . '/includes/database/system-log.php';
