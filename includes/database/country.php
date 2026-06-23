362c6748d97a427068f1e1b8b3e997fad8f0acf0
John Leo Samante <johnleomuitsamante@gmail.com>
2026-06-23 11:06:56 +0800

    Add null coalescing for country and citizenship queries to handle empty results

--- includes/database/country.php
+++ includes/database/country.php
@@ -7,13 +7,13 @@ function countries()
 
 function country($country_id)
 {
-    return find("SELECT `id`, `name` FROM `countries` WHERE `id` = ? LIMIT 1", [$country_id]);
+    return find("SELECT `id`, `name` FROM `countries` WHERE `id` = ? LIMIT 1", [$country_id]) ?: null;
 }
 
 // citizenships
 function citizenships()
 {
-    return query("SELECT `id`, `country_id`, `name` FROM `citizenships` ORDER BY `name` ASC");
+    return query("SELECT `id`, `country_id`, `name` FROM `citizenships` ORDER BY `name` ASC") ?: null;
 }
 
 function citizenship($citizenship_id)
