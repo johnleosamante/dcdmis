b7d88eda928be51da37bd446cfa42ca8065865ca
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-14 06:03:59 +0800

    Fix 404 error page message: update text to "Not found"

--- modules/error/404.php
+++ modules/error/404.php
@@ -4,7 +4,7 @@
 
 <div class="text-center py-0">
     <div class="error mx-auto" data-text="404">404</div>
-    <p class="lead text-gray-800 mt-1 mb-0">Page not found</p>
+    <p class="lead text-gray-800 mt-1 mb-0">Not found</p>
     <p class="text-gray-600 mb-4">Sorry, we couldn't find what you're looking for...</p>
 
     <?php if (isset($userId)): ?>
