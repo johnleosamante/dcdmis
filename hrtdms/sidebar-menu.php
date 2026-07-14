825f64d0997364e90de60aa09914ba99c918ce77
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-14 08:46:55 +0800

    feat: Add 'Program' menu item to sidebar for better navigation

--- hrtdms/sidebar-menu.php
+++ hrtdms/sidebar-menu.php
@@ -12,6 +12,7 @@ $sectionCount = number_format(count(sections()));
 sidebarModalItem(uri() . '/modules/trainings/save-training-dialog.php', 'Add Training', 'fa-plus');
 sidebarMenuItem(customUri('hrtdms', 'Scheduled Trainings'), 'Scheduled', 'fa-calendar-alt', isset($url) && str_contains($url, 'Scheduled'), $countScheduled);
 sidebarMenuItem(customUri('hrtdms', 'Conducted Trainings'), 'Conducted', 'fa-chalkboard-teacher', isset($url) && str_contains($url, 'Conducted'));
+sidebarMenuItem(customUri('hrtdms', 'Program'), 'Program', 'fa-cogs', isset($url) && str_contains($url, 'Program'));
 sidebarDivider();
 sidebarMenuItem(customUri('hrtdms', 'Employees'), 'Employees', 'fa-users', isset($url) && str_contains($url, 'Employees'), $countActive);
 sidebarMenuItem(customUri('hrtdms', 'Districts'), 'Districts', 'fa-map-marked-alt', isset($url) && str_contains($url, 'District'), $districtCount);
