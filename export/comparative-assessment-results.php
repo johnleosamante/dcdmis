6ac3c41088eac4c8fa401df940fc768f919f82ec
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-21 13:58:39 +0800

    feat: Update comparative assessment results table layout and enhance applicant information display

--- export/comparative-assessment-results.php
+++ export/comparative-assessment-results.php
@@ -50,25 +50,25 @@ if ($results) {
 <table>
     <tbody>
         <tr>
-            <td colspan="12" style="font-weight: bold; font-size: 16px; text-align: center;">COMPARATIVE ASSESSMENT
+            <td colspan="16" style="font-weight: bold; font-size: 16px; text-align: center;">COMPARATIVE ASSESSMENT
                 RESULTS</td>
         </tr>
         <tr>
             <td style="font-weight: bold;">Code:</td>
-            <td colspan="11"><?= e($publication['code'] ?? 'N/A') ?></td>
+            <td colspan="15" style="mso-number-format:'\@';"><?= e($publication['code'] ?? 'N/A') ?></td>
         </tr>
         <tr>
             <td style="font-weight: bold;">Title:</td>
-            <td colspan="11"><?= e($publication['title'] ?? 'N/A') ?></td>
+            <td colspan="15"><?= e($publication['title'] ?? 'N/A') ?></td>
         </tr>
         <tr>
             <td style="font-weight: bold;">Description:</td>
-            <td colspan="11"><?= e($publication['description'] ?? 'N/A') ?>
+            <td colspan="15"><?= e($publication['description'] ?? 'N/A') ?>
             </td>
         </tr>
         <tr>
             <td style="font-weight: bold;">Export Date:</td>
-            <td colspan="11"><?= date('F d, Y g:i A') ?></td>
+            <td colspan="15"><?= date('F d, Y g:i A') ?></td>
         </tr>
     </tbody>
 </table>
@@ -79,7 +79,7 @@ if ($results) {
     <table>
         <thead>
             <tr>
-                <th colspan="12" style="font-weight: bold; background-color: #d0d0d0; font-size: 14px; text-align: left;">
+                <th colspan="16" style="font-weight: bold; background-color: #d0d0d0; font-size: 14px; text-align: left;">
                     POSITION: <?= strtoupper(e($pos['official_title'])) ?> (SG <?= e($pos['salary_grade']) ?>)
                 </th>
             </tr>
@@ -87,7 +87,15 @@ if ($results) {
                 <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
                     Rank</th>
                 <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
-                    Applicant Name</th>
+                    Applicant Code</th>
+                <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
+                    Last Name</th>
+                <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
+                    First Name</th>
+                <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
+                    Name Extension</th>
+                <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
+                    Middle Name</th>
                 <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
                     Education</th>
                 <th style="font-weight: bold; background-color: #f2f2f2; border: 1px solid #000000; text-align: center;">
@@ -130,12 +138,22 @@ if ($results) {
                     }
                 }
                 foreach ($posResults as $index => $res):
-                    $name = applicantName($res['application_code']);
+                    $applicantId = $res['application_code_id'] ?? applicantId($res['application_code']);
+                    $person = $applicantId ? (employee($applicantId) ?: applicant($applicantId)) : null;
+                    $lastName = $person['last_name'] ?? '';
+                    $firstName = $person['first_name'] ?? '';
+                    $nameExtension = $person['name_extension'] ?? '';
+                    $middleName = $person['middle_name'] ?? '';
                     $isAssessed = ($res['total_accumulated_score'] !== null);
                     ?>
                     <tr style="text-transform: uppercase;">
                         <td style="text-align: center; border: 1px solid #000000;"><?= $ranks[$index] ?></td>
-                        <td style="border: 1px solid #000000;"><?= strtoupper(e($name)) ?></td>
+                        <td style="text-align: center; border: 1px solid #000000; mso-number-format:'\@';">
+                            <?= e($res['application_code'] ?? '') ?></td>
+                        <td style="border: 1px solid #000000;"><?= strtoupper(e($lastName)) ?></td>
+                        <td style="border: 1px solid #000000;"><?= strtoupper(e($firstName)) ?></td>
+                        <td style="border: 1px solid #000000;"><?= strtoupper(e($nameExtension)) ?></td>
+                        <td style="border: 1px solid #000000;"><?= strtoupper(e($middleName)) ?></td>
                         <td style="text-align: center; border: 1px solid #000000;">
                             <?= $isAssessed ? number_format($res['education_score'], 3, '.', '') : '0.000' ?>
                         </td>
@@ -168,7 +186,7 @@ if ($results) {
                 <?php endforeach; ?>
             <?php else: ?>
                 <tr>
-                    <td colspan="12" style="text-align: center; font-style: italic; color: #777; border: 1px solid #000000;">No
+                    <td colspan="16" style="text-align: center; font-style: italic; color: #777; border: 1px solid #000000;">No
                         qualified applicants have been assessed for this position yet.</td>
                 </tr>
             <?php endif; ?>
