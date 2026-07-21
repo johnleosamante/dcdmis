44f8b2a4ace5d35070ea306c3e45de13c48fe4ec
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-21 09:20:26 +0800

    feat: Rearrange dashboard layout and ensure proper display of school employees section

--- pis/dashboard.php
+++ pis/dashboard.php
@@ -11,10 +11,31 @@ contentTitle('Dashboard');
     card('201 Files', customUri('pis', '201 Files', $userId), 'fa-folder-open', 'info');
     card('Trainings', customUri('pis', 'Trainings', $userId), 'fa-chalkboard-teacher', 'warning');
     card('Payslip', customUri('pis', 'Payslip', $userId), 'fa-money-check', 'danger');
-
-    $schoolInfo = schoolByHead($userId);
-    if ($schoolInfo) {
-        card('School Employees', customUri('pis', 'Employees'), 'fa-users', 'secondary');
-    }
     ?>
 </div>
+
+<?php
+if (!$isNonDivision && ($showMonitoringTools || $showOverview)): ?>
+    <hr class="mt-0">
+    <div class="row mt-4">
+        <?php
+        if ($showMonitoringTools) {
+            card('Monitoring Tools', customUri('pis', 'Monitoring Tools'), 'fa-binoculars', 'primary');
+        }
+        if ($showOverview) {
+            card('System Overview', customUri('pis', 'System Overview'), 'fa-network-wired', $showMonitoringTools ? 'success' : 'primary');
+        } ?>
+    </div>
+<?php endif;
+
+$schoolInfo = schoolByHead($userId);
+if ($schoolInfo): ?>
+    <hr class="mt-0">
+    <div class="row mt-4">
+        <?php
+        card('School Employees', customUri('pis', 'School Employees'), 'fa-users', 'primary');
+        if ($isSchoolPortal || $isNonDivision) {
+            card('Request Transfer', customUri('pis', 'Request Transfer'), 'fa-exchange-alt', 'success');
+        } ?>
+    </div>
+<?php endif;
