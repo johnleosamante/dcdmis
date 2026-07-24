fa2a8675c6a2006e08aee387aa331b567ed3b864
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-24 13:08:26 +0800

    fix: Escape verifier input to prevent potential XSS vulnerabilities

--- modules/vacancies/save-vacancy-dialog.php
+++ modules/vacancies/save-vacancy-dialog.php
@@ -56,7 +56,7 @@ $modalTitle = 'Add Vacancy';
             </div>
 
             <div class="modal-footer">
-                <input type="hidden" name="verifier" value="<?= $_GET['id'] ?? null ?>">
+                <input type="hidden" name="verifier" value="<?= e($_GET['id'] ?? '') ?>">
                 <button class="btn btn-primary" name="save-vacancy" type="submit">Continue</button>
                 <?php cancelModalButton() ?>
             </div>
