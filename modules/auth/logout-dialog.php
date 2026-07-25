5b38d55fc51eb698016f246ea4557fd421935100
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-26 05:47:44 +0800

    fix: Remove unnecessary POST method from logout form

--- modules/auth/logout-dialog.php
+++ modules/auth/logout-dialog.php
@@ -15,7 +15,7 @@ $modalTitle = 'Logout';
         </div>
 
         <div class="modal-footer">
-            <form action="<?= "{$baseUri}/modules/auth" ?>" method="POST">
+            <form action="<?= "{$baseUri}/modules/auth" ?>">
                 <?= csrf_field(); ?>
                 <button type="submit" class="btn btn-danger">Continue</button>
             </form>
