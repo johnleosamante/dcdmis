ada16d218d25bb91fb85edc8d5d5d128e9cb5897
John Leo Samante <johnleomuitsamante@gmail.com>
2026-07-31 16:26:56 +0800

    fix: Add application registration instructions and process overview to publications page

--- hrmis/apply/publications.php
+++ hrmis/apply/publications.php
@@ -4,6 +4,32 @@
     </div>
 
     <div class="card-body">
+        <p>Please complete the required registration process to
+            obtain your official Applicant ID, prior to submission of application.</p>
+
+        <p>Note: Registration is only required once. Your unique Applicant ID will serve and can be
+            used
+            for current and future call for applications.</p>
+
+        <h5>Application Process</h5>
+
+        <ol class="pl-4">
+            <li class="pl-2">Provide the needed
+                personal
+                information to register and obtain your Applicant ID.</li>
+            <li class="pl-2">Select from the available call for applications below.</li>
+            <li class="pl-2">Provide your Applicant ID and select the vacant positions you wish to apply for.</li>
+            <li class="pl-2">Submit your application to be part of the official roster of applicants of the selected
+                call for application.</li>
+        </ol>
+
+        <div class="text-center">
+            <a href="<?= uri() . '/hrmis/register' ?>"
+                class="btn font-weight-bold btn-lg btn-success round">REGISTER</a>
+        </div>
+
+        <hr>
+
         <div class="table-responsive">
             <table class="table table-hover mb-0 text-center" id="data-table" width="100%" cellspacing="0">
                 <thead>
@@ -45,7 +71,7 @@
                 </tbody>
 
                 <tfoot>
-                    <tr>
+                    <tr class="small">
                         <th class="align-middle" width="50%">Title / Description / Application Deadline</th>
                         <th class="align-middle" width="30%">Vacancies</th>
                         <th class="align-middle" width="20%">Action</th>
