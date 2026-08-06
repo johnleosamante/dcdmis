809012a8713fd07ddcc1c1ebe80b69fc08d13dcc
John Leo Samante <johnleomuitsamante@gmail.com>
2026-08-06 15:12:01 +0800

    style: Refactor dashboard.php for improved conditional rendering and code clarity

--- pis/dashboard.php
+++ pis/dashboard.php
@@ -2,9 +2,6 @@
 // pis/page.php
 messageAlert($showAlert, $message, $success);
 contentTitle('Dashboard');
-
-$schoolInfo = schoolByHead($userId);
-$canSeeMonitoring = $showMonitoringTools || (bool) $schoolInfo;
 ?>
 
 <div class="row mt-4">
@@ -14,32 +11,35 @@ $canSeeMonitoring = $showMonitoringTools || (bool) $schoolInfo;
     card('201 Files', customUri('pis', '201 Files', $userId), 'fa-folder-open', 'info');
     card('Trainings', customUri('pis', 'Trainings', $userId), 'fa-chalkboard-teacher', 'warning');
     card('Payslip', customUri('pis', 'Payslip', $userId), 'fa-money-check', 'danger');
-    card('IPCRF', customUri('pis', 'IPCRF', $userId), 'fa-chart-line', 'secondary');
+    if (!$isNonDivision) {
+        card('IPCRF', customUri('pis', 'IPCRF', $userId), 'fa-chart-line', 'secondary');
+    }
     ?>
 </div>
 
-<?php if (!$isNonDivision && ($canSeeMonitoring || $showOverview)): ?>
+<?php if ($isNonDivision || $schoolInfo): ?>
     <hr class="mt-0">
     <div class="row mt-4">
         <?php
-        if ($canSeeMonitoring) {
-            card('Monitoring Tools', customUri('pis', 'Monitoring Tools'), 'fa-binoculars', 'primary');
+        if ($isNonDivision) {
+            card('Request Transfer', customUri('pis', 'Request Transfer'), 'fa-exchange-alt', 'primary');
         }
-        if ($showOverview) {
-            card('System Overview', customUri('pis', 'System Overview'), 'fa-network-wired', $canSeeMonitoring ? 'success' : 'primary');
-        } ?>
+        if ($schoolInfo) {
+            card('School Employees', customUri('pis', 'School Employees'), 'fa-users', $isNonDivision ? 'success' : 'primary');
+        }
+        ?>
     </div>
 <?php endif; ?>
 
-<?php if ($schoolInfo): ?>
+<?php if ($showMonitoringTools || $showOverview): ?>
     <hr class="mt-0">
     <div class="row mt-4">
         <?php
-        card('School Employees', customUri('pis', 'School Employees'), 'fa-users', 'primary');
-
-        if ($isSchoolPortal || $isNonDivision) {
-            card('Request Transfer', customUri('pis', 'Request Transfer'), 'fa-exchange-alt', 'success');
+        if ($showMonitoringTools) {
+            card('Monitoring Tools', customUri('pis', 'Monitoring Tools'), 'fa-binoculars', 'primary');
         }
-        ?>
+        if ($showOverview) {
+            card('System Overview', customUri('pis', 'System Overview'), 'fa-network-wired', $showMonitoringTools ? 'success' : 'primary');
+        } ?>
     </div>
 <?php endif; ?>
