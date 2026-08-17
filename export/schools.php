ad60eab6f225a41f3764f28b821f753ddcb92ba8
John Leo Samante <johnleomuitsamante@gmail.com>
2026-08-17 14:40:30 +0800

    fix: Update personnel table structure to include additional categories and ensure accurate data representation

--- export/schools.php
+++ export/schools.php
@@ -19,20 +19,22 @@ require_once(root() . '/includes/database/utility.php');
             <th rowspan="3">District</th>
             <th rowspan="3">Category</th>
             <th rowspan="3">Head of Office</th>
-            <th colspan="9">Personnel</th>
+            <th colspan="11">Personnel</th>
         </tr>
         <tr>
-            <th colspan="4">Male</th>
-            <th colspan="4">Female</th>
+            <th colspan="5">Male</th>
+            <th colspan="5">Female</th>
             <th rowspan="2">Total</th>
         </tr>
         <tr>
             <th>Teaching</th>
-            <th>Teaching-Related</th>
+            <th>Related-Teaching</th>
+            <th>School Administration</th>
             <th>Non-Teaching</th>
             <th>Total</th>
             <th>Teaching</th>
-            <th>Teaching-Related</th>
+            <th>Related-Teaching</th>
+            <th>School Administration</th>
             <th>Non-Teaching</th>
             <th>Total</th>
         </tr>
@@ -56,17 +58,19 @@ require_once(root() . '/includes/database/utility.php');
 
                 <td><?= e($count['tmale']) ?></td>
                 <td><?= e($count['trmale']) ?></td>
+                <td><?= e($count['samale'] ?? 0) ?></td>
                 <td><?= e($count['ntmale']) ?></td>
                 <td><strong><?= e($count['male']) ?></strong></td>
                 <td><?= e($count['tfemale']) ?></td>
                 <td><?= e($count['trfemale']) ?></td>
+                <td><?= e($count['safemale'] ?? 0) ?></td>
                 <td><?= e($count['ntfemale']) ?></td>
                 <td><strong><?= e($count['female']) ?></strong></td>
                 <td><strong><?= e($count['total']) ?></strong></td>
             </tr>
         <?php endforeach ?>
         <tr>
-            <td colspan="15"><?= 'Data as of ' . date("F j, Y, g:i a") ?></td>
+            <td colspan="17"><?= 'Data as of ' . date("F j, Y, g:i a") ?></td>
         </tr>
     </tbody>
 </table>
