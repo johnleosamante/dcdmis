d2335dd40404efd31de485da3315822a7783a8b7
John Leo Samante <johnleomuitsamante@gmail.com>
2026-08-18 15:52:51 +0800

    refactor: Replace modal confirmation function with inline modal dialog for qualifying applications

--- modules/vacancies/qualify-application-dialog.php
+++ modules/vacancies/qualify-application-dialog.php
@@ -5,10 +5,23 @@ require_once root() . '/includes/string.php';
 require_once root() . '/includes/layout/components.php';
 
 $applicationId = $_GET['id'] ?? null;
+?>
 
-modalConfirmDelete(
-    'This action will qualify the applicant for this position. Are you sure you want to continue?',
-    'Qualify Application?',
-    'qualify-application',
-    $applicationId
-);
+<div class="modal-dialog">
+    <div class="modal-content">
+        <?php modalHeader('Qualify Application?') ?>
+
+        <div class="modal-body">
+            This action will qualify the applicant for this position. Are you sure you want to continue?
+        </div>
+
+        <div class="modal-footer">
+            <form action="" method="POST" role="form">
+                <?= csrf_field(); ?>
+                <input type="hidden" name="verifier" value="<?= e($applicationId) ?>">
+                <button type="submit" class="btn btn-primary" name="qualify-application">Yes, Continue</button>
+                <?php cancelModalButton() ?>
+            </form>
+        </div>
+    </div>
+</div>
