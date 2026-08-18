597575bf77cd24166ffaf023c1a9d7a284ba0f5e
John Leo Samante <johnleomuitsamante@gmail.com>
2026-08-18 13:25:20 +0800

    feat: Add re-add vacancy dialog for restoring closed positions

---
+++ modules/vacancies/readd-vacancy-dialog.php
@@ -0,0 +1,32 @@
+<?php
+// modules/vacancies/readd-vacancy-dialog.php
+require_once '../../includes/function.php';
+require_once root() . '/includes/string.php';
+require_once root() . '/includes/layout/components.php';
+
+$pubId = $_GET['pub_id'] ?? null;
+$vacancyId = $_GET['vacancy_id'] ?? null;
+?>
+
+<div class="modal-dialog">
+    <div class="modal-content">
+        <?php modalHeader('Re-add Position to Vacancies?') ?>
+
+        <div class="modal-body">
+            This action will add this vacant plantilla item position back to the pool of vacant positions for future
+            Call for Applications without removing it from this closed call for application. Are you sure you want to
+            continue?
+        </div>
+
+        <div class="modal-footer">
+            <form action="" method="POST" role="form">
+                <?= csrf_field(); ?>
+                <input type="hidden" name="verifier" value="<?= e($pubId) ?>">
+                <input type="hidden" name="data-verifier" value="<?= e($vacancyId) ?>">
+                <button type="submit" class="btn btn-primary" name="readd-vacancy"><i
+                        class="fas fa-plus-circle mr-1"></i> Yes, Continue</button>
+                <?php cancelModalButton() ?>
+            </form>
+        </div>
+    </div>
+</div>
