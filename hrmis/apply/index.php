93348498061cbbadd7d337b7221685477de3740f
John Leo Samante <johnleomuitsamante@gmail.com>
2026-09-02 16:16:57 +0800

    feat: Add missing database includes for religion and ethnic group

--- hrmis/apply/index.php
+++ hrmis/apply/index.php
@@ -4,6 +4,8 @@ require_once '../../includes/function.php';
 require_once root() . '/includes/string.php';
 require_once root() . '/includes/email.php';
 require_once root() . '/includes/database/database.php';
+require_once root() . '/includes/database/religion.php';
+require_once root() . '/includes/database/ethnic-group.php';
 require_once root() . '/includes/database/employee.php';
 require_once root() . '/includes/database/school.php';
 require_once root() . '/includes/database/position.php';
